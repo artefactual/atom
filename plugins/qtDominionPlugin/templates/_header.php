02e53c1efaed56835fa4d6fc23e98f604445a9a1
Jesús García Crespo <jesus@sevein.com>
2012-09-18 23:05:42 -0700

    Some work in the top admin navigation bar

--- plugins/qtDominionPlugin/templates/_header.php
+++ plugins/qtDominionPlugin/templates/_header.php
@@ -2,50 +2,52 @@
 
   <div class="container">
 
-    <div class="row">
+    <?php if ($sf_user->isAuthenticated()): ?>
+      <div class="row" id="header-admin">
+        <div class="span12">
+          <div class="navbar navbar-fixed-top navbar-inverse">
+            <div class="navbar-inner">
+              <ul class="nav">
 
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+
+                <?php echo get_component('menu', 'mainMenu') ?>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <div class="row" id="header-lvl1">
       <div class="span12">
-
         <a id="header-council" href="http://www.cdncouncilarchives.ca/"><?php echo image_tag('/plugins/qtDominionPlugin/images/council.png', array('width' => '156', 'height' => '42')) ?></a>
-
         <ul id="header-nav" class="nav nav-pills">
-
           <li><?php echo link_to(__('Home'), '@homepage') ?></li>
           <li><?php echo link_to(__('Sitemap'), array('module' => 'staticpage', 'slug' => 'sitemap')) ?></li>
           <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
-
+          <li><?php echo link_to(__('Log in'), array('module' => 'user', 'action' => 'login')) ?></li>
           <?php foreach (array('en', 'fr') as $item): ?>
             <?php if ($sf_user->getCulture() != $item): ?>
               <li><?php echo link_to(format_language($item, $item), array('sf_culture' => $item) + $sf_request->getParameterHolder()->getAll()) ?></li>
               <?php break; ?>
             <?php endif; ?>
           <?php endforeach; ?>
-
-          <?php // echo get_component('i18n', 'changeLanguageList') ?>
-
-          <?php // echo get_component('menu', 'quickLinks') ?>
-
-          <?php // echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-
-          <?php // echo get_component_slot('header') ?>
-
         </ul>
-
       </div>
-
     </div>
 
-    <div class="row">
-
+    <div class="row" id="header-lvl2">
       <?php // Restore old snippet (settings: app_toggleLogo, app_toggleTitle, app_toggleDescription, app_sitleTitle, app_sitleDescription) ?>
       <div id="logo-and-name" class="span6">
         <h1><?php echo link_to(image_tag('/plugins/qtDominionPlugin/images/logo'), '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?></h1>
       </div>
-
       <div id="header-search">
         <?php echo get_component('search', 'box') ?>
       </div>
-
     </div>
 
   </div>
