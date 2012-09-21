281588be8bfcb1246ce09221e5472566a4b6e8aa
Jesús García Crespo <jesus@sevein.com>
2012-09-21 13:16:02 -0700

    Update path s/descriptionUpdates/updates

--- plugins/qtDominionPlugin/modules/staticpage/templates/indexSuccess.php
+++ plugins/qtDominionPlugin/modules/staticpage/templates/indexSuccess.php
@@ -11,7 +11,7 @@
         <li><?php echo link_to(image_tag('/plugins/qtDominionPlugin/images/icons-large/icon-people.png', array('width' => '42', 'height' => '42')).' '.__('People &amp; Organizations'), array('module' => 'actor', 'action' => 'browse')) ?></li>
         <li><?php echo link_to(image_tag('/plugins/qtDominionPlugin/images/icons-large/icon-places.png', array('width' => '42', 'height' => '42')).' '.__('Places'), array('module' => 'taxonomy', 'action' => 'browse', 'id' => 42)) ?></li>
         <li><?php echo link_to(image_tag('/plugins/qtDominionPlugin/images/icons-large/icon-media.png', array('width' => '42', 'height' => '42')).' '.__('Media'), array('module' => 'digitalobject', 'action' => 'browse')) ?></li>
-        <li><?php echo link_to(image_tag('/plugins/qtDominionPlugin/images/icons-large/icon-new.png', array('width' => '42', 'height' => '42')).' '.__('Newest additions'), array('module' => 'search', 'action' => 'descriptionUpdates')) ?></li>
+        <li><?php echo link_to(image_tag('/plugins/qtDominionPlugin/images/icons-large/icon-new.png', array('width' => '42', 'height' => '42')).' '.__('Newest additions'), array('module' => 'search', 'action' => 'updates')) ?></li>
       </ul>
 
     </div>
