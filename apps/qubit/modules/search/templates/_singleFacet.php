055e9afa3e4ac0c978fe462c178e918866ff23f7
Jesús García Crespo <jesus@sevein.com>
2012-09-22 08:42:46 -0700

    Add new horizontal facet partial

---
+++ apps/qubit/modules/search/templates/_singleFacet.php
@@ -0,0 +1,34 @@
+<div class="btn-group top-options">
+
+  <?php echo link_to(
+    __('All'),
+    array(
+      $facet => null,
+      'page' => null) + $sf_request->getParameterHolder()->getAll(),
+    array('class' => 'btn' . ('' == $sf_request->getParameter($facet) ? ' active' : ''))) ?>
+
+  <?php if (isset($pager->facets[$facet])): ?>
+
+    <?php foreach ($pager->facets[$facet]['terms'] as $id => $term): ?>
+
+      <?php $active = in_array($id, (array)@$filters[$facet]) ? true : false; ?>
+
+      <?php echo link_to(
+        __($term['term']).'<span class="count">'.$term['count'].'</span>',
+        array(
+          $facet => (
+            @$filters[$facet]
+              ?
+                implode(',', array_diff(
+                  array_merge(@$filters[$facet], array($id)),
+                  array_intersect(@$filters[$facet], array($id))))
+              :
+                $id),
+          'page' => null) + $sf_request->getParameterHolder()->getAll(),
+        array('class' => 'btn' . ($active ? ' active' : ''))) ?>
+
+    <?php endforeach; ?>
+
+  <?php endif; ?>
+
+</div>
