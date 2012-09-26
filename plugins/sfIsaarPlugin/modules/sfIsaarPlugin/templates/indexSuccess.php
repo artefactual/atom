00d2610f3dd413dce792b23b263d3b4e550a12f6
Jesús García Crespo <jesus@sevein.com>
2012-09-25 22:11:28 -0700

    Update ISAAR view template with new layout

--- plugins/sfIsaarPlugin/modules/sfIsaarPlugin/templates/indexSuccess.php
+++ plugins/sfIsaarPlugin/modules/sfIsaarPlugin/templates/indexSuccess.php
@@ -1,157 +1,182 @@
-<h1><?php echo __('View authority record') ?></h1>
+<div class="row">
 
-<h1 class="label">
-<?php echo link_to_if(QubitAcl::check($resource, 'update'), render_title($resource), array($resource, 'module' => 'actor', 'action' => 'edit'), array('title' => __('Edit authority record'))) ?>
-</h1>
+  <div class="span3" id="left-column">
 
-<?php if (isset($errorSchema)): ?>
-  <div class="messages error">
-    <ul>
-      <?php foreach ($errorSchema as $error): ?>
-        <li><?php echo $error ?></li>
-      <?php endforeach; ?>
+    <?php include_component('actor', 'contextMenu') ?>
+
+  </div>
+
+  <div class="span9" id="main-column">
+
+    <h1><?php echo link_to_if(QubitAcl::check($resource, 'update'), render_title($resource), array($resource, 'module' => 'actor', 'action' => 'edit'), array('title' => __('Edit authority record'))) ?></h1>
+
+    <ul class="breadcrumb">
+      <li><?php echo link_to(__('Institutions'), array('module' => 'repository', 'action' => 'browse')) ?></li>
+      <li class="active"><?php echo render_title($resource) ?></li>
     </ul>
-  </div>
-<?php endif; ?>
 
-<div class="section" id="identityArea">
+    <!--
+    <?php if (isset($errorSchema)): ?>
+      <div class="messages error">
+        <ul>
+          <?php foreach ($errorSchema as $error): ?>
+            <li><?php echo $error ?></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    <?php endif; ?>
+    -->
 
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Identity area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'identityArea', 'title' => __('Edit identity area'))) ?>
+    <div class="row">
 
-  <?php echo render_show(__('Type of entity'), render_value($resource->entityType)) ?>
+      <div class="span9" id="content">
 
-  <?php echo render_show(__('Authorized form of name'), render_value($resource->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?>
+        <div class="section" id="identityArea">
 
-  <div class="field">
-    <h3><?php echo __('Parallel form(s) of name') ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->getOtherNames(array('typeId' => QubitTerm::PARALLEL_FORM_OF_NAME_ID)) as $item): ?>
-          <li><?php echo render_value($item->__toString()) ?></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
+          <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Identity area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'identityArea', 'title' => __('Edit identity area'))) ?>
 
-  <div class="field">
-    <h3><?php echo __('Standardized form(s) of name according to other rules') ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->getOtherNames(array('typeId' => QubitTerm::STANDARDIZED_FORM_OF_NAME_ID)) as $item): ?>
-          <li><?php echo render_value($item->__toString()) ?></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
+          <?php echo render_show(__('Type of entity'), render_value($resource->entityType)) ?>
 
-  <div class="field">
-    <h3><?php echo __('Other form(s) of name') ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->getOtherNames(array('typeId' => QubitTerm::OTHER_FORM_OF_NAME_ID)) as $item): ?>
-          <li><?php echo render_value($item->__toString()) ?></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
+          <?php echo render_show(__('Authorized form of name'), render_value($resource->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?>
 
-  <?php echo render_show(__('Identifiers for corporate bodies'), render_value($resource->corporateBodyIdentifiers)) ?>
+          <div class="field">
+            <h3><?php echo __('Parallel form(s) of name') ?></h3>
+            <div>
+              <ul>
+                <?php foreach ($resource->getOtherNames(array('typeId' => QubitTerm::PARALLEL_FORM_OF_NAME_ID)) as $item): ?>
+                  <li><?php echo render_value($item->__toString()) ?></li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </div>
 
-</div>
+          <div class="field">
+            <h3><?php echo __('Standardized form(s) of name according to other rules') ?></h3>
+            <div>
+              <ul>
+                <?php foreach ($resource->getOtherNames(array('typeId' => QubitTerm::STANDARDIZED_FORM_OF_NAME_ID)) as $item): ?>
+                  <li><?php echo render_value($item->__toString()) ?></li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </div>
 
-<div class="section" id="descriptionArea">
+          <div class="field">
+            <h3><?php echo __('Other form(s) of name') ?></h3>
+            <div>
+              <ul>
+                <?php foreach ($resource->getOtherNames(array('typeId' => QubitTerm::OTHER_FORM_OF_NAME_ID)) as $item): ?>
+                  <li><?php echo render_value($item->__toString()) ?></li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </div>
 
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Description area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'descriptionArea', 'title' => __('Edit description area'))) ?>
+          <?php echo render_show(__('Identifiers for corporate bodies'), render_value($resource->corporateBodyIdentifiers)) ?>
 
-  <?php echo render_show(__('Dates of existence'), render_value($resource->getDatesOfExistence(array('cultureFallback' => true)))) ?>
+        </div>
 
-  <?php echo render_show(__('History'), render_value($resource->getHistory(array('cultureFallback' => true)))) ?>
+        <div class="section" id="descriptionArea">
 
-  <?php echo render_show(__('Places'), render_value($resource->getPlaces(array('cultureFallback' => true)))) ?>
+          <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Description area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'descriptionArea', 'title' => __('Edit description area'))) ?>
 
-  <?php echo render_show(__('Legal status'), render_value($resource->getLegalStatus(array('cultureFallback' => true)))) ?>
+          <?php echo render_show(__('Dates of existence'), render_value($resource->getDatesOfExistence(array('cultureFallback' => true)))) ?>
 
-  <?php echo render_show(__('Functions, occupations and activities'), render_value($resource->getFunctions(array('cultureFallback' => true)))) ?>
+          <?php echo render_show(__('History'), render_value($resource->getHistory(array('cultureFallback' => true)))) ?>
 
-  <?php echo render_show(__('Mandates/sources of authority'), render_value($resource->getMandates(array('cultureFallback' => true)))) ?>
+          <?php echo render_show(__('Places'), render_value($resource->getPlaces(array('cultureFallback' => true)))) ?>
 
-  <?php echo render_show(__('Internal structures/genealogy'), render_value($resource->getInternalStructures(array('cultureFallback' => true)))) ?>
+          <?php echo render_show(__('Legal status'), render_value($resource->getLegalStatus(array('cultureFallback' => true)))) ?>
 
-  <?php echo render_show(__('General context'), render_value($resource->getGeneralContext(array('cultureFallback' => true)))) ?>
+          <?php echo render_show(__('Functions, occupations and activities'), render_value($resource->getFunctions(array('cultureFallback' => true)))) ?>
 
-</div>
+          <?php echo render_show(__('Mandates/sources of authority'), render_value($resource->getMandates(array('cultureFallback' => true)))) ?>
 
-<div class="section" id="relationshipsArea">
+          <?php echo render_show(__('Internal structures/genealogy'), render_value($resource->getInternalStructures(array('cultureFallback' => true)))) ?>
 
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Relationships area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'relationshipsArea', 'title' => __('Edit relationships area'))) ?>
+          <?php echo render_show(__('General context'), render_value($resource->getGeneralContext(array('cultureFallback' => true)))) ?>
 
-  <?php foreach ($resource->getActorRelations() as $item): ?>
-    <?php $relatedEntity = $item->getOpposedObject($resource->id) ?>
-    <div class="field">
-      <h3><?php echo __('Related entity') ?></h3>
-      <div>
+        </div>
 
-        <?php echo link_to(render_title($relatedEntity), array($relatedEntity, 'module' => ('QubitRepository' == $relatedEntity->className) ? 'repository' : 'actor')) ?><?php if (isset($relatedEntity->datesOfExistence)): ?> <span class="note2">(<?php echo render_value($relatedEntity->getDatesOfExistence(array('cultureFallback' => true))) ?>)</span><?php endif; ?>
+        <div class="section" id="relationshipsArea">
 
-        <?php echo render_show(__('Identifier of the related entity'), render_value($relatedEntity->descriptionIdentifier)) ?>
+          <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Relationships area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'relationshipsArea', 'title' => __('Edit relationships area'))) ?>
 
-        <?php echo render_show(__('Category of the relationship'), render_value($item->type)) ?>
+          <?php foreach ($resource->getActorRelations() as $item): ?>
+            <?php $relatedEntity = $item->getOpposedObject($resource->id) ?>
+            <div class="field">
+              <h3><?php echo __('Related entity') ?></h3>
+              <div>
 
-        <?php echo render_show(__('Dates of the relationship'), Qubit::renderDateStartEnd($item->date, $item->startDate, $item->endDate)) ?>
+                <?php echo link_to(render_title($relatedEntity), array($relatedEntity, 'module' => ('QubitRepository' == $relatedEntity->className) ? 'repository' : 'actor')) ?><?php if (isset($relatedEntity->datesOfExistence)): ?> <span class="note2">(<?php echo render_value($relatedEntity->getDatesOfExistence(array('cultureFallback' => true))) ?>)</span><?php endif; ?>
 
-        <?php echo render_show(__('Description of relationship'), render_value($item->description)) ?>
+                <?php echo render_show(__('Identifier of the related entity'), render_value($relatedEntity->descriptionIdentifier)) ?>
+
+                <?php echo render_show(__('Category of the relationship'), render_value($item->type)) ?>
+
+                <?php echo render_show(__('Dates of the relationship'), Qubit::renderDateStartEnd($item->date, $item->startDate, $item->endDate)) ?>
+
+                <?php echo render_show(__('Description of relationship'), render_value($item->description)) ?>
+
+              </div>
+            </div>
+          <?php endforeach; ?>
+
+          <?php foreach ($functions as $item): ?>
+            <?php echo render_show(__('Related function'), link_to(render_title($item), array($item, 'module' => 'function'))) ?>
+          <?php endforeach; ?>
+
+        </div>
+
+        <div class="section" id="controlArea">
+
+          <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Control area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'controlArea', 'title' => __('Edit control area'))) ?>
+
+          <?php echo render_show(__('Description identifier'), render_value($resource->descriptionIdentifier)) ?>
+
+          <?php echo render_show(__('Institution identifier'), render_value($resource->getInstitutionResponsibleIdentifier(array('cultureFallback' => true)))) ?>
+
+          <?php echo render_show(__('Rules and/or conventions used'), render_value($resource->getRules(array('cultureFallback' => true)))) ?>
+
+          <?php echo render_show(__('Status'), render_value($resource->descriptionStatus)) ?>
+
+          <?php echo render_show(__('Level of detail'), render_value($resource->descriptionDetail)) ?>
+
+          <?php echo render_show(__('Dates of creation, revision and deletion'), render_value($resource->getRevisionHistory(array('cultureFallback' => true)))) ?>
+
+          <div class="field">
+            <h3><?php echo __('Language(s)') ?></h3>
+            <div>
+              <ul>
+                <?php foreach ($resource->language as $code): ?>
+                  <li><?php echo format_language($code) ?></li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </div>
+
+          <div class="field">
+            <h3><?php echo __('Script(s)') ?></h3>
+            <div>
+              <ul>
+                <?php foreach ($resource->script as $code): ?>
+                  <li><?php echo format_script($code) ?></li>
+                <?php endforeach; ?>
+              </ul>
+            </div>
+          </div>
+
+          <?php echo render_show(__('Sources'), render_value($resource->getSources(array('cultureFallback' => true)))) ?>
+
+          <?php echo render_show(__('Maintenance notes'), render_value($isaar->maintenanceNotes)) ?>
+
+        </div>
 
       </div>
+
     </div>
-  <?php endforeach; ?>
 
-  <?php foreach ($functions as $item): ?>
-    <?php echo render_show(__('Related function'), link_to(render_title($item), array($item, 'module' => 'function'))) ?>
-  <?php endforeach; ?>
-
-</div>
-
-<div class="section" id="controlArea">
-
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Control area').'</h2>', array($resource, 'module' => 'actor', 'action' => 'edit'), array('anchor' => 'controlArea', 'title' => __('Edit control area'))) ?>
-
-  <?php echo render_show(__('Description identifier'), render_value($resource->descriptionIdentifier)) ?>
-
-  <?php echo render_show(__('Institution identifier'), render_value($resource->getInstitutionResponsibleIdentifier(array('cultureFallback' => true)))) ?>
-
-  <?php echo render_show(__('Rules and/or conventions used'), render_value($resource->getRules(array('cultureFallback' => true)))) ?>
-
-  <?php echo render_show(__('Status'), render_value($resource->descriptionStatus)) ?>
-
-  <?php echo render_show(__('Level of detail'), render_value($resource->descriptionDetail)) ?>
-
-  <?php echo render_show(__('Dates of creation, revision and deletion'), render_value($resource->getRevisionHistory(array('cultureFallback' => true)))) ?>
-
-  <div class="field">
-    <h3><?php echo __('Language(s)') ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->language as $code): ?>
-          <li><?php echo format_language($code) ?></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
   </div>
-
-  <div class="field">
-    <h3><?php echo __('Script(s)') ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->script as $code): ?>
-          <li><?php echo format_script($code) ?></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
-
-  <?php echo render_show(__('Sources'), render_value($resource->getSources(array('cultureFallback' => true)))) ?>
-
-  <?php echo render_show(__('Maintenance notes'), render_value($isaar->maintenanceNotes)) ?>
 
 </div>
 
