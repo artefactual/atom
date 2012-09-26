4447d8bff9709a260e2e1c9f7ccf9f8c52d967c6
Jesús García Crespo <jesus@sevein.com>
2012-09-25 20:18:01 -0700

    Add basic behaviour for .simple-map gmaps

--- js/dominion.js
+++ js/dominion.js
@@ -37,6 +37,37 @@
 
   /****
    ****
+   ****  Google Maps
+   ****
+   ****/
+
+  $(function ()
+    {
+      var $container = $('.simple-map');
+
+      window.initializeSimpleMap = function()
+        {
+          var location = new google.maps.LatLng($container.data('latitude'), $container.data('longitude'));
+          var map = new google.maps.Map($container.get(0), {
+              zoom: 16,
+              center: location,
+              panControl: false,
+              mapTypeControl: true,
+              zoomControl: true,
+              scaleControl: true,
+              streetViewControl: false,
+              mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU },
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              zoomControlOptions: { style: google.maps.ZoomControlStyle.SMALL },
+            });
+          var marker = new google.maps.Marker({ position: location, map: map});
+        };
+
+      $.getScript('https://maps.google.com/maps/api/js?sensor=false&callback=initializeSimpleMap&key=' + $container.data('key'));
+    });
+
+  /****
+   ****
    ****  jQuery Masonry
    ****
    ****/
