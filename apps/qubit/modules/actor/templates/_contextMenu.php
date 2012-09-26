04e741fefb08354cb2fe83232e744b5297f2911e
Jesús García Crespo <jesus@sevein.com>
2012-09-25 22:18:31 -0700

    Update layout

--- apps/qubit/modules/actor/templates/_contextMenu.php
+++ apps/qubit/modules/actor/templates/_contextMenu.php
@@ -1,6 +1,6 @@
 <div class="section">
   <h3><?php echo __('Subject of') ?></h3>
-  <div>
+  <div class="content">
     <ul>
       <?php foreach ($subjectInfoObjects as $item): ?>
         <li><?php echo link_to(render_title($item), array($item, 'module' => 'informationobject')) ?></li>
@@ -10,9 +10,9 @@
 </div>
 
 <?php foreach ($relatedInfoObjects as $role => $informationObjects): ?>
-  <div>
+  <div class="section">
     <h3><?php echo __('%1% of', array('%1%' => $role)) ?></h3>
-    <div>
+    <div class="content">
       <ul>
         <?php foreach ($informationObjects as $informationObject): ?>
           <li><?php echo link_to(render_title($informationObject), array($informationObject, 'module' => 'informationobject')) ?><?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $informationObject->getPublicationStatus()->status->id): ?> <span class="publicationStatus"><?php echo $informationObject->getPublicationStatus()->status ?></span><?php endif; ?></li>
--- apps/qubit/modules/actor/templates/_format.php
+++ apps/qubit/modules/actor/templates/_format.php
@@ -1,9 +1,9 @@
 <div class="section">
 
-  <h2><?php echo __('Export') ?></h2>
+  <h3><?php echo __('Export') ?></h3>
 
   <div class="content">
-    <ul class="clearfix">
+    <ul>
       <li><?php echo link_to(__('EAC'), array($resource, 'module' => 'sfEacPlugin', 'sf_format' => 'xml')) ?></li>
     </ul>
   </div>
