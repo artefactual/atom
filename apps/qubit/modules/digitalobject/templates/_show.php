c89546a5ed806df1fd35bd77cc4630a6f8915c7b
Jesús García Crespo <jesus@sevein.com>
2012-09-27 14:09:08 -0700

    Update digital object ref layout

--- apps/qubit/modules/digitalobject/templates/_show.php
+++ apps/qubit/modules/digitalobject/templates/_show.php
@@ -1,1 +1,3 @@
-<?php echo get_component('digitalobject', $showComponent, array('iconOnly' => $iconOnly, 'link' => $link, 'resource' => $resource, 'usageType' => $usageType)) ?>
+<div class="digital-object-reference">
+  <?php echo get_component('digitalobject', $showComponent, array('iconOnly' => $iconOnly, 'link' => $link, 'resource' => $resource, 'usageType' => $usageType)) ?>
+</div>
