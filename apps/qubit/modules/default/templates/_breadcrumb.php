202f6b19f5d0c9a2b7c126779514d3a43f6a3408
Jesús García Crespo <jesus@sevein.com>
2012-09-27 15:46:11 -0700

    Update breadcrumb with new style using text-overflow ellipsis

--- apps/qubit/modules/default/templates/_breadcrumb.php
+++ apps/qubit/modules/default/templates/_breadcrumb.php
@@ -5,7 +5,7 @@
       <?php foreach ($objects as $object): ?>
         <?php if (isset($object->parent)): // FIXME Implement something like ->slice(1) or [1:] ?>
           <?php if (isset($resource) && $object == $resource): ?>
-            <li class="active"><?php echo render_title($object) ?></li>
+            <li class="active"><span><?php echo render_title($object) ?></span></li>
           <?php else: ?>
             <li><?php echo link_to(render_title($object), array($object, 'module' => 'informationobject')) ?></li>
           <?php endif; ?>
