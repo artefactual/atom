1f456f8d57f698aaf1314d46416dd8bdba7d5cf9
= <=>
2012-10-25 21:48:31 -0700

    Spit EAD XML template up so it can be re-used for bulk export.

---
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessHeader.xml.php
@@ -0,0 +1,2 @@
+<?php echo '<?xml version="1.0" encoding="'.sfConfig::get('sf_charset', 'UTF-8')."\" ?>\n" ?>
+<!DOCTYPE ead PUBLIC "+//ISBN 1-931666-00-8//DTD ead.dtd (Encoded Archival Description (EAD) Version 2002)//EN" "http://lcweb2.loc.gov/xmlcommon/dtds/ead2002/ead.dtd">
