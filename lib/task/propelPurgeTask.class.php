a172db7c2c9be38b4fdf8ac4d24bb6a2c7b5d090
Mike Cantelon <mcantelon@gmail.com>
2012-12-05 13:58:06 -0800

    Added task to purge Qubit data.

---
+++ lib/task/propelPurgeTask.class.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the AccesstoMemory (AtoM) software.
+ *
+ * AccesstoMemory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AccesstoMemory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AccesstoMemory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Regenerate nested set column values
+ *
+ * @package    symfony
+ * @subpackage task
+ * @author     Mike Cantelon <mike@artefactual.com>
+ */
+class propelBuildNestedSetTask extends sfBaseTask
+{
+  /**
+   * @see sfTask
+   */
+  protected function configure()
+  {
+    $this->addArguments(array(
+    ));
+
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', true),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'cli'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'propel'),
+    ));
+
+    $this->namespace = 'propel';
+    $this->name = 'purge';
+    $this->briefDescription = 'Purge all data.';
+
+    $this->detailedDescription = <<<EOF
+Purge all data.
+EOF;
+  }
+
+  /**
+   * @see sfTask
+   */
+  public function execute($arguments = array(), $options = array())
+  {
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $conn = $databaseManager->getDatabase('propel')->getConnection();
+
+    $insertSql = new sfPropelInsertSqlTask($this->dispatcher, $this->formatter);
+    $insertSql->setCommandApplication($this->commandApplication);
+    $insertSql->setConfiguration($this->configuration);
+    $insertSql->run();
+
+    $configuration = ProjectConfiguration::getApplicationConfiguration('qubit', 'test', false);
+    $sf_context = sfContext::createInstance($configuration);
+    sfInstall::loadData();
+
+    $this->logSection('propel', 'Done!');
+  }
+}
