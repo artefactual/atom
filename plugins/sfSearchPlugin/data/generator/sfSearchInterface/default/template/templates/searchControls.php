3c9cda4708f77f75b63613a2dfd22e884465874a
Jesús García Crespo <jesus@sevein.com>
2012-12-08 19:09:01 -0800

    Get rid of sfSearchPlugin

--- plugins/sfSearchPlugin/data/generator/sfSearchInterface/default/template/templates/searchControls.php
+++
@@ -1,4 +0,0 @@
-[?php use_helper('I18N') ?]
-<h2>[?php echo __('<?php echo $this->get('mode.simple.controls.heading', 'Search') ?>') ?]</h2>
-
-[?php include_partial('simpleControls', array('form' => $form)) ?]
--- plugins/sfSearchPlugin/test/functional/fixtures/apps/xfSearchWrapper/config/xfSearchWrapperConfiguration.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class xfSearchWrapperConfiguration extends xfSearchConfiguration
-{
-}
