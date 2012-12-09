308a1db6a4d8843771f277f92109c638ef810fdf
Jesús García Crespo <jesus@sevein.com>
2012-12-08 18:35:38 -0800

    Update license code in some files missed

--- plugins/arElasticSearchPlugin/lib/model/QubitPdoActor.class.php
+++ plugins/arElasticSearchPlugin/lib/model/QubitPdoActor.class.php
@@ -1,21 +1,21 @@
 <?php
 
 /*
-* This file is part of Qubit Toolkit.
-*
-* Qubit Toolkit is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Qubit Toolkit is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Qubit Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * Lightweight version of QubitActor which uses PDO directly instead of the Propel ORM
