7983e6bb149c22a4d90ce74a0a1de7570e43a603
Jesús García Crespo <jesus@sevein.com>
2012-12-10 12:31:04 -0800

    Uncomment get_component, fixes #552

--- apps/qubit/templates/_footer.php
+++ apps/qubit/templates/_footer.php
@@ -4,7 +4,7 @@
     <?php echo link_to(image_tag('xhtml10', array('alt' => __('This page is valid XHTML 1.0'))), 'http://validator.w3.org/check?'.http_build_query(array('uri' => $sf_request->getUri().'?'.http_build_query(array(session_name() => session_id())), 'ss' => 1))) ?>
 
     <?php if (QubitAcl::check('userInterface', 'translate')): ?>
-      <?php // echo get_component('sfTranslatePlugin', 'translate') ?>
+      <?php echo get_component('sfTranslatePlugin', 'translate') ?>
     <?php endif; ?>
 
     <?php echo get_component_slot('footer') ?>
