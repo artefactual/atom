75b08f281829bc6a58172db15581a938bde05c20
Mike Cantelon <mcantelon@gmail.com>
2012-12-13 19:08:31 -0800

    fixed #4434 bad indentation in EAD templates

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
@@ -1,42 +1,42 @@
-<?php if (0 < count($creators = $$resourceVar->getCreators())): ?>
-    <bioghist encodinganalog="3.2.2">
-      <chronlist>
-<?php foreach($$resourceVar->getDates(array('type_id' => QubitTerm::CREATION_ID)) as $date): ?>
-<?php $creator = QubitActor::getById($date->actorId); ?>
-        <chronitem>
-<?php if ($date->date): ?>
-          <date type="creation"><?php echo $date->date; ?></date>
-<?php endif; ?>
-<?php if ($date->startDate): ?>
-          <date type="creation_start"><?php echo $date->startDate; ?></date>
-<?php endif; ?>
-<?php if ($data->endDate): ?>
-          <date type="creation_end"><?php echo $date->endDate; ?></date>
-<?php endif; ?>
-          <eventgrp>
-            <event>
-              <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
-                <note><?php echo esc_specialchars($value) ?></note>
+  <?php if (0 < count($creators = $$resourceVar->getCreators())): ?>
+  <bioghist encodinganalog="3.2.2">
+    <chronlist>
+      <?php foreach($$resourceVar->getDates(array('type_id' => QubitTerm::CREATION_ID)) as $date): ?>
+      <?php $creator = QubitActor::getById($date->actorId); ?>
+      <chronitem>
+        <?php if ($date->date): ?>
+        <date type="creation"><?php echo $date->date; ?></date>
+        <?php endif; ?>
+        <?php if ($date->startDate): ?>
+        <date type="creation_start"><?php echo $date->startDate; ?></date>
+        <?php endif; ?>
+        <?php if ($data->endDate): ?>
+        <date type="creation_end"><?php echo $date->endDate; ?></date>
+        <?php endif; ?>
+        <eventgrp>
+          <event>
+            <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
+            <note><?php echo esc_specialchars($value) ?></note>
+            <?php endif; ?>
+            <origination encodinganalog="3.2.1">
+              <?php if ($type = $creator->getEntityTypeId()): ?>
+              <?php if ($type == QubitTerm::PERSON_ID): ?>
+              <persname><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></persname>
               <?php endif; ?>
-              <origination encodinganalog="3.2.1">
-<?php if ($type = $creator->getEntityTypeId()): ?>
-  <?php if ($type == QubitTerm::PERSON_ID): ?>
-      <persname><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></persname>
+              <?php if ($type == QubitTerm::FAMILY_ID): ?>
+              <famname><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></famname>
+              <?php endif; ?>
+              <?php if ($type == QubitTerm::CORPORATE_BODY_ID): ?>
+              <corpname><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></corpname>
+              <?php endif; ?>
+              <?php else: ?>
+              <name><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></name>
+              <?php endif; ?>
+            </origination>
+          </event>
+        </eventgrp>
+      </chronitem>
+      <?php endforeach; ?>
+    </chronlist>
+  </bioghist>
   <?php endif; ?>
-  <?php if ($type == QubitTerm::FAMILY_ID): ?>
-      <famname><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></famname>
-  <?php endif; ?>
-  <?php if ($type == QubitTerm::CORPORATE_BODY_ID): ?>
-      <corpname><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></corpname>
-  <?php endif; ?>
-<?php else: ?>
-      <name><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></name>
-<?php endif; ?>
-              </origination>
-            </event>
-          </eventgrp>
-        </chronitem>
-<?php endforeach; ?>
-      </chronlist>
-    </bioghist>
-<?php endif; ?>
