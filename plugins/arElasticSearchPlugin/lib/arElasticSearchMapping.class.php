0bf47ad0c00037ecbb5933a58fe67d161d92e4bd
Jesús García Crespo <jesus@sevein.com>
2012-12-26 19:06:39 -0800

    Set dynamic option to strict, map i18n fields
    I thought that it would be a good practice to set the dynamic mode
    in 'strict' for both root and inner objects. There is no performance
    overhead in objects being dynamic but it is a safe mechanism so
    malformed objects won't index data that we don't want to be indexed.
    This strategy forced me to map any i18n field available, using
    TablleMap classes for that. I also did some cleanups.

--- plugins/arElasticSearchPlugin/lib/arElasticSearchMapping.class.php
+++ plugins/arElasticSearchPlugin/lib/arElasticSearchMapping.class.php
@@ -19,6 +19,11 @@
 
 class arElasticSearchMapping
 {
+  /**
+   * Inner objects
+   *
+   * @return array
+   */
   protected $nestedTypes = null;
 
   /**
@@ -45,9 +50,16 @@ class arElasticSearchMapping
         throw new sfException('A mapping.yml must only contain 1 entry.');
       }
 
+      // Get rid of first level (mapping:)
       $this->mapping = $mapping_array['mapping'];
 
+      $this->camelizeFieldNames();
+
       $this->fixYamlShorthands();
+
+      $this->excludeNestedOnlyTypes();
+
+      $this->cleanYamlShorthands($this->mapping);
     }
   }
 
@@ -69,56 +81,80 @@ class arElasticSearchMapping
   }
 
   /**
-   * Fixes shorthands
+   * Camelize field names by moving array items
    */
-  protected function fixYamlShorthands()
+  protected function camelizeFieldNames()
   {
-    // Collect nested types since some of them are intended to be only nested
-    // as properties of other types, so we are excluding them as main types
-    foreach ($this->mapping as $typeName => &$typeProperties)
-    {
-      $nested = isset($typeProperties['_attributes']['nested']);
-      $nestable = isset($typeProperties['_attributes']['nestable']);
-
-      if (!$nested && !$nestable)
-      {
-        continue;
-      }
-
-      // Unset the attribute since we are already processing it
-      if ($nested) unset($typeProperties['_attributes']['nested']);
-      if ($nestable) unset($typeProperties['_attributes']['nestable']);
-
-      // Store it and parse other attributes (i18n, timestamp, etc...)
-      $this->nestedTypes[$typeName] = array('type' => 'object', 'properties' => $this->processPropertyAttributes($typeProperties));
-
-      // Avoid this nested type to be mapped as a regular type
-      if (!$nestable)
-      {
-        unset($this->mapping[$typeName]);
-      }
-    }
-
     // Iterate over types (actor, information_object, ...)
-    foreach ($this->mapping as $typeName => &$typeProperties)
+    foreach ($this->mapping as $typeName => $typeProperties)
     {
-      // Parse attributes
-      $typeProperties = $this->processPropertyAttributes($typeProperties);
-
-      // Nest foreign types
-      foreach ($typeProperties as $propertyName => &$propertyValue)
+      foreach ($typeProperties['properties'] as $propertyName => &$propertyValue)
       {
-        if (isset($propertyValue['_attributes']['foreignType']))
-        {
-          $foreignType = $propertyValue['_attributes']['foreignType'];
+        $camelized = lcfirst(sfInflector::camelize($propertyName));
 
-          $typeProperties[$propertyName] = $this->nestedTypes[$foreignType];
+        // Abort field change if the name matches with its original version
+        if ($camelized == $propertyName)
+        {
+          continue;
         }
+
+        // Create new item with the new camelized key
+        $this->mapping[$typeName]['properties'][$camelized] = $propertyValue;
+
+        // Unset the old one
+        unset($this->mapping[$typeName]['properties'][$propertyName]);
       }
     }
   }
 
-  protected function processPropertyAttributes(array $typeProperties)
+  /**
+   * Fixes YAML shorthands
+   */
+  protected function fixYamlShorthands()
+  {
+    // First, process special attributes
+    foreach ($this->mapping as $typeName => &$typeProperties)
+    {
+      $this->processPropertyAttributes($typeName, $typeProperties);
+    }
+
+    // Next iteration to embed nested types
+    foreach ($this->mapping as $typeName => &$typeProperties)
+    {
+      $this->processForeignTypes($typeProperties);
+    }
+  }
+
+  /**
+   * Clean YAML shorthands
+   */
+  protected function cleanYamlShorthands(array &$typeProperties)
+  {
+    foreach ($typeProperties as $key => &$value)
+    {
+      switch ($key)
+      {
+        case '_attributes':
+        case '_foreign_types':
+          unset($typeProperties[$key]);
+
+          break;
+
+        default:
+          if (is_array($value))
+          {
+            $this->cleanYamlShorthands($value);
+          }
+
+          break;
+      }
+    }
+  }
+
+  /**
+   * Given a mapping, it parses its special attributes and update it accordingly
+   */
+  protected function processPropertyAttributes($typeName, array &$typeProperties)
   {
     // Stop execution if any special attribute was set
     if (!isset($typeProperties['_attributes']))
@@ -134,39 +170,112 @@ class arElasticSearchMapping
       switch ($attributeName)
       {
         case 'i18n':
-          $this->setIfNotSet($typeProperties, 'source_culture', array('type' => 'string', 'index' => 'not_analyzed', 'include_in_all' => false));
+          $this->setIfNotSet($typeProperties['properties'], 'sourceCulture', array('type' => 'string', 'index' => 'not_analyzed', 'include_in_all' => false));
 
-          // For each culture, map an object. This is how it will look like:
-          // i18n{object}:
-          //   en{object}: ... (title, etc... dynamically built)
-          //   fr{object}: ... (title, etc... dynamically built)
+          // We are using the same mapping for all the i18n fields
+          $i18nFieldMapping = array('type' => 'string', 'index' => 'not_analyzed', 'include_in_all' => false);
+
+          $nestedI18nFields = array();
+          foreach ($this->getI18nFields(lcfirst(sfInflector::camelize($typeName))) as $fieldName)
+          {
+            $nestedI18nFields[$fieldName] = $i18nFieldMapping;
+          }
+
+          if (isset($typeProperties['_attributes']['i18nExtra']))
+          {
+            foreach ($this->getI18nFields(lcfirst(sfInflector::camelize($typeProperties['_attributes']['i18nExtra']))) as $fieldName)
+            {
+              $nestedI18nFields[$fieldName] = $i18nFieldMapping;
+            }
+          }
+
+          // i18n documents (one per culture)
           $nestedI18nObjects = array();
           foreach (QubitSetting::getByScope('i18n_languages') as $setting)
           {
             $culture = $setting->getValue(array('sourceCulture' => true));
             $nestedI18nObjects[$culture] = array(
               'type' => 'object',
-              'include_in_parent' => false);
+              'dynamic' => 'strict',
+              'include_in_parent' => false,
+              'properties' => $nestedI18nFields);
           }
 
-          $this->setIfNotSet($typeProperties, 'i18n', array(
+          // Main i18n object
+          $this->setIfNotSet($typeProperties['properties'], 'i18n', array(
             'type' => 'object',
+            'dynamic' => 'strict',
             'include_in_root' => true,
             'properties' => $nestedI18nObjects));
 
           break;
 
         case 'timestamp':
-          $this->setIfNotSet($typeProperties, 'created_at', array('type' => 'date'));
-          $this->setIfNotSet($typeProperties, 'updated_at', array('type' => 'date'));
+          $this->setIfNotSet($typeProperties['properties'], 'createdAt', array('type' => 'date'));
+          $this->setIfNotSet($typeProperties['properties'], 'updatedAt', array('type' => 'date'));
 
           break;
       }
     }
+  }
 
-    unset($typeProperties['_attributes']);
+  /*
+   * Given a class name (eg. Repository or QubitRepostiroy), returns
+   * an array of i18n fields
+   */
+  public static function getI18nFields($class)
+  {
+    // Use table maps to find existing i18n columns
+    $className = str_replace('Qubit', '', $class) . 'I18nTableMap';
+    $map = new $className;
 
-    return $typeProperties;
+    $fields = array();
+    foreach ($map->getColumns() as $column)
+    {
+      if (!$column->isPrimaryKey() && !$column->isForeignKey())
+      {
+        $colName = $column->getPhpName();
+
+        $fields[] = $colName;
+      }
+    }
+
+    return $fields;
+  }
+
+  /**
+   * Given a mapping, adds other objects within it
+   */
+  protected function processForeignTypes(array &$typeProperties)
+  {
+    // Stop execution if any foreign type was assigned
+    if (!isset($typeProperties['_foreign_types']))
+    {
+      return;
+    }
+
+    foreach ($typeProperties['_foreign_types'] as $fieldName => $foreignTypeName)
+    {
+      $typeProperties['properties'][$fieldName] = $this->mapping[$foreignTypeName];
+    }
+  }
+
+  /**
+   * Exclude nested types if there are not root objects using them
+   */
+  protected function excludeNestedOnlyTypes()
+  {
+    // Iterate over types (actor, information_object, ...)
+    foreach ($this->mapping as $typeName => $typeProperties)
+    {
+      // Pass if nested_only is not set
+      if (!isset($typeProperties['_attributes']['nested_only']))
+      {
+        continue;
+      }
+
+      unset($this->mapping[$typeName]);
+    }
   }
 
   /**
