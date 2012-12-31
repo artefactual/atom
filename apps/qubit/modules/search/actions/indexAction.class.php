431de521553043542d68d1dfffa77e35d5ac9be8
Jesús García Crespo <jesus@sevein.com>
2012-12-30 16:26:34 -0800

    Refactor search/index, extend default/browse

--- apps/qubit/modules/search/actions/indexAction.class.php
+++ apps/qubit/modules/search/actions/indexAction.class.php
@@ -17,258 +17,133 @@
  * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class SearchIndexAction extends sfAction
+class SearchIndexAction extends DefaultBrowseAction
 {
-  public static
-    $NAMES = array(
-      );
-
   public static
     $FACETS = array(
       'repository.id',
       'subjects.id',
       'digitalObject.mediaTypeId',
       'places.id',
-      'names.id');
+      'names.id',
+      array('type' => 'range', 'name' => 'dates.startDate', 'from' => null, 'to' => null),
+      array('type' => 'range', 'name' => 'dates.endDate', 'from' => null, 'to' => null));
 
-  protected function parseQuery()
+  protected function populateFacet($name, $ids)
   {
-    // if querystring is empty, use match_all
-    if ('' == preg_replace('/[\s\t\r\n]*/', '', $this->request->query))
+    switch ($name)
     {
-      return new Elastica_Query_MatchAll();
-    }
+      case 'repository.id':
+        $criteria = new Criteria;
+        $criteria->add(QubitRepository::ID, array_keys($ids), Criteria::IN);
 
-    try
-    {
-      // Parse query string, Elastica_Query_QueryString
-      $query = QubitSearch::getInstance()->parse($this->request->query);
-    }
-    catch (Exception $e)
-    {
-      $this->error = $e->getMessage();
-
-      return null;
-    }
-
-    return $query;
-  }
-
-  protected function filterQuery($query)
-  {
-    $this->filters = array();
-
-    $queryTerm = new Elastica_Query_Term();
-    $queryBool = new Elastica_Query_Bool();
-
-    if ($query instanceof Elastica_Query_QueryString)
-    {
-      $queryBool->addMust($query);
-    }
-
-    foreach ($this->request->getGetParameters() as $param => $value)
-    {
-      if (in_array(strtr($param, '_', '.'), self::$FACETS))
-      {
-        foreach (explode(',', $value) as $facetValue)
+        foreach (QubitRepository::get($criteria) as $item)
         {
-          // don't include empty filters (querystring sanitization)
-          if ('' != preg_replace('/[\s\t\r\n]*/', '', $facetValue))
-          {
-            $this->filters[$param][] = $facetValue;
-            $query = $queryBool->addMust($queryTerm->setTerm(strtr($param, '_', '.'), $facetValue));
-          }
+          $this->types[$item->id] = $item->getName(array('cultureFallback' => true));
         }
-      }
-    }
 
-    return $query;
+        break;
+
+      case 'subjects.id':
+        $criteria = new Criteria;
+        $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
+
+        foreach (QubitTerm::get($criteria) as $item)
+        {
+          $this->types[$item->id] = $item->getName(array('cultureFallback' => true));
+        }
+
+        break;
+
+      case 'digitalObject.mediaTypeId':
+        $criteria = new Criteria;
+        $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
+
+        foreach (QubitTerm::get($criteria) as $item)
+        {
+          $this->types[$item->id] = $item->getName(array('cultureFallback' => true));
+        }
+
+        break;
+
+      case 'dates.startDate':
+        $facets[strtr($name, '.', '_')] = $facet['ranges'][0];  // FIXME: is this the best way?
+
+        break;
+
+      case 'places.id':
+        $criteria = new Criteria;
+        $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
+
+        foreach (QubitTerm::get($criteria) as $item)
+        {
+          $this->types[$item->id] = $item->getName(array('cultureFallback' => true));
+        }
+
+        break;
+
+      case 'names.id':
+        $criteria = new Criteria;
+        $criteria->add(QubitActor::ID, array_keys($ids), Criteria::IN);
+
+        foreach (QubitActor::get($criteria) as $item)
+        {
+          $this->types[$item->id] = $item->getAuthorizedFormOfName(array('cultureFallback' => true));
+        }
+
+        break;
+    }
   }
 
-  protected function facetQuery($query)
+  public function execute($request)
   {
-    foreach (self::$FACETS as $field)
+    parent::execute($request);
+
+    if ('print' == $request->getGetParameter('media'))
     {
-      $facet = new Elastica_Facet_Terms($field);
-      $facet->setField($field);
-      $facet->setSize(50);
-      $query->addFacet($facet);
+      $this->getResponse()->addStylesheet('print-preview', 'last');
     }
 
-    $facet = new Elastica_Facet_Range('dates.startDate');
-    $facet->setField('dates.startDate');
-    $facet->addRange(null, null);
-    $query->addFacet($facet);
+    $queryText = new Elastica_Query_QueryString($request->query);
+    $queryText->setDefaultOperator('AND');
+    # _all? $queryText->setDefaultField(sprintf('i18n.%s.authorizedFormOfName', $this->context->user->getCulture()));
+    $this->queryBool->addMust($queryText);
 
-    $facet = new Elastica_Facet_Range('dates.endDate');
-    $facet->setField('dates.endDate');
-    $facet->addRange(null, null);
-    $query->addFacet($facet);
+    // ACL check
 
-    return $query;
-  }
-
-  protected function buildQuery()
-  {
-    // Parse query string - Elastica_Query_{MatchAll,QueryString}
-    $query = $this->parseQuery();
-
-    // Filter by selected facets - Elastica_Query_Bool
-    $query = $this->filterQuery($query);
-
-    // Elastica_Query
-    $query = new Elastica_Query($query);
-
-    // Add facets - Elastica_Query
-    $query = $this->facetQuery($query);
-
-    $query->setSort(array('_score' => 'desc', 'slug' => 'asc'));
-    $query->setLimit($this->request->limit);
-
+    // Sort, From, Limit
+    $this->query->setSort(array('_score' => 'desc'));
+    $this->query->setLimit($this->request->limit);
     if (!empty($this->request->page))
     {
       $query->setFrom(($this->request->page - 1) * $this->request->limit);
     }
 
+    // Realm filter
     if (isset($this->request->realm) && is_int($this->request->realm))
     {
-      $query->setQuery(new Elastica_Query_Term(array('repository._id' => $this->request->realm)));
+      $this->queryBool->addMust(new Elastica_Query_Term(array('repository._id' => $this->request->realm)));
     }
 
-    return $query;
-  }
+    $this->query->setQuery($this->queryBool);
 
-  protected function populateFacets()
-  {
-    if (!$this->pager->hasFacets())
+    $resultSet = QubitSearch::getInstance()->index->getType('QubitInformationObject')->search($this->query);
+
+    $this->pager = new QubitSearchPager($resultSet);
+    $this->pager->setPage($request->page ? $request->page : 1);
+
+    if ('print' != $request->getGetParameter('media'))
     {
-      return false;
+      $this->pager->setMaxPerPage($request->limit);
     }
-
-    $facets = array();
-
-    foreach ($this->pager->getFacets() as $name => $facet)
+    else
     {
-      if (isset($facet['terms']))
-      {
-        $ids = array();
-        foreach ($facet['terms'] as $term)
-        {
-          $ids[$term['term']] = $term['count'];
-        }
-      }
-
-      switch ($name)
-      {
-        case 'repository.id':
-          $criteria = new Criteria;
-          $criteria->add(QubitRepository::ID, array_keys($ids), Criteria::IN);
-
-          $repos = QubitRepository::get($criteria);
-
-          foreach ($repos as $repo)
-          {
-            $reponames[$repo->id] = $repo->getAuthorizedFormOfName(array('cultureFallback' => true, 'culture' => $this->context->user->getCulture()));
-          }
-
-          foreach ($facet['terms'] as $term)
-          {
-            $facets[strtr($name, '.', '_')]['terms'][$term['term']] = array(
-              'count' => $term['count'],
-              'term' => $reponames[$term['term']]);
-          }
-
-          break;
-
-        case 'subjects.id':
-          $criteria = new Criteria;
-          $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
-
-          $subjectAPs = QubitTerm::get($criteria);
-
-          foreach ($subjectAPs as $subjectAP)
-          {
-            $subjectAPnames[$subjectAP->id] = $subjectAP->getName(array('cultureFallback' => true, 'culture' => $this->context->user->getCulture()));
-          }
-
-          foreach ($facet['terms'] as &$term)
-          {
-            $facets[strtr($name, '.', '_')]['terms'][$term['term']] = array(
-              'count' => $term['count'],
-              'term' => $subjectAPnames[$term['term']]);
-          }
-
-          break;
-
-        case 'digitalObject.mediaTypeId':
-          $criteria = new Criteria;
-          $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
-
-          $mediaTypes = QubitTerm::get($criteria);
-
-          foreach ($mediaTypes as $mediaType)
-          {
-            $mediaTypeNames[$mediaType->id] = $mediaType->getName(array('cultureFallback' => true, 'culture' => $this->context->user->getCulture()));
-          }
-
-          foreach ($facet['terms'] as $term)
-          {
-            $facets[strtr($name, '.', '_')]['terms'][$term['term']] = array(
-              'count' => $term['count'],
-              'term' => $mediaTypeNames[$term['term']]);
-          }
-
-          break;
-
-        case 'dates.startDate':
-          $facets[strtr($name, '.', '_')] = $facet['ranges'][0];  // FIXME: is this the best way?
-
-          break;
-
-        case 'places.id':
-          $criteria = new Criteria;
-          $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
-
-          $placeAPs = QubitTerm::get($criteria);
-
-          foreach ($placeAPs as $placeAP)
-          {
-            $placeAPnames[$placeAP->id] = $placeAP->getName(array('cultureFallback' => true, 'culture' => $this->context->user->getCulture()));
-          }
-
-          foreach ($facet['terms'] as &$term)
-          {
-            $facets[strtr($name, '.', '_')]['terms'][$term['term']] = array(
-              'count' => $term['count'],
-              'term' => $placeAPnames[$term['term']]);
-          }
-
-          break;
-
-        case 'names.id':
-          $criteria = new Criteria;
-          $criteria->add(QubitActor::ID, array_keys($ids), Criteria::IN);
-
-          $nameAPs = QubitActor::get($criteria);
-
-          foreach ($nameAPs as $nameAP)
-          {
-            $nameAPnames[$nameAP->id] = $nameAP->getAuthorizedFormOfName(array('cultureFallback' => true, 'culture' => $this->context->user->getCulture()));
-          }
-
-          foreach ($facet['terms'] as &$term)
-          {
-            $facets[strtr($name, '.', '_')]['terms'][$term['term']] = array(
-              'count' => $term['count'],
-              'term' => $nameAPnames[$term['term']]);
-          }
-
-          break;
-
-      }
-
-      $this->pager->facets = $facets;
+      $this->pager->setMaxPerPage(500);
     }
+
+    $this->populateFacets($resultSet);
+
+    // -------------------------------------------------------------------------
 
     // Populate level of descriptions
     $this->pager->levelsOfDescription = array();
@@ -306,68 +181,6 @@ class SearchIndexAction extends sfAction
       $this->pager->ancestors[$ancestor->id] = array(
         'slug' => $ancestor->slug,
         'title' => $ancestor->title);
-    }
-  }
-
-  public function execute($request)
-  {
-    if ('print' == $request->getGetParameter('media'))
-    {
-      $this->getResponse()->addStylesheet('print-preview', 'last');
-    }
-
-    if (!isset($request->limit))
-    {
-      $request->limit = sfConfig::get('app_hits_per_page');
-    }
-
-    $this->form = new sfForm;
-    $this->form->getValidatorSchema()->setOption('allow_extra_fields', true);
-
-    foreach ($this::$NAMES as $name)
-    {
-      $this->addField($name);
-    }
-
-    $this->form->bind($request->getGetParameters());
-
-    if ($this->form->isValid())
-    {
-      try
-      {
-        $query = $this->buildQuery();
-
-        $resultSet = QubitSearch::getInstance()->index->getType('QubitInformationObject')->search($query);
-      }
-      catch (Exception $e)
-      {
-        $this->error = $e->getMessage();
-
-        return;
-      }
-
-      // Build pager
-      $this->pager = new QubitSearchPager($resultSet);
-      $this->pager->setPage($request->page ? $request->page : 1);
-
-      if ('print' != $request->getGetParameter('media'))
-      {
-        $this->pager->setMaxPerPage($request->limit);
-      }
-      else
-      {
-        $this->pager->setMaxPerPage(500);
-      }
-
-      if ($this->pager->hasResults())
-      {
-        // Populate facets
-        $this->populateFacets();
-      }
-      else if (empty($this->error))
-      {
-        $this->error = $this->context->i18n->__('No results found.');
-      }
     }
   }
 }
