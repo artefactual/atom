aedf85fa7531a4019c7fa006bdcbdb15a7b627ae
Jesús García Crespo <jesus@sevein.com>
2013-01-11 15:12:15 -0800

    Small tweaks in the editTheme layout

--- apps/qubit/modules/repository/actions/editThemeAction.class.php
+++ apps/qubit/modules/repository/actions/editThemeAction.class.php
@@ -35,7 +35,7 @@ class RepositoryEditThemeAction extends sfAction
       case 'backgroundColor':
         $this->form->setDefault('backgroundColor', $this->resource->backgroundColor);
         $this->form->setValidator('backgroundColor', new sfValidatorRegex(array('pattern' => '/^#(?:[0-9a-fA-F]{3}){1,2}$/'), array('invalid' => $this->context->i18n->__('Only hexadecimal color value'))));
-        $this->form->setWidget('backgroundColor', new sfWidgetFormInput(array(), array('class' => 'color-picker')));
+        $this->form->setWidget('backgroundColor', new sfWidgetFormInput(array('type' => 'minicolors')));
 
         break;
 
--- apps/qubit/modules/repository/templates/editThemeSuccess.php
+++ apps/qubit/modules/repository/templates/editThemeSuccess.php
@@ -1,83 +1,96 @@
-<?php echo $form->renderGlobalErrors() ?>
-
-<?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'repository', 'action' => 'editTheme'))) ?>
-
 <div class="row">
 
-  <div class="span12" id="main-column">
+  <div class="span3">
 
-    <h1><?php echo render_title($resource) ?></h1>
+    <div id="left-column">
 
-    <ul class="breadcrumb">
-      <li><?php echo link_to(__('Institutions'), array('module' => 'repository', 'action' => 'browse')) ?></li>
-      <li><?php echo link_to(render_title($resource), array($resource, 'module' => 'repository')) ?></li>
-      <li><span><?php echo __('Edit theme') ?></span></li>
-    </ul>
-
-    <div class="row">
-
-      <div class="span9" id="content">
-
-        <fieldset class="collapsible" id="styleArea">
-
-          <legend><?php echo __('Style') ?></legend>
-
-          <div class="control-group">
-            <label class="control-label" for="html"><?php echo __('Background color') ?></label>
-            <div class="controls">
-              <?php echo $form->backgroundColor ?>
-            </div>
-          </div>
-
-          <div class="control-group">
-            <label class="control-label" for="html"><?php echo __('Banner') ?></label>
-            <div class="controls">
-              <?php echo $form->banner ?>
-              <span class="help-block"><?php echo __('Requirements: PNG format, 256K max. size. Recommended dimensions of %1%x%2%px, it will be cropped if ImageMagick is installed.', array('%1%' => arRepositoryThemeCropValidatedFile::BANNER_MAX_WIDTH, '%2%' => arRepositoryThemeCropValidatedFile::BANNER_MAX_HEIGHT)) ?></span>
-            </div>
-          </div>
-
-          <div class="control-group">
-            <label class="control-label" for="html"><?php echo __('Logo') ?></label>
-            <div class="controls">
-              <?php echo $form->logo ?>
-              <span class="help-block"><?php echo __('Requirements: PNG format, 256K max. size. Recommended dimensions of %1%x%2%px, it will be cropped if ImageMagick is installed.', array('%1%' => arRepositoryThemeCropValidatedFile::LOGO_MAX_WIDTH, '%2%' => arRepositoryThemeCropValidatedFile::LOGO_MAX_HEIGHT)) ?></span>
-            </div>
-          </div>
-
-        </fieldset>
-
-        <fieldset class="collapsible" id="pageContentArea">
-
-          <legend><?php echo __('Page content') ?></legend>
-
-          <div class="control-group">
-            <label class="control-label" for="html"><?php echo __('HTML snippet') ?></label>
-            <div class="controls">
-              <?php echo $form->htmlSnippet ?>
-            </div>
-          </div>
-
-        </fieldset>
-
+      <div class="logo">
+        <?php if (file_exists(sfConfig::get('sf_upload_dir').'/r/'.$resource->slug.'/conf/logo.png')): ?>
+          <?php echo image_tag('/uploads/r/'.$resource->slug.'/conf/logo.png') ?>
+        <?php else: ?>
+          <h2><?php echo render_title($resource) ?></h2>
+        <?php endif; ?>
       </div>
 
-      <div class="span3" id="right-column">
-
-      </div>
+      <?php include_component('repository', 'contextMenu') ?>
 
     </div>
 
   </div>
 
-  <div class="actions section">
-    <div class="content">
-      <br />
-      <ul class="clearfix links">
-        <li><input class="form-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
-        <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'repository'), array('class' => 'btn', 'title' => __('Edit'))) ?></li>
-      </ul>
+  <div class="span9">
+
+    <div id="main-column">
+
+      <?php echo $form->renderGlobalErrors() ?>
+
+      <?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'repository', 'action' => 'editTheme')), array('class' => 'form-horizontal')) ?>
+
+        <h1><?php echo render_title($resource) ?></h1>
+
+        <ul class="breadcrumb">
+          <li><?php echo link_to(__('Institutions'), array('module' => 'repository', 'action' => 'browse')) ?></li>
+          <li><?php echo link_to(render_title($resource), array($resource, 'module' => 'repository')) ?></li>
+          <li><span><?php echo __('Edit theme') ?></span></li>
+        </ul>
+
+        <div id="content">
+
+          <fieldset class="collapsible" id="styleArea">
+
+            <legend><?php echo __('Style') ?></legend>
+
+            <div class="control-group">
+              <label class="control-label" for="html"><?php echo __('Background color') ?></label>
+              <div class="controls">
+                <?php echo $form->backgroundColor ?>
+              </div>
+            </div>
+
+            <div class="control-group">
+              <label class="control-label" for="html"><?php echo __('Banner') ?></label>
+              <div class="controls">
+                <?php echo $form->banner ?>
+                <span class="help-block"><?php echo __('Requirements: PNG format, 256K max. size. Recommended dimensions of %1%x%2%px, it will be cropped if ImageMagick is installed.', array('%1%' => arRepositoryThemeCropValidatedFile::BANNER_MAX_WIDTH, '%2%' => arRepositoryThemeCropValidatedFile::BANNER_MAX_HEIGHT)) ?></span>
+              </div>
+            </div>
+
+            <div class="control-group">
+              <label class="control-label" for="html"><?php echo __('Logo') ?></label>
+              <div class="controls">
+                <?php echo $form->logo ?>
+                <span class="help-block"><?php echo __('Requirements: PNG format, 256K max. size. Recommended dimensions of %1%x%2%px, it will be cropped if ImageMagick is installed.', array('%1%' => arRepositoryThemeCropValidatedFile::LOGO_MAX_WIDTH, '%2%' => arRepositoryThemeCropValidatedFile::LOGO_MAX_HEIGHT)) ?></span>
+              </div>
+            </div>
+
+          </fieldset>
+
+          <fieldset class="collapsible" id="pageContentArea">
+
+            <legend><?php echo __('Page content') ?></legend>
+
+            <div class="control-group">
+              <label class="control-label" for="html"><?php echo __('HTML snippet') ?></label>
+              <div class="controls">
+                <?php echo $form->htmlSnippet ?>
+              </div>
+            </div>
+
+          </fieldset>
+
+        </div>
+
+        <section class="actions">
+          <ul>
+            <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
+            <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'repository'), array('class' => 'c-btn', 'title' => __('Edit'))) ?></li>
+          </ul>
+        </section>
+
+      </form>
+
     </div>
+
   </div>
 
 </div>
