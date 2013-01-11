8a1b15a6e87cd4fbb06521d20590eb3f2494c593
Jesús García Crespo <jesus@sevein.com>
2013-01-11 15:07:27 -0800

    Remove unneeded code

--- js/dominion.js
+++ js/dominion.js
@@ -90,25 +90,6 @@
 
   /****
    ****
-   ****  jQuery miniColors
-   ****
-   ****/
-
-  $(function ()
-    {
-      if ($.isFunction($.fn.miniColors))
-      {
-        $('input.color-picker').miniColors({
-            opacity: false,
-            readonly: false,
-            letterCase: 'lowercase',
-            disabled: false
-          });
-      }
-    });
-
-  /****
-   ****
    ****  Autocomplete plugin
    ****
    ****/
