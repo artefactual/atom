84a16e0ac3a97029b2bc015df4703106e5bce30e
Jesús García Crespo <jesus@sevein.com>
2013-01-12 12:52:13 -0800

    Add dropdown in informationobject/actions

--- apps/qubit/modules/informationobject/templates/_actions.php
+++ apps/qubit/modules/informationobject/templates/_actions.php
@@ -1,47 +1,54 @@
-<div class="actions section">
+<section class="actions">
 
-  <h2 class="element-invisible"><?php echo __('Actions') ?></h2>
-
-  <div class="content">
-    <ul class="clearfix links">
+  <ul>
 
       <?php if (QubitAcl::check($resource, 'update') || (QubitAcl::check($resource, 'translate'))): ?>
-        <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'informationobject', 'action' => 'edit')) ?></li>
+        <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'informationobject', 'action' => 'edit'), array('class' => 'c-btn c-btn-submit')) ?></li>
       <?php endif; ?>
 
       <?php if (QubitAcl::check($resource, 'delete')): ?>
-        <li><?php echo link_to(__('Delete'), array($resource, 'module' => 'informationobject', 'action' => 'delete'), array('class' => 'delete')) ?></li>
+        <li><?php echo link_to(__('Delete'), array($resource, 'module' => 'informationobject', 'action' => 'delete'), array('class' => 'c-btn c-btn-delete')) ?></li>
       <?php endif; ?>
 
       <?php if (QubitAcl::check($resource, 'create')): ?>
-        <li><?php echo link_to(__('Add new'), array('module' => 'informationobject', 'action' => 'add', 'parent' => url_for(array($resource, 'module' => 'informationobject')))) ?></li>
-        <li><?php echo link_to(__('Duplicate'), array('module' => 'informationobject', 'action' => 'copy', 'source' => $resource->id)) ?></li>
+        <li><?php echo link_to(__('Add new'), array('module' => 'informationobject', 'action' => 'add', 'parent' => url_for(array($resource, 'module' => 'informationobject'))), array('class' => 'c-btn')) ?></li>
+        <li><?php echo link_to(__('Duplicate'), array('module' => 'informationobject', 'action' => 'copy', 'source' => $resource->id), array('class' => 'c-btn')) ?></li>
       <?php endif; ?>
 
       <?php if (QubitAcl::check($resource, 'update')): ?>
 
-        <li><?php echo link_to(__('Move'), array($resource, 'module' => 'default', 'action' => 'move')) ?></li>
+        <li><?php echo link_to(__('Move'), array($resource, 'module' => 'default', 'action' => 'move'), array('class' => 'c-btn')) ?></li>
 
-          <?php if (0 < count($resource->digitalObjects)): ?>
+        <li class="divider"></li>
 
-            <li><?php echo link_to(__('Edit digital object'), array($resource->digitalObjects[0], 'module' => 'digitalobject', 'action' => 'edit')) ?></li>
-          <?php else: ?>
+        <li>
+          <div class="btn-group dropup">
+            <a class="c-btn dropdown-toggle" data-toggle="dropdown" href="#">
+              <?php echo __('More') ?>
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
 
-            <li><?php echo link_to(__('Link digital object'), array($resource, 'module' => 'informationobject', 'action' => 'addDigitalObject')) ?></li>
+              <li><?php echo link_to(__('Link physical storage'), array($resource, 'module' => 'informationobject', 'action' => 'editPhysicalObjects')) ?></li>
 
-          <?php endif; // has digital object ?>
+              <li class="divider"></li>
 
-        <?php if (null === $resource->repository || 0 != $resource->repository->uploadLimit): ?>
+              <?php if (0 < count($resource->digitalObjects)): ?>
+                <li><?php echo link_to(__('Edit digital object'), array($resource->digitalObjects[0], 'module' => 'digitalobject', 'action' => 'edit')) ?></li>
+              <?php else: ?>
+                <li><?php echo link_to(__('Link digital object'), array($resource, 'module' => 'informationobject', 'action' => 'addDigitalObject')) ?></li>
+              <?php endif; // has digital object ?>
 
-          <li><?php echo link_to(__('Import digital objects'), array($resource, 'module' => 'informationobject', 'action' => 'multiFileUpload')) ?></li>
+              <?php if (null === $resource->repository || 0 != $resource->repository->uploadLimit): ?>
+                <li><?php echo link_to(__('Import digital objects'), array($resource, 'module' => 'informationobject', 'action' => 'multiFileUpload')) ?></li>
+              <?php endif; // upload quota is non-zero ?>
 
-        <?php endif; // upload quota is non-zero ?>
-
-        <li><?php echo link_to(__('Link physical storage'), array($resource, 'module' => 'informationobject', 'action' => 'editPhysicalObjects')) ?></li>
+            </ul>
+          </div>
+        </li>
 
       <?php endif; // user has update permission ?>
 
-    </ul>
-  </div>
+  </ul>
 
-</div>
+</section>
