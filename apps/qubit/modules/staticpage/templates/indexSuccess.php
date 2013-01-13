f05901031fc79ee98530e9696a8512cffc6db0f8
Jesús García Crespo <jesus@sevein.com>
2013-01-13 14:55:11 -0800

    Layout tweaks

--- apps/qubit/modules/staticpage/templates/indexSuccess.php
+++ apps/qubit/modules/staticpage/templates/indexSuccess.php
@@ -7,17 +7,11 @@
   </div>
 
   <?php if (SecurityCheck::hasPermission($sf_user, array('module' => 'staticpage', 'action' => 'update'))): ?>
-    <div class="actions section">
-
-      <h2 class="element-invisible"><?php echo __('Actions') ?></h2>
-
-      <div class="content">
-        <ul class="links">
-          <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'))) ?></li>
-        </ul>
-      </div>
-
-    </div>
+    <section class="actions">
+      <ul>
+        <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'))) ?></li>
+      </ul>
+    </section>
   <?php endif; ?>
 
 </div>
