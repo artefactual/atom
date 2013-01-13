f4f57421981215870b832673a5a259d0bb0e0c8f
Jesús García Crespo <jesus@sevein.com>
2013-01-13 14:58:47 -0800

    Create staticpage/home
    The homepage is likely to add some extra features for the homepage,
    like staff picks, popular searches, virtual exhibits. I thought
    that creating a separate action would be a better approach and
    easier to extend from plugins/.

---
+++ apps/qubit/modules/staticpage/templates/homeSuccess.php
@@ -0,0 +1,17 @@
+<div class="page">
+
+  <h1><?php echo render_title($resource->getTitle(array('cultureFallback' => true))) ?></h1>
+
+  <div>
+    <?php echo render_value($resource->getContent(array('cultureFallback' => true))) ?>
+  </div>
+
+  <?php if (SecurityCheck::hasPermission($sf_user, array('module' => 'staticpage', 'action' => 'update'))): ?>
+    <section class="actions">
+      <ul>
+        <li><?php echo link_to(__('Edit'), array($resource, 'module' => 'staticpage', 'action' => 'edit'), array('title' => __('Edit this page'))) ?></li>
+      </ul>
+    </section>
+  <?php endif; ?>
+
+</div>
