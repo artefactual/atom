7ac6efc9b53fa3b2cb3b29173f19c36f59ae0591
Jesús García Crespo <jesus@sevein.com>
2013-01-13 17:35:38 -0800

    More layout and CSS tweaks

--- apps/qubit/modules/menu/templates/_mainMenu.php
+++ apps/qubit/modules/menu/templates/_mainMenu.php
@@ -1,12 +1,14 @@
-<div class="navbar navbar-inverse navbar-fixed-top" id="header-admin">
-  <div class="navbar-inner">
-    <ul class="nav">
-      <?php // Using $sf_user->hasGroup() since it relies on database,
-            // $sf_user->hasCredential('administrator') relies on session storage
-            // This adds more db access but we are caching anyways
-            // See also issue 2266
-            $isAdministrator = $sf_user->hasGroup(QubitAclGroup::ADMINISTRATOR_ID) ?>
-      <?php echo QubitMenu::displayHierarchyAsList($mainMenu, 0, array('overrideVisibility' => array('admin' => $isAdministrator))) ?>
-    </ul>
+<div id="header-admin">
+  <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+      <ul class="nav">
+        <?php // Using $sf_user->hasGroup() since it relies on database,
+              // $sf_user->hasCredential('administrator') relies on session storage
+              // This adds more db access but we are caching anyways
+              // See also issue 2266
+              $isAdministrator = $sf_user->hasGroup(QubitAclGroup::ADMINISTRATOR_ID) ?>
+        <?php echo QubitMenu::displayHierarchyAsList($mainMenu, 0, array('overrideVisibility' => array('admin' => $isAdministrator))) ?>
+      </ul>
+    </div>
   </div>
 </div>
