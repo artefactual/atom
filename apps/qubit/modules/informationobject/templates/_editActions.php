717c79a45b0e34412eb8a95a699ebcf91255ffb4
Jesús García Crespo <jesus@sevein.com>
2013-01-13 18:17:10 -0800

    Update informationobject/editActions layout

--- apps/qubit/modules/informationobject/templates/_editActions.php
+++ apps/qubit/modules/informationobject/templates/_editActions.php
@@ -1,23 +1,15 @@
-<div class="actions section">
-
-  <h2 class="element-invisible"><?php echo __('Actions') ?></h2>
-
-  <div class="content">
-    <ul class="clearfix links">
-
-      <?php if (isset($resource->id)): ?>
-        <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'informationobject')) ?></li>
-        <li><input class="form-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
+<section class="actions">
+  <ul>
+    <?php if (isset($resource->id)): ?>
+      <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'informationobject'), array('class' => 'c-btn')) ?></li>
+      <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
+    <?php else: ?>
+      <?php if (isset($sf_request->parent)): ?>
+        <li><?php echo link_to(__('Cancel'), array($resource->parent, 'module' => 'informationobject'), array('class' => 'c-btn')) ?></li>
       <?php else: ?>
-        <?php if (isset($sf_request->parent)): ?>
-          <li><?php echo link_to(__('Cancel'), array($resource->parent, 'module' => 'informationobject')) ?></li>
-        <?php else: ?>
-          <li><?php echo link_to(__('Cancel'), array('module' => 'informationobject', 'action' => 'list')) ?></li>
-        <?php endif; ?>
-        <li><input class="form-submit" type="submit" value="<?php echo __('Create') ?>"/></li>
+        <li><?php echo link_to(__('Cancel'), array('module' => 'informationobject', 'action' => 'list'), array('class' => 'c-btn')) ?></li>
       <?php endif; ?>
-
-    </ul>
-  </div>
-
-</div>
+      <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Create') ?>"/></li>
+    <?php endif; ?>
+  </ul>
+</section>
