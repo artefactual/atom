7fea30041933d8fa9e4e9dd55fb0bbd4964f937c
Jesús García Crespo <jesus@sevein.com>
2013-01-24 13:57:38 -0800

    Add layout without .container

---
+++ apps/qubit/templates/layout_basic.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="<?php echo $sf_user->getCulture() ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction ?>">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="<?php echo public_path('favicon.ico') ?>"/>
+    <?php include_stylesheets() ?>
+    <?php include_component_slot('css') ?>
+    <?php if ($sf_context->getConfiguration()->isDebug()): ?>
+      <script type="text/javascript" charset="utf-8">
+        less = { env: 'development', optimize: 0 };
+      </script>
+    <?php endif; ?>
+    <?php include_javascripts() ?>
+  </head>
+  <body class="<?php echo $sf_context->getModuleName() ?> <?php echo $sf_context->getActionName() ?> user-<?php echo $sf_user->isAuthenticated() ? 'logged' : 'anonymous' ?>">
+
+    <?php echo get_partial('header') ?>
+
+    <div id="wrapper">
+
+      <?php echo $sf_content ?>
+
+    </div>
+
+    <?php echo get_partial('footer') ?>
+
+    <div id="print-date">
+      <?php echo __('Printed: %d%', array('%d%' => date('Y-m-d'))) ?>
+    </div>
+
+  </body>
+</html>
