dbf985b864ed3784951c1217957af0f8d10874b3
Jesús García Crespo <jesus@sevein.com>
2013-01-24 11:25:36 -0800

    Remove qtTrilliumPlugin

--- plugins/qtTrilliumPlugin/modules/i18n/templates/_changeLanguageList.php
+++
@@ -1,9 +0,0 @@
-<a class="menu" href="#"><?php echo __('Language') ?></a>
-
-<ul>
-  <?php foreach (sfConfig::getAll() as $name => $value): ?>
-    <?php if ('app_i18n_languages' == substr($name, 0, 18)): ?>
-      <li<?php if ($sf_user->getCulture() == $value): ?> class="active"<?php endif; ?>><?php echo link_to(format_language($value, $value), array('sf_culture' => $value) + $sf_request->getParameterHolder()->getAll()) ?></li>
-    <?php endif; ?>
-  <?php endforeach; ?>
-</ul>
--- plugins/qtTrilliumPlugin/templates/_sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="column sidebar" id="sidebar-first">
-  <div class="section">
-
-    <?php echo get_component_slot('sidebar') ?>
-
-    <?php echo get_component('menu', 'browseMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
-
-  </div> <!-- /.section -->
-</div> <!-- /.column.sidebar#sidebar-first -->
