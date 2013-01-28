54bb5b73260d052da6a8e2b396e71e28f163fb6a
Jesús García Crespo <jesus@sevein.com>
2013-01-28 14:59:16 -0800

    Use new layout with <section> tag

--- apps/qubit/modules/repository/templates/deleteSuccess.php
+++ apps/qubit/modules/repository/templates/deleteSuccess.php
@@ -2,17 +2,11 @@
 
 <?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'repository', 'action' => 'delete')), array('method' => 'delete')) ?>
 
-  <div class="actions section">
-
-    <h2 class="element-invisible"><?php echo __('Actions') ?></h2>
-
-    <div class="content">
-      <ul class="clearfix links">
-        <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'repository')) ?></li>
-        <li><input class="form-submit danger" type="submit" value="<?php echo __('Delete') ?>"/></li>
-      </ul>
-    </div>
-
-  </div>
+  <section class="actions">
+    <ul>
+      <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'repository'), array('class' => 'c-btn')) ?></li>
+      <li><input class="c-btn c-btn-delete" type="submit" value="<?php echo __('Delete') ?>"/></li>
+    </ul>
+  </section>
 
 </form>
