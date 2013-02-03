8457adeebbe0d07e3cc992e1bc1d4fa7caedbf82
Jesús García Crespo <jesus@sevein.com>
2013-02-03 13:01:34 -0800

    Add i18n

--- apps/qubit/modules/informationobject/templates/_printPreviewBar.php
+++ apps/qubit/modules/informationobject/templates/_printPreviewBar.php
@@ -1,6 +1,6 @@
 <?php if ('print' == $sf_request->getParameter('media')): ?>
   <div id="preview-message">
     <?php echo __('Print preview') ?>
-    <?php echo link_to('Close', array($resource, 'module' => 'informationobject')) ?>
+    <?php echo link_to(__('Close'), array($resource, 'module' => 'informationobject')) ?>
   </div>
 <?php endif; ?>
