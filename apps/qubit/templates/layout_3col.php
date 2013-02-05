6bd5b5c0ca414099518a42a3c798377693e0013b
Jesús García Crespo <jesus@sevein.com>
2013-02-04 17:20:13 -0800

    Add slot based layouts

---
+++ apps/qubit/templates/layout_3col.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="<?php echo $sf_user->getCulture() ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction ?>">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="<?php echo public_path('favicon.ico') ?>"/>
+    <?php include_stylesheets() ?>
+    <?php include_component_slot('css') ?>
+    <?php if ($sf_context->getConfiguration()->isDebug()): ?>
+      <script type="text/javascript" charset="utf-8">
+        less = { env: 'development', optimize: 0, relativeUrls: true };
+      </script>
+    <?php endif; ?>
+    <?php include_javascripts() ?>
+  </head>
+  <body class="yui-skin-sam <?php echo $sf_context->getModuleName() ?> <?php echo $sf_context->getActionName() ?>">
+
+    <?php echo get_partial('header') ?>
+
+    <div id="wrapper" class="container">
+
+      <div class="row">
+
+        <div class="span3">
+
+          <div id="sidebar">
+
+            <?php include_slot('sidebar') ?>
+
+          </div>
+
+        </div>
+
+        <div class="span9">
+
+          <div id="main-column">
+
+            <?php include_slot('title') ?>
+
+            <div class="row">
+
+              <div class="span7">
+
+                <?php include_slot('before-content') ?>
+
+                <?php if (!include_slot('content')): ?>
+                  <div id="content">
+                    <?php echo $sf_content ?>
+                  </div>
+                <?php endif; ?>
+
+                <?php include_slot('after-content') ?>
+
+              </div>
+
+              <div class="span2">
+
+                <div id="context-menu">
+
+                  <?php include_slot('context-menu') ?>
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <?php echo get_partial('footer') ?>
+
+  </body>
+</html>
