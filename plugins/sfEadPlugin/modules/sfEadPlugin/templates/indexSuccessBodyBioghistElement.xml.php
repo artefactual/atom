e1d6c996f11c22efd4ba30147d54f043105e5885
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-02-26 20:20:15 +0100

    Change EAD <bioghist> export.
    Now uses one bioghist tag for each creator

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
@@ -4,39 +4,39 @@
   $events = $$resourceVar->getActorEvents(array('eventTypeId' => QubitTerm::CREATION_ID));
 
   if (0 < count($creators)): ?>
-    <bioghist encodinganalog="3.2.2">
-      <chronlist>
-        <?php foreach($events as $date): ?>
-        <?php $creator = QubitActor::getById($date->actorId); ?>
-        <chronitem>
-          <?php echo $ead->renderEadDateFromEvent('creation', $date) ?>
-          <eventgrp>
-            <event>
-              <?php if ($value = $date->getDescription(array('cultureFallback' => true))): ?>
-                <note type="eventNote"><p><?php echo esc_specialchars($value) ?></p></note>
-              <?php endif; ?>
-              <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
-                <note><p><?php echo esc_specialchars($value) ?></p></note>
-              <?php endif; ?>
-              <origination encodinganalog="3.2.1">
-                <?php if ($type = $creator->getEntityTypeId()): ?>
-                  <?php if ($type == QubitTerm::PERSON_ID): ?>
-                    <persname source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></persname>
-                  <?php endif; ?>
-                  <?php if ($type == QubitTerm::FAMILY_ID): ?>
-                    <famname source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></famname>
-                  <?php endif; ?>
-                  <?php if ($type == QubitTerm::CORPORATE_BODY_ID): ?>
-                    <corpname source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></corpname>
-                  <?php endif; ?>
-                <?php else: ?>
-                  <name source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></name>
+    <?php foreach($events as $date): ?>
+      <?php $creator = QubitActor::getById($date->actorId); ?>
+      <bioghist encodinganalog="3.2.2">
+        <chronlist>
+          <chronitem>
+            <?php echo $ead->renderEadDateFromEvent('creation', $date) ?>
+            <eventgrp>
+              <event>
+                <?php if ($value = $date->getDescription(array('cultureFallback' => true))): ?>
+                  <note type="eventNote"><p><?php echo esc_specialchars($value) ?></p></note>
                 <?php endif; ?>
-              </origination>
-            </event>
-          </eventgrp>
-        </chronitem>
-        <?php endforeach; ?>
-      </chronlist>
-    </bioghist>
+                <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
+                  <note><p><?php echo esc_specialchars($value) ?></p></note>
+                <?php endif; ?>
+                <origination encodinganalog="3.2.1">
+                  <?php if ($type = $creator->getEntityTypeId()): ?>
+                    <?php if ($type == QubitTerm::PERSON_ID): ?>
+                      <persname source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></persname>
+                    <?php endif; ?>
+                    <?php if ($type == QubitTerm::FAMILY_ID): ?>
+                      <famname source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></famname>
+                    <?php endif; ?>
+                    <?php if ($type == QubitTerm::CORPORATE_BODY_ID): ?>
+                      <corpname source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></corpname>
+                    <?php endif; ?>
+                  <?php else: ?>
+                    <name source="<?php echo esc_specialchars($creator->datesOfExistence) ?>"><?php echo esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true))) ?></name>
+                  <?php endif; ?>
+                </origination>
+              </event>
+            </eventgrp>
+          </chronitem>
+        </chronlist>
+      </bioghist>
+    <?php endforeach; ?>
   <?php endif; ?>
