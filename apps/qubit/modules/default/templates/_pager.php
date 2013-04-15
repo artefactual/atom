a18a2807ba2431cd4a29bfd17b5586276f2e9cc0
Jesús García Crespo <jesus@sevein.com>
2013-04-15 16:07:15 -0700

    Add links to the first and last pages in the pager

--- apps/qubit/modules/default/templates/_pager.php
+++ apps/qubit/modules/default/templates/_pager.php
@@ -34,19 +34,46 @@
       <div class="pagination pagination-centered">
         <ul>
 
+          <?php $items = 7 ?>
+
           <?php if (1 < $pager->getPage()): ?>
             <li class="previous">
               <?php echo link_to('&laquo; '. __('Previous'), array('page' => $pager->getPage() - 1) + $sf_request->getParameterHolder()->getAll()) ?>
             </li>
           <?php endif; ?>
 
-          <?php foreach ($pager->getLinks(10) as $page): ?>
+          <?php foreach ($pager->getLinks($items) as $key => $page): ?>
+
+            <?php if (0 === $key): ?>
+
+              <?php if ($pager->getPage() == $page): ?>
+                <li class="active"><span>1</span></li>
+              <?php else: ?>
+                <li><?php echo link_to(1, array('page' => 1) + $sf_request->getParameterHolder()->getAll(), array('title' => __('Go to page %1%', array('%1%' => 1)))) ?></li>
+              <?php endif; ?>
+
+              <?php if (1 == $page): ?>
+                <?php continue; ?>
+              <?php else: ?>
+                <li class="dots"><span>...</span></li>
+              <?php endif; ?>
+
+            <?php endif; ?>
+
             <?php if ($pager->getPage() == $page): ?>
-              <li class="active"><a href="#"><?php echo $page ?></a></li>
+              <li class="active"><span><?php echo $page ?></span></li>
             <?php else: ?>
               <li><?php echo link_to($page, array('page' => $page) + $sf_request->getParameterHolder()->getAll(), array('title' => __('Go to page %1%', array('%1%' => $page)))) ?></li>
             <?php endif; ?>
+
           <?php endforeach ?>
+
+          <?php if (0  < ($pager->getLastPage() - $pager->getPage())): ?>
+            <li class="dots"><span>...</span></li>
+            <li class="last">
+              <?php echo link_to($pager->getLastPage(), array('page' => $pager->getLastPage()) + $sf_request->getParameterHolder()->getAll()) ?>
+            </li>
+          <?php endif; ?>
 
           <?php if ($pager->getLastPage() > $pager->getPage()): ?>
             <li class="next">
