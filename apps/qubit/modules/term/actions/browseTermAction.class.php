5100bd28c749ac626505a180862a18ac32b92aaf
Jesús García Crespo <jesus@sevein.com>
2013-04-22 14:36:50 -0700

    Refactor term browser

--- apps/qubit/modules/term/actions/browseTermAction.class.php
+++ apps/qubit/modules/term/actions/browseTermAction.class.php
@@ -17,30 +17,41 @@
  * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class TermBrowseTermAction extends sfAction
+class TermBrowseTermAction extends DefaultBrowseAction
 {
+  // Arrays not allowed in class constants
+  public static
+    $FACETS = array();
+
   public function execute($request)
   {
-    if (!isset($request->limit))
+    $this->resource = $this->getRoute()->resource;
+    if (!$this->resource instanceof QubitTerm)
     {
-      $request->limit = sfConfig::get('app_hits_per_page');
+      $this->forward404();
     }
 
-    $this->resource = $this->getRoute()->resource;
+    // Check that this isn't the root
+    if (!isset($this->resource->parent))
+    {
+      $this->forward404();
+    }
 
-    $queryBool = new Elastica_Query_Bool();
+    parent::execute($request);
+
+    $this->queryBool->addMust(new Elastica_Query_Terms('terms.id', array($this->resource->id)));
 
     switch ($this->resource->taxonomyId)
     {
       case QubitTaxonomy::PLACE_ID:
         $this->icon = 'places';
-        $queryBool->addMust(new Elastica_Query_Term(array('places.id' => $this->resource->id)));
+        $this->queryBool->addMust(new Elastica_Query_Terms('terms.taxonomyId', array(QubitTaxonomy::PLACE_ID)));
 
         break;
 
       case QubitTaxonomy::SUBJECT_ID:
         $this->icon = 'subjects';
-        $queryBool->addMust(new Elastica_Query_Term(array('subjects.id' => $this->resource->id)));
+        $this->queryBool->addMust(new Elastica_Query_Terms('terms.taxonomyId', array(QubitTaxonomy::SUBJECT_ID)));
 
         break;
 
@@ -51,146 +62,19 @@ class TermBrowseTermAction extends sfAction
         break;
     }
 
-    $queryBool->addMust(new Elastica_Query_Term(array('publicationStatusId' => QubitTerm::PUBLICATION_STATUS_PUBLISHED_ID)));
+    // Filter drafts
+    $this->query = QubitAclSearch::filterDrafts($this->query);
 
-    $this->filters = array();
-    foreach ($this->request->getGetParameters() as $param => $value)
-    {
-      if (in_array(strtr($param, '_', '.'), array('digitalObject.mediaTypeId')))
-      {
-        foreach (explode(',', $value) as $facetValue)
-        {
-          // don't include empty filters (querystring sanitization)
-          if ('' != preg_replace('/[\s\t\r\n]*/', '', $facetValue))
-          {
-            $this->filters[$param][] = $facetValue;
+    $this->query->setQuery($this->queryBool);
 
-            $queryBool->addMust(new Elastica_Query_Term(array(
-              strtr($param, '_', '.') => $facetValue)));
-          }
-        }
-      }
-    }
+    $resultSet = QubitSearch::getInstance()->index->getType('QubitInformationObject')->search($this->query);
 
-    $query = new Elastica_Query();
-    $query->setSort(array('_score' => 'desc', 'slug' => 'asc'));
-    $query->setLimit($request->limit);
-    $query->setQuery($queryBool);
-
-    if (!empty($request->page))
-    {
-      $query->setFrom(($request->page - 1) * $request->limit);
-    }
-
-    $facet = new Elastica_Facet_Terms('digitalObject.mediaTypeId');
-    $facet->setField('digitalObject.mediaTypeId');
-    $facet->setSize(50);
-    $query->addFacet($facet);
-
-    $facet = new Elastica_Facet_Range('dates.startDate');
-    $facet->setField('dates.startDate');
-    $facet->addRange(null, null);
-    $query->addFacet($facet);
-
-    try
-    {
-      $resultSet = QubitSearch::getInstance()->index->getType('QubitInformationObject')->search($query);
-    }
-    catch (Exception $e)
-    {
-      $this->error = $e->getMessage();
-
-      return;
-    }
-
+    // Page results
     $this->pager = new QubitSearchPager($resultSet);
     $this->pager->setPage($request->page ? $request->page : 1);
     $this->pager->setMaxPerPage($request->limit);
+    $this->pager->init();
 
-    if ($this->pager->hasResults())
-    {
-      foreach ($resultSet->getFacets() as $name => $facet)
-      {
-        if (isset($facet['terms']))
-        {
-          $ids = array();
-          foreach ($facet['terms'] as $term)
-          {
-            $ids[$term['term']] = $term['count'];
-          }
-        }
-
-        switch ($name)
-        {
-          case 'digitalObject.mediaTypeId':
-            $criteria = new Criteria;
-            $criteria->add(QubitTerm::ID, array_keys($ids), Criteria::IN);
-
-            $mediaTypes = QubitTerm::get($criteria);
-
-            foreach ($mediaTypes as $mediaType)
-            {
-              $mediaTypeNames[$mediaType->id] = $mediaType->getName(array(
-                'cultureFallback' => true,
-                'culture' => $this->context->user->getCulture()));
-            }
-
-            foreach ($facet['terms'] as $term)
-            {
-              $facets[strtr($name, '.', '_')]['terms'][$term['term']] = array(
-                'count' => $term['count'],
-                'term' => $mediaTypeNames[$term['term']]);
-            }
-
-            break;
-
-          case 'dates.startDate':
-            // FIXME: is this the best way?
-            $facets[strtr($name, '.', '_')] = $facet['ranges'][0];
-
-            break;
-        }
-      }
-
-      $this->pager->facets = $facets;
-
-      $this->pager->levelsOfDescription = array();
-      foreach (QubitTerm::getLevelsOfDescription() as $lod)
-      {
-        $this->pager->levelsOfDescription[$lod->id] = $lod->getName(array(
-          'cultureFallback' => true,
-          'culture' => $this->context->user->getCulture()));
-      }
-
-      $ancestorsIds = array();
-      foreach ($this->pager->getResults() as $hit)
-      {
-        $doc = $hit->getData();
-        foreach ($doc['ancestors'] as $item)
-        {
-          if (in_array($item, $ancestorsIds))
-          {
-            continue;
-          }
-
-          $ancestorsIds[] = $item;
-        }
-      }
-
-      $sql = 'SELECT
-          io.id,
-          o.slug,
-          io.title
-        FROM '.QubitInformationObjectI18n::TABLE_NAME.' AS io
-        LEFT JOIN '.QubitSlug::TABLE_NAME.' AS o ON (o.object_id = io.id AND io.culture = ?)
-        WHERE o.object_id IN ('.implode(',', $ancestorsIds).')';
-      $this->pager->ancestors = array();
-      foreach (QubitPdo::fetchAll($sql, array($this->context->user->getCulture())) as $ancestor)
-      {
-        $this->pager->ancestors[$ancestor->id] = array(
-          'slug' => $ancestor->slug,
-          'title' => $ancestor->title);
-      }
-    }
+    $this->populateFacets($resultSet);
   }
 }
--- apps/qubit/modules/term/templates/browseTermSuccess.php
+++ apps/qubit/modules/term/templates/browseTermSuccess.php
@@ -1,87 +1,34 @@
+<?php decorate_with('layout_2col') ?>
 <?php use_helper('Text') ?>
 
-<div id="search-results">
+<?php slot('title') ?>
 
-  <div class="row">
-
-    <div class="span12">
-      <h1>
-        <?php if (isset($icon)): ?>
-          <?php echo image_tag('/images/icons-large/icon-'.$icon.'.png', array('width' => '42', 'height' => '42')) ?>
-        <?php endif; ?>
-        <?php echo render_title($resource->taxonomy) ?> - <?php echo render_title($resource) ?>
-        <strong class="hidden-phone">
-          <?php echo __('%1% search results', array('%1%' => $pager->getNbResults())) ?>
-          <?php if (sfConfig::get('app_multi_repository')): ?>
-            <?php echo __('in %1% institutions', array('%1%' => count($pager->facets['repository_id']['terms']))) ?>
-          <?php endif; ?>
-        </strong>
-      </h1>
-    </div>
-
-    <div id="phone-filter" class="span12 visible-phone">
-      <h2 class="widebtn btn-huge" data-toggle="collapse" data-target="#facets, #top-facet"><?php echo __('Filter %1% Results', array(
-        '%1%' => $pager->getNbResults())) ?></h2>
-    </div>
-
-  </div>
-
-  <div class="row">
-
-    <div class="span3" id="facets">
-
-      <?php if (isset($pager->facets['digitalObject_mediaTypeId'])): ?>
-        <div class="section">
-          <h2 class="visible-phone widebtn btn-huge" data-toggle="collapse" data-target="#mediatypes"><?php echo __('Media Type') ?></h2>
-          <h2 class="hidden-phone"><?php echo __('Media Type') ?></h2>
-          <div class="scrollable" id="mediatypes">
-            <ul>
-              <li <?php if ('' == $sf_request->getParameter('digitalObject_mediaTypeId')) echo 'class="active"' ?>><?php echo link_to(__('All'), array('digitalObject_mediaTypeId' => null, 'page' => null) + $sf_request->getParameterHolder()->getAll()) ?></li>
-              <?php foreach($pager->facets['digitalObject_mediaTypeId']['terms'] as $id => $term): ?>
-                <li <?php if (in_array($id, @$filters['digitalObject_mediaTypeId'])) echo 'class="active"' ?>><?php echo link_to(__($term['term']).'<span>'.$term['count'].'</span>', array('digitalObject_mediaTypeId' => (@$filters['digitalObject_mediaTypeId'] ? implode(',', array_diff(array_merge(@$filters['digitalObject_mediaTypeId'], array($id)), array_intersect(@$filters['digitalObject_mediaTypeId'], array($id)))) : $id), 'page' => null) + $sf_request->getParameterHolder()->getAll()) ?></li>
-              <?php endforeach; ?>
-            </ul>
-          </div>
-        </div>
+  <div class="hidden-phone">
+    <h1>
+      <?php if (isset($icon)): ?>
+        <?php echo image_tag('/images/icons-large/icon-'.$icon.'.png', array('width' => '42', 'height' => '42')) ?>
       <?php endif; ?>
+      <?php echo __('Browse %1% %2%', array(
+        '%1%' => render_title($resource->taxonomy),
+        '%2%' => render_title($resource))) ?>
+    </h1>
+  </div>
 
-      <div class="section">
+<?php end_slot() ?>
 
-        <h2 class="visible-phone widebtn btn-huge" data-toggle="collapse" data-target="#dates"><?php echo __('Creation date') ?></h2>
-        <h2 class="hidden-phone"><?php echo __('Creation date') ?></h2>
+<?php slot('sidebar') ?>
 
-        <div class="scrollable dates" id="dates">
-          <input type="text" value="<?php echo $pager->facets['dates_startDate']['min'] ?>" name="from" /> - <input type="text" value="<?php echo $pager->facets['dates_startDate']['max'] ?>" name="to" />
-        </div>
-
-      </div>
-
-      <hr />
-
-      <?php echo link_to(__('Show all %1%', array('%1%' => $resource->taxonomy->__toString())),
-        array('module' => 'taxonomy', 'action' => 'browse', 'id' => $resource->taxonomyId),
-        array('class' => 'widebtn')) // HACK Use id deliberately because "Subjects" and "Places" menus still use id ?>
-
-    </div>
-
-    <div class="span9" id="content">
-
-      <div class="listings">
-
-        <?php foreach ($pager->getResults() as $item): ?>
-
-          <?php $doc = build_i18n_doc($item, array('creators')) ?>
-          <?php echo include_partial('search/searchResult', array('doc' => $doc, 'pager' => $pager)) ?>
-
-        <?php endforeach; ?>
-
-        <?php echo get_partial('default/pager', array('pager' => $pager)) ?>
-
-      </div>
-
-    </div>
+  <div id="facets">
 
   </div>
 
-</div>
+<?php end_slot() ?>
 
+<?php foreach ($pager->getResults() as $hit): ?>
+  <?php $doc = $hit->getData() ?>
+  <?php echo include_partial('search/searchResult', array('doc' => $doc, 'pager' => $pager)) ?>
+<?php endforeach; ?>
+
+<?php slot('after-content') ?>
+  <?php echo get_partial('default/pager', array('pager' => $pager)) ?>
+<?php end_slot() ?>
