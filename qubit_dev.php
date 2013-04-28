19d1883fc6b70e7329b86e3db2e71fae9a201d10
Jesús García Crespo <jesus@sevein.com>
2013-04-28 12:29:52 -0700

    Add ATOM_DEBUG_IP to qubit_dev.php whitelist

--- qubit_dev.php
+++ qubit_dev.php
@@ -1,8 +1,16 @@
 <?php
 
-// this check prevents access to debug front controllers that are deployed by accident to production servers.
-// feel free to remove this, extend it or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+// This check prevents access to debug front controllers that are deployed by
+// accident to production servers. Feel free to remove this, extend it or make
+// something more sophisticated.
+
+$allowedIps = array('127.0.0.1', '::1');
+if (false !== $envIp = getenv('ATOM_DEBUG_IP'))
+{
+  $allowedIps[] = $envIp;
+}
+
+if (!in_array(@$_SERVER['REMOTE_ADDR'], $allowedIps))
 {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
