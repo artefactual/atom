ec38a4b213a4f525616ab5bbd213ca7896c55ca6
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-04-30 04:32:15 +0200

    Change edition statement tooltip in RAD template
    Fixes #4760.

--- plugins/sfRadPlugin/modules/sfRadPlugin/templates/editSuccess.php
+++ plugins/sfRadPlugin/modules/sfRadPlugin/templates/editSuccess.php
@@ -125,11 +125,11 @@
     <legend><?php echo __('Edition area') ?></legend>
 
     <?php echo render_field($form->edition
-      ->help(__('"Transcribe the edition statement relating to the item being described." (RAD 1.2B1) "If the item being described lacks an edition statement but is known to contain significant changes from other editions, supply a suitable brief statement in the language and script of the title proper and enclose it in square brackets." (RAD 1.2B3)'))
+      ->help(__('"Use this area only in item level description to record statements relating to versions of items existing in two or more versions or states in single or multiple copies." (RAD 1.2A1) "Transcribe the edition statement relating to the item being described." (RAD 1.2B1) "If the item being described lacks an edition statement but is known to contain significant changes from other editions, supply a suitable brief statement in the language and script of the title proper and enclose it in square brackets." (RAD 1.2B3)'))
       ->label(__('Edition statement')), $resource) ?>
 
     <?php echo render_field($form->editionStatementOfResponsibility
-      ->help(__('"Use this area only in item level description to record statements relating to versions of items existing in two or more versions or states in single or multiple copies." (RAD 1.2A1) "Transcribe a statement of responsibility relating to one or more editions, but not to all editions, of the item being described following the edition statement if there is one." (RAD 1.2.C1) "When describing the first edition, give all statements of responsibility in the Title and statement of responsibility area." (RAD 1.2C2)'))
+      ->help(__('"Transcribe a statement of responsibility relating to one or more editions, but not to all editions, of the item being described following the edition statement if there is one." (RAD 1.2.C1) "When describing the first edition, give all statements of responsibility in the Title and statement of responsibility area." (RAD 1.2C2)'))
       ->label(__('Statement of responsibility relating to the edition')), $rad) ?>
 
   </fieldset> <!-- #editionArea -->
