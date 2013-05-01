artefactual/atom

commit 2c0bbd01841fc6551f16604664692d3d3aaf6f28

Rename widget class

lib/form/arWidgetFormSelectRadio.class.php
@@ -1,23 +1,23 @@
 <?php
 
 /*
- * This file is part of Qubit Toolkit.
+ * This file is part of the Access to Memory (AtoM) software.
  *
- * Qubit Toolkit is free software: you can redistribute it and/or modify
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Qubit Toolkit is distributed in the hope that it will be useful,
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Qubit Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class qtWidgetFormSelectRadio extends sfWidgetFormSelectRadio
+class arWidgetFormInputFileEditable extends sfWidgetFormInputFileEditable
 {
   protected function configure($options = array(), $attributes = array())
   {

apps/qubit/modules/search/actions/updatesAction.class.php
@@ -74,7 +74,7 @@ class SearchUpdatesAction extends sfAction
         );
 
         $this->form->setValidator($name, new sfValidatorChoice(array('choices' => array_keys($choices))));
-        $this->form->setWidget($name, new qtWidgetFormSelectRadio(array('choices' => $choices, 'class' => 'radio inline')));
+        $this->form->setWidget($name, new arWidgetFormSelectRadio(array('choices' => $choices, 'class' => 'radio inline')));
 
         break;
 
@@ -86,7 +86,7 @@ class SearchUpdatesAction extends sfAction
         );
 
         $this->form->setValidator($name, new sfValidatorChoice(array('choices' => array_keys($choices))));
-        $this->form->setWidget($name, new qtWidgetFormSelectRadio(array('choices' => $choices, 'class' => 'radio inline')));
+        $this->form->setWidget($name, new arWidgetFormSelectRadio(array('choices' => $choices, 'class' => 'radio inline')));
 
         break;
 

commit c871c0a45fe87d3048762bab16906df653a025dc

Fix class declaration
It seems that i broke it in 2c0bbd01841fc.

lib/form/arWidgetFormSelectRadio.class.php
@@ -17,7 +17,7 @@
  * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class arWidgetFormInputFileEditable extends sfWidgetFormInputFileEditable
+class arWidgetFormSelectRadio extends sfWidgetFormSelectRadio
 {
   protected function configure($options = array(), $attributes = array())
   {
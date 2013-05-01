c871c0a45fe87d3048762bab16906df653a025dc
Jesús García Crespo <jesus@sevein.com>
2013-05-01 15:06:30 -0700

    Fix class declaration
    It seems that i broke it in 2c0bbd01841fc.

--- lib/form/arWidgetFormSelectRadio.class.php
+++ lib/form/arWidgetFormSelectRadio.class.php
@@ -17,7 +17,7 @@
  * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class arWidgetFormInputFileEditable extends sfWidgetFormInputFileEditable
+class arWidgetFormSelectRadio extends sfWidgetFormSelectRadio
 {
   protected function configure($options = array(), $attributes = array())
   {
