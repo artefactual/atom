adb9905400346ee9df85cc19e92ba1f23feed432
Jesús García Crespo <jesus@sevein.com>
2013-05-04 14:32:35 -0700

    Add wide layout

---
+++ apps/qubit/templates/layout_wide.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="<?php echo $sf_user->getCulture() ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction ?>">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="<?php echo public_path('favicon.ico') ?>"/>
+    <?php include_stylesheets() ?>
+    <?php include_component_slot('css') ?>
+    <?php if ($sf_context->getConfiguration()->isDebug()): ?>
+      <script type="text/javascript" charset="utf-8">
+        less = { env: 'development', optimize: 0, relativeUrls: true };
+      </script>
+    <?php endif; ?>
+    <?php include_javascripts() ?>
+  </head>
+  <body class="yui-skin-sam <?php echo $sf_context->getModuleName() ?> <?php echo $sf_context->getActionName() ?>">
+
+    <?php echo get_partial('header') ?>
+
+    <?php include_slot('pre') ?>
+
+    <?php if (!include_slot('content')): ?>
+      <div class="container-fluid">
+        <?php echo $sf_content ?>
+      </div>
+    <?php endif; ?>
+
+    <?php include_slot('post') ?>
+
+    <?php echo get_partial('footer') ?>
+
+  </body>
+</html>
