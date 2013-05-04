8109eecfabef3abaaef9a5f28253eab93527490d
Jesús García Crespo <jesus@sevein.com>
2013-05-04 16:58:26 -0700

    Use font-awesome in informationobject/_actionIcons

--- apps/qubit/modules/informationobject/templates/_actionIcons.php
+++ apps/qubit/modules/informationobject/templates/_actionIcons.php
@@ -3,39 +3,39 @@
 
     <li>
       <a href="<?php echo url_for(array($resource, 'module' => 'informationobject', 'action' => 'reports')) ?>">
-        <?php echo image_tag('report.png') ?>
+        <i class="icon-print"></i>
         <?php echo __('Reports') ?>
       </a>
     </li>
 
-    <li class="separator"><?php echo __('Import') ?></li>
+    <li class="separator"><h4><?php echo __('Import') ?></h4></li>
 
     <li>
       <a href="<?php echo url_for(array($resource, 'module' => 'object', 'action' => 'importSelect', 'type' => 'xml')) ?>">
-        <?php echo image_tag('report.png') ?>
+        <i class="icon-double-angle-left"></i>
         <?php echo __('XML') ?>
       </a>
     </li>
 
     <li>
       <a href="<?php echo url_for(array($resource, 'module' => 'object', 'action' => 'importSelect', 'type' => 'csv')) ?>">
-        <?php echo image_tag('report.png') ?>
+        <i class="icon-double-angle-left"></i>
         <?php echo __('CSV') ?>
       </a>
     </li>
 
-    <li class="separator"><?php echo __('Export') ?></li>
+    <li class="separator"><h4><?php echo __('Export') ?></h4></li>
 
     <li>
       <a href="<?php echo url_for(array($resource, 'module' => 'sfDcPlugin', 'sf_format' => 'xml')) ?>">
-        <?php echo image_tag('report.png') ?>
+        <i class="icon-double-angle-right"></i>
         <?php echo __('Dublin Core 1.1 XML') ?>
       </a>
     </li>
 
     <li>
       <a href="<?php echo url_for(array($resource, 'module' => 'sfEadPlugin', 'sf_format' => 'xml')) ?>">
-        <?php echo image_tag('report.png') ?>
+        <i class="icon-double-angle-right"></i>
         <?php echo __('EAD 2002 XML') ?>
       </a>
     </li>
@@ -43,7 +43,7 @@
     <?php if ('sfModsPlugin' == $sf_context->getModuleName()): ?>
       <li>
         <a href="<?php echo url_for(array($resource, 'module' => 'sfModsPlugin', 'sf_format' => 'xml')) ?>">
-          <?php echo image_tag('report.png') ?>
+          <i class="icon-print"></i>
           <?php echo __('MODS 3.3 XML') ?>
         </a>
       </li>
