d9eb7018207b4434c920582050bd82d3696f7675
Jesús García Crespo <jesus@sevein.com>
2013-05-09 18:03:43 -0700

    Use the expected method signature by get_component

--- plugins/sfDcPlugin/modules/sfDcPlugin/templates/indexSuccess.xml.php
+++ plugins/sfDcPlugin/modules/sfDcPlugin/templates/indexSuccess.xml.php
@@ -1,3 +1,3 @@
 <?php echo '<?xml version="1.0" encoding="'.sfConfig::get('sf_charset', 'UTF-8')."\" ?>\n" ?>
 
-<?php echo get_component('sfDcPlugin', 'dc', array('resource' => $resource)); ?>
+<?php echo get_component('sfDcPlugin', 'dc', array('resource' => $resource)) ?>
