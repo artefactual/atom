8fcd76ed8ccf2fa66521ba1b0b4c5323965345eb
Jesús García Crespo <jesus@sevein.com>
2013-05-12 12:16:33 -0700

    Add license

--- lib/QubitCsvTransform.class.php
+++ lib/QubitCsvTransform.class.php
@@ -1,7 +1,24 @@
-<?
+<?php
 
-class QubitCsvTransform extends QubitFlatfileImport {
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
 
+class QubitCsvTransform extends QubitFlatfileImport
+{
   public
     $setupLogic,
     $transformLogic,
@@ -25,7 +42,7 @@ class QubitCsvTransform extends QubitFlatfileImport {
       || $options['skipOptionsAndEnvironmentCheck'] == false
     )
     {
-      $this->checkTaskOptionsAndEnvironment($options['options']); 
+      $this->checkTaskOptionsAndEnvironment($options['options']);
     }
 
     // unset options not allowed in parent class
