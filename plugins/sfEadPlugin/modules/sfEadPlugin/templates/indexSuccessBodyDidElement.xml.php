785cbd5c6c01302f19852b2148718b0eef2d4b42
Jesús García Crespo <jesus@sevein.com>
2013-05-16 12:59:52 -0700

    Use strlen, fixes segfault

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyDidElement.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyDidElement.xml.php
@@ -4,12 +4,12 @@
     <?php $objects = $$resourceVar->getPhysicalObjects() ?>
     <?php $counter = 0 ?>
     <?php foreach ($objects as $object): ?>
-      <?php if($object->location): ?>
+      <?php if (0 < strlen($object->location)): ?>
         <?php $counter++ ?>
         <physloc id="<?php echo 'physloc'.str_pad($counter, 4, '0', STR_PAD_LEFT) ?>"><?php echo escape_dc(esc_specialchars($object->location)) ?></physloc>
       <?php endif; ?>
-      <container <?php echo $ead->getEadContainerAttributes($object) ?><?php if($object->location): ?> parent="<?php echo 'physloc'.str_pad($counter, 4, '0', STR_PAD_LEFT) ?>"<?php endif; ?>>
-        <?php if($object->name): ?>
+      <container <?php echo $ead->getEadContainerAttributes($object) ?><?php if (0 < strlen($object->location)): ?> parent="<?php echo 'physloc'.str_pad($counter, 4, '0', STR_PAD_LEFT) ?>"<?php endif; ?>>
+        <?php if (0 < strlen($object->name)): ?>
           <?php echo escape_dc(esc_specialchars($object->name)) ?>
         <?php endif; ?>
       </container>
