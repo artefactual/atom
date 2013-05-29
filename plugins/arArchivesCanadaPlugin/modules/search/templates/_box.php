3e7bfe00fbe3ae85197f8b7528c04ab4032a13d2
Jesús García Crespo <jesus@sevein.com>
2013-05-29 09:44:52 -0700

    Tweaks in ArchivesCanada header stylesheet

---
+++ plugins/arArchivesCanadaPlugin/modules/search/templates/_box.php
@@ -0,0 +1,57 @@
+<div id="search-form-wrapper">
+
+  <form action="<?php echo url_for(array('module' => 'search')) ?>" data-autocomplete="<?php echo url_for(array('module' => 'search', 'action' => 'autocomplete')) ?>" autocomplete="off">
+
+    <div class="input-append">
+
+      <input type="text" name="query"<?php if (isset($sf_request->query)) echo ' class="focused"' ?> value="<?php echo esc_entities($sf_request->query) ?>" placeholder="<?php echo __('Search') ?>"/>
+
+      <div class="btn-group">
+
+        <button id="btn-advanced-search" class="btn">
+          <span class="icon-cog"></span>
+        </button>
+
+        <button class="btn dropdown-toggle" data-toggle="dropdown">
+          <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu pull-right">
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-archival.png', array('width' => '24', 'height' => '24')).' '.__('Archival descriptions'), array('module' => 'informationobject', 'action' => 'browse')) ?></li>
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-institutions.png', array('width' => '24', 'height' => '24')).' '.__('Institutions'), array('module' => 'repository', 'action' => 'browse')) ?></li>
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-subjects.png', array('width' => '24', 'height' => '24')).' '.__('Subjects'), array('module' => 'taxonomy', 'action' => 'browse', 'id' => 35)) ?></li>
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-people.png', array('width' => '24', 'height' => '24')).' '.__('People &amp; Organizations'), array('module' => 'actor', 'action' => 'browse')) ?></li>
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-places.png', array('width' => '24', 'height' => '24')).' '.__('Places'), array('module' => 'taxonomy', 'action' => 'browse', 'id' => 42)) ?></li>
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-media.png', array('width' => '24', 'height' => '24')).' '.__('Media'), array('module' => 'digitalobject', 'action' => 'browse')) ?></li>
+          <li><?php echo link_to(image_tag('/images/icons-large/icon-new.png', array('width' => '24', 'height' => '24')).' '.__('Newest additions'), array('module' => 'search', 'action' => 'updates')) ?></li>
+        </ul>
+
+      </div>
+
+    </div>
+
+    <div id="search-realm" class="search-popover">
+
+      <?php $sf_route = $sf_request->getAttribute('sf_route') ?>
+      <?php if (isset($sf_route->resource)): ?>
+        <?php if ($sf_route->resource instanceof QubitRepository): ?>
+          <div>
+            <label>
+              <input name="realm" type="radio" value="<?php echo $sf_route->resource->id ?>"/>
+              <?php echo __('Search <strong>%1%</strong>', array('%1%' => render_title($sf_route->resource))) ?>
+            </label>
+          </div>
+        <?php endif; ?>
+      <?php endif; ?>
+
+      <div>
+        <label>
+          <input name="realm" type="radio" value="all" checked="checked">
+          <?php echo __('Global search') ?>
+        </label>
+      </div>
+
+    </div>
+
+  </form>
+
+</div>
--- plugins/arArchivesCanadaPlugin/templates/_header.php
+++ plugins/arArchivesCanadaPlugin/templates/_header.php
@@ -9,52 +9,40 @@
 
   <div class="container">
 
-    <div class="row" id="header-lvl1">
-      <div class="span12">
-        <a id="header-council" href="http://www.cdncouncilarchives.ca/"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.png', array('width' => '156', 'height' => '42')) ?></a>
-        <ul id="header-nav" class="nav nav-pills">
+    <div id="header-lvl1">
+      <div class="row">
+        <div class="span12">
+          <a id="header-council" href="http://www.cdncouncilarchives.ca/"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.png', array('width' => '156', 'height' => '42')) ?></a>
+          <ul id="header-nav" class="nav nav-pills">
 
-          <li><?php echo link_to(__('Home'), '@homepage') ?></li>
+            <li><?php echo link_to(__('Home'), '@homepage') ?></li>
 
-          <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
 
-          <?php foreach (array('en', 'fr') as $item): ?>
-            <?php if ($sf_user->getCulture() != $item): ?>
-              <li><?php echo link_to(format_language($item, $item), array('sf_culture' => $item) + $sf_request->getParameterHolder()->getAll()) ?></li>
-              <?php break; ?>
-            <?php endif; ?>
-          <?php endforeach; ?>
+            <?php foreach (array('en', 'fr') as $item): ?>
+              <?php if ($sf_user->getCulture() != $item): ?>
+                <li><?php echo link_to(format_language($item, $item), array('sf_culture' => $item) + $sf_request->getParameterHolder()->getAll()) ?></li>
+                <?php break; ?>
+              <?php endif; ?>
+            <?php endforeach; ?>
 
-        </ul>
+          </ul>
+        </div>
       </div>
     </div>
 
-    <div class="row" id="header-lvl2">
+    <div id="header-lvl2">
+      <div class="row">
 
-      <div id="logo-and-name" class="span6">
-        <h1><?php echo link_to(image_tag('/plugins/arArchivesCanadaPlugin/images/logo'), '@homepage', array('rel' => 'home', 'title' => __('Home'))) ?></h1>
-      </div>
+        <div id="logo-and-name" class="span6">
+          <h1><?php echo link_to(image_tag('/plugins/arArchivesCanadaPlugin/images/logo'), '@homepage', array('rel' => 'home')) ?></h1>
+        </div>
 
-      <div id="header-search" class="span6">
-
-        <?php echo get_component('search', 'box') ?>
-
-        <div id="browse-menu" class="btn-group dropdown">
-          <button class="btn dropdown-toggle" data-toggle="dropdown">
-            <span class="caret"></span>
-          </button>
-          <ul class="dropdown-menu">
-            <li><?php echo link_to(image_tag('/images/icons-large/icon-institutions.png', array('width' => '24', 'height' => '24')).' '.__('Institutions'), array('module' => 'repository', 'action' => 'browse')) ?></li>
-            <li><?php echo link_to(image_tag('/images/icons-large/icon-subjects.png', array('width' => '24', 'height' => '24')).' '.__('Subjects'), array('module' => 'taxonomy', 'action' => 'browse', 'id' => 35)) ?></li>
-            <li><?php echo link_to(image_tag('/images/icons-large/icon-people.png', array('width' => '24', 'height' => '24')).' '.__('People &amp; Organizations'), array('module' => 'actor', 'action' => 'browse')) ?></li>
-            <li><?php echo link_to(image_tag('/images/icons-large/icon-places.png', array('width' => '24', 'height' => '24')).' '.__('Places'), array('module' => 'taxonomy', 'action' => 'browse', 'id' => 42)) ?></li>
-            <li><?php echo link_to(image_tag('/images/icons-large/icon-media.png', array('width' => '24', 'height' => '24')).' '.__('Media'), array('module' => 'digitalobject', 'action' => 'browse')) ?></li>
-            <li><?php echo link_to(image_tag('/images/icons-large/icon-new.png', array('width' => '24', 'height' => '24')).' '.__('Newest additions'), array('module' => 'search', 'action' => 'updates')) ?></li>
-          </ul>
+        <div id="header-search" class="span6">
+          <?php echo get_component('search', 'box') ?>
         </div>
 
       </div>
-
     </div>
 
   </div>
