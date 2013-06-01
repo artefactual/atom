2d80ff67c1391f9adb5b815e37bbb7b67aa2ecbd
Jesús García Crespo <jesus@sevein.com>
2013-06-01 12:46:14 -0700

    Create datesFacet partial

--- apps/qubit/modules/informationobject/templates/browseSuccess.php
+++ apps/qubit/modules/informationobject/templates/browseSuccess.php
@@ -13,40 +13,7 @@
 <?php slot('sidebar') ?>
   <section id="facets">
 
-    <section class="facet facet-date">
-      <div class="facet-header">
-        <p><?php echo __('Dates') ?></p>
-      </div>
-      <div class="facet-body" id="dates">
-        <form name="dates" class="form" method="get" action="<?php echo url_for(array('module' => 'informationobject', 'action' => 'browse')) ?>">
-
-          <?php if (
-            (isset($sf_request->from) && false !== ctype_digit($sf_request->from))
-            || (isset($sf_request->to) && false !== ctype_digit($sf_request->to))): ?>
-            <a href="#" class="facet-dates-clear"><?php echo __('Clear') ?></a>
-          <?php endif; ?>
-
-          <ul>
-            <li><label><?php echo __('From') ?></label></li>
-            <?php if (isset($sf_request->from) && false !== ctype_digit($sf_request->from)): ?>
-              <li><input type="text" name="from" value="<?php echo $sf_request->from ?>" /></li>
-            <?php else: ?>
-              <li><input type="text" name="from" /></li>
-            <?php endif; ?>
-            <li><label><?php echo __('to') ?></label></li>
-            <?php if (isset($sf_request->to) && false !== ctype_digit($sf_request->to)): ?>
-              <li><input type="text" name="to" value="<?php echo $sf_request->to ?>" /></li>
-            <?php else: ?>
-              <li><input type="text" name="to" /></li>
-            <?php endif; ?>
-            <li>
-              <button type="submit" class="btn btn-small"><i class="icon-play-sign"></i></button>
-            </li>
-          </ul>
-
-        </form>
-      </div>
-    </section>
+    <?php echo get_partial('search/datesFacet') ?>
 
     <?php echo get_partial('search/facet', array(
       'target' => '#facet-repository',
