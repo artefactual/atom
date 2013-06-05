0f900efcf9d8ff0d255f68ecd7b8207d0ad1630d
Jesús García Crespo <jesus@sevein.com>
2013-06-05 16:47:49 -0700

    Use .list-menu in sidebar lists

--- apps/qubit/modules/actor/templates/_contextMenu.php
+++ apps/qubit/modules/actor/templates/_contextMenu.php
@@ -1,23 +1,27 @@
-<section>
+<section class="list-menu">
+
   <h4><?php echo __('Subject of') ?></h4>
-  <div class="content">
-    <ul>
-      <?php foreach ($subjectInfoObjects as $item): ?>
-        <li><?php echo link_to(render_title($item), array($item, 'module' => 'informationobject')) ?></li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+
+  <ul>
+    <?php foreach ($subjectInfoObjects as $item): ?>
+      <li><?php echo link_to(render_title($item), array($item, 'module' => 'informationobject')) ?></li>
+    <?php endforeach; ?>
+  </ul>
+
 </section>
 
 <?php foreach ($relatedInfoObjects as $role => $informationObjects): ?>
-  <section>
+
+  <section class="list-menu">
+
     <h4><?php echo __('%1% of', array('%1%' => $role)) ?></h4>
-    <div class="content">
-      <ul>
-        <?php foreach ($informationObjects as $informationObject): ?>
-          <li><?php echo link_to(render_title($informationObject), array($informationObject, 'module' => 'informationobject')) ?><?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $informationObject->getPublicationStatus()->status->id): ?> <span class="publicationStatus"><?php echo $informationObject->getPublicationStatus()->status ?></span><?php endif; ?></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
+
+    <ul>
+      <?php foreach ($informationObjects as $informationObject): ?>
+        <li><?php echo link_to(render_title($informationObject), array($informationObject, 'module' => 'informationobject')) ?><?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $informationObject->getPublicationStatus()->status->id): ?> <span class="publicationStatus"><?php echo $informationObject->getPublicationStatus()->status ?></span><?php endif; ?></li>
+      <?php endforeach; ?>
+    </ul>
+
   </section>
+
 <?php endforeach; ?>
