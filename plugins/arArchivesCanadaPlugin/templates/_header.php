bdc8b49dd71fc53c191f7fe22f29228ed521ecf3
Jesús García Crespo <jesus@sevein.com>
2013-06-10 13:51:40 -0700

    Some i18n fixes in ArchivesCanada

--- plugins/arArchivesCanadaPlugin/templates/_header.php
+++ plugins/arArchivesCanadaPlugin/templates/_header.php
@@ -12,12 +12,22 @@
     <div id="header-lvl1">
       <div class="row">
         <div class="span12">
-          <a id="header-council" href="http://www.cdncouncilarchives.ca/"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.png', array('width' => '156', 'height' => '42')) ?></a>
+
+          <?php if ('fr' == $sf_user->getCulture()): ?>
+            <a id="header-council" href="http://www.cdncouncilarchives.ca/"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.fr.png', array('width' => '156', 'height' => '42')) ?></a>
+          <?php else: ?>
+            <a id="header-council" href="http://www.cdncouncilarchives.ca/"><?php echo image_tag('/plugins/arArchivesCanadaPlugin/images/council.en.png', array('width' => '156', 'height' => '42')) ?></a>
+          <?php endif; ?>
+
           <ul id="header-nav" class="nav nav-pills">
 
             <li><?php echo link_to(__('Home'), '@homepage') ?></li>
 
-            <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php if ('fr' == $sf_user->getCulture()): ?>
+              <li><?php echo link_to(__('Contactez-nous'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php else: ?>
+              <li><?php echo link_to(__('Contact us'), array('module' => 'staticpage', 'slug' => 'contact')) ?></li>
+            <?php endif; ?>
 
             <?php foreach (array('en', 'fr') as $item): ?>
               <?php if ($sf_user->getCulture() != $item): ?>
