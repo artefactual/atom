8410dda5641303ce539d2c68db518e364955efcf
David Juhasz <djjuhasz@gmail.com>
2013-06-11 17:30:46 -0700

    Don't assume values for columns that are blank
    - levelOfDetail and descriptionStatus should not have a value if the source data does not.
    - A blank publication status was throwing a bogus warning.

--- lib/task/import/csvImportTask.class.php
+++ lib/task/import/csvImportTask.class.php
@@ -375,7 +375,7 @@ EOF;
         }
 
         // set level of detail
-        if (isset($self->rowStatusVars['levelOfDetail']))
+        if (isset($self->rowStatusVars['levelOfDetail']) && 0 < strlen($self->rowStatusVars['levelOfDetail']))
         {
           $levelOfDetailTermId = array_search(
             (trim($self->rowStatusVars['levelOfDetail'])) ? $self->rowStatusVars['levelOfDetail'] : 'Full',
@@ -395,7 +395,8 @@ EOF;
 
         foreach($languageProperties as $serializeProperty)
         {
-          if (isset($self->rowStatusVars[$serializeProperty]) && 0 < strlen($self->rowStatusVars[$serializeProperty]))
+          if (isset($self->rowStatusVars[$serializeProperty])
+            && 0 < strlen($self->rowStatusVars[$serializeProperty]))
           {
             $data = explode('|', $self->rowStatusVars[$serializeProperty]);
 
@@ -407,7 +408,8 @@ EOF;
         }
 
         // set description status
-        if (isset($self->rowStatusVars['descriptionStatus']))
+        if (isset($self->rowStatusVars['descriptionStatus'])
+          && 0 < strlen($self->rowStatusVars['descriptionStatus']))
         {
           $statusTermId = array_search(
             (trim($self->rowStatusVars['descriptionStatus'])) ? $self->rowStatusVars['descriptionStatus'] : 'Final',
@@ -418,7 +420,8 @@ EOF;
         }
 
         // set publication status
-        if (isset($self->rowStatusVars['publicationStatus']))
+        if (isset($self->rowStatusVars['publicationStatus'])
+          && 0 < strlen($self->rowStatusVars['publicationStatus']))
         {
           $pubStatusTermId = array_search(
             ucwords($self->rowStatusVars['publicationStatus']),
