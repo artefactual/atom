bec64405ef757fbd99c452a6f8c274a12bb42e5f
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-07-31 02:45:28 +0200

    Changes in settings global form, fixes #5179.
    Create a form widget to show the upload space available,
    move this field inside the global form, etc.

---
+++ lib/form/arWidgetFormUploadQuota.class.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class arWidgetFormUploadQuota extends sfWidgetFormInput
+{
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+  }
+
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    if (sfConfig::get('app_upload_limit') == 0)
+    {
+      return '<label>' . __('Digital object upload is disabled') . '</label>';
+    }
+
+    return '<label>' . __('%1% used of %2%', array('%1%' => hr_filesize(Qubit::getDirectorySize(sfConfig::get('sf_upload_dir'))),
+                '%2%' => sfConfig::get('app_upload_limit') < 0 ? '<i>Unlimited</i>' : sfConfig::get('app_upload_limit') . ' GB')) . '</label>';
+  }
+}
