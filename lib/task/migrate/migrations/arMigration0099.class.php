35267db2530568ca92bfe8a81a74bdf9f0511c0a
Jesús García Crespo <jesus@sevein.com>
2013-08-20 22:35:27 -0700

    Bring new migrations added in 2.x

---
+++ lib/task/migrate/migrations/arMigration0099.class.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Activate Dominion theme
+ *
+ * @package    AccesstoMemory
+ * @subpackage migration
+ */
+class arMigration0097
+{
+  const
+    VERSION = 97, // The new database version
+    MIN_MILESTONE = 2; // The minimum milestone required
+
+  /**
+   * Upgrade
+   *
+   * @return bool True if the upgrade succeeded, False otherwise
+   */
+  public function up($configuration)
+  {
+    foreach (array(
+      'taxonomies' => 'taxonomy/browse',
+      'browseDigitalObjects' => 'digitalobject/browse') as $key => $value)
+    {
+      if (null !== $menu = QubitMenu::getByName($key))
+      {
+        $menu->path = $value;
+
+        $menu->save();
+      }
+    }
+
+    return true;
+  }
+}
