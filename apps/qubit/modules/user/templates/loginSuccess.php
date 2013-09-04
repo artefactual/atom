ac14d04e959533b24e07fecbaa1daf392a538930
Jesús García Crespo <jesus@sevein.com>
2013-09-04 13:31:31 -0700

    Fix user/login layout

--- apps/qubit/modules/user/templates/loginSuccess.php
+++ apps/qubit/modules/user/templates/loginSuccess.php
@@ -1,36 +1,44 @@
+<?php decorate_with('layout_1col') ?>
 <?php use_helper('Javascript') ?>
 
-<div class="row">
+<?php slot('content') ?>
 
-  <div class="span4 offset4">
+  <div class="row">
 
-    <?php if ('user' != $sf_request->module || 'login' != $sf_request->action): ?>
-      <div class="messages status">
-        <?php echo __('Please log in to access that page') ?>
+    <div class="offset4 span4">
+
+      <div id="content">
+
+        <h1><?php echo __('Please log in to access that page') ?></h1>
+
+        <?php if ('user' != $sf_request->module || 'login' != $sf_request->action): ?>
+          <div class="messages status">
+            <?php echo __('Please log in to access that page') ?>
+          </div>
+        <?php endif; ?>
+
+        <?php if ($form->hasErrors()): ?>
+          <?php echo $form->renderGlobalErrors() ?>
+        <?php endif; ?>
+
+        <?php echo $form->renderFormTag(url_for(array('module' => 'user', 'action' => 'login'))) ?>
+
+          <?php echo $form->renderHiddenFields() ?>
+
+          <?php echo $form->email->renderRow(array('autofocus' => 'autofocus', 'class' => 'input-block-level')) ?>
+
+          <?php echo $form->password->renderRow(array('class' => 'input-block-level')) ?>
+
+          <section class="actions">
+            <button type="submit" class="btn btn-primary btn-block btn-large"><?php echo _('Sign in') ?></button>
+          </section>
+
+        </form>
+
       </div>
-    <?php endif; ?>
 
-    <legend><?php echo _('Sign in') ?></legend>
+    </div>
 
-    <?php if ($form->hasErrors()): ?>
-      <div class="alert alert-error">
-        <a class="close" data-dismiss="alert" href="#">×</a>
-        <?php echo $form->renderGlobalErrors() ?>
-      </div>
-    <?php endif; ?>
-
-    <?php echo $form->renderFormTag(url_for(array('module' => 'user', 'action' => 'login'))) ?>
-
-      <?php echo $form->renderHiddenFields() ?>
-
-      <?php echo $form->email->renderRow(array('autofocus' => 'autofocus', 'class' => 'input-block-level')) ?>
-
-      <?php echo $form->password->renderRow(array('class' => 'input-block-level')) ?>
-
-      <div class="form-actions">
-        <button type="submit" class="btn btn-primary btn-block btn-large"><?php echo _('Sign in') ?></button>
-      </div>
-
-    </form>
   </div>
-</div>
+
+<?php end_slot() ?>
