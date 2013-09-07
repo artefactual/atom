0214ddf12173ce3b10c9acbb220decfe43c09ca4
Jesús García Crespo <jesus@sevein.com>
2013-09-07 13:13:53 -0700

    Update htmlSnippet's toolitp, fixes #4638

--- apps/qubit/modules/repository/templates/editThemeSuccess.php
+++ apps/qubit/modules/repository/templates/editThemeSuccess.php
@@ -45,7 +45,7 @@
 
         <?php echo render_field($form->htmlSnippet
           ->label(__('Description'))
-          ->help(__('An abstract, table of contents or description of the resource\'s scope and contents.')), $resource, array('class' => 'resizable')) ?>
+          ->help(__('Content in this area will appear below an uploaded banner and above the institution\'s description areas. It can be used to offer a summary of the institution\'s mandate, include a tag line or important information, etc. HTML and inline CSS can be used to style the contents.')), $resource, array('class' => 'resizable')) ?>
 
       </fieldset>
 
