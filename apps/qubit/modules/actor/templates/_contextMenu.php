60f97b86e2417e210e9a25e3ca1b1359368ddac4
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-09-12 21:38:06 +0200

    Padding related resources links, fixes #5288

--- apps/qubit/modules/actor/templates/_contextMenu.php
+++ apps/qubit/modules/actor/templates/_contextMenu.php
@@ -1,27 +1,35 @@
-<section class="list-menu">
+<div id="list-menu-wrapper">
 
-  <h4><?php echo __('Subject of') ?></h4>
+  <?php if (isset($subjectInfoObjects)): ?>
 
-  <ul>
-    <?php foreach ($subjectInfoObjects as $item): ?>
-      <li><?php echo link_to(render_title($item), array($item, 'module' => 'informationobject')) ?></li>
-    <?php endforeach; ?>
-  </ul>
+    <section class="list-menu">
 
-</section>
+      <h4><?php echo __('Subject of') ?></h4>
 
-<?php foreach ($relatedInfoObjects as $role => $informationObjects): ?>
+      <ul>
+        <?php foreach ($subjectInfoObjects as $item): ?>
+          <li><?php echo link_to(render_title($item), array($item, 'module' => 'informationobject')) ?></li>
+        <?php endforeach; ?>
+      </ul>
 
-  <section class="list-menu">
+    </section>
 
-    <h4><?php echo __('%1% of', array('%1%' => $role)) ?></h4>
+  <?php endif; ?>
 
-    <ul>
-      <?php foreach ($informationObjects as $informationObject): ?>
-        <li><?php echo link_to(render_title($informationObject), array($informationObject, 'module' => 'informationobject')) ?><?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $informationObject->getPublicationStatus()->status->id): ?> <span class="publicationStatus"><?php echo $informationObject->getPublicationStatus()->status ?></span><?php endif; ?></li>
-      <?php endforeach; ?>
-    </ul>
+  <?php foreach ($relatedInfoObjects as $role => $informationObjects): ?>
 
-  </section>
+    <section class="list-menu">
 
-<?php endforeach; ?>
+      <h4><?php echo __('%1% of', array('%1%' => $role)) ?></h4>
+
+      <ul>
+        <?php foreach ($informationObjects as $informationObject): ?>
+          <li><?php echo link_to(render_title($informationObject), array($informationObject, 'module' => 'informationobject')) ?><?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $informationObject->getPublicationStatus()->status->id): ?> <span class="publicationStatus"><?php echo $informationObject->getPublicationStatus()->status ?></span><?php endif; ?></li>
+        <?php endforeach; ?>
+      </ul>
+
+    </section>
+
+  <?php endforeach; ?>
+
+</div>
