9feb9bf6302f20d20e57b29e4820ea8ade640412
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-09-18 02:52:02 +0200

    Change template for edit static page, fixes #5593

--- apps/qubit/modules/staticpage/templates/editSuccess.php
+++ apps/qubit/modules/staticpage/templates/editSuccess.php
@@ -1,39 +1,56 @@
-<h1><?php echo __('Edit page') ?></h1>
+<?php decorate_with('layout_1col.php') ?>
 
-<h1 class="label"><?php echo render_title($resource) ?></h1>
+<?php slot('title') ?>
+  <h1 class="multiline">
+    <?php echo __('Edit page') ?>
+    <span class="sub"><?php echo render_title($resource) ?></span>
+  </h1>
+<?php end_slot() ?>
 
-<?php echo $form->renderGlobalErrors() ?>
+<?php slot('content') ?>
 
-<?php if (isset($sf_request->getAttribute('sf_route')->resource)): ?>
-  <?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'staticpage', 'action' => 'edit'))) ?>
-<?php else: ?>
-  <?php echo $form->renderFormTag(url_for(array('module' => 'staticpage', 'action' => 'add'))) ?>
-<?php endif; ?>
+  <?php echo $form->renderGlobalErrors() ?>
+
+  <?php if (isset($sf_request->getAttribute('sf_route')->resource)): ?>
+    <?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'staticpage', 'action' => 'edit'))) ?>
+  <?php else: ?>
+    <?php echo $form->renderFormTag(url_for(array('module' => 'staticpage', 'action' => 'add'))) ?>
+  <?php endif; ?>
 
   <?php echo $form->renderHiddenFields() ?>
 
-  <?php echo render_field($form->title, $resource) ?>
+  <section id="content">
 
-  <?php if ($resource->isProtected()): ?>
-    <?php echo $form->slug->renderRow(array('class' => 'readOnly', 'disabled' => 'disabled')) ?>
-  <?php else: ?>
-    <?php echo $form->slug->renderRow() ?>
-  <?php endif; ?>
+    <fieldset class="collapsible" id="elementsArea">
 
-  <?php echo render_field($form->content, $resource, array('class' => 'resizable')) ?>
+      <legend><?php echo __('Elements area') ?></legend>
 
-  <section class="actions">
+      <?php echo render_field($form->title, $resource) ?>
 
-    <ul>
-      <?php if (isset($sf_request->getAttribute('sf_route')->resource)): ?>
-        <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'staticpage')) ?></li>
-        <li><input class="form-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
+      <?php if ($resource->isProtected()): ?>
+        <?php echo $form->slug->renderRow(array('class' => 'readOnly', 'disabled' => 'disabled')) ?>
       <?php else: ?>
-        <li><?php echo link_to(__('Cancel'), array('module' => 'staticpage', 'action' => 'list')) ?></li>
-        <li><input class="form-submit" type="submit" value="<?php echo __('Create') ?>"/></li>
+        <?php echo $form->slug->renderRow() ?>
       <?php endif; ?>
-    </ul>
+
+      <?php echo render_field($form->content, $resource, array('class' => 'resizable')) ?>
+
+    </fieldset>
 
   </section>
 
-</form>
+    <section class="actions">
+      <ul class="clearfix links">
+        <?php if (isset($sf_request->getAttribute('sf_route')->resource)): ?>
+          <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'staticpage'), array('title' => __('Cancel'), 'class' => 'c-btn')) ?></li>
+          <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
+        <?php else: ?>
+          <li><?php echo link_to(__('Cancel'), array('module' => 'staticpage', 'action' => 'list'), array('title' => __('Cancel'), 'class' => 'c-btn')) ?></li>
+          <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Create') ?>"/></li>
+        <?php endif; ?>
+      </ul>
+    </section>
+
+  </form>
+
+<?php end_slot() ?>
