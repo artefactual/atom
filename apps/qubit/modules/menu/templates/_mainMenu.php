5b002c11aa153cd39b1de10b6ea414ed019c06a0
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-09-27 20:27:11 +0200

    Change main menu order, fixes #5585

--- apps/qubit/modules/menu/templates/_mainMenu.php
+++ apps/qubit/modules/menu/templates/_mainMenu.php
@@ -1,29 +1,33 @@
-<?php foreach ($menus as $menu): ?>
+<?php foreach (array($adminMenu, $importMenu, $manageMenu, $addMenu) as $menu): ?>
 
-  <div id="<?php echo $menu->getName() ?>-menu" data-toggle="tooltip" data-title="<?php echo $menu->getLabel(array('cultureFallback' => true)) ?>">
+  <?php if (($menu->getName() == 'add' || $menu->getName() == 'manage') || $sf_user->isAdministrator()): ?>
 
-    <a class="top-item" data-toggle="dropdown" data-target="#"><?php echo $menu->getLabel(array('cultureFallback' => true)) ?></a>
+    <div id="<?php echo $menu->getName() ?>-menu" data-toggle="tooltip" data-title="<?php echo $menu->getLabel(array('cultureFallback' => true)) ?>">
 
-    <div class="top-dropdown-container">
+      <a class="top-item" data-toggle="dropdown" data-target="#"><?php echo $menu->getLabel(array('cultureFallback' => true)) ?></a>
 
-      <div class="top-dropdown-arrow">
-        <div class="arrow"></div>
+      <div class="top-dropdown-container">
+
+        <div class="top-dropdown-arrow">
+          <div class="arrow"></div>
+        </div>
+
+        <div class="top-dropdown-header">
+          <?php echo $menu->getLabel(array('cultureFallback' => true)) ?>
+        </div>
+
+        <div class="top-dropdown-body">
+          <ul>
+            <?php echo QubitMenu::displayHierarchyAsList($menu, 0, array('overrideVisibility' => array('admin' => $sf_user->isAdministrator()))) ?>
+          </ul>
+        </div>
+
+        <div class="top-dropdown-bottom"></div>
+
       </div>
-
-      <div class="top-dropdown-header">
-        <?php echo $menu->getLabel(array('cultureFallback' => true)) ?>
-      </div>
-
-      <div class="top-dropdown-body">
-        <ul>
-          <?php echo QubitMenu::displayHierarchyAsList($menu, 0, array('overrideVisibility' => array('admin' => $sf_user->isAdministrator()))) ?>
-        </ul>
-      </div>
-
-      <div class="top-dropdown-bottom"></div>
 
     </div>
 
-  </div>
+  <?php endif; ?>
 
 <?php endforeach; ?>
