8c48f5424f3dc6ab00f09fd9d40e75f6a0014a53
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-09-28 03:36:07 +0200

    Add existing parameters to suggested searches
    Fixes #5618

--- apps/qubit/modules/search/templates/noResultsSuccess.php
+++ apps/qubit/modules/search/templates/noResultsSuccess.php
@@ -7,10 +7,11 @@
     <?php echo __('No results found.') ?>
 
     <?php if (isset($suggestion)): ?>
+      <?php $sf_params->set('query', esc_entities($suggestion['text'])) ?>
       <span class="suggestion">
         <?php echo __('Did you mean %1%?', array(
           '%1%' => link_to($suggestion['text'],
-            array('module' => 'search', 'query' => esc_entities($suggestion['text']))))) ?>
+            $sf_params->getAll()))) ?>
       </span>
     <?php endif; ?>
 
