656edbbe9706feebdbaa0fe5735983bd2c90f8b0
Jesús García Crespo <jesus@sevein.com>
2013-10-01 23:13:37 -0700

    Cosmetic changes

--- apps/qubit/modules/staticpage/templates/editSuccess.php
+++ apps/qubit/modules/staticpage/templates/editSuccess.php
@@ -17,30 +17,30 @@
     <?php echo $form->renderFormTag(url_for(array('module' => 'staticpage', 'action' => 'add'))) ?>
   <?php endif; ?>
 
-  <?php echo $form->renderHiddenFields() ?>
+    <?php echo $form->renderHiddenFields() ?>
 
-  <section id="content">
+    <section id="content">
 
-    <fieldset class="collapsible" id="elementsArea">
+      <fieldset class="collapsible" id="elementsArea">
 
-      <legend><?php echo __('Elements area') ?></legend>
+        <legend><?php echo __('Elements area') ?></legend>
 
-      <?php echo render_field($form->title, $resource) ?>
+        <?php echo render_field($form->title, $resource) ?>
 
-      <?php if ($resource->isProtected()): ?>
-        <?php echo $form->slug->renderRow(array('class' => 'readOnly', 'disabled' => 'disabled')) ?>
-      <?php else: ?>
-        <?php echo $form->slug->renderRow() ?>
-      <?php endif; ?>
+        <?php if ($resource->isProtected()): ?>
+          <?php echo $form->slug->renderRow(array('class' => 'readOnly', 'disabled' => 'disabled')) ?>
+        <?php else: ?>
+          <?php echo $form->slug->renderRow() ?>
+        <?php endif; ?>
 
-      <?php echo render_field($form->content, $resource, array('class' => 'resizable')) ?>
+        <?php echo render_field($form->content, $resource, array('class' => 'resizable')) ?>
 
-    </fieldset>
+      </fieldset>
 
-  </section>
+    </section>
 
     <section class="actions">
-      <ul class="clearfix links">
+      <ul>
         <?php if (isset($sf_request->getAttribute('sf_route')->resource)): ?>
           <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'staticpage'), array('title' => __('Cancel'), 'class' => 'c-btn')) ?></li>
           <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
--- apps/qubit/modules/staticpage/templates/listSuccess.php
+++ apps/qubit/modules/staticpage/templates/listSuccess.php
@@ -1,6 +1,6 @@
 <h1><?php echo __('List pages') ?></h1>
 
-<table class="table sticky-enabled table-bordered">
+<table class="sticky-enabled table table-bordered">
   <thead>
     <tr>
       <th>
@@ -25,9 +25,7 @@
 <?php echo get_partial('default/pager', array('pager' => $pager)) ?>
 
 <section class="actions">
-
- <ul>
+  <ul>
     <li><?php echo link_to(__('Add new'), array('module' => 'staticpage', 'action' => 'add'), array('class' => 'c-btn c-btn-submit')) ?></li>
- </ul>
-
+  </ul>
 </section>
