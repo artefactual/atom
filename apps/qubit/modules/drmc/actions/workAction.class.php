fd2ba771e6b7694e3d52a979bc287605c616d337
Jesús García Crespo <jesus@sevein.com>
2013-11-18 13:40:17 -0800

    Load d3 from cdnjs
    Also, d3 is not an Angular service, it's a 3rd party lib. So unless you want
    to load it asynchronously using promises, it doesn't make sense to have it
    under services.

--- apps/qubit/modules/drmc/actions/workAction.class.php
+++ apps/qubit/modules/drmc/actions/workAction.class.php
@@ -23,8 +23,8 @@ class DRMCWorkAction extends sfAction
   {
     $this->response->addJavaScript('//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js', 'last');
     $this->response->addJavaScript('//cdnjs.cloudflare.com/ajax/libs/angular.js/1.1.5/angular.js', 'last');
+    $this->response->addJavaScript('//cdnjs.cloudflare.com/ajax/libs/d3/3.3.9/d3.min.js', 'last');
     $this->response->addJavaScript('/apps/qubit/modules/drmc/frontend/app/scripts/lib/plumb.js', 'last');
-    $this->response->addJavaScript('/apps/qubit/modules/drmc/frontend/app/scripts/services/d3.v3.js', 'last');
     $this->response->addJavaScript('/apps/qubit/modules/drmc/frontend/app/scripts/app.js', 'last');
     $this->response->addJavaScript('/apps/qubit/modules/drmc/frontend/app/scripts/directives/plumb-graph.js', 'last');
     $this->response->addJavaScript('/apps/qubit/modules/drmc/frontend/app/scripts/services/jsPlumb.js', 'last');
