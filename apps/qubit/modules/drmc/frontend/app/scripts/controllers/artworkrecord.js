8a37434fa49f84a587e6228e73d080f5f9ddc622
Heather Anderson <heatherandersoncreative@gmail.com>
2013-11-19 15:49:20 -0800

    Positioning of fancy new nodes!

--- apps/qubit/modules/drmc/frontend/app/scripts/controllers/artworkrecord.js
+++ apps/qubit/modules/drmc/frontend/app/scripts/controllers/artworkrecord.js
@@ -6,28 +6,24 @@ angular.module('momaApp')
     $scope.atomGlobals = atomGlobals;
 
     $scope.collection = [
-      { id: 0, title: 'Screen Test: Ivy Nicholson', level: 'Work', children: [
-        { id: 10, title: 'Retouched Film', level: 'Expression', children:
-          [
-            { id: 20, title: 'Exhibition Documentation', level: 'Manifestation', children:
+      { id: 0, title: 'Play Dead; Real Time', level: 'Work', children: [
+            { id: 20, title: 'Components', level: 'Expression', children:
               [
-                { id: 30, title: 'Exhibition<br />Walkthrough', level: 'Component' }
+                { id: 31, title: 'DVD', level: 'Component' },
+                { id: 32, title: 'DVD', level: 'Component' },
+                { id: 33, title: 'DVD', level: 'Component' },
+                { id: 34, title: 'Digital Betacam', level: 'Component' },
+                { id: 35, title: 'Digital Betacam', level: 'Component' },
+                { id: 36, title: 'Digital Betacam', level: 'Component' },
+                { id: 37, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
+                { id: 38, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
+                { id: 39, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
+                { id: 40, title: '.mov H264', level: 'Component' },
+                { id: 41, title: '.mov H264', level: 'Component' },
+                { id: 42, title: '.mov H264', level: 'Component' }
               ]
-            }
-          ]
         },
-        { id: 11, title: 'Original Film', level: 'Expression', children:
-          [
-            { id: 21, title: 'Artwork<br />Components', level: 'Manifestation', children:
-              [
-                 { id: 31, title: 'Reversal positive "original"', level: 'Component' },
-                 { id: 32, title: 'Internegative', level: 'Component' },
-                 { id: 33, title: 'Print', level: 'Component' },
-                 { id: 34, title: 'Print', level: 'Component' },
-                 { id: 35, title: 'DPX  Scan', level: 'Component' }
-              ]
-            }
-          ]}
+        { id: 11, title: 'Installation Documentation', level: 'Expression' }
       ]}
     ];
 
