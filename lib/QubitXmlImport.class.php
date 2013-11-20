ae6215bd66fa6e0d4b08564a9d9fde33dc4131a8
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-11-20 04:20:56 +0100

    Improve EAD bioghist import, fixes #5870
    If there isn't a chronlist node inside the bioghist add it as
    history for the existing creator (by order) or create new ones

--- lib/QubitXmlImport.class.php
+++ lib/QubitXmlImport.class.php
@@ -438,7 +438,7 @@ class QubitXmlImport
             break;
 
           default:
-            foreach ($nodeList2 as $domNode2)
+            foreach ($nodeList2 as $key => $domNode2)
             {
               // normalize the node text; NB: this will strip any child elements, eg. HTML tags
               $nodeValue = self::normalizeNodeValue($domNode2);
