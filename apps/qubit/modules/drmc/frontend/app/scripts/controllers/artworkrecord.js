993698fc903b4a5be7a2f59c0ccb68c94f08b7aa
Jesús García Crespo <jesus@sevein.com>
2013-11-21 22:42:01 -0800

    Cosmetic changes

--- apps/qubit/modules/drmc/frontend/app/scripts/controllers/artworkrecord.js
+++ apps/qubit/modules/drmc/frontend/app/scripts/controllers/artworkrecord.js
@@ -7,24 +7,20 @@ angular.module('momaApp')
 
     $scope.collection = [
       { id: 0, title: 'Play Dead; Real Time', level: 'Work', children: [
-            { id: 20, title: 'Components', level: 'Expression', children:
-              [
-                { id: 31, title: 'DVD', level: 'DigitalObject' },
-                { id: 32, title: 'DVD', level: 'DigitalObject' },
-                { id: 33, title: 'DVD', level: 'DigitalObject' },
-                { id: 34, title: 'Digital Betacam', level: 'DigitalObject' },
-                { id: 35, title: 'Digital Betacam', level: 'DigitalObject' },
-                { id: 36, title: 'Digital Betacam', level: 'DigitalObject' },
-                { id: 37, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
-                { id: 38, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
-                { id: 39, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
-                { id: 40, title: '.mov H264', level: 'Component' },
-                { id: 41, title: '.mov H264', level: 'Component' },
-                { id: 42, title: '.mov H264', level: 'Component' }
-              ]
-        },
-        { id: 11, title: 'Installation Documentation', level: 'Expression' }
-      ]}
+        { id: 20, title: 'Components', level: 'Expression', children: [
+          { id: 31, title: 'DVD', level: 'DigitalObject' },
+          { id: 32, title: 'DVD', level: 'DigitalObject' },
+          { id: 33, title: 'DVD', level: 'DigitalObject' },
+          { id: 34, title: 'Digital Betacam', level: 'DigitalObject' },
+          { id: 35, title: 'Digital Betacam', level: 'DigitalObject' },
+          { id: 36, title: 'Digital Betacam', level: 'DigitalObject' },
+          { id: 37, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
+          { id: 38, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
+          { id: 39, title: '.mov Uncompressed 10bit PAL', level: 'Component' },
+          { id: 40, title: '.mov H264', level: 'Component' },
+          { id: 41, title: '.mov H264', level: 'Component' },
+          { id: 42, title: '.mov H264', level: 'Component' } ]},
+        { id: 11, title: 'Installation Documentation', level: 'Expression' }]}
     ];
 
     $scope.relations = [
