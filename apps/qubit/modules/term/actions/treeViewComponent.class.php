d029f854933da8eb11ef43d9b3302cfae30e88a1
José Raddaoui Marín <raddaouimarin@gmail.com>
2013-12-30 12:53:07 +0100

    Fix treeview in taxonomy index page, refs #5434
    Don't show treeview if the taxonomy doesn't have terms

--- apps/qubit/modules/term/actions/treeViewComponent.class.php
+++ apps/qubit/modules/term/actions/treeViewComponent.class.php
@@ -43,39 +43,42 @@ class TermTreeViewComponent extends sfComponent
       $this->getChildrensAndShowActive = false;
     }
 
-    $this->ancestors = $this->resource->getAncestors()->orderBy('lft');
-
-    $this->browser = isset($this->browser) && true === $this->browser;
-
-    $this->hasPrevSiblings = false;
-    $this->hasNextSiblings = false;
-
-    // Child descriptions
-    if ($this->getChildrensAndShowActive && $this->resource->hasChildren())
+    if (isset($this->resource))
     {
-      list($this->children, $this->hasNextSiblings) = $this->resource->getTreeViewChildren(array('numberOfPreviousOrNextSiblings' => $numberOfPreviousOrNextSiblings));
-    }
-    // Show siblings if there's no children
-    else
-    {
-      // Previous siblings
-      // Get an extra sibling just to know if the + button is necessary
-      $this->prevSiblings = $this->resource->getTreeViewSiblings(array('limit' => $numberOfPreviousOrNextSiblings + 1, 'position' => 'previous'));
-      $this->hasPrevSiblings = count($this->prevSiblings) > $numberOfPreviousOrNextSiblings;
-      if ($this->hasPrevSiblings)
+      $this->ancestors = $this->resource->getAncestors()->orderBy('lft');
+
+      $this->browser = isset($this->browser) && true === $this->browser;
+
+      $this->hasPrevSiblings = false;
+      $this->hasNextSiblings = false;
+
+      // Child descriptions
+      if ($this->getChildrensAndShowActive && $this->resource->hasChildren())
       {
-        array_pop($this->prevSiblings);
+        list($this->children, $this->hasNextSiblings) = $this->resource->getTreeViewChildren(array('numberOfPreviousOrNextSiblings' => $numberOfPreviousOrNextSiblings));
       }
-
-      // Reverse array
-      $this->prevSiblings = array_reverse($this->prevSiblings);
-
-      // Next siblings, same logic than above with the + button
-      $this->nextSiblings = $this->resource->getTreeViewSiblings(array('limit' => $numberOfPreviousOrNextSiblings + 1, 'position' => 'next'));
-      $this->hasNextSiblings = count($this->nextSiblings) > $numberOfPreviousOrNextSiblings;
-      if ($this->hasNextSiblings)
+      // Show siblings if there's no children
+      else
       {
-        array_pop($this->nextSiblings);
+        // Previous siblings
+        // Get an extra sibling just to know if the + button is necessary
+        $this->prevSiblings = $this->resource->getTreeViewSiblings(array('limit' => $numberOfPreviousOrNextSiblings + 1, 'position' => 'previous'));
+        $this->hasPrevSiblings = count($this->prevSiblings) > $numberOfPreviousOrNextSiblings;
+        if ($this->hasPrevSiblings)
+        {
+          array_pop($this->prevSiblings);
+        }
+
+        // Reverse array
+        $this->prevSiblings = array_reverse($this->prevSiblings);
+
+        // Next siblings, same logic than above with the + button
+        $this->nextSiblings = $this->resource->getTreeViewSiblings(array('limit' => $numberOfPreviousOrNextSiblings + 1, 'position' => 'next'));
+        $this->hasNextSiblings = count($this->nextSiblings) > $numberOfPreviousOrNextSiblings;
+        if ($this->hasNextSiblings)
+        {
+          array_pop($this->nextSiblings);
+        }
       }
     }
   }
