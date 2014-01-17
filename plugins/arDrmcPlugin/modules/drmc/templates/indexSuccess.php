3ea9b7ad076b5bbb7f5fac37ecfd9208c6ff509f
Jesús García Crespo <jesus@sevein.com>
2014-01-17 10:36:04 -0800

    Remove unused menu

--- plugins/arDrmcPlugin/modules/drmc/templates/indexSuccess.php
+++ plugins/arDrmcPlugin/modules/drmc/templates/indexSuccess.php
@@ -1,25 +1,6 @@
 <div ng-app="momaApp">
 
   <!-- Menu -->
-  <!--
-  <ul class="nav nav-pills">
-    <li><a ng-class="{ active: $route.current.activetab == 'dashboard' }" ng-href="#/dashboard">Dashboard</a></li>
-    <li class="dropdown"><a data-toggle="dropdown" class="dropdown-toggle" ng-class="{ active: $route.current.activetab == 'artwork-record' }" tabindex="-1">Artwork Record<b class="caret"></b></a>
-        <ul class="dropdown-menu" role="menu" aria-labelledby="">
-            <li>
-                <a ng-class="{ active: $route.current.activetab == 'artwork-record' }" ng-href="#/artwork-record" class="" role="menuitem" tabindex="-1">Artwork Record 1</a>
-            </li>
-            <li>
-                <a ng-class="{ active: $route.current.activetab == 'artwork-record2' }" ng-href="#/artwork-record2" class="" role="menuitem" tabindex="-1">Artwork Record 2</a>
-            </li>
-        </ul>
-    </li>
-    <li><a ng-class="{ active: $route.current.activetab == 'technology-record' }" ng-href="#/technology-record">Technology Record</a></li>
-    <li><a ng-href="#/rest-tests">REST tests</a></li>
-  </ul>
-  -->
-
-  <!-- Menu -->
   <ul class="nav nav-pills">
 
     <li ng-class="{ active: $state.includes('dashboard') }">
