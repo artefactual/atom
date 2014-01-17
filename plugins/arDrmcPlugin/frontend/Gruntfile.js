b6c8985013fc66eac82b3491d515d026763e8bda
Jesús García Crespo <jesus@sevein.com>
2014-01-17 07:40:51 -0800

    Add Gruntfile.js and package.json to DRMC-MA

---
+++ plugins/arDrmcPlugin/frontend/Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function (grunt) {
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task
+  // grunt.registerTask('default', ['jshint']);
+
+  // Print a timestamp (useful for when watching)
+  grunt.registerTask('timestamp', function() {
+    grunt.log.subhead(Date());
+  });
+
+  grunt.initConfig({
+    distdir: 'dist',
+    pkg: grunt.file.readJSON('package.json'),
+    src: {
+      js: ['app/scripts/app.js', 'app/scripts/services/*.js', 'app/scripts/lib/*.js', 'app/scripts/controllers/*.js', 'app/scripts/directives/*.js']
+    },
+    watch: {
+      scripts: {
+        files: ['<%= src.js %>'],
+        tasks: ['timestamp']
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', '<%= src.js %>'],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        boss: true,
+        eqnull: true,
+        globals: {}
+      }
+    }
+  });
+
+};
