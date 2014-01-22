f46ad72824e120e4b3a9cd3efe32644e01e6be70
Jesús García Crespo <jesus@sevein.com>
2014-01-21 22:15:06 -0800

    Js-lint in moma-toggle.js

--- plugins/arDrmcPlugin/frontend/app/scripts/directives/moma-toggle.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/directives/moma-toggle.js
@@ -3,42 +3,34 @@
 angular.module('momaApp.directives')
   .directive('momaToggle', function() {
 
-      //This directive includes a hide/show with animation
-      //To use, put ng-click="toggle()" on the link/button,etc
-      //Use attribute/element name with moma-toggle="isVisible"
+      // This directive includes a hide/show with animation.
+      // To use, put ng-click="toggle()" on the link/button, etc
+      // Use attribute/element name with moma-toggle="isVisible"
 
       return {
         restrict: 'AE',
         $scope: true,
         link: function($scope, element, attributes) {
 
-        //watched expression
-        var expression = attributes.momaToggle;
+          var expression = attributes.momaToggle;
+          if (!$scope.$eval(expression)) {
+            element.hide();
+          }
 
-        if (! $scope.$eval( expression ) ) {
-          element.hide();
-        };
+          $scope.$watch(expression, function(newVal, oldVal) {
 
-        $scope.$watch(expression, function( newVal, oldVal ) {
-
-            console.log("WATCHED exp", newVal, oldVal);
-
-           if (newVal === oldVal) {
-                return;
-                }
-
-
-            //show hidden element
-            if( newVal ) {
-              element
-                .stop( true, true )
-                .slideDown ();
-            } else {
-              element
-                .stop( true, true )
-                .slideUp();
+            if (newVal === oldVal) {
+              return;
             }
-          }); //watch
-      }
-  }
-});
+
+            // Show hidden element
+            if (newVal) {
+              element.stop(true, true).slideDown();
+            } else {
+              element.stop(true, true).slideUp();
+            }
+
+          });
+        }
+      };
+    });
