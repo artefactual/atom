a504723117466e91c8663fde4aba2233b195c613
Heather Anderson <handerson@artefactual.com>
2014-01-27 17:29:54 +0000

    Stop using double slash

--- plugins/arDrmcPlugin/frontend/app/scripts/services/AIPService.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/services/AIPService.js
@@ -5,7 +5,7 @@ module.exports = function ($http, ATOM_CONFIG) {
   this.getAIPs = function () {
     return $http({
       method: 'GET',
-      url: ATOM_CONFIG.frontendPath + 'api/dashboard'
+      url: ATOM_CONFIG.frontendPath + 'api/aips'
     });
   };
 
