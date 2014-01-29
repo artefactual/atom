eb473bc3e93e419d4126ba17d82b6c62c50a2f2d
Jesús García Crespo <jesus@sevein.com>
2014-01-28 16:32:50 -0800

    Remove unused code

--- plugins/arDrmcPlugin/frontend/app/scripts/lib/cbd/utils.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/lib/cbd/utils.js
@@ -2,22 +2,6 @@
 
   'use strict';
 
-  var Node = require('./node');
-  var Graph = require('./graph');
-
-  module.exports.createGraph = function (data) {
-    console.log('createGraph, data received:', data);
-    var nodes = {};
-    var graph = new Graph();
-    for (var i = 0; i < 10; i++) {
-      nodes[i] = new Node(i);
-    }
-    for (var id in nodes) {
-      graph.addNode(nodes[id]);
-    }
-    return graph;
-  };
-
   module.exports.values = function (object) {
     return Object.keys(object).map(function (key) {
       return object[key];
