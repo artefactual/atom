b0b03d68fc6acc9fa4005bb718f1a309a77ed862
Mike Cantelon <mcantelon@gmail.com>
2014-02-09 07:05:33 -0800

    Added TMS objects to mock API and sample data.

--- plugins/arDrmcPlugin/frontend/mock_api/resources/tms/get.js
+++ plugins/arDrmcPlugin/frontend/mock_api/resources/tms/get.js
@@ -1,14 +1,21 @@
 var urlParts = url.split('/'),
     mainRoutes = {},
-    resultData;
+    criteria = {};
 
 // remove empty first element
 urlParts.shift();
 
+// HTTP GET /tms/GetTombstoneData
 mainRoutes.GetTombstoneData = function() {
-  var resultData = {};
-  resultData.message = 'Called GetTombstoneData.';
-  return resultData;
+  var results = [];
+
+  // fetch TMS objects matching criteria
+  dpd.tmsraw.get(criteria, function(tmsObjects) {
+    tmsObjects.forEach(function(tmsObject) {
+      results.push(tmsObject);
+    });
+    setResult(results);
+  });
 };
 
 mainRoutes.GetTombstoneDataRest = function() {};
@@ -23,6 +30,4 @@ if (typeof mainRoutes[urlParts[0]] != 'undefined') {
   resultData = mainRoutes[urlParts[0]]();
 } else {
   resultData = {'message': 'Bad URL.'};
-} 
-
-setResult(resultData);
+}
