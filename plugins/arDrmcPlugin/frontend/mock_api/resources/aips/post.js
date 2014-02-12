1a56471249dcca5af21cd3e4576b0614b58d4e57
Mike Cantelon <mcantelon@gmail.com>
2014-02-12 14:33:43 -0800

    Added better AIP update error reporting.

--- plugins/arDrmcPlugin/frontend/mock_api/resources/aips/post.js
+++ plugins/arDrmcPlugin/frontend/mock_api/resources/aips/post.js
@@ -1,11 +1,23 @@
 var postData = context().req.body;
 
-dpd.aipsraw.put(postData, function(result, error) {
-  var message;
-  if (error) {
-    message = 'Error updating.';
-  } else {
-    message = 'Update successful.';
-  }
-  setResult({'message': message});
-});
+// translate updates to "classification" to match schema
+postData['class'] = postData.classification;
+delete postData.classification;
+
+if (typeof postData.id != 'undefined') {
+  dpd.aipsraw.put(postData, function(result, error) {
+    var message,
+        responseData = {};
+
+    if (error) {
+      responseData.message = JSON.parse(error).message;
+      responseData.error = true;
+    } else {
+      responseData.message = 'Update successful.';
+    }
+
+    setResult(responseData);
+  });
+} else {
+  setResult({'message': 'You must specify an ID when updating.', 'error': true});
+}
