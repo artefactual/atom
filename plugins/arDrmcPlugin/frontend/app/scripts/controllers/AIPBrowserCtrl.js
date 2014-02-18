402fcc03a9be0421ce82c382e59f924b80d4a3b8
Heather Anderson <handerson@artefactual.com>
2014-02-18 01:03:49 +0000

    Gen. options from id/optimize AIPS page/files for AIP view

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/AIPBrowserCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/AIPBrowserCtrl.js
@@ -9,19 +9,22 @@ module.exports = function ($scope, $modal, $q, ATOM_CONFIG, AIPService) {
   $scope.criteria = {};
 
   // watch for criteria changes
-  $scope.$watch ('criteria', function () {
-    console.log('Updating AIP data...');
+  $scope.$watch('criteria', function () {
+    $scope.pull();
+  }, true); // check properties when watching
+
+  $scope.pull = function () {
     AIPService.getAIPs($scope.criteria)
       .success(function (data) {
         $scope.data = data;
       });
-  }, true); // check properties when watching
+  };
 
   $scope.classifications = [
-    { id: 1, name: 'Artwork Component' },
-    { id: 2, name: 'Artwork Material' },
-    { id: 3, name: 'Supporting Documentation' },
-    { id: 4, name: 'Supporting Technology' }
+    { id: 10, name: 'Artwork Component' },
+    { id: 20, name: 'Artwork Material' },
+    { id: 30, name: 'Supporting Documentation' },
+    { id: 40, name: 'Supporting Technology' }
   ];
 
   // Support overview toggling
@@ -46,7 +49,23 @@ module.exports = function ($scope, $modal, $q, ATOM_CONFIG, AIPService) {
     var modalInstance = $modal.open({
       templateUrl: ATOM_CONFIG.viewsPath + '/partials/reclassify-aips.html',
       backdrop: true,
-      controller: modalInstanceCtrl,
+      controller: function ($scope, $modalInstance) {
+
+        $scope.reclassify = function () {
+          AIPService.reclassifyAIP($scope.aip.id, $scope.aip.class)
+            .success(function (data, status) {
+              $scope.pull();
+              $modalInstance.close($scope.aip.class);
+            }).error(function (data, status) {
+              $modalInstance.dismiss('Your new classification could not be assigned');
+            });
+        };
+
+        $scope.cancel = function () {
+          $modalInstance.dismiss('cancel');
+        };
+
+      },
       scope: $scope,
       resolve: {
         classifications: function () {
@@ -58,30 +77,5 @@ module.exports = function ($scope, $modal, $q, ATOM_CONFIG, AIPService) {
     modalInstance.result.then(function (result) {
       aip.class = result;
     });
-  };
-
-
-  var modalInstanceCtrl = function ($scope, $q, $modalInstance) {
-
-    $scope.reclassifyAIP = function (modalScope, classificationData) {
-
-      $q.when(modalScope.modalInstance).then(function () {
-
-        AIPService.reclassifyAIP ($scope.aip.id, classificationData.name)
-          .success(function  (data, status, aip) {
-            AIPService.getAIPs($scope.criteria);
-            aip.class = classificationData.name;
-            $modalInstance.close(classificationData.name);
-
-          }).error(function (data, status) {
-              console.error(data, status);
-              $modalInstance.dismiss('Your new classification could not be assigned');
-            });
-      });
-
-      $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-      };
-    };
   };
 };
--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/AIPViewCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/AIPViewCtrl.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function ($scope, $stateParams) {
+module.exports = function ($scope, ATOM_CONFIG, $stateParams, AIPService) {
+
+  // console.dir($scope);
   $scope.id = $stateParams.id;
+
+  // This passes parameter for specific
+  //  aip id via its uuid
+
+  AIPService.getAIPs({ uuid: $scope.id })
+    .success(function (data) {
+      $scope.data = data;
+      console.log(data);
+      $scope.aip = data.aips.results[0];
+      console.log($scope.aip);
+    });
+
 };
