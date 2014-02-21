51426000bd4bcdd2b7867e8b81f3b7fc9f489486
andy koch <koch.andy@gmail.com>
2014-02-21 08:45:25 -0800

    Fixing double repository logo on some pages.

--- apps/qubit/modules/repository/templates/_contextMenu.php
+++ apps/qubit/modules/repository/templates/_contextMenu.php
@@ -1,5 +1,3 @@
-<?php echo get_component('repository', 'logo') ?>
-
 <?php if (QubitAcl::check($resource, 'update')): ?>
   <?php include_component('repository', 'uploadLimit', array('resource' => $resource)) ?>
 <?php endif; ?>
