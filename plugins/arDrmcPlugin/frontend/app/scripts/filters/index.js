8e2ce3f4947f3165d58dae0b1d496f587292181d
Heather Anderson <handerson@artefactual.com>
2014-02-22 00:47:26 +0000

    Add filter to handle empty data fields

--- plugins/arDrmcPlugin/frontend/app/scripts/filters/index.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/filters/index.js
@@ -5,6 +5,7 @@
   var angular = require('angular');
 
   module.exports = angular.module('momaApp.filters', [])
+    .filter('EmptyFilter', require('./EmptyFilter'))
     .filter('UnitFilter', require('./UnitFilter'));
 
 })();
