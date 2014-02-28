bd883536098812559ab22e652d32709897ad236a
Jesús García Crespo <jesus@sevein.com>
2014-02-28 11:43:54 -0800

    Add levelId to tree mock up

--- plugins/arRestApiPlugin/modules/api/actions/informationobjectsTreeAction.class.php
+++ plugins/arRestApiPlugin/modules/api/actions/informationobjectsTreeAction.class.php
@@ -30,26 +30,26 @@ class APIInformationObjectsTreeAction extends QubitAPIAction
   {
     // Temporary
     return array(
-      array('id' => 1, 'title' => 'Play Dead; Real Time', 'children' => array(
-        array('id' => 2, 'title' => 'MoMA 2012', 'children' => array(
-          array('id' => 3, 'title' => 'Installation documentation'),
-          array('id' => 4, 'title' => 'Exhibition files', 'children' => array(
-            array('id' => 5, 'title' => '1098.2005.a.AV'),
-            array('id' => 6, 'title' => '1098.2005.b.AV'),
-            array('id' => 7, 'title' => '1098.2005.c.AV'))))),
-        array('id' => 8, 'title' => 'Supplied by artist', 'children' => array(
-          array('id' => 9, 'title' => '1098.2005.a.x1', 'children' => array(
-            array('id' => 10, 'title' => '1098.2005.a.x2'),
-            array('id' => 11, 'title' => '1098.2005.a.x3'))),
-          array('id' => 12, 'title' => '1098.2005.b.x1', 'children' => array(
-            array('id' => 13, 'title' => '1098.2005.b.x2'),
-            array('id' => 14, 'title' => '1098.2005.b.x3'))),
-          array('id' => 15, 'title' => '1098.2005.c.x1', 'children' => array(
-            array('id' => 16, 'title' => '1098.2005.c.x2'),
-            array('id' => 17, 'title' => '1098.2005.c.x3'))))),
-        array('id' => 30, 'title' => 'Digital archival masters', 'children' => array(
-          array('id' => 31, 'title' => '1098.2005.a.x4'),
-          array('id' => 32, 'title' => '1098.2005.b.x4'),
-          array('id' => 33, 'title' => '1098.2005.c.x4'))))));
+      array('id' => 1, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artwork_record_id'), 'title' => 'Play Dead; Real Time', 'children' => array(
+        array('id' => 2, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_description_id'), 'title' => 'MoMA 2012', 'children' => array(
+          array('id' => 3, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_equipment_id'), 'title' => 'Installation documentation'),
+          array('id' => 4, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_description_id'), 'title' => 'Exhibition files', 'children' => array(
+            array('id' => 5, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_exhibition_format_id'), 'title' => '1098.2005.a.AV'),
+            array('id' => 6, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_exhibition_format_id'), 'title' => '1098.2005.b.AV'),
+            array('id' => 7, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_exhibition_format_id'), 'title' => '1098.2005.c.AV'))))),
+        array('id' => 8, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_description_id'), 'title' => 'Supplied by artist', 'children' => array(
+          array('id' => 9, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_supplied_master_id'), 'title' => '1098.2005.a.x1', 'children' => array(
+            array('id' => 10, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_verified_proof_id'), 'title' => '1098.2005.a.x2'),
+            array('id' => 11, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_verified_proof_id'), 'title' => '1098.2005.a.x3'))),
+          array('id' => 12, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_supplied_master_id'), 'title' => '1098.2005.b.x1', 'children' => array(
+            array('id' => 13, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_verified_proof_id'), 'title' => '1098.2005.b.x2'),
+            array('id' => 14, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_verified_proof_id'), 'title' => '1098.2005.b.x3'))),
+          array('id' => 15, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_supplied_master_id'), 'title' => '1098.2005.c.x1', 'children' => array(
+            array('id' => 16, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_verified_proof_id'), 'title' => '1098.2005.c.x2'),
+            array('id' => 17, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_artist_verified_proof_id'), 'title' => '1098.2005.c.x3'))))),
+        array('id' => 30, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_description_id'), 'title' => 'Digital archival masters', 'children' => array(
+          array('id' => 31, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_archival_master_id'), 'title' => '1098.2005.a.x4'),
+          array('id' => 32, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_archival_master_id'), 'title' => '1098.2005.b.x4'),
+          array('id' => 33, 'levelOfDescriptionId' => (int) sfConfig::get('app_drmc_lod_archival_master_id'), 'title' => '1098.2005.c.x4'))))));
   }
 }
