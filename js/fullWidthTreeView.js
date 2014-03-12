70ab76f4077b19afb4bd5f608ae2057ea3d28dfc
andy koch <koch.andy@gmail.com>
2014-03-12 10:22:31 -0700

    Moved the treeview into the #main-column area. Handle updating #main-column without losing treeview.

--- js/fullWidthTreeView.js
+++ js/fullWidthTreeView.js
@@ -6,7 +6,7 @@
 
   var treeview_open = false;
   var url = '/informationobject/fullWidthTreeView'
-  var html = "<div class=\"row\" id=\"fullwidth-treeview-row\">" + 
+  var html = "<div id=\"fullwidth-treeview-row\">" + 
                "<div id=\"fullwidth-treeview\">" +
                 "<img src=\"/images/loading.gif\" id=\"fullwidth-treeview-loading\" />" +
                "</div>" +
@@ -30,7 +30,7 @@
       // check we've not already inserted the DOM elements for this
       if( !$('#fullwidth-treeview-row').length )
       {
-        $('#wrapper').prepend($(html));
+        $('#main-column h1').after($(html));
         $('#fullwidth-treeview-row').animate({height: '100px'}, 500);
       }
 
@@ -48,11 +48,18 @@
         // window.location = window.location.origin + data.node.a_attr.href
         var url = window.location.origin + '/index.php/' + data.node.a_attr.href;
         $.get(url, function(response){
-          $('#main-column').replaceWith($(response).find('#main-column'));
+          // copy the treeview to the new object information response
+          // insert the new object information into the existing page
+          response = $(response);
+          $('#fullwidth-treeview-row').insertAfter(response.find('#main-column h1'));
+          $('#main-column').replaceWith($(response.find('#main-column')));
+
+          // update the url, TODO save the state
           window.history.pushState({}, $('#main-column h1').first().text(), url);
         });
       });
 
+      // TODO restore window.history states
       $(window).bind('popstate', function() {
 
       });
