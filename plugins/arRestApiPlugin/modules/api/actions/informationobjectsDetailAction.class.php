2c3034737dfa85bc151aca1f3c38e2ea4eace05b
Jesús García Crespo <jesus@sevein.com>
2014-03-14 05:48:51 -0700

    Cleanups in /informationobjects/:id

--- plugins/arRestApiPlugin/modules/api/actions/informationobjectsDetailAction.class.php
+++ plugins/arRestApiPlugin/modules/api/actions/informationobjectsDetailAction.class.php
@@ -39,37 +39,20 @@ class ApiInformationObjectsDetailAction extends QubitApiAction
     $data['title'] = get_search_i18n($doc, 'title');
     $data['level_of_description_id'] = (int)$doc['levelOfDescriptionId'];
 
-    if (sfConfig::get('app_drmc_lod_artwork_record_id') == $doc['levelOfDescriptionId'])
-    {
-      $data['tms'] = array(
-        'accessionNumber' => '1098.2005.a-c',
-        'objectId' => '100620',
-        'title' => 'Play Dead; Real Time',
-        'year' => '2003',
-        'artist' => 'Douglas Gordon',
-        'classification' => 'Installation',
-        'medium' => 'Three-channel video',
-        'dimensions' => '19:11 min, 14:44 min. (on larger screens), 21:58 min. (on monitor). Minimum Room Size: 24.8m x 13.07m',
-        'description' => 'Exhibition materials: 3 DVD and players, 2 projectors, 3 monitor, 2 screens. The complete work is a three-screen piece, consisting of '
-      );
-    }
-    else
-    {
-      $data['dc'] = array(
-        'identifier' => $doc['identifier'],
-        'title' => get_search_i18n($doc, 'title')
-        // 'description' => $io->getScopeAndContent(array('cultureFallback' => true))
-        // 'subjects' => ['Elephants', 'Circus', 'Zoo', 'Animals'],
-        // 'description' => 'Exhibition materials: 3 DVD and players, 2 projectors, 3 monitor, 2 screens. The complete work is a three-screen piece, consisting of one retro projection, one front projection and one monitor. See file for installation instructions. One monitor and two projections on screens 19.69 X 11.38 feet. Viewer must be able to walk around screens.',
-        // 'type' => ['image'],
-        // 'format' => 'You tell me',
-        // 'source' => 'Somewhere',
-        // 'language' => ['English'],
-        // 'isLocatedAt' => ['MoMA'],
-        // 'spatial' => ['New York'],
-        // 'rights' => 'Many rights'
-      );
-    }
+    $data['dc'] = array(
+      'identifier' => $doc['identifier'],
+      'title' => get_search_i18n($doc, 'title')
+      // 'description' => $io->getScopeAndContent(array('cultureFallback' => true))
+      // 'subjects' => ['Elephants', 'Circus', 'Zoo', 'Animals'],
+      // 'description' => 'Exhibition materials: 3 DVD and players, 2 projectors, 3 monitor, 2 screens. The complete work is a three-screen piece, consisting of one retro projection, one front projection and one monitor. See file for installation instructions. One monitor and two projections on screens 19.69 X 11.38 feet. Viewer must be able to walk around screens.',
+      // 'type' => ['image'],
+      // 'format' => 'You tell me',
+      // 'source' => 'Somewhere',
+      // 'language' => ['English'],
+      // 'isLocatedAt' => ['MoMA'],
+      // 'spatial' => ['New York'],
+      // 'rights' => 'Many rights'
+    );
 
     return $data;
   }
