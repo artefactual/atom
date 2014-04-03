87158603f2982eae39e27a77345aba09c5bda9df
Jesús García Crespo <jesus@sevein.com>
2014-04-03 12:23:12 -0700

    Improvements in TMS lookup

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/TmsBrowserCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/TmsBrowserCtrl.js
@@ -1,19 +1,46 @@
 'use strict';
 
 module.exports = function ($scope, $http) {
+  $scope.submitted = false;
 
-  $scope.search = function () {
+  var endpoints = {
+    'ObjectID': {
+      url: 'http://vmdrmcatom02.museum.moma.org/tms/TMSAPI/TmsObjectSvc/TmsObjects.svc/GetTombstoneDataRest/ObjectID/',
+      group: 'GetTombstoneDataRestIdResult',
+      regex: /^[0-9]+$/
+    },
+    'AccessionNo': {
+      url: 'http://vmdrmcatom02.museum.moma.org/tms/TMSAPI/TmsObjectSvc/TmsObjects.svc/GetTombstoneDataRest/Object/',
+      group: 'GetTombstoneDataRestResult'
+    }
+  };
+
+  $scope.submit = function () {
     if (!$scope.query || $scope.query.length < 1) {
-      delete $scope.results;
-      delete $scope.thumbnail;
       return;
     }
-    $http.get('http://vmdrmcatom02.museum.moma.org/tms/TMSAPI/TmsObjectSvc/TmsObjects.svc/GetTombstoneDataRest/ObjectID/' + $scope.query)
+    $scope.endpoint = endpoints.AccessionNo;
+    if (endpoints.ObjectID.regex.test($scope.query)) {
+      $scope.endpoint = endpoints.ObjectID;
+    }
+    $http.get($scope.endpoint.url + $scope.query)
       .then(function (response) {
-        $scope.results = response.data.GetTombstoneDataRestIdResult;
-        $scope.thumbnail = response.data.GetTombstoneDataRestIdResult.Thumbnail;
-        console.log($scope.thumbnail);
+        if (response.data[$scope.endpoint.group].ObjectID === 0) {
+          $scope.found = false;
+          return;
+        }
+        $scope.found = true;
+        $scope.results = response.data[$scope.endpoint.group];
+        $scope.thumbnail = response.data[$scope.endpoint.group].Thumbnail;
+      }, function () {
+        $scope.found = false;
+      }).finally(function () {
+        $scope.submitted = true;
       });
   };
 
+  $scope.zoom = function () {
+    console.log($scope.results.FullImage);
+    $scope.thumbnail = $scope.results.FullImage;
+  };
 };
