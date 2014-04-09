9b38aa4b169d157bb473142f1ab89943c961bd45
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-04-09 22:13:28 +0200

    Fix date created facet for artworks
    Create event start date with proper date from TMS Object dated

--- plugins/qtSwordPlugin/lib/qtPackageExtractorMETSArchivematicaDIP.class.php
+++ plugins/qtSwordPlugin/lib/qtPackageExtractorMETSArchivematicaDIP.class.php
@@ -221,7 +221,18 @@ class qtPackageExtractorMETSArchivematicaDIP extends qtPackageExtractorBase
         {
           $event->date = QubitFlatfileImport::parseDate($date);
         }
+      }
+    }
 
+    if ($options['tmsDate'])
+    {
+      if (strlen($options['tmsDate']) == 4 && ctype_digit($options['tmsDate']))
+      {
+        $event->startDate = QubitFlatfileImport::parseDate($options['tmsDate'].'0101');
+      }
+      else
+      {
+        $event->startDate = QubitFlatfileImport::parseDate($options['tmsDate']);
       }
     }
 
@@ -866,6 +877,7 @@ class qtPackageExtractorMETSArchivematicaDIP extends qtPackageExtractorBase
             // Creation event
             case 'Dated':
               $creation['date'] = $value;
+              $creation['tmsDate'] = $value;
 
               break;
 
