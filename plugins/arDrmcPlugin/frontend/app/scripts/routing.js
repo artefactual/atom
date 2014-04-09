3cc799d48366ddabb496ded3322c0b20dcf40ebb
Mike Cantelon <mcantelon@gmail.com>
2014-04-09 06:56:40 -0700

    Added client-side redirection to login if not authorized.

--- plugins/arDrmcPlugin/frontend/app/scripts/routing.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/routing.js
@@ -9,6 +9,12 @@ module.exports = function ($locationProvider, $stateProvider, $urlRouterProvider
   // Default route
   $urlRouterProvider.otherwise(SETTINGS.DRMCPath + '404');
 
+  function redirectIfNotAuthorized($state, AuthenticationService) {
+    if (!AuthenticationService.isAuthenticated()) {
+      $state.go('login');
+    }
+  }
+
   // Define ui-router states
   $stateProvider
 
@@ -29,84 +35,99 @@ module.exports = function ($locationProvider, $stateProvider, $urlRouterProvider
     .state('dashboard', {
       url: SETTINGS.DRMCPath.replace(/\/$/, ''),
       controller: 'DashboardCtrl',
-      templateUrl: SETTINGS.viewsPath + '/dashboard.html'
+      templateUrl: SETTINGS.viewsPath + '/dashboard.html',
+      onEnter: redirectIfNotAuthorized
     })
 
     // AIPs
     .state('aips', {
       abstract: true,
       url: SETTINGS.DRMCPath + 'aips',
-      template: '<ui-view/>'
+      template: '<ui-view/>',
+      onEnter: redirectIfNotAuthorized
     })
     .state('aips.browser', {
       url: '',
       controller: 'AIPBrowserCtrl',
-      templateUrl: SETTINGS.viewsPath + '/aips.browser.html'
+      templateUrl: SETTINGS.viewsPath + '/aips.browser.html',
+      onEnter: redirectIfNotAuthorized
     })
     .state('aips.view', {
       url: '/{uuid:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}',
       controller: 'AIPViewCtrl',
-      templateUrl: SETTINGS.viewsPath + '/aips.view.html'
+      templateUrl: SETTINGS.viewsPath + '/aips.view.html',
+      onEnter: redirectIfNotAuthorized
     })
 
     // Works
     .state('works', {
       abstract: true,
       url: SETTINGS.DRMCPath + 'works',
-      template: '<ui-view/>'
+      template: '<ui-view/>',
+      onEnter: redirectIfNotAuthorized
     })
     .state('works.browser', {
       url: '',
       controller: 'WorkBrowserCtrl',
-      templateUrl: SETTINGS.viewsPath + '/works.browser.html'
+      templateUrl: SETTINGS.viewsPath + '/works.browser.html',
+      onEnter: redirectIfNotAuthorized
     })
     .state('works.view', {
       url: '/{id}',
       controller: 'WorkViewCtrl',
-      templateUrl: SETTINGS.viewsPath + '/works.view.html'
+      templateUrl: SETTINGS.viewsPath + '/works.view.html',
+      onEnter: redirectIfNotAuthorized
     })
 
     // Technology Records
     .state('technology-records', {
       abstract: true,
       url: SETTINGS.DRMCPath + 'technology-records',
-      template: '<ui-view/>'
+      template: '<ui-view/>',
+      onEnter: redirectIfNotAuthorized
     })
     .state('technology-records.browser', {
       url: '',
       controller: 'TechnologyRecordBrowserCtrl',
-      templateUrl: SETTINGS.viewsPath + '/technology-records.browser.html'
+      templateUrl: SETTINGS.viewsPath + '/technology-records.browser.html',
+      onEnter: redirectIfNotAuthorized
+
     })
     .state('technology-records.view', {
       url: '/{id}',
       controller: 'TechnologyRecordViewCtrl',
-      templateUrl: SETTINGS.viewsPath + '/technology-records.view.html'
+      templateUrl: SETTINGS.viewsPath + '/technology-records.view.html',
+      onEnter: redirectIfNotAuthorized
     })
 
     // TMS
     .state('tms', {
       abstract: true,
       url: SETTINGS.DRMCPath + 'tms',
-      template: '<ui-view/>'
+      template: '<ui-view/>',
+      onEnter: redirectIfNotAuthorized
     })
     .state('tms.browser', {
       url: '',
       controller: 'TmsBrowserCtrl',
-      templateUrl: SETTINGS.viewsPath + '/tms.browser.html'
+      templateUrl: SETTINGS.viewsPath + '/tms.browser.html',
+      onEnter: redirectIfNotAuthorized
     })
 
     // Search
     .state('search', {
       abstract: true,
       url: SETTINGS.DRMCPath + 'search',
-      template: '<ui-view/>'
+      template: '<ui-view/>',
+      onEnter: redirectIfNotAuthorized
     })
     .state('search.entity', {
       url: '/{entity}',
       controller: 'SearchCtrl',
       templateUrl: function (stateParams) {
         return SETTINGS.viewsPath + '/' + stateParams.entity + '.search.html';
-      }
+      },
+      onEnter: redirectIfNotAuthorized
     });
 
 };
