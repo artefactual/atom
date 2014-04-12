dd1c4a1777ab8ffdccead63106d0eb4692d25241
Mike Cantelon <mcantelon@gmail.com>
2014-04-12 15:53:00 -0700

    Fixed misleading error reporting.

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/LoginCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/LoginCtrl.js
@@ -4,21 +4,23 @@ module.exports = function ($scope, AuthenticationService, $state) {
 
   $scope.form = {};
 
-  $scope.notifyOfError = function () {
-    window.alert('Please enter both a username and a password.');
+  $scope.notifyOfError = function ($error) {
+    window.alert($error);
   };
 
   $scope.submit = function () {
+    console.log('UN:' + $scope.form.username);
+    console.log('PW:' + $scope.form.password);
     if ($scope.form.username && $scope.form.password) {
       AuthenticationService.authenticate($scope.form.username,  $scope.form.password)
       .success(function () {
         $state.go('dashboard');
       })
       .error(function () {
-        $scope.notifyOfError();
+        $scope.notifyOfError('Incorrect username or password.');
       });
     } else {
-      $scope.notifyOfError();
+      $scope.notifyOfError('Please enter both a username and a password.');
     }
   };
 };
