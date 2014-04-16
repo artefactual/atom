12eef91bb4ba4555546d9136a4dd54183d38e0d1
andy koch <koch.andy@gmail.com>
2014-04-16 13:30:22 -0700

    Improving how modals work
    Taking over the display of modals from bootstrap so we can maintain a
    single modal at all times, and ensure it is cleared when done with.

--- apps/qubit/modules/right/templates/_relatedRights.php
+++ apps/qubit/modules/right/templates/_relatedRights.php
@@ -25,3 +25,60 @@
   <?php endforeach; ?>
 
 <?php endif; ?>
+
+<div id="modalContainer"></div>
+
+<script type="text/javascript">
+  (function(){
+    var container = jQuery('#modalContainer')
+
+    function setFieldsForBasis()
+    {
+      console.log('updating fields for basis');
+      var selected = container.find('select[name=basis]')[0];
+      var type = false;
+      if(selected) {
+        type = jQuery(selected.selectedOptions[0]).text().toLowerCase();
+      }
+
+      jQuery('#modalContainer .basis-group').each(function(){
+        $this = jQuery(this);
+        $this.toggle($this.hasClass(type));
+      });
+    }
+
+    // setup Edit button events
+    // fetch modal html from server
+    // inject into #modalContainer
+    // display the modal
+    // call any prep required
+    jQuery('#rightsArea').on('click', '[data-modal=true]', function(event){
+      event.preventDefault();
+      
+      // clear anything in the modalContainer
+      container.empty()
+
+      var $this = jQuery(this);
+      var data = {};
+
+      // pass data-id value (rights record id)
+      if($this.attr('data-id')) {
+        data.id = $this.attr('data-id');
+      }
+      jQuery.get('/right/modal', data, function(data) {
+        container.append(data);
+        container.find('.modal').modal('show');
+        setFieldsForBasis();
+      });
+    });
+
+    // add event listener for basis select changes
+    container.on('change','select[name="basis"]', setFieldsForBasis);
+
+    // // add event listener for submit buttons
+    // jQuery('.modal').on('click', '.btn-primary', function(){
+    //   var form = jQuery(this).parent().parent().find('form');
+    //   jQuery.post(form.attr('action'), form.serialize());
+    // });
+  })();
+</script>
--- apps/qubit/modules/right/templates/_right.php
+++ apps/qubit/modules/right/templates/_right.php
@@ -2,23 +2,9 @@
   <h3><?php echo __('Related right') ?></h3>
   <div>
 
-    <a data-target="#basisModal<?php echo $resource->id ?>" role="button" class="btn" data-toggle="modal">Edit</a>
-
-    <div class="modal fade hide" id="basisModal<?php echo $resource->id ?>" tabindex="-1" 
-      role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" 
-      data-remote="/right/modal?id=<?php echo $resource->id ?>">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h3 id="myModalLabel">Set/Modify Rights</h3>
-      </div>
-      <div class="modal-body">
-      </div>
-      <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
-        <button class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-    
+    <a role="button" class="btn" data-id="<?php echo $resource->id ?>" data-modal="true">Edit</a>
+    <a role="button" class="btn" data-modal="true">Edit</a>
+   
     <?php if (isset($inherit)): ?>
       <?php echo link_to(render_title($inherit), array($inherit, 'module' => 'informationobject'), array('title' => __('Inherited from %1%', array('%1%' => $inherit)))) ?>
     <?php endif; ?>
