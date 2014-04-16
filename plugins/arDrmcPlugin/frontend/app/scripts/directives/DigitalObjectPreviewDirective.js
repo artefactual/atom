8039e8b2419feebe79be5d8e2b6ac0bcf6ae4ad5
Heather Anderson <handerson@artefactual.com>
2014-04-16 09:42:08 -0700

    Generate dynamic templates; paging; media type templates

--- plugins/arDrmcPlugin/frontend/app/scripts/directives/DigitalObjectPreviewDirective.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/directives/DigitalObjectPreviewDirective.js
@@ -1,10 +1,25 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function ($compile, $http, ModalDigitalObjectViewerService) {
   return {
     restrict: 'E',
-    link: function () {
+    replace: true,
+    scope: {
+      file: '='
     },
-    template: '<div><iframe width="420" height="315" src="//www.youtube.com/embed/Q-XD6fuf0ho" frameborder="0" allowfullscreen></iframe></div>'
+    link: function (scope, element) {
+      // template: '<div><iframe width="420" height="315" src="//www.youtube.com/embed/Q-XD6fuf0ho" frameborder="0" allowfullscreen></iframe></div>',
+      var mediaTypeId = scope.file.mediaTypeId;
+      var templateUrl = ModalDigitalObjectViewerService.mediaTypes[mediaTypeId].templateUrl;
+
+      // Fetch the template, bind it to a new scope and compile
+      // TODO: doesn't AngularJS have a mixing for this?
+      // TODO: should make use of preLink / postLink (compile vs link)?
+      $http.get(templateUrl).then(function (response) {
+        var templateScope = scope.$new();
+        element.html(response.data);
+        $compile(element.contents())(templateScope);
+      });
+    }
   };
 };
--- plugins/arDrmcPlugin/frontend/app/scripts/services/ModalDigitalObjectViewerService.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/services/ModalDigitalObjectViewerService.js
@@ -8,6 +8,35 @@ module.exports = function ($modal, SETTINGS) {
     controller: 'DigitalObjectViewerCtrl'
   };
 
+  // Media types
+  this.mediaTypes = {
+    135: {
+      class: 'audio',
+      name: 'Audio',
+      templateUrl: SETTINGS.viewsPath + '/partials/digital-object-preview.audio.html'
+    },
+    136: {
+      class: 'image',
+      name: 'Image',
+      templateUrl: SETTINGS.viewsPath + '/partials/digital-object-preview.image.html'
+    },
+    137: {
+      class: 'text',
+      name: 'Text',
+      templateUrl: SETTINGS.viewsPath + '/partials/digital-object-preview.text.html'
+    },
+    138: {
+      class: 'video',
+      name: 'Video',
+      templateUrl: SETTINGS.viewsPath + '/partials/digital-object-preview.video.html'
+    },
+    139: {
+      class: 'other',
+      name: 'Other',
+      templateUrl: SETTINGS.viewsPath + '/partials/digital-object-preview.other.html'
+    }
+  };
+
   this.open = function (files) {
     configuration.resolve.files = function () {
       // Use only the first three items in the array
