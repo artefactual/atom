19cc4af22adaf54b43d6b19c1c49c288849188fa
Jesús García Crespo <jesus@sevein.com>
2014-04-21 00:56:56 -0700

    Add constant to TaxonomyService: techrecs

--- plugins/arDrmcPlugin/frontend/app/scripts/services/TaxonomyService.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/services/TaxonomyService.js
@@ -6,7 +6,8 @@ module.exports = function ($http, SETTINGS) {
   // This should be probably a feature provided by the API
   var taxonomies = {
     'EVENT_TYPE': 40,
-    'DC_TYPES': 54
+    'DC_TYPES': 54,
+    'SUPORTING_TECHNOLOGY_RELATION_TYPES': SETTINGS.drmc.taxonomy_supporting_technologies_relation_types_id
   };
 
   this.getTerms = function (taxonomy) {
