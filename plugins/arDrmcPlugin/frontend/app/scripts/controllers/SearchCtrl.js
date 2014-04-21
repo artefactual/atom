8808a373080c9f2b1709ed0884975354ac1aebfb
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-04-21 16:10:29 +0200

    Changes in search AIPs, refs #6462
    - Include overview
    - Add reclassify modal
    - Change list type to grid

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/SearchCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/SearchCtrl.js
@@ -4,7 +4,7 @@
  * SearchCtrl contains the business logic for all our share pages. It shares
  * state with other controllers via SearchService.
  */
-module.exports = function ($scope, $stateParams, SearchService, $filter) {
+module.exports = function ($scope, $stateParams, SearchService, $filter, $modal, SETTINGS) {
   // Search state
   $scope.criteria = {};
   $scope.criteria.limit = 10;
@@ -46,6 +46,12 @@ module.exports = function ($scope, $stateParams, SearchService, $filter) {
         console.log('Failed', reason);
         delete $scope.data;
       });
+    if ($stateParams.entity === 'aips') {
+      SearchService.getAIPTypes()
+        .success(function (data) {
+          $scope.classifications = data.terms;
+        });
+    }
   }
 
   $scope.getTermLabel = function (facet, id) {
@@ -77,5 +83,32 @@ module.exports = function ($scope, $stateParams, SearchService, $filter) {
         return $scope.data.facets[facet].ranges[range].label;
       }
     }
+  };
+
+  // Support AIP overview toggling
+  $scope.showOverview = true;
+  $scope.toggleOverview = function () {
+    $scope.showOverview = !$scope.showOverview;
+  };
+
+  $scope.openReclassifyModal = function (aip) {
+    // Current AIP selected equals to AIP in the modal
+    $scope.aip = aip;
+    // It happens that $modal.open returns a promise :)
+    var modalInstance = $modal.open({
+      templateUrl: SETTINGS.viewsPath + '/modals/reclassify-aips.html',
+      backdrop: true,
+      controller: 'AIPReclassifyCtrl',
+      scope: $scope, // TODO: isolate with .new()?
+      resolve: {
+        classifications: function () {
+          return $scope.classifications;
+        }
+      }
+    });
+    // This is going to happen only if the $modal succeeded
+    modalInstance.result.then(function (result) {
+      aip.class = result;
+    });
   };
 };
