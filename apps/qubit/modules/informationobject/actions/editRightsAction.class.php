244c37b0afead26724ff6c02ffb41af35ea25924
andy koch <koch.andy@gmail.com>
2014-04-22 07:22:15 -0700

    Creating form for rights management.

---
+++ apps/qubit/modules/informationobject/actions/editRightsAction.class.php
@@ -0,0 +1,303 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class InformationObjectEditRightsAction extends sfAction
+{
+  // Arrays not allowed in class constants
+  public static
+    $NAMES = array(
+      'act',
+      'basis',
+      'endDate',
+      'startDate',
+      'restriction',
+      'rightsHolder',
+      'rightsNote',
+      'copyrightStatus',
+      'copyrightStatusDate',
+      'copyrightJurisdiction',
+      'copyrightNote',
+      'licenseIdentifier',
+      'licenseTerms',
+      'licenseNote',
+      'statuteJurisdiction',
+      'statuteCitation',
+      'statuteDeterminationDate',
+      'statuteNote');
+
+  protected function addField($name)
+  {
+    switch ($name)
+    {
+      case 'endDate':
+        $this->form->setValidator('endDate', new sfValidatorString);
+        $this->form->setWidget('endDate', new sfWidgetFormInput);
+
+        $this->form->getWidgetSchema()->endDate->setLabel($this->context->i18n->__('End'));
+
+        break;
+
+      case 'startDate':
+        $dt = new DateTime;
+        $this->form->setDefault('startDate', $dt->format('Y-m-d'));
+        $this->form->setValidator('startDate', new sfValidatorString);
+        $this->form->setWidget('startDate', new sfWidgetFormInput);
+
+        $this->form->getWidgetSchema()->startDate->setLabel($this->context->i18n->__('Start'));
+
+        break;
+
+      case 'restriction':
+        $choices[1] = $this->context->i18n->__('Allow');
+        $choices[0] = $this->context->i18n->__('Disallow');
+
+        $this->form->setValidator('restriction', new sfValidatorBoolean);
+        $this->form->setWidget('restriction', new sfWidgetFormSelect(array('choices' => $choices)));
+
+        break;
+
+      case 'statuteDeterminationDate':
+      case 'copyrightStatusDate':
+        $this->form->setValidator($name, new sfValidatorString);
+        $this->form->setWidget($name, new sfWidgetFormInput);
+
+        break;
+
+      case 'act':
+        foreach (QubitTaxonomy::getTermsById(QubitTaxonomy::RIGHT_ACT_ID) as $item)
+        {
+          $choices[$this->context->routing->generate(null, array($item, 'module' => 'term'))] = $item->__toString();
+        }
+
+        $this->form->setValidator('act', new sfValidatorString);
+        $this->form->setWidget('act', new sfWidgetFormSelect(array('choices' => $choices)));
+
+        break;
+
+      case 'basis':
+        foreach (QubitTaxonomy::getTermsById(QubitTaxonomy::RIGHT_BASIS_ID) as $item)
+        {
+          if (QubitTerm::RIGHT_BASIS_POLICY_ID == $item->id)
+          {
+            $this->form->setDefault('basis', $this->context->routing->generate(null, array($item, 'module' => 'term')));
+          }
+
+          $choices[$this->context->routing->generate(null, array($item, 'module' => 'term'))] = $item->__toString();
+        }
+
+        $this->form->setValidator('basis', new sfValidatorString);
+        $this->form->setWidget('basis', new sfWidgetFormSelect(array('choices' => $choices)));
+
+        break;
+
+      case 'copyrightStatus':
+        foreach (QubitTaxonomy::getTermsById(QubitTaxonomy::COPYRIGHT_STATUS_ID) as $item)
+        {
+          $choices[$this->context->routing->generate(null, array($item, 'module' => 'term'))] = $item->__toString();
+        }
+
+        $this->form->setValidator('copyrightStatus', new sfValidatorString);
+        $this->form->setWidget('copyrightStatus', new sfWidgetFormSelect(array('choices' => $choices)));
+
+        break;
+
+      case 'rightsHolder':
+        $this->form->setValidator('rightsHolder', new sfValidatorString);
+        $this->form->setWidget('rightsHolder', new sfWidgetFormSelect(array('choices' => array())));
+
+        break;
+
+      case 'copyrightJurisdiction':
+        $this->form->setValidator('copyrightJurisdiction', new sfValidatorI18nChoiceCountry);
+        $this->form->setWidget('copyrightJurisdiction', new sfWidgetFormI18nChoiceCountry(array('add_empty' => true, 'culture' => $this->context->user->getCulture())));
+
+        break;
+
+      case 'copyrightNote':
+      case 'licenseNote':
+      case 'statuteJurisdiction':
+      case 'statuteCitation':
+      case 'statuteNote':
+      case 'rightsNote':
+        $this->form->setValidator($name, new sfValidatorString);
+        $this->form->setWidget($name, new sfWidgetFormTextarea);
+
+        break;
+
+      case 'licenseIdentifier':
+      case 'licenseTerms':
+        $this->form->setValidator($name, new sfValidatorString);
+        $this->form->setWidget($name, new sfWidgetFormInput);
+
+        break;
+    }
+  }
+
+  protected function processField($field)
+  {
+    switch ($field->getName())
+    {
+      case 'act':
+      case 'basis':
+      case 'copyrightStatus':
+      case 'rightsHolder':
+        unset($this->right[$field->getName()]);
+
+        $value = $this->form->getValue($field->getName());
+        if (isset($value))
+        {
+          $params = $this->context->routing->parse(Qubit::pathInfo($value));
+          $this->right[$field->getName()] = $params['_sf_route']->resource;
+        }
+
+        break;
+
+      default:
+        $this->right[$field->getName()] = $this->form->getValue($field->getName());
+    }
+  }
+
+  public function processForm()
+  {
+    if (isset($this->nameFormat))
+    {
+      $name = preg_replace('/\[.*\]/', 's', $this->nameFormat);
+      $params = $this->request[$name];
+    }
+    else
+    {
+      $params = $this->request->editRights;
+    }
+
+    foreach ((array)$params as $item)
+    {
+      // Continue only if user typed something
+      foreach ($item as $value)
+      {
+        if (0 < strlen($value))
+        {
+          break;
+        }
+      }
+
+      if (1 > strlen($value))
+      {
+        continue;
+      }
+
+      $this->form->bind($item);
+      if ($this->form->isValid())
+      {
+        if (isset($item['id']))
+        {
+          $params = $this->context->routing->parse(Qubit::pathInfo($item['id']));
+          $this->right = $params['_sf_route']->resource;
+        }
+        else
+        {
+          $this->right = new QubitRights;
+        }
+
+        foreach ($this->form as $field)
+        {
+          if (isset($item[$field->getName()]))
+          {
+            $this->processField($field);
+          }
+        }
+
+        $this->right->save();
+
+        if (!isset($item['id']))
+        {
+          $this->relation = new QubitRelation;
+          $this->relation->object = $this->right;
+          $this->relation->typeId = QubitTerm::RIGHT_ID;
+
+          $this->resource->relationsRelatedBysubjectId[] = $this->relation;
+        }
+      }
+    }
+
+    // Stop here if duplicating
+    if (isset($this->request->sourceId))
+    {
+      return;
+    }
+
+    if (isset($this->nameFormat))
+    {
+      preg_match('/\d+/', $this->nameFormat, $usageId);
+      $deleteRights = $this->request['deleteRights_'.$usageId[0]];
+    }
+    else
+    {
+      $deleteRights = $this->request->deleteRights;
+    }
+
+    if (isset($deleteRights))
+    {
+      foreach ($deleteRights as $item)
+      {
+        $params = $this->context->routing->parse(Qubit::pathInfo($item));
+        $params['_sf_route']->resource->delete();
+      }
+    }
+  }
+
+  protected function earlyExecute()
+  {
+    $this->resource = $this->getRoute()->resource;
+
+    // Check that this isn't the root
+    if (!isset($this->resource->parent))
+    {
+      $this->forward404();
+    }
+
+    // Check user authorization
+    if (!QubitAcl::check($this->resource, 'update'))
+    {
+      QubitAcl::forwardUnauthorized();
+    }
+  }
+
+
+  public function execute($request)
+  {
+    $this->earlyExecute();
+
+    $this->form = new sfForm;
+    $this->form->getValidatorSchema()->setOption('allow_extra_fields', true);
+
+    if (isset($this->nameFormat))
+    {
+      $this->form->getWidgetSchema()->setNameFormat($this->nameFormat);
+    }
+    else
+    {
+      $this->form->getWidgetSchema()->setNameFormat('editRight[%s]');
+    }
+
+    foreach ($this::$NAMES as $name)
+    {
+      $this->addField($name);
+    }
+  }
+}
---
+++ apps/qubit/modules/informationobject/templates/editRightsSuccess.php
@@ -0,0 +1,119 @@
+<?php decorate_with('layout_1col.php') ?>
+
+<?php slot('title') ?>
+  <h1 class="multiline">
+    <?php echo render_title($resource) ?>
+    <span class="sub">Rights Management</span>
+  </h1>
+<?php end_slot() ?>
+
+<?php slot('content') ?>
+
+  <?php echo $form->renderGlobalErrors() ?>
+
+  <?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'informationobject', 'action' => 'editRights'))) ?>
+
+    <?php echo $form->renderHiddenFields() ?>
+
+    <div id="content">
+      <fieldset class="collapsible">
+        <legend><?php echo __('Add New Rights Basis') ?></legend>
+
+          <?php echo $form->basis
+            ->renderRow() ?>
+
+          <?php echo $form->copyrightStatus
+            ->renderRow() ?>
+
+          <?php echo $form->copyrightStatusDate->renderRow() ?>
+
+          <?php echo $form->copyrightJurisdiction
+
+            ->renderRow() ?>
+
+          <?php echo $form->copyrightNote
+            ->renderRow() ?>
+
+          <?php echo $form->licenseIdentifier
+            ->renderRow() ?>
+
+          <?php echo $form->licenseTerms
+            ->renderRow() ?>
+
+          <?php echo $form->licenseNote
+            ->renderRow() ?>
+
+          <?php echo $form->statuteJurisdiction
+            ->renderRow() ?>
+
+          <?php echo $form->statuteCitation
+            ->renderRow() ?>
+
+          <?php echo $form->statuteDeterminationDate
+            ->renderRow() ?>
+
+          <?php echo $form->statuteNote
+            ->renderRow() ?>
+
+          <?php echo $form->act
+            ->renderRow() ?>
+
+          <?php echo $form->restriction
+            ->renderRow() ?>
+
+          <?php echo $form->startDate
+            ->renderRow() ?>
+
+          <?php echo $form->endDate
+            ->renderRow() ?>
+
+          <div class="form-item">
+            <?php echo $form->rightsHolder->renderLabel() ?>
+            <?php echo $form->rightsHolder->render(array('class' => 'form-autocomplete')) ?>
+            <input class="add" type="hidden" value="<?php echo url_for(array('module' => 'rightsholder', 'action' => 'add')) ?> #authorizedFormOfName"/>
+            <input class="list" type="hidden" value="<?php echo url_for(array('module' => 'rightsholder', 'action' => 'autocomplete')) ?>"/>
+          </div>
+
+          <?php echo $form->rightsNote
+            ->label(__('Rights note(s)'))
+            ->renderRow() ?>
+
+
+      </fieldset>
+
+    </div>
+
+    <section class="actions">
+      <ul>
+        <li><?php echo link_to(__('Cancel'), array($resource, 'module' => 'informationobject'), array('class' => 'c-btn')) ?></li>
+        <li><input class="c-btn c-btn-submit" type="submit" value="<?php echo __('Save') ?>"/></li>
+      </ul>
+    </section>
+
+  </form>
+
+  <script type="text/javascript">
+    (function(){
+      BasisSelect = {
+        fieldsets: {
+          copyright: jQuery('#content div[class *= form-item-copyright]'),
+          license: jQuery('#content div[class *= form-item-license]'),
+          statute: jQuery('#content div[class *= form-item-statute]'),
+        },
+
+        update: function()
+        {
+          var selectValue = jQuery(this).attr('value').match('[^/]*$')[0];
+          jQuery.each(BasisSelect.fieldsets, function(value, fields) {
+            console.log([selectValue, value]);
+            fields.toggle(selectValue == value);
+          });
+        }
+      }
+
+      jQuery('#editRight_basis').on('change', BasisSelect.update).trigger('change');
+
+    })();
+  </script>
+
+<?php end_slot() ?>
