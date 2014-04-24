16efcb3e0fc9d833c7b6a7bc3be1379322771068
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-04-24 05:53:26 +0200

    Add date and size range pickers in facets

--- plugins/arDrmcPlugin/frontend/app/scripts/directives/RangeFacetDirective.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/directives/RangeFacetDirective.js
@@ -15,9 +15,28 @@ module.exports = function (SETTINGS) {
     },
     link: function (scope) {
       scope.collapsed = false;
+      scope.collapsedRangePicker = true;
+      scope.dateRangePickerFrom = undefined;
+      scope.dateRangePickerTo = undefined;
+      scope.sizeRangePickerFrom = undefined;
+      scope.sizeRangePickerTo = undefined;
+      scope.units = [
+        { label: 'bytes', value: 1 },
+        { label: 'KB', value: 1024 },
+        { label: 'MB', value: 1048576 },
+        { label: 'GB', value: 1073741824 },
+        { label: 'TB', value: 1099511627776 },
+        { label: 'PB', value: 1125899906842624 }
+      ];
+      scope.sizeRangePickerFromUnit = scope.units[0];
+      scope.sizeRangePickerToUnit = scope.units[0];
 
       scope.toggle = function () {
         scope.collapsed = !scope.collapsed;
+      };
+
+      scope.toggleRangePicker = function () {
+        scope.collapsedRangePicker = !scope.collapsedRangePicker;
       };
 
       scope.select = function (from, to) {
@@ -45,6 +64,32 @@ module.exports = function (SETTINGS) {
           return scope.callback({arg1: 'dateCreated', arg2: from, arg3: to});
         }
         return scope.callback({arg1: from, arg2: to});
+      };
+
+      scope.resetRangePicker = function () {
+        scope.sizeRangePickerFrom = scope.dateRangePickerFrom = scope.from = undefined;
+        scope.sizeRangePickerTo = scope.dateRangePickerTo = scope.to = undefined;
+        scope.sizeRangePickerFromUnit = scope.units[0];
+        scope.sizeRangePickerToUnit = scope.units[0];
+      };
+
+      scope.submitRangePicker = function () {
+        if (scope.type === 'date') {
+          if (scope.dateRangePickerFrom !== undefined) {
+            scope.from = new Date(scope.dateRangePickerFrom).getTime();
+          }
+          if (scope.dateRangePickerTo !== undefined) {
+            scope.to = new Date(scope.dateRangePickerTo).getTime();
+          }
+        }
+        if (scope.type === 'size') {
+          if (scope.sizeRangePickerFrom !== undefined) {
+            scope.from = parseInt(scope.sizeRangePickerFrom) * scope.sizeRangePickerFromUnit.value;
+          }
+          if (scope.sizeRangePickerTo !== undefined) {
+            scope.to = parseInt(scope.sizeRangePickerTo) * scope.sizeRangePickerToUnit.value;
+          }
+        }
       };
     }
   };
