faf3e03d316c4e2f5c4270cf76cd0802e790ed06
Heather Anderson <handerson@artefactual.com>
2014-04-30 14:58:50 -0700

    Add HTML for reports tables

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/ReportsBrowserCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/ReportsBrowserCtrl.js
@@ -1,5 +1,10 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function ($scope) {
 
+  // Support Reports overview toggling
+  $scope.showOverview = true;
+  $scope.toggleOverview = function () {
+    $scope.showOverview = !$scope.showOverview;
+  };
 };
