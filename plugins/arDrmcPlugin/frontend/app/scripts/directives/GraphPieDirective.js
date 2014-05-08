672a08633f043dcb6eb95bcd22596c33821c385e
Heather Anderson <handerson@artefactual.com>
2014-05-07 17:30:29 -0700

    Directive to use real API data

--- plugins/arDrmcPlugin/frontend/app/scripts/directives/GraphPieDirective.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/directives/GraphPieDirective.js
@@ -1,76 +1,89 @@
 'use strict';
 
-var myd3 = require('d3');
+var arD3 = require('d3');
 
 module.exports = function () {
   return {
     restrict: 'E',
-    replace: true,
     scope: {
     },
-    template: '<div><rs-y-axis></rs-y-axis><rs-chart></rs-chart><rs-x-axis></rs-x-axis><rs-legend></rs-legend></div>',
+    template: '<rs-y-axis></rs-y-axis><rs-chart></rs-chart><rs-x-axis></rs-x-axis><rs-legend></rs-legend>',
     link: function (scope, element, attrs) {
       // var datum = angular.fromJson(attrs.chartData);
       attrs.$observe('data', function (newAttrs) {
 
-        if (newAttrs) {
-          var datum = newAttrs;
+        if (newAttrs && attrs.width) {
+          //Width and height
+          var w = attrs.width;
+          var h = attrs.height;
 
-          // ---------------------------
-          // from storage codec directive
-          // ---------------------------
+          newAttrs = JSON.parse(newAttrs);
 
-          var w = attrs.width,
-            h = attrs.height,
-            r = w / 2 * 0.8,
-            //inner =  w * 0.1,
-            color = myd3.scale.category20c();
+          var dataset = newAttrs;
 
-          var svg = myd3.select(element[0])
-            .append('svg:svg')
-            .data([datum])
-            .style ('stroke', 'white')
+          var outerRadius = w / 2;
+          var innerRadius = 0;
+          var arc = arD3.svg.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(outerRadius);
+
+          var pie = arD3.layout.pie()
+            .value(function (d) {
+              return d.count;
+            });
+
+          //Easy colors accessible via a 10-step ordinal scale
+          var color = arD3.scale.category10();
+
+          //Create SVG element
+          var svg = arD3.select(element[0])
+            .append('svg')
             .attr('width', w)
-            .attr('height', h)
-            .append('svg:g')
-            .attr('transform', 'translate(' + r * 1.1 + ',' + r * 1.1 + ')');
+            .attr('height', h);
 
-          var arc = myd3.svg.arc()
-            //.innerRadius(inner)
-            .outerRadius(r);
-
-          var pie = myd3.layout.pie()
-            .value(function (d) { return d; });
-
-
-          var arcs = svg.selectAll('g.slice')
-            .data(pie)
+          //Set up groups
+          var arcs = svg.selectAll('g.arc')
+            .data(pie(dataset))
             .enter()
-            .append('svg:g')
-            .attr('class', 'slice');
+            .append('g')
+            .attr('class', 'arc')
+            .attr('transform', 'translate(' + outerRadius + ',' + outerRadius + ')');
 
-          arcs.append('svg:path')
-              .attr('fill', function (d, i) { return color(i);})
-              .attr('d', arc);
+          //Draw arc paths
+          arcs.append('path')
+            .attr('fill', function (d, i) {
+              return color(i);
+            })
+            .attr('d', arc);
 
+          //Labels
+          arcs.append('text')
+            .attr('transform', function (d) {
+              return 'translate(' + arc.centroid(d) + ')';
+            })
+            .attr('text-anchor', 'middle')
+            .text(function (d) {
+              return d.data.media_type;
+            });
+
+          // Legend
           var label_width = Number.MIN_VALUE;
 
-          for (var i = 0; i < datum.length; i++) {
-            label_width = Math.max(label_width, datum[i].label.length);
-            console.log(datum[i]);
+          for (var i = 0; i < dataset.length; i++) {
+            label_width = Math.max(label_width, dataset[i].media_type.length);
           }
           // round up to 10th
           label_width = label_width * 8;
-          //var label_height = datum.length * 18;
+          var label_height = dataset.length * 20;
 
-          var legend = myd3.select(element[0]).append('svg')
+          var legend = svg.append('svg')
             .attr('class', 'legend')
-            .attr('width', w)
-            .attr('height', h)
+            .attr('width', label_width)
+            .attr('height', label_height)
             .selectAll('g')
             .data(color.domain().slice().reverse())
             .enter().append('g')
-            .attr('transform', function (d, i) { return 'translate(0,' + i * 18 + ')';});
+            .attr('transform', function (d, i) { return 'translate(0,' + i * 20 + ')'; });
 
           legend.append('rect')
             .attr('width', 18)
@@ -82,13 +95,11 @@ module.exports = function () {
             .attr('y', 9)
             .attr('dy', '.35em')
             .text(function (d) {
-              var label = datum[d].label;
+              var label = dataset[d].media_type;
               return label;
-            }
-          );
+            });
         }
       });
-
     }
   };
 };
