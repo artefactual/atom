765c25fe7b4c7cb439571b5374ea08e4e3650332
Heather Anderson <handerson@artefactual.com>
2014-05-07 17:33:41 -0700

    Small style changes; bring data to ctrl

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/DashboardCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/DashboardCtrl.js
@@ -2,8 +2,7 @@
 
 module.exports = function ($scope, $q, StatisticsService) {
 
-  $scope.dashboardGraphs = {};
-  $scope.dashboardGraphs.radioModel = 'number';
+  $scope.radioModel = 'size';
   /**
    * Run queries parallely
    * Alphabetized by name
@@ -13,18 +12,22 @@ module.exports = function ($scope, $q, StatisticsService) {
     //var artworkByMonthSummary = StatisticsService.getArtworkByMonthSummary();
     var downloadActivity = StatisticsService.getDownloadActivity();
     var ingestionActivity = StatisticsService.getIngestionActivity();
-    //var ingestionSummary = StatisticsService.getIngestionSummary();
-    var storageCodec = StatisticsService.getRunningTotalByFormats();
+    var ingestionSummary = StatisticsService.getIngestionSummary();
+    var storageCodec = StatisticsService.getRunningTotalByCodec();
+    var storageFormats = StatisticsService.getRunningTotalByFormats();
 
     $scope.responses = {};
-    $q.all([downloadActivity, ingestionActivity, storageCodec]).then(function (responses) {
+    $q.all([downloadActivity, ingestionActivity, ingestionSummary, storageCodec, storageFormats]).then(function (responses) {
       $scope.responses.downloadActivity = responses[0].data.results;
       $scope.responses.ingestionActivity = responses[1].data.results;
-      $scope.responses.storageCodec = responses[2].data.results;
+      $scope.responses.ingestionSummary = responses[2].data.results;
+      $scope.responses.storageCodec = responses[3].data.results;
+      $scope.responses.storageFormats = responses[4].data.results;
     });
 
   };
   pull();
+  console.log('scope in ctrl at end', $scope);
   // TODO: DashboardIngestionCtrl and DashboardRecentActivityCtrl... unused now!
 
 };
