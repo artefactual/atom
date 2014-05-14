2a3d82121812d36a32ed87b419879c07ba9859bc
Jesús García Crespo <jesus@sevein.com>
2014-05-14 15:10:18 -0700

    Remove unused directives

--- plugins/arDrmcPlugin/frontend/app/scripts/directives/index.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/directives/index.js
@@ -5,17 +5,17 @@
   var angular = require('angular');
 
   module.exports = angular.module('momaApp.directives', [])
+
     .directive('arContextBrowserArtwork', require('./ContextBrowserArtworkDirective'))
     .directive('arContextBrowserTechnology', require('./ContextBrowserTechnologyDirective'))
     .directive('arSortHeader', require('./SortHeaderDirective'))
     .directive('arDigitalObjectPreview', require('./DigitalObjectPreviewDirective'))
     .directive('arFacet', require('./FacetDirective'))
     .directive('arPager', require('./PagerDirective'))
-    .directive('arMultiselect', require('./MultiselectDirective'))
-    .directive('arMultiselectPopup', require('./MultiselectPopupDirective'))
     .directive('arToggleSlide', require('./ToggleSlideDirective'))
     .directive('arSearchBox', require('./SearchBoxDirective'))
     .directive('arRangeFacet', require('./RangeFacetDirective'))
     .directive('arGraphLine', require('./GraphLineDirective'))
     .directive('arGraphPie', require('./GraphPieDirective'));
+
 })();
