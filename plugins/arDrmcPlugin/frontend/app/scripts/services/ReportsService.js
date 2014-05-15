4eaeffcffc73f3e061cf907b291ddfbd92872f3e
Heather Anderson <handerson@artefactual.com>
2014-05-15 14:33:50 -0700

    Replace dummy data with less dummier data

--- plugins/arDrmcPlugin/frontend/app/scripts/services/ReportsService.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/services/ReportsService.js
@@ -3,11 +3,59 @@
 module.exports = function ($q, $timeout) {
 
   var resolution = {
-    'report1': 'report1Stuff',
-    'report2': 'report2Stuff',
-    'report3': 'report3Stuff',
-    'report4': 'report4Stuff',
-    'report5': 'report5Stuff'
+    'mockData':
+    [
+      {
+        'name': 'Media and Performance Art',
+        'results': [
+          {
+            'The user': 'Ben',
+            'aips_downloaded': 1,
+            'files_downloaded': 4,
+            'Total_filesize': 361707,
+            'parent_artwork': {
+              'Id': 12345,
+              'name': 'Semiotics of the Kitchen'
+            }
+          },
+          {
+            'user': 'Kate',
+            'aips_downloaded': 2,
+            'files_downloaded': 9,
+            'Total_filesize': 524000,
+            'parent_artwork': {
+              'id': 345,
+              'Name': 'Official Welcome'
+            }
+          }
+        ]
+      },
+      {
+        'name': 'Architecture and Design ',
+        'results': [
+          {
+            'The user': 'Ben',
+            'aips_downloaded': 1,
+            'files_downloaded': 4,
+            'Total_filesize': 550283,
+            'parent_artwork': {
+              'Id': 3445,
+              'Name': 'SimCity 2000'
+            }
+          },
+          {
+            'user': 'Kate',
+            'aips_downloaded': 10,
+            'files_downloaded': 59,
+            'Total_filesize': 54353,
+            'parent_artwork': {
+              'id': 234,
+              'Name': 'Space Invaders'
+            }
+          }
+        ]
+      }
+    ]
   };
 
   this.asyncReportData = function () {
