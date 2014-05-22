ef7612dc1bf8ea353a9f2e600d077a1a87989c1b
Heather Anderson <handerson@artefactual.com>
2014-05-21 17:25:16 -0700

    Cleanup whitespacing

--- plugins/arDrmcPlugin/frontend/app/scripts/services/FixityReportService.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/services/FixityReportService.js
@@ -3,13 +3,10 @@
 module.exports = function (SETTINGS, $http) {
 
   this.getFixityStatus = function (uuid) {
-
     return $http({
       method: 'GET',
       url: SETTINGS.frontendPath + 'api/fixityreports/' + uuid
     });
   };
-
-
 
 };
