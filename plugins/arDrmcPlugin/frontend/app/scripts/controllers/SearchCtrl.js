5f81cd6f2b268b3a5d4f5bc5314c2f117b4c3fbd
Jesús García Crespo <jesus@sevein.com>
2014-05-27 15:13:02 -0700

    Better fit of searches in the loop

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/SearchCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/SearchCtrl.js
@@ -5,32 +5,11 @@
  * state with other controllers via SearchService.
  */
 module.exports = function ($scope, $stateParams, SearchService, $filter, ModalSaveSearchService, SETTINGS) {
+
   $scope.criteria = {};
   $scope.criteria.limit = 10;
   $scope.page = 1; // Don't delete this, it's an important default for the loop
   $scope.selectedEntity = $stateParams.entity;
-
-  // If coming with slug: load template, obtain search, load values and make search
-  if ($stateParams.slug !== undefined) {
-    SearchService.getSearchBySlug($stateParams.slug).then(function (response) {
-      $scope.name = response.name;
-      $stateParams.entity = $scope.selectedEntity = response.type;
-      $scope.criteria = response.criteria;
-      $scope.include = SETTINGS.viewsPath + '/' + response.type + '.search.html';
-      SearchService.setQuery(response.criteria.query);
-      $scope.search();
-    }, function (response) {
-      throw response;
-    });
-  }
-
-  // Remove query when leaving loaded search
-  $scope.$on('$locationChangeSuccess', function (event, next, current) {
-    if (current.indexOf('/search/saved/') !== -1) {
-      $scope.criteria.query = undefined;
-      SearchService.setQuery($scope.criteria.query);
-    }
-  });
 
   // Reference to types of searches
   $scope.tabs = SearchService.searches;
@@ -49,10 +28,15 @@ module.exports = function ($scope, $stateParams, SearchService, $filter, ModalSa
 
   // Watch for criteria changes
   $scope.$watch('criteria', function () {
-    $scope.search();
-  }, true); // check properties when watching
+    // Saved search? If we have its permalink...
+    if (angular.isDefined($stateParams.slug)) {
+      savedSearch();
+    } else {
+      search();
+    }
+  }, true);
 
-  $scope.search = function () {
+  var search = function () {
     SearchService.search($stateParams.entity, $scope.criteria)
       .then(function (response) {
         $scope.data = response.data;
@@ -62,6 +46,50 @@ module.exports = function ($scope, $stateParams, SearchService, $filter, ModalSa
         delete $scope.data;
       });
   };
+
+  var savedSearch = function () {
+    SearchService.getSearchBySlug($stateParams.slug).then(function (response) {
+      $scope.name = response.name;
+      console.log($scope.name);
+      $stateParams.entity = $scope.selectedEntity = response.type;
+      $scope.criteria = response.criteria;
+      $scope.include = SETTINGS.viewsPath + '/' + response.type + '.search.html';
+      SearchService.setQuery(response.criteria.query);
+      search();
+    }, function (response) {
+      throw response;
+    });
+  };
+
+  $scope.unsetQuery = function () {
+    $scope.criteria.query = undefined;
+    SearchService.setQuery($scope.criteria.query, $stateParams.entity);
+    search();
+  };
+
+  // Support overview toggling (AIPs and searches)
+  $scope.showOverview = true;
+  $scope.toggleOverview = function () {
+    $scope.showOverview = !$scope.showOverview;
+  };
+
+  // Saved search modal
+  $scope.openSaveSearchModal = function (criteria) {
+    ModalSaveSearchService.create(criteria, $scope.selectedEntity);
+  };
+
+  // Remove query when leaving loaded search
+  $scope.$on('$locationChangeSuccess', function (event, next, current) {
+    if (current.indexOf('/search/saved/') !== -1) {
+      $scope.criteria.query = undefined;
+      SearchService.setQuery($scope.criteria.query);
+    }
+  });
+
+
+  /**
+   * Facets
+   */
 
   $scope.getTermLabel = function (facet, id) {
     if (undefined === $scope.data) {
@@ -92,22 +120,6 @@ module.exports = function ($scope, $stateParams, SearchService, $filter, ModalSa
         return $scope.data.facets[facet].ranges[range].label;
       }
     }
-  };
-
-  $scope.unsetQuery = function () {
-    $scope.criteria.query = undefined;
-    SearchService.setQuery($scope.criteria.query, $stateParams.entity);
-    $scope.search();
-  };
-
-  $scope.openSaveSearchModal = function (criteria) {
-    ModalSaveSearchService.create(criteria, $scope.selectedEntity);
-  };
-
-  // Support overview toggling (AIPs and searches)
-  $scope.showOverview = true;
-  $scope.toggleOverview = function () {
-    $scope.showOverview = !$scope.showOverview;
   };
 
 };
