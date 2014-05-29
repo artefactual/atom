b01d0a07595aa580fa21a75671f3a8a9f06a5320
Heather Anderson <handerson@artefactual.com>
2014-05-29 15:58:46 -0700

    Remote DateStampFilter; set to ng 'short' date filter

--- plugins/arDrmcPlugin/frontend/app/scripts/filters/index.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/filters/index.js
@@ -7,7 +7,6 @@
   module.exports = angular.module('momaApp.filters', [])
     .filter('EmptyFilter', require('./EmptyFilter'))
     .filter('ConvertSecondsFilter', require('./ConvertSecondsFilter'))
-    .filter('DateStampFilter', require('./DateStampFilter'))
     .filter('UnitFilter', require('./UnitFilter'));
 
 })();
