b2605f60d798dcfb0c76c0296f4fa6ec50d95ce5
Heather Anderson <handerson@artefactual.com>
2014-06-04 12:30:47 -0700

    Add form directive and HTML fixes to generate report modal

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/GenerateReportCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/GenerateReportCtrl.js
@@ -2,9 +2,15 @@
 
 module.exports = function ($scope, $modalInstance, ReportsService) {
 
+  // HACK: form scoping issue within modals, see
+  // - http://stackoverflow.com/a/19931221/2628967
+  // - https://github.com/angular-ui/bootstrap/issues/969
+  $scope.modalContainer = {};
+
   // Create object to define and set defaults
   $scope.criteria = {};
-  $scope.criteria.range = 'all';
+  $scope.criteria.range = {};
+  $scope.dateRange = 'all';
 
   $scope.reportTypes = [
     {
@@ -43,6 +49,9 @@ module.exports = function ($scope, $modalInstance, ReportsService) {
 
   // Checks for valid dates and generate report (with or without save)
   $scope.generateReport = function (criteria) {
+    if ($scope.dateRange === 'all') {
+      $scope.criteria.range = {};
+    }
     ReportsService.generateReport(criteria).success(function (response) {
       $modalInstance.close(response);
     });
@@ -51,8 +60,9 @@ module.exports = function ($scope, $modalInstance, ReportsService) {
   // Reset all fields to empty
   $scope.resetFields = function () {
     $scope.criteria.reportType = {};
-    $scope.criteria.range.value = {};
-    $scope.radioOpt = 'all';
+    $scope.criteria.range = 'all';
+    $scope.criteria.savedName = {};
+    $scope.criteria.savedDescription = {};
   };
 
   $scope.cancel = function () {
