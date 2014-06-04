468830e379c9f83b8d4900af77502fb3a9a752ca
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-06-04 20:04:00 +0200

    Cosmetic changes

--- plugins/arDrmcPlugin/frontend/app/scripts/filters/ConvertSecondsFilter.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/filters/ConvertSecondsFilter.js
@@ -5,28 +5,29 @@ module.exports = function () {
   return function (time) {
 
     if (!Number(time)) {
-      // Not available
       return;
-    } else {
-      // Calculate
-      var days = Math.floor(time / 86400);
-      time -= days * 86400;
-      var hours = Math.floor(time / 3600);
-      time -= hours * 3600;
-      var minutes = Math.floor(time / 60);
-      time -= minutes * 60;
-      var seconds = time;
-      // Return
-      if (days > 0) {
-        return days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
-      } else if (hours > 0) {
-        return hours + 'h ' + minutes + 'm ' + seconds + 's';
-      } else if (minutes > 0) {
-        return minutes + 'm ' + seconds + 's';
-      } else {
-        return seconds + 's';
-      }
     }
+
+    // Calculate
+    var days = Math.floor(time / 86400);
+    time -= days * 86400;
+    var hours = Math.floor(time / 3600);
+    time -= hours * 3600;
+    var minutes = Math.floor(time / 60);
+    time -= minutes * 60;
+    var seconds = time;
+
+    // Return
+    if (days > 0) {
+      return days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's';
+    } else if (hours > 0) {
+      return hours + 'h ' + minutes + 'm ' + seconds + 's';
+    } else if (minutes > 0) {
+      return minutes + 'm ' + seconds + 's';
+    } else {
+      return seconds + 's';
+    }
+
   };
 
 };
