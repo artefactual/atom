d2998ad443b3f1039adeda46b4db373d3d5ed96b
Heather Anderson <handerson@artefactual.com>
2014-06-05 17:40:12 -0700

    Tweak controller and view to compensate for new data entry point

--- plugins/arDrmcPlugin/frontend/app/scripts/controllers/ReportsViewCtrl.js
+++ plugins/arDrmcPlugin/frontend/app/scripts/controllers/ReportsViewCtrl.js
@@ -1,21 +1,20 @@
 'use strict';
 
-module.exports = function ($scope, $stateParams, ReportsService) {
+module.exports = function ($scope, $stateParams, ReportsService, routeData) {
 
   ReportsService.reportsViewData().then(function (data) {
     $scope.viewData = data;
   });
 
-  ReportsService.getData($stateParams.type).then(function (response) {
-    $scope.reportId = $stateParams.id;
-
-    if ($stateParams.type === 'high_level_ingest') {
-      $scope.highLevelIngest = response.data;
-      $scope.reportMeta = {
-        title: 'High Level Ingest',
-        category: 'Activity Report'
-      };
-    }
-  });
+  // Data comes to view before view is loaded through routing.js
+  // TODO: Check against data report types
+  // Maybe better to do in service once endpoint returns report type
+  if (angular.isDefined(routeData)) {
+    $scope.highLevelIngest = routeData.data;
+    $scope.reportMeta = {
+      title: 'High Level Ingest',
+      category: 'Activity Report'
+    };
+  }
 
 };
