651edcd3d25c8cf05bb16c0286c64ca9d0d6540e
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-06-06 19:22:17 +0200

    Add save and delete methods in QubitSavedQuery

--- lib/model/QubitSavedQuery.php
+++ lib/model/QubitSavedQuery.php
@@ -2,9 +2,9 @@
 
 
 /**
- * Skeleton subclass for representing a row from the 'saved_query' table.
+ * Skeleton subclass for representing a row from the 'drmc_query' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -12,6 +12,27 @@
  *
  * @package    lib.model
  */
-class QubitSavedQuery extends BaseSavedQuery {
+class QubitSavedQuery extends BaseSavedQuery
+{
+  /**
+   * Additional save functionality (e.g. update search index)
+   *
+   * @param mixed $connection a database connection object
+   * @return QubitDrmcQuery self-reference
+   */
+  public function save($connection = null)
+  {
+    parent::save($connection);
 
-} // QubitSavedQuery
+    QubitSearch::getInstance()->update($this);
+
+    return $this;
+  }
+
+  public function delete($connection = null)
+  {
+    QubitSearch::getInstance()->delete($this);
+
+    parent::delete($connection);
+  }
+}
