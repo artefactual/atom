64c7e837a0cd86f78bf8cfebe6bc26a78fd4b589
Jesús García Crespo <jesus@sevein.com>
2014-07-24 20:51:52 -0700

    Handle cached sessions and secure cookies

--- lib/QubitCacheSessionStorage.class.php
+++ lib/QubitCacheSessionStorage.class.php
@@ -17,7 +17,7 @@
  * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class QubitSessionStorage extends sfSessionStorage
+class QubitCacheSessionStorage extends sfCacheSessionStorage
 {
   public function initialize($options = null)
   {
