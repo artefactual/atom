4ad96c8f16aba048e56a9c7960883e57cb2cd1d6
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-08-12 19:24:07 +0200

    Changes in files endpoint
    Get mime type from DO and check filename using strpos()

--- plugins/arRestApiPlugin/modules/api/actions/informationobjectsFilesAction.class.php
+++ plugins/arRestApiPlugin/modules/api/actions/informationobjectsFilesAction.class.php
@@ -90,12 +90,12 @@ class ApiInformationObjectsFilesAction extends QubitApiAction
       $this->addItemToArray($item, 'aip_title', $doc['aipName']);
       $this->addItemToArray($item, 'original_relative_path_within_aip', $doc['originalRelativePathWithinAip']);
 
-      // Add fixed mime_type to AIP METS file and get size
+      // Add mime_type and size to AIP METS file
       // from digital object instead of metsData. See #7002
       if (isset($item['filename']) && isset($item['aip_uuid'])
-       && $item['filename'] == 'METS.'.$item['aip_uuid'].'.xml')
+       && false !== strpos($item['filename'], $item['aip_uuid']))
       {
-        $this->addItemToArray($item, 'mime_type', 'application/xml');
+        $this->addItemToArray($item, 'mime_type', $doc['digitalObject']['mimeType']);
         $this->addItemToArray($item, 'byte_size', $doc['digitalObject']['byteSize']);
       }
 
