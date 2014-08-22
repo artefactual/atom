a9b8db07928cc1a17279f94865902ef918f8e895
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-08-22 15:06:30 +0200

    Add links to other cultures in info obj templates
    Added in index pages of ISAD, RAD, MODS, DC and DACS plugins
    
    Fixes #7122

---
+++ apps/qubit/modules/informationobject/templates/_translationsLinks.php
@@ -0,0 +1,13 @@
+<?php if (count($translations) > 0): ?>
+  <div id="translations-links">
+    <?php echo __('Other languages available:') ?>
+    <ul>
+      <?php foreach ($translations as $culture => $title): ?>
+        <li>
+          <?php echo '['.$culture.']' ?>
+          <?php echo link_to($title, array($resource, 'module' => 'informationobject', 'sf_culture' => $culture)) ?>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+<?php endif; ?>
