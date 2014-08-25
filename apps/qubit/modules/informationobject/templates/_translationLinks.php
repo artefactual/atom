4dfc366c7eb8a054d1650978cad9d88b05f3319c
José Raddaoui Marín <raddaouimarin@gmail.com>
2014-08-25 13:22:52 +0200

    Implement translationLinks as a component

--- apps/qubit/modules/informationobject/templates/_translationLinks.php
+++ apps/qubit/modules/informationobject/templates/_translationLinks.php
@@ -1,13 +1,11 @@
-<?php if (count($translations) > 0): ?>
-  <div class="translation-links">
-    <?php echo __('Other languages available:') ?>
-    <ul>
-      <?php foreach ($translations as $culture => $title): ?>
-        <li>
-          <?php echo '['.$culture.']' ?>
-          <?php echo link_to($title, array($resource, 'module' => 'informationobject', 'sf_culture' => $culture)) ?>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
-<?php endif; ?>
+<div class="translation-links">
+  <?php echo __('Other languages available:') ?>
+  <ul>
+    <?php foreach ($translations as $culture => $title): ?>
+      <li>
+        <?php echo '['.$culture.']' ?>
+        <?php echo link_to($title, array($resource, 'module' => 'informationobject', 'sf_culture' => $culture)) ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</div>
