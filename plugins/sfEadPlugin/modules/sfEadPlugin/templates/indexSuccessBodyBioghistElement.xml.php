bbee166ce7165bcb8dab01c0cb834d18a74bbc2e
Misty De Meo <mdemeo@artefactual.com>
2014-09-26 17:33:21 -0700

    EAD export: bioghist - banish <chronlist>, refs #5094

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
@@ -7,39 +7,29 @@
   <?php foreach($events as $date): ?>
     <?php $creator = QubitActor::getById($date->actorId); ?>
     <bioghist id="<?php echo 'md5-' . md5(url_for(array($creator, 'module' => 'actor'), true)) ?>" encodinganalog="<?php echo $ead->getMetadataParameter('bioghist') ?>">
-      <chronlist>
-        <chronitem>
-          <?php echo $ead->renderEadDateFromEvent('creation', $date) ?>
-          <eventgrp>
-            <event>
-              <?php if ($value = $date->getDescription(array('cultureFallback' => true))): ?>
-                <note type="eventNote"><p><?php echo escape_dc(esc_specialchars($value)) ?></p></note>
-              <?php endif; ?>
-              <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
-                <note><p><?php echo escape_dc(esc_specialchars($value)) ?></p></note>
-              <?php endif; ?>
-              <origination encodinganalog="<?php echo $ead->getMetadataParameter('origination') ?>">
-                <?php if ($type = $creator->getEntityTypeId()): ?>
-                  <?php if (QubitTerm::PERSON_ID == $type): ?>
-                    <persname><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></persname>
-                  <?php endif; ?>
-                  <?php if (QubitTerm::FAMILY_ID == $type): ?>
-                    <famname><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></famname>
-                  <?php endif; ?>
-                  <?php if (QubitTerm::CORPORATE_BODY_ID == $type): ?>
-                    <corpname><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></corpname>
-                  <?php endif; ?>
-                <?php else: ?>
-                  <name><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></name>
-                <?php endif; ?>
-              </origination>
-              <?php if ($creator->datesOfExistence): ?>
-                <date type="existence"><?php echo escape_dc(esc_specialchars($creator->datesOfExistence)) ?></date>
-              <?php endif; ?>
-            </event>
-          </eventgrp>
-        </chronitem>
-      </chronlist>
+      <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
+        <note><p><?php echo escape_dc(esc_specialchars($value)) ?></p></note>
+      <?php endif; ?>
+      <?php if ($creator->datesOfExistence): ?>
+        <date type="existence"><?php echo escape_dc(esc_specialchars($creator->datesOfExistence)) ?></date>
+      <?php endif; ?>
     </bioghist>
+
+    <origination encodinganalog="<?php echo $ead->getMetadataParameter('origination') ?>">
+      <?php if ($type = $creator->getEntityTypeId()): ?>
+        <?php if (QubitTerm::PERSON_ID == $type): ?>
+          <persname><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></persname>
+        <?php endif; ?>
+        <?php if (QubitTerm::FAMILY_ID == $type): ?>
+          <famname><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></famname>
+        <?php endif; ?>
+        <?php if (QubitTerm::CORPORATE_BODY_ID == $type): ?>
+          <corpname><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></corpname>
+        <?php endif; ?>
+      <?php else: ?>
+        <name><?php echo escape_dc(esc_specialchars($creator->getAuthorizedFormOfName(array('cultureFallback' => true)))) ?></name>
+      <?php endif; ?>
+    </origination>
+
   <?php endforeach; ?>
 <?php endif; ?>
