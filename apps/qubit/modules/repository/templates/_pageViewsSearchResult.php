69369ec82e4e6507874ce45bd0bf1dd68478887b
Mike Cantelon <mcantelon@gmail.com>
2014-11-11 20:57:28 -0800

    Optimized page view counter admin page.
    Used query from David Juhasz to speed up page view counter admin page.

--- apps/qubit/modules/repository/templates/_pageViewsSearchResult.php
+++ apps/qubit/modules/repository/templates/_pageViewsSearchResult.php
@@ -1,21 +1,29 @@
+<?php $resource = $resources[$hit->object_id] ?>
+
 <tr>
   <td>
     <?php echo $rank ?>
   </td>
   <td>
-    <?php echo $referenceCodes[($hit['ID'])] ?>
+    <?php echo $resource->referenceCode ?>
   </td>
   <td>
-    <p class="title"><?php echo link_to($hit['TITLE'], array('slug' => $hit['SLUG'])) ?></p>
+    <p class="title"><?php echo link_to($resource->title, array('slug' => $resource->slug)) ?></p>
   </td>
   <td>
-    <?php if ($hit['PARENT_ID'] > 1): ?>
-      <?php $parent = $parents[($hit['ID'])] ?>
-      <?php $linkText = sprintf('%s (%s)', $parent->title, $parent->identifier) ?>
+    <?php if ($hit->bot_parent_id >  QubitInformationObject::ROOT_ID): ?>
+      <?php $parent = $parents[($hit->object_id)] ?>
+
+      <?php if (!empty($parent->identifier)): ?>
+        <?php $linkText = sprintf('%s (%s)', $parent->title, $parent->identifier) ?>
+      <?php else: ?>
+        <?php $linkText = $parent->title ?>
+      <?php endif ?>
+
       <p class="title"><?php echo link_to($linkText, array('slug' => $parent->slug)) ?></p>
     <?php endif; ?>
   </td>
   <td>
-    <?php echo $hit['hits'] ?>
+    <?php echo $hit->visits ?>
   </td>
 </tr>
