ec6584d997dd93f5d29b71cdc3dd55de6c24dd0d
Mike Cantelon <mcantelon@gmail.com>
2014-11-28 15:30:32 -0800

    Include about tag in OAI list output. refs #7468
    Created a separate template for the about tag and made it so
    the about tag is included in OAI-PHP output for the listRecords
    verb (as well as getRecord).

---
+++ plugins/arOaiPlugin/modules/arOaiPlugin/templates/_about.xml.php
@@ -0,0 +1,18 @@
+      <?php if (count($record->digitalObjects)): ?>
+        <about>
+          <feed xmlns="http://www.w3.org/2005/Atom">
+            <?php foreach($record->digitalObjects as $digitalObject): ?>
+              <?php if ($digitalObject->usageId == QubitTerm::MASTER_ID && QubitAcl::check($record, 'readMaster')): ?>
+                <?php $digitalObjectUrl = (string)QubitSetting::getByName('siteBaseUrl') . $digitalObject->path . $digitalObject->name ?>
+                <?php echo include_partial('getRecordAtomFeedEntry', array('object' => $digitalObject, 'url' => $digitalObjectUrl, 'usage' => 'master')) ?>
+              <?php elseif($digitalObject->usageId == QubitTerm::EXTERNAL_URI_ID): ?>
+                <?php echo include_partial('getRecordAtomFeedEntry', array('object' => $digitalObject, 'url' => $digitalObject->path, 'usage' => 'external')) ?>
+              <?php endif; ?>
+
+              <?php $thumbnail = $digitalObject->getChildByUsageId(QubitTerm::THUMBNAIL_ID) ?>
+              <?php $thumbnailUrl = (string)QubitSetting::getByName('siteBaseUrl') . $thumbnail->path . $thumbnail->name ?>
+              <?php echo include_partial('getRecordAtomFeedEntry', array('object' => $thumbnail, 'url' => $thumbnailUrl, 'usage' => 'thumbnail')) ?>
+            <?php endforeach; ?>
+          </feed>
+        </about>
+      <?php endif; ?>
--- plugins/arOaiPlugin/modules/arOaiPlugin/templates/_getRecord.xml.php
+++ plugins/arOaiPlugin/modules/arOaiPlugin/templates/_getRecord.xml.php
@@ -9,22 +9,8 @@
         <?php echo get_component('sfDcPlugin', 'dc', array('resource' => $informationObject)) ?>
       </metadata>
       <?php if (count($informationObject->digitalObjects)): ?>
-        <about>
-          <feed xmlns="http://www.w3.org/2005/Atom">
-            <?php foreach($informationObject->digitalObjects as $digitalObject): ?>
-              <?php if ($digitalObject->usageId == QubitTerm::MASTER_ID && QubitAcl::check($informationObject, 'readMaster')): ?>
-                <?php $digitalObjectUrl = (string)QubitSetting::getByName('siteBaseUrl') . $digitalObject->path . $digitalObject->name ?>
-                <?php echo include_partial('getRecordAtomFeedEntry', array('object' => $digitalObject, 'url' => $digitalObjectUrl, 'usage' => 'master')) ?>
-              <?php elseif($digitalObject->usageId == QubitTerm::EXTERNAL_URI_ID): ?>
-                <?php echo include_partial('getRecordAtomFeedEntry', array('object' => $digitalObject, 'url' => $digitalObject->path, 'usage' => 'external')) ?>
-              <?php endif; ?>
-
-              <?php $thumbnail = $digitalObject->getChildByUsageId(QubitTerm::THUMBNAIL_ID) ?>
-              <?php $thumbnailUrl = (string)QubitSetting::getByName('siteBaseUrl') . $thumbnail->path . $thumbnail->name ?>
-              <?php echo include_partial('getRecordAtomFeedEntry', array('object' => $thumbnail, 'url' => $thumbnailUrl, 'usage' => 'thumbnail')) ?>
-            <?php endforeach; ?>
-          </feed>
-        </about>
+        <?php $record = $informationObject ?>
+        <?php include('_about.xml.php') ?>  
       <?php endif; ?>
     </record>
   </GetRecord>
