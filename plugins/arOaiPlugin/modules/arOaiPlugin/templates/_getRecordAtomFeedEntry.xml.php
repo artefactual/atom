9d17be9cd6f14e0c6ab5d0b5d5384960d138a701
Mike Cantelon <mcantelon@gmail.com>
2014-11-28 16:37:44 -0800

    Replaced primary key with slug. refs #7468
    Replaced the use of a digital object's primary key with its
    slug.

--- plugins/arOaiPlugin/modules/arOaiPlugin/templates/_getRecordAtomFeedEntry.xml.php
+++ plugins/arOaiPlugin/modules/arOaiPlugin/templates/_getRecordAtomFeedEntry.xml.php
@@ -1,5 +1,5 @@
 <entry>
-  <id><?php echo $object->id ?></id>
+  <id><?php echo $object->slug ?></id>
   <title><?php echo $object->name ?></title>
   <link href="<?php echo $url ?>" rel="self"></link>
   <category term="<?php echo $usage ?>" label="<?php echo ucwords($usage) ?>"></category>
