f1d50f907d61d2a4032480be1568fcddad90d4c5
Jesús García Crespo <jesus@sevein.com>
2014-12-12 10:52:12 -0800

    Use empty() to avoid PHP notice

--- apps/qubit/modules/digitalobject/templates/_show.php
+++ apps/qubit/modules/digitalobject/templates/_show.php
@@ -1,6 +1,6 @@
 <div class="digital-object-reference">
-  <?php if ($refMessage): ?>
-    <?php echo $refMessage; ?>
+  <?php if (!empty($refMessage)): ?>
+    <?php echo $refMessage ?>
   <?php else: ?>
     <?php echo get_component('digitalobject', $showComponent, array('iconOnly' => $iconOnly, 'link' => $link, 'resource' => $resource, 'usageType' => $usageType)) ?>
   <?php endif; ?>
