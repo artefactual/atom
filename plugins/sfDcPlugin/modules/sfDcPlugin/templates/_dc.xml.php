dabf00a21d00779834c60c845b0936d742961eb8
Jesús García Crespo <jesus@sevein.com>
2014-12-12 11:06:10 -0800

    Fix DC plugin

--- plugins/sfDcPlugin/modules/sfDcPlugin/templates/_dc.xml.php
+++ plugins/sfDcPlugin/modules/sfDcPlugin/templates/_dc.xml.php
@@ -3,43 +3,43 @@
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
 
-  <title><?php echo esc_specialchars($resource->title) ?></title>
+  <title><?php echo $resource->title ?></title>
 
   <?php foreach ($resource->getCreators() as $item): ?>
-    <creator><?php echo esc_specialchars((string)$item) ?></creator>
+    <creator><?php echo $item ?></creator>
   <?php endforeach; ?>
 
   <?php foreach ($dc->subject as $item): ?>
-    <subject><?php echo esc_specialchars((string)$item) ?></subject>
+    <subject><?php echo $item ?></subject>
   <?php endforeach; ?>
 
-  <description><?php echo esc_specialchars($resource->scopeAndContent) ?></description>
+  <description><?php echo $resource->scopeAndContent ?></description>
 
   <?php foreach ($resource->getPublishers() as $item): ?>
-    <publisher><?php echo esc_specialchars((string)$item) ?></publisher>
+    <publisher><?php echo $item ?></publisher>
   <?php endforeach; ?>
 
   <?php foreach ($resource->getContributors() as $item): ?>
-    <contributor><?php echo esc_specialchars((string)$item) ?></contributor>
+    <contributor><?php echo $item ?></contributor>
   <?php endforeach; ?>
 
   <?php foreach ($dc->date as $item): ?>
-    <date><?php echo esc_specialchars((string)$item) ?></date>
+    <date><?php echo $item ?></date>
   <?php endforeach; ?>
 
   <?php foreach ($dc->type as $item): ?>
-    <type><?php echo esc_specialchars((string)$item) ?></type>
+    <type><?php echo $item ?></type>
   <?php endforeach; ?>
 
   <?php foreach ($dc->format as $item): ?>
-    <format><?php echo esc_specialchars((string)$item) ?></format>
+    <format><?php echo $item ?></format>
   <?php endforeach; ?>
 
   <identifier><?php echo url_for(array($resource, 'module' => 'informationobject'), true) ?></identifier>
 
-  <identifier><?php echo esc_specialchars($resource->identifier) ?></identifier>
+  <identifier><?php echo $resource->identifier ?></identifier>
 
-  <source><?php echo esc_specialchars($resource->locationOfOriginals) ?></source>
+  <source><?php echo $resource->locationOfOriginals ?></source>
 
   <?php foreach ($resource->language as $code): ?>
     <language xsi:type="dcterms:ISO639-3"><?php echo strtolower($iso639convertor->getID3($code)) ?></language>
@@ -47,13 +47,13 @@
 
   <?php if (isset($resource->repository)): ?>
     <relation><?php echo url_for(array($resource->repository, 'module' => 'repository'), true) ?></relation>
-    <relation><?php echo esc_specialchars($resource->repository->authorizedFormOfName) ?></relation>
+    <relation><?php echo $resource->repository->authorizedFormOfName ?></relation>
   <?php endif; ?>
 
   <?php foreach ($dc->coverage as $item): ?>
-    <coverage><?php echo esc_specialchars((string)$item) ?></coverage>
+    <coverage><?php echo $item ?></coverage>
   <?php endforeach; ?>
 
-  <rights><?php echo esc_specialchars($resource->accessConditions) ?></rights>
+  <rights><?php echo $resource->accessConditions ?></rights>
 
 </oai_dc:dc>
