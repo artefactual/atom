a073d795f2bbe39760caf15310ca4d7f7492698f
Jesús García Crespo <jesus@sevein.com>
2014-12-14 22:52:28 -0800

    Fix escaping in uploadLimitExceeded

--- apps/qubit/modules/repository/templates/uploadLimitExceededSuccess.php
+++ apps/qubit/modules/repository/templates/uploadLimitExceededSuccess.php
@@ -12,20 +12,12 @@
   <?php echo __('To upload a new %1%', array('%1%' => strtolower(sfConfig::get('app_ui_label_digitalobject')))) ?>
   <ul>
     <li><?php echo __('Email your <a href="mailto:%1%">system administrator</a> and request a larger upload limit', array('%1%' => QubitUser::getSystemAdmin()->email)) ?></li>
-    <li><?php echo __('Delete an <a href="%1%">existing %2%</a> to reduce disk usage', array(
-      '%1%' => url_for(array(null, 'module' => 'search', 'query' => '+repositorySlug:'.$resource->slug.' +hasDigitalObject:true')),
-      '%2%' => strtolower(sfConfig::get('app_ui_label_digitalobject')))) ?></li>
+    <li><?php echo __('Delete an existing %1% to reduce disk usage', array('%1%' => strtolower(sfConfig::get('app_ui_label_digitalobject')))) ?></li>
   </ul>
 </div>
 
-<div class="actions section">
-
-  <h2 class="element-invisible"><?php echo __('Actions') ?></h2>
-
-  <div class="content">
-    <ul class="clearfix links">
-      <li><a href="#" onClick="history.back(); return false;"><?php echo __('Back') ?></a></li>
-    </ul>
-  </div>
-
-</div>
+<section class="actions">
+  <ul>
+    <li><a class="c-btn" href="#" onClick="history.back(); return false;"><?php echo __('Back') ?></a></li>
+  </ul>
+</section>
