c74e11f3b12ed9505b34deff77ae84f470d28ca3
David Juhasz <djjuhasz@gmail.com>
2015-02-25 16:00:02 -0800

    Generic label for admin/bio history, refs #7948
    Squashed commit of the following:
    
    commit 7d09b007927e907f6395108e1ecadc1c91b61e40
    Author: David Juhasz <djjuhasz@gmail.com>
    Date:   Mon Feb 23 17:31:33 2015 -0800
    
        Fixes to match coding standard. Refs #7948
    
        * Use "null" not "NULL"
        * Avoid unnecessary trailing ";" in templates
    
    commit 343b883deef0a4b93b0ef834638d493b41230c49
    Author: David Juhasz <djjuhasz@gmail.com>
    Date:   Fri Feb 13 15:41:30 2015 -0800
    
        Show generic label for unknown entity. Refs #7948
    
    commit 9e354baa739ef95a38e9b3c8420301761e0b7c86
    Author: David Juhasz <djjuhasz@gmail.com>
    Date:   Mon Feb 9 14:39:38 2015 -0800
    
        Add generic history label for DACS. Refs #7947
    
    commit 62d1ebdeab633642d4697cc370e18ebe8b2b8118
    Author: David Juhasz <djjuhasz@gmail.com>
    Date:   Mon Feb 9 14:28:27 2015 -0800
    
        Add generic history label for RAD. Refs #7947
    
    commit 318fce0c9fa8b6e8280de3d7cf3838948d5206b3
    Author: David Juhasz <djjuhasz@gmail.com>
    Date:   Mon Feb 9 14:22:00 2015 -0800
    
        Add generic history label for ISAD(G). Refs #7947

--- apps/qubit/modules/informationobject/templates/_creatorDetail.php
+++ apps/qubit/modules/informationobject/templates/_creatorDetail.php
@@ -16,11 +16,11 @@
       <?php if (0 < count($resource->getCreators())): ?>
         <div class="field">
           <h3>
-            <?php if (QubitTerm::CORPORATE_BODY_ID == $item->entityTypeId): ?>
-              <?php echo __('Administrative history') ?>
-            <?php else: ?>
-              <?php echo __('Biographical history') ?>
-            <?php endif; ?>
+          <?php if (isset($creatorHistoryLabels[$item->entityTypeId])): ?>
+            <?php echo $creatorHistoryLabels[$item->entityTypeId] ?>
+          <?php else: ?>
+            <?php echo $creatorHistoryLabels[null] // Use generic label ?>
+          <?php endif; ?>
           </h3><div>
             <?php echo render_value($item->getHistory(array('cultureFallback' => true))) ?>
           </div>
