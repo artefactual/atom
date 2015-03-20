25fa69bdb7dd845e4b27a372d0976ea32b995d2f
José Raddaoui Marín <raddaouimarin@gmail.com>
2015-03-20 23:29:09 +0100

    Fix ACL check and redirect in RightDeleteAction
    Refs #7742

--- apps/qubit/modules/right/actions/deleteAction.class.php
+++ apps/qubit/modules/right/actions/deleteAction.class.php
@@ -22,15 +22,16 @@ class RightDeleteAction extends sfAction
   public function execute($request)
   {
     $this->right = $this->getRoute()->resource;
+    $relatedObject = $this->right->relationsRelatedByobjectId[0]->subject;
 
-    // Check user authorization
-    if (!QubitAcl::check($this->resource, 'delete'))
+    // Check user authorization against the related object
+    if (!QubitAcl::check($relatedObject, 'delete'))
     {
       QubitAcl::forwardUnauthorized();
     }
 
     $this->right->delete();
 
-    return $this->redirect(array($this->resource, 'module' => 'informationobject'));
+    return $this->redirect(array($relatedObject));
   }
 }
