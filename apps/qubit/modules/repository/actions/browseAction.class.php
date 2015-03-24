106029f6d57e3fa159eca34ff8e0284aee5e1412
José Raddaoui Marín <raddaouimarin@gmail.com>
2015-03-24 14:06:02 +0100

    Proper fix for region and locality repo facets
    Refs #7791

--- apps/qubit/modules/repository/actions/browseAction.class.php
+++ apps/qubit/modules/repository/actions/browseAction.class.php
@@ -40,7 +40,7 @@ class RepositoryBrowseAction extends DefaultBrowseAction
               'size' => 10),
       'regions' =>
         array('type' => 'term',
-              'field' => 'contactInformations.i18n.en.region',
+              'field' => 'contactInformations.i18n.en.region.untouched',
               'size' => 10),
       'geographicSubregions' =>
         array('type' => 'term',
@@ -48,7 +48,7 @@ class RepositoryBrowseAction extends DefaultBrowseAction
               'size' => 10),
       'locality' =>
         array('type' => 'term',
-              'field' => 'contactInformations.i18n.en.city',
+              'field' => 'contactInformations.i18n.en.city.untouched',
               'size' => 10),
       'thematicAreas' =>
         array('type' => 'term',
