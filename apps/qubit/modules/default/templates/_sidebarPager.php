c0f692583651d6ada4eba480435e5c5acbf32cfc
Jesús García Crespo <jesus@sevein.com>
2015-04-05 10:01:26 -0700

    Small layout tweaks

--- apps/qubit/modules/default/templates/_sidebarPager.php
+++ apps/qubit/modules/default/templates/_sidebarPager.php
@@ -3,16 +3,15 @@
   <section>
 
     <div class="pagination-centered sidebar-pager-results">
-      <?php echo __('Results <span id="result-start">%1%</span> to <span id="result-end">%2%</span> of %3%',
-            array('%1%' => $pager->getFirstIndice(), '%2%' => $pager->getLastIndice(), '%3%' => $pager->getNbResults())) ?>
-      <br>
-      <button class="previous btn">
-        <?php echo '&laquo; '.__('Previous') ?>
-      </button>
 
-      <button class="next btn">
-        <?php echo __('Next').' &raquo;'?>
-      </button>
+      <?php echo __('Results <span id="result-start">%1%</span> to <span id="result-end">%2%</span> of %3%',
+        array('%1%' => $pager->getFirstIndice(), '%2%' => $pager->getLastIndice(), '%3%' => $pager->getNbResults())) ?>
+
+      <br>
+
+      <button class="previous btn btn-small">&laquo;<?php echo __('Previous') ?></button>
+      <button class="next btn btn-small"><?php echo __('Next') ?>&raquo;</button>
+
     </div>
 
   </section>
