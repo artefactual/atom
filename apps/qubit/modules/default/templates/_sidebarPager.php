26cafb2232a4a98cab94a5881bca020ad6bb63aa
Jesús García Crespo <jesus@sevein.com>
2015-04-05 09:58:31 -0700

    Remove unneeded condition

--- apps/qubit/modules/default/templates/_sidebarPager.php
+++ apps/qubit/modules/default/templates/_sidebarPager.php
@@ -1,22 +1,20 @@
 <?php if ($pager->haveToPaginate()): ?>
 
   <section>
-    <?php if (0 < $pager->getNbResults()): ?>
 
-      <div class="pagination-centered sidebar-pager-results">
-        <?php echo __('Results <span id="result-start">%1%</span> to <span id="result-end">%2%</span> of %3%',
-              array('%1%' => $pager->getFirstIndice(), '%2%' => $pager->getLastIndice(), '%3%' => $pager->getNbResults())) ?>
-        <br>
-        <button class="previous btn">
-          <?php echo '&laquo; '.__('Previous') ?>
-        </button>
+    <div class="pagination-centered sidebar-pager-results">
+      <?php echo __('Results <span id="result-start">%1%</span> to <span id="result-end">%2%</span> of %3%',
+            array('%1%' => $pager->getFirstIndice(), '%2%' => $pager->getLastIndice(), '%3%' => $pager->getNbResults())) ?>
+      <br>
+      <button class="previous btn">
+        <?php echo '&laquo; '.__('Previous') ?>
+      </button>
 
-        <button class="next btn">
-          <?php echo __('Next').' &raquo;'?>
-        </button>
-      </div>
+      <button class="next btn">
+        <?php echo __('Next').' &raquo;'?>
+      </button>
+    </div>
 
-    <?php endif; ?>
   </section>
 
 <?php endif; ?>
