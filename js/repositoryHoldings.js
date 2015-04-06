e385b5a704eb9210cbe13d52898a9be75f1cbad0
Jesús García Crespo <jesus@sevein.com>
2015-04-06 10:14:55 -0700

    Spinner and busy state

--- js/repositoryHoldings.js
+++ js/repositoryHoldings.js
@@ -6,21 +6,27 @@
   {
     this.$element = element;
 
-    this.currentPage = 1;
     this.url = this.$element.data('url');
-    this.totalPages = this.$element.data('total-pages');
+
+    this.currentPage = 1;
+    this.totalPages = parseInt(this.$element.data('total-pages'), 10);
 
     // Stop execution when pagination is not needed
-    if (this.totalPages < 2) {
+    if (this.totalPages < 2)
+    {
       return;
     }
 
+    this.$prev = this.$element.find('.prev').hide();
     this.$next = this.$element.find('.next');
-    this.$prev = this.$element.find('.prev');
+
     this.$results = this.$element.find('ul');
-    this.$currentPage = this.$element.find('#holdings-page');
-    this.$resultStart = this.$element.find('#result-start');
-    this.$resultEnd = this.$element.find('#result-end');
+    this.$spinner = this.$element.find('#spinner');
+    this.$resultStart = this.$element.find('.result-start');
+    this.$resultEnd = this.$element.find('.result-end');
+
+    // Threshold for the spinning timer
+    this.THRESHOLD = 200;
 
     this.init();
   };
@@ -30,9 +36,8 @@
     constructor: holdingsView,
     init: function()
     {
-      this.$element
-        .on('mousedown', '.next', $.proxy(this.next, this))
-        .on('mousedown', '.previous', $.proxy(this.prev, this));
+      this.$next.on('mousedown', $.proxy(this.next, this));
+      this.$prev.on('mousedown', $.proxy(this.prev, this));
     },
 
     next: function (e)
@@ -45,10 +50,44 @@
       this.fetchResults(this.currentPage - 1);
     },
 
-    // Return next or previous page of results. fetchResults will return the next page
-    // if getNext is true, or the previous page if false.
+    // Setter/getter of busy state
+    busy: function (busy)
+    {
+      // Getter
+      if (typeof busy === 'undefined') {
+        return this._busy;
+      }
+
+      // Setter
+      this._busy = busy;
+      this.$next.toggleClass('disabled', busy);
+      this.$prev.toggleClass('disabled', busy);
+
+      if (busy)
+      {
+        var $spinner = this.$spinner;
+
+        this.timer && clearTimeout(this.timer);
+        this.timer = setTimeout(function ()
+          {
+            $spinner.removeClass('hidden').show();
+          }, this.THRESHOLD);
+      }
+      else
+      {
+        clearTimeout(this.timer);
+        this.$spinner.hide();
+      }
+    },
+
+    // Fetch items for a given page and inject the results in the DOM
     fetchResults: function (page)
     {
+      if (this.busy())
+      {
+        return;
+      }
+
       if (page < 1 || page > this.totalPages)
       {
         return;
@@ -62,34 +101,34 @@
         data: { page: page },
         beforeSend: function()
           {
+            this.busy(true);
           },
         success: function (data)
           {
+            this.currentPage = page;
+
             this.$results.empty();
 
-            for (var i = 0; i < data['holdings'].length; i++)
+            var len = data['holdings'].length
+            for (var i = 0; i < len; i++)
             {
               this.$results.append(
                 $('<li>').append(
                   $('<a>').attr('href', data['holdings'][i]['url'])
                           .attr('title', data['holdings'][i]['title'])
-                          .append(data['holdings'][i]['title'])
-              ));
+                          .append(data['holdings'][i]['title'])));
             }
 
-            this.currentPage = page;
+            this.$resultStart.html(data['start']);
+            this.$resultEnd.html(data['end']);
 
-            this.$resultStart.empty();
-            this.$resultEnd.empty();
-
-            this.$resultStart.append(data['start']);
-            this.$resultEnd.append(data['end']);
+            // Show or hide prev/next buttons according to the current page
+            this.$prev.toggle(this.currentPage !== 1);
+            this.$next.toggle(this.currentPage !== this.totalPages);
           },
         complete: function()
           {
-          },
-        error: function ()
-          {
+            this.busy(false);
           }
         });
     }
