da6dbdad776f63bc33efcefa5774b6d3aa365ddf
Mike Gale <mikeg@artefactual.com>
2015-04-10 13:34:27 -0700

    Start making table columns sortable

--- apps/qubit/modules/repository/templates/_browseTableView.php
+++ apps/qubit/modules/repository/templates/_browseTableView.php
@@ -1,33 +1,68 @@
 <table class="table table-bordered sticky-enabled">
-  <thead>
+  <thead class="tableheader-processed">
     <tr>
-      <td style="width: 40%"><?php echo __('Name') ?></td>
-      <td style="width: 20%"><?php echo __('Region') ?></td>
-      <td style="width: 20%"><?php echo __('Locality') ?></td>
-      <td style="width: 20%"><?php echo __('Thematic Area') ?></td>
+      <th class="sortable" style="width: 40%">
+        <?php echo link_to(__('Name'), array('sort' => ('nameUp' == $sf_request->sort) ? 'nameDown' : 'nameUp') +
+                           $sf_data->getRaw('sf_request')->getParameterHolder()->getAll(),
+                           array('title' => __('Sort'), 'class' => 'sortable')) ?>
+
+        <?php if ('nameUp' == $sf_request->sort): ?>
+          <?php echo image_tag('up.gif') ?>
+        <?php elseif ('nameDown' == $sf_request->sort): ?>
+          <?php echo image_tag('down.gif') ?>
+        <?php endif; ?>
+      </th>
+
+      <th class="sortable" style="width: 20%">
+        <?php echo link_to(__('Region'), array('sort' => ('regionUp' == $sf_request->sort) ? 'regionDown' : 'regionUp') +
+                           $sf_data->getRaw('sf_request')->getParameterHolder()->getAll(),
+                          array('title' => __('Sort'), 'class' => 'sortable')) ?>
+
+        <?php if ('regionUp' == $sf_request->sort): ?>
+          <?php echo image_tag('up.gif') ?>
+        <?php elseif ('regionDown' == $sf_request->sort): ?>
+          <?php echo image_tag('down.gif') ?>
+        <?php endif; ?>
+      </th>
+
+      <th class="sortable" style="width: 20%">
+        <?php echo link_to(__('Locality'), array('sort' => ('localityUp' == $sf_request->sort) ? 'localityDown' : 'localityUp') +
+                           $sf_data->getRaw('sf_request')->getParameterHolder()->getAll(),
+                          array('title' => __('Sort'), 'class' => 'sortable')) ?>
+
+        <?php if ('localityUp' == $sf_request->sort): ?>
+          <?php echo image_tag('up.gif') ?>
+        <?php elseif ('localityDown' == $sf_request->sort): ?>
+          <?php echo image_tag('down.gif') ?>
+        <?php endif; ?>
+      </th>
+
+      <th style="width: 20%">
+        <?php echo __('Thematic Area') ?>
+      </th>
     </tr>
   </thead>
 
   <?php foreach ($pager->getResults() as $hit): ?>
     <?php $resource = QubitRepository::getById($hit->getId()); ?>
     <tr>
+
       <td>
         <?php if ($resource->existsLogo()): ?>
           <?php echo image_tag($resource->getLogoPath(), array('height' => '10%', 'width' => '10%')) ?>
         <?php endif; ?>
         <?php echo link_to(render_title($resource), array('module' => 'repository', 'slug' => $resource->slug)) ?>
       </td>
-      <td>
-        <?php foreach ($resource->getTermRelations(QubitTaxonomy::GEOGRAPHIC_SUBREGION_ID) as $item): ?>
-          <li><?php echo __(render_value($item->term)) ?></li>
-        <?php endforeach; ?>
-      </td>
-      <td></td>
+
+      <td><?php echo $resource->getRegion() ?></td>
+      <td><?php echo $resource->getCity() ?></td>
+
       <td>
         <?php foreach ($resource->getTermRelations(QubitTaxonomy::THEMATIC_AREA_ID) as $item): ?>
           <li><?php echo __(render_value($item->term)) ?></li>
         <?php endforeach; ?>
       </td>
+
     </tr>
   <?php endforeach; ?>
 </table>
