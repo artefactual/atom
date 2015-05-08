4369fa331b91c312100515e1668cb06f04d37361
Mark Triggs <mark@teaspoon-consulting.com>
2015-05-09 06:51:34 +1000

    Use the dc prefix in ListRecords responses (addresses issue 8159)

--- plugins/sfDcPlugin/modules/sfDcPlugin/templates/_dc.xml.php
+++ plugins/sfDcPlugin/modules/sfDcPlugin/templates/_dc.xml.php
@@ -1,59 +1,61 @@
-<oai_dc:dc xmlns="http://purl.org/dc/elements/1.1/"
+<oai_dc:dc
     xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
+    xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
+    http://www.openarchives.org/OAI/2.0/oai_dc.xsd">
 
-  <title><?php echo esc_specialchars(strval($resource->title)) ?></title>
+  <dc:title><?php echo esc_specialchars(strval($resource->title)) ?></dc:title>
 
   <?php foreach ($resource->getCreators() as $item): ?>
-    <creator><?php echo esc_specialchars(strval($item)) ?></creator>
+    <dc:creator><?php echo esc_specialchars(strval($item)) ?></dc:creator>
   <?php endforeach; ?>
 
   <?php foreach ($dc->subject as $item): ?>
-    <subject><?php echo esc_specialchars(strval($item)) ?></subject>
+    <dc:subject><?php echo esc_specialchars(strval($item)) ?></dc:subject>
   <?php endforeach; ?>
 
-  <description><?php echo esc_specialchars(strval($resource->scopeAndContent)) ?></description>
+  <dc:description><?php echo esc_specialchars(strval($resource->scopeAndContent)) ?></dc:description>
 
   <?php foreach ($resource->getPublishers() as $item): ?>
-    <publisher><?php echo esc_specialchars(strval($item)) ?></publisher>
+    <dc:publisher><?php echo esc_specialchars(strval($item)) ?></dc:publisher>
   <?php endforeach; ?>
 
   <?php foreach ($resource->getContributors() as $item): ?>
-    <contributor><?php echo esc_specialchars(strval($item)) ?></contributor>
+    <dc:contributor><?php echo esc_specialchars(strval($item)) ?></dc:contributor>
   <?php endforeach; ?>
 
   <?php foreach ($dc->date as $item): ?>
-    <date><?php echo esc_specialchars(strval($item)) ?></date>
+    <dc:date><?php echo esc_specialchars(strval($item)) ?></dc:date>
   <?php endforeach; ?>
 
   <?php foreach ($dc->type as $item): ?>
-    <type><?php echo esc_specialchars(strval($item)) ?></type>
+    <dc:type><?php echo esc_specialchars(strval($item)) ?></dc:type>
   <?php endforeach; ?>
 
   <?php foreach ($dc->format as $item): ?>
-    <format><?php echo esc_specialchars(strval($item)) ?></format>
+    <dc:format><?php echo esc_specialchars(strval($item)) ?></dc:format>
   <?php endforeach; ?>
 
-  <identifier><?php echo esc_specialchars(strval(url_for(array($resource, 'module' => 'informationobject')), true)) ?></identifier>
+  <dc:identifier><?php echo esc_specialchars(url_for(array($resource, 'module' => 'informationobject'), true)) ?></dc:identifier>
 
-  <identifier><?php echo esc_specialchars(strval($resource->identifier)) ?></identifier>
+  <dc:identifier><?php echo esc_specialchars(strval($resource->identifier)) ?></dc:identifier>
 
-  <source><?php echo esc_specialchars(strval($resource->locationOfOriginals)) ?></source>
+  <dc:source><?php echo esc_specialchars(strval($resource->locationOfOriginals)) ?></dc:source>
 
   <?php foreach ($resource->language as $code): ?>
-    <language xsi:type="dcterms:ISO639-3"><?php echo esc_specialchars(strval(strtolower($iso639convertor->getID3($code)))) ?></language>
+    <dc:language xsi:type="dcterms:ISO639-3"><?php echo esc_specialchars(strval(strtolower($iso639convertor->getID3($code)))) ?></dc:language>
   <?php endforeach; ?>
 
   <?php if (isset($resource->repository)): ?>
-    <relation><?php echo esc_specialchars(strval(url_for(array($resource->repository, 'module' => 'repository')), true)) ?></relation>
-    <relation><?php echo esc_specialchars(strval($resource->repository->authorizedFormOfName)) ?></relation>
+    <dc:relation><?php echo esc_specialchars(url_for(array($resource->repository, 'module' => 'repository'), true)) ?></dc:relation>
+    <dc:relation><?php echo esc_specialchars(strval($resource->repository->authorizedFormOfName)) ?></dc:relation>
   <?php endif; ?>
 
   <?php foreach ($dc->coverage as $item): ?>
-    <coverage><?php echo esc_specialchars(strval($item)) ?></coverage>
+    <dc:coverage><?php echo esc_specialchars(strval($item)) ?></dc:coverage>
   <?php endforeach; ?>
 
-  <rights><?php echo esc_specialchars(strval($resource->accessConditions)) ?></rights>
+  <dc:rights><?php echo esc_specialchars(strval($resource->accessConditions)) ?></dc:rights>
 
 </oai_dc:dc>
