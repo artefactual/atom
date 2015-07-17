301b1d86634da6ed438612620074a152610dc3e6
Mike Cantelon <mcantelon@gmail.com>
2015-07-16 17:14:47 -0700

    Remove treeview top-level ACL check. refs #8551
    Removed the ACL check when fetching the top level of the treeview.
    Replaced it with logic to filter out drafts if not authenticated.

--- lib/model/QubitInformationObject.php
+++ lib/model/QubitInformationObject.php
@@ -2559,16 +2559,20 @@ class QubitInformationObject extends BaseInformationObject
     // Find first child visible
     $criteria = new Criteria;
     $criteria->add(QubitInformationObject::PARENT_ID, $this->id);
+
+    // If not authenticated, restrict access to published descriptions
+    if (!sfContext::getInstance()->user->isAuthenticated())
+    {
+      $criteria->addJoin(QubitInformationObject::ID, QubitStatus::OBJECT_ID);
+      $criteria->add(QubitStatus::STATUS_ID, QubitTerm::PUBLICATION_STATUS_PUBLISHED_ID);
+    }
+
     $criteria = QubitInformationObject::addTreeViewSortCriteria($criteria);
+
     foreach (QubitInformationObject::get($criteria) as $item)
     {
-      // ACL checks
-      if (QubitAcl::check($item, 'read'))
-      {
-        $firstChild = $item;
-
-        break;
-      }
+      $firstChild = $item;
+      break;
     }
 
     $items = array();
