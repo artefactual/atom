e10126e6722b23f32ce648682da4b091bc1abb88
José Raddaoui Marín <raddaouimarin@gmail.com>
2015-07-28 21:17:04 +0200

    Improve adv. search date range, refs #8648
    - Don't change user input value when enter is pressed
    - Allow and fix only year dates on form submit

--- js/advancedSearch.js
+++ js/advancedSearch.js
@@ -20,6 +20,28 @@
             }
           };
 
-          jQuery('#sd, #ed').datepicker(opts);
+          // Don't change user input value when enter is pressed
+          jQuery('#sd, #ed').bind('keydown', function (event) {
+            if (event.which == 13) {
+              var e = jQuery.Event('keydown');
+              e.which = 9;
+              e.keyCode = 9;
+              $(this).trigger(e);
+
+              return false;
+            }
+          }).datepicker(opts);
+
+          // Fix only year dates on form submit
+          jQuery('form').bind('submit', function (event) {
+            var sd = $(this).find('#sd');
+            if (/^\d{4}$/.test(sd.val())) {
+              sd.val(sd.val() + '-01-01');
+            }
+            var ed = $(this).find('#ed');
+            if (/^\d{4}$/.test(ed.val())) {
+              ed.val(ed.val() + '-12-31');
+            }
+          });
         }};
   })(jQuery);
