0bf23711bca7f638d678d1372d06749a6754caa8
José Raddaoui Marín <raddaouimarin@gmail.com>
2015-07-30 00:11:22 +0200

    Remove spaces inside <li> in sidebar lists, #8754

--- apps/qubit/modules/staticpage/templates/homeSuccess.php
+++ apps/qubit/modules/staticpage/templates/homeSuccess.php
@@ -14,11 +14,7 @@
       <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID) ?>
       <?php if ($browseMenu->hasChildren()): ?>
         <?php foreach ($browseMenu->getChildren() as $item): ?>
-          <li>
-            <a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>">
-              <?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?>
-            </a>
-          </li>
+          <li><a href="<?php echo url_for($item->getPath(array('getUrl' => true, 'resolveAlias' => true))) ?>"><?php echo esc_specialchars($item->getLabel(array('cultureFallback' => true))) ?></a></li>
         <?php endforeach; ?>
       <?php endif; ?>
     </ul>
