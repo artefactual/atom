8cf315f6b97fa307e391bade58e1ccc3599f9c36
Jesús García Crespo <jesus@sevein.com>
2015-08-06 13:14:31 -0700

    Highlight settings menu nodes, refs #8783

--- apps/qubit/modules/settings/templates/_menu.php
+++ apps/qubit/modules/settings/templates/_menu.php
@@ -1,21 +1,17 @@
 <div class="sidebar-lowering settings-menu">
-    <table class="table table-bordered table-hover table-condensed">
-      <tbody>
-        <tr><td><?php echo link_to(__('Global'), array('module' => 'settings', 'action' => 'global')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Site information'), array('module' => 'settings', 'action' => 'siteInformation')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Default page elements'), array('module' => 'settings', 'action' => 'pageElements')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Default template'), array('module' => 'settings', 'action' => 'template')) ?></td></tr>
-        <tr><td><?php echo link_to(__('User interface label'), array('module' => 'settings', 'action' => 'interfaceLabel')) ?></td></tr>
-        <tr><td><?php echo link_to(__('I18n languages'), array('module' => 'settings', 'action' => 'language')) ?></td></tr>
-        <?php if ($sf_context->getConfiguration()->isPluginEnabled('arOaiPlugin')): ?>
-            <tr><td><?php echo link_to(__('OAI repository'), array('module' => 'settings', 'action' => 'oai')) ?></td></tr>
+  <table class="table table-bordered table-hover table-condensed">
+    <tbody>
+      <?php foreach ($nodes as $node): ?>
+        <?php if ($node['active']): ?>
+          <tr class="info">
+        <?php else: ?>
+          <tr>
         <?php endif; ?>
-        <tr><td><?php echo link_to(__('Finding Aid'), array('module' => 'settings', 'action' => 'findingAid')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Security'), array('module' => 'settings', 'action' => 'security')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Permissions'), array('module' => 'settings', 'action' => 'permissions')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Inventory'), array('module' => 'settings', 'action' => 'inventory')) ?></td></tr>
-        <tr><td><?php echo link_to(__('Digital object derivatives'), array('module' => 'settings', 'action' => 'digitalObjectDerivatives')) ?></td></tr>
-        <tr><td><?php echo link_to(__('DIP upload'), array('module' => 'settings', 'action' => 'dipUpload')) ?></td></tr>
-      </tbody>
-    </table>
+          <td>
+            <?php echo link_to($node['label'], array('module' => 'settings', 'action' => $node['action'])) ?>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
 </div>
