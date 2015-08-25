20fe7b41e87094ecb6ab5a71c0b1ca23a6a007b2
Jesús García Crespo <jesus@sevein.com>
2015-08-24 17:25:57 -0700

    Update AR_AUTO_LINK_RE, refs #8836
    Commit ec8a9f34c31784a0e543ef640b17b0e43de2c7cc updated SF_AUTO_LINK_RE in
    order to accept slashes in the query string. However, AtoM has been used a
    new custom constant named AR_AUTO_LINK_RE (we found this during testing). This
    commit makes the same update to this new expression.

--- lib/helper/QubitHelper.php
+++ lib/helper/QubitHelper.php
@@ -557,7 +557,7 @@ if (!defined('AR_AUTO_LINK_RE'))
       (?:\.[-\w]+)*                                 # remaining subdomains or domain
       (?::\d+)?                                     # port
       (?:/(?:(?:[\~\w\+%-]|(?:[,.;:][^\s$]))+)?)*   # path
-      (?:\?[\w\+%&=.;-]+)?                          # query string
+      (?:\?[\w\+\/%&=.;-]+)?                        # query string
       (?:\#[\w\-/\?!=]*)?                           # trailing anchor
     )
     ([[:punct:]]|\s|<|$)                            # trailing text
