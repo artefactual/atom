2fd310cceaa7e2a0f5e3e2f274daa6ebd916a05b
José Raddaoui Marín <raddaouimarin@gmail.com>
2015-09-01 17:14:11 +0200

    Fix popular this week counts display, refs #8754

--- apps/qubit/modules/default/templates/_popular.php
+++ apps/qubit/modules/default/templates/_popular.php
@@ -4,7 +4,7 @@
   <ul>
     <?php foreach ($popularThisWeek as $item): ?>
       <?php $object = QubitObject::getById($item[0]); ?>
-      <li><a href="<?php echo url_for(array($object)) ?>"><?php echo render_title(esc_entities($object->__toString())) ?><strong><?php echo __('%1% visits', array('%1%' => $item[1])) ?></strong></a></li>
+      <li><a href="<?php echo url_for(array($object)) ?>"><?php echo render_title(esc_entities($object->__toString())) ?><strong>&nbsp;&nbsp;<?php echo __('%1% visits', array('%1%' => $item[1])) ?></strong></a></li>
     <?php endforeach; ?>
   </ul>
 
