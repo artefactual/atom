fe5cb6b9e845266a6b00f198cb2ad3d3a07449a9
Mike Cantelon <mcantelon@gmail.com>
2015-11-12 18:09:04 -0800

    Added code to remove alerts upon treeview click. refs #8759

--- apps/qubit/templates/_alerts.php
+++ apps/qubit/templates/_alerts.php
@@ -1,12 +1,12 @@
 <?php if ($sf_user->hasFlash('notice')): ?>
-  <div class="alert">
+  <div id="notice-alerts" class="alert">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
     <?php echo $sf_user->getFlash('notice', ESC_RAW) ?>
   </div>
 <?php endif; ?>
 
 <?php if ($sf_user->hasFlash('error')): ?>
-  <div class="alert alert-error">
+  <div id="error-alerts" class="alert alert-error">
     <button type="button" class="close" data-dismiss="alert">&times;</button>
     <?php echo $sf_user->getFlash('error', ESC_RAW) ?>
   </div>
