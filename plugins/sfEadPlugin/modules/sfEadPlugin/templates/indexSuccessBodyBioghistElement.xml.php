ba525dc501889f433ee3ffea0bc5cecfb83b1eab
Mike Gale <mikeg@artefactual.com>
2016-03-18 16:11:39 -0700

    Avoid exporting empty bioghist, refs #9564

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBodyBioghistElement.xml.php
@@ -3,7 +3,7 @@
   <?php foreach($events as $date): ?>
     <?php $creator = QubitActor::getById($date->actorId); ?>
 
-    <?php if (($value = $creator->getHistory(array('cultureFallback' => true))) || $creator->datesOfExistence): ?>
+    <?php if ($value = $creator->getHistory(array('cultureFallback' => true))): ?>
       <bioghist id="<?php echo 'md5-' . md5(url_for(array($creator, 'module' => 'actor'), true)) ?>" encodinganalog="<?php echo $ead->getMetadataParameter('bioghist') ?>">
         <?php if ($value): ?>
           <note><p><?php echo escape_dc(esc_specialchars($value)) ?></p></note>
