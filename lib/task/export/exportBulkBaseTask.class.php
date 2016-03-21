52d16ca9f258d52dcb406f0b38224cc656266c23
Mike Gale <mikeg@artefactual.com>
2016-03-21 16:04:31 -0700

    Incl. EAC/MODS plugins in export refs #9602 #9603

--- lib/task/export/exportBulkBaseTask.class.php
+++ lib/task/export/exportBulkBaseTask.class.php
@@ -90,12 +90,14 @@ abstract class exportBulkBaseTask extends sfBaseTask
   {
     $appRoot = sfConfig::get('sf_root_dir');
 
-    include($appRoot .'/plugins/sfEadPlugin/lib/sfEadPlugin.class.php');
-    include($appRoot .'/vendor/symfony/lib/helper/UrlHelper.php');
-    include($appRoot .'/vendor/symfony/lib/helper/I18NHelper.php');
-    include($appRoot .'/vendor/FreeBeerIso639Map.php');
-    include($appRoot .'/vendor/symfony/lib/helper/EscapingHelper.php');
-    include($appRoot .'/lib/helper/QubitHelper.php');
+    include $appRoot.'/plugins/sfEadPlugin/lib/sfEadPlugin.class.php';
+    include $appRoot.'/plugins/sfModsPlugin/lib/sfModsPlugin.class.php';
+    include $appRoot.'/plugins/sfEacPlugin/lib/sfEacPlugin.class.php';
+    include $appRoot.'/vendor/symfony/lib/helper/UrlHelper.php';
+    include $appRoot.'/vendor/symfony/lib/helper/I18NHelper.php';
+    include $appRoot.'/vendor/FreeBeerIso639Map.php';
+    include $appRoot.'/vendor/symfony/lib/helper/EscapingHelper.php';
+    include $appRoot.'/lib/helper/QubitHelper.php';
   }
 
   public static function captureResourceExportTemplateOutput($resource, $format, $options)
