f5207e0e1b2786b198a2ceecf01f259754ecef38
José Raddaoui Marín <raddaouimarin@gmail.com>
2016-03-23 09:22:12 +0100

    Remove unneeded sfDrupalPlugin files

--- plugins/sfDrupalPlugin/vendor/drupal/scripts/cron-curl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $Id: cron-curl.sh,v 1.3 2006/08/22 07:38:24 dries Exp $
-
-curl --silent --compressed http://example.com/cron.php
--- plugins/sfDrupalPlugin/vendor/drupal/scripts/cron-lynx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $Id: cron-lynx.sh,v 1.3 2006/08/22 07:38:24 dries Exp $
-
-/usr/bin/lynx -source http://example.com/cron.php > /dev/null 2>&1
