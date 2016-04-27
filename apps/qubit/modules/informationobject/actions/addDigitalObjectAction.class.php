artefactual/atom

commit 03c17eaf4546457e6ac00b2f4a0b517bbf96a9fb

Add skip-derivatives option, refs #9556
* Add csv:import --skip-derivatives option
* Skip derivative creation for local and remote resources when using flag
* Skip download of remote resources when using flag
* Add dedicated method to get filename from URI
* Move download retry code to QubitDigitalObject::downloadExternalObject()
* Throw exception when remote resource download fails
* Catch and log download exception in csvImportTask
* Catch importFromUri() exceptions in addDigitalObjectAction.class.php
* Fix fatal error when using --update and adding digital object

apps/qubit/modules/informationobject/actions/addDigitalObjectAction.class.php
@@ -118,9 +118,18 @@ class InformationObjectAddDigitalObjectAction extends sfAction
     }
     else if (null !== $this->form->getValue('url'))
     {
-      $digitalObject->importFromURI($this->form->getValue('url'));
+      // Catch errors trying to download remote resource
+      try
+      {
+        $digitalObject->importFromURI($this->form->getValue('url'));
+        $this->resource->digitalObjects[] = $digitalObject;
+      }
+      catch (sfException $e)
+      {
+        // Log download exception
+        $this->logMessage($e->getMessage, 'err');
+      }
     }
 
-    $this->resource->digitalObjects[] = $digitalObject;
   }
 }

lib/model/QubitDigitalObject.php
@@ -1386,53 +1386,119 @@ class QubitDigitalObject extends BaseDigitalObject
    * Download external file via sfWebBrowser and return its temporary location
    *
    * @param string URI
-   * @return mixed FALSE if error, array otherwise
+   * @param array options optional arguments
+   *
+   * @return string contents
    */
-  private function downloadExternalObject($uri)
+  private function downloadExternalObject($uri, $options = array())
+  {
+    // Initialize web browser
+    $timeout = sfConfig::get("app_download_timeout");
+    $browser = new sfWebBrowser(array(), 'sfCurlAdapter', array('Timeout' => $timeout));
+
+    // Set retries from optional argument
+    $retries = (isset($options['downloadRetries']) && 0 < $options['downloadRetries']) ? $options['downloadRetries'] : 0;
+
+    // Attempt to download the digital object, with possible retries
+    for ($i=0; $i <= $retries; $i++)
+    {
+      try
+      {
+        $browser->get($uri);
+      }
+      catch (Exception $e)
+      {
+        // If request times out
+        if ($e->getCode() === CURLE_OPERATION_TIMEDOUT)
+        {
+          // Try again, up to $retries
+          continue;
+        }
+
+        throw $e;
+      }
+
+      // If response code is an error (4xx or 5xx)
+      if ($browser->responseIsError())
+      {
+        // Try again, up to $retries
+        continue;
+      }
+
+      if (false !== $contents = $browser->getResponseText())
+      {
+        return $contents;
+      }
+    }
+
+    // Throw exception on failure
+    throw new sfException(sprintf('Error downloading "%s" (attempts: %s).', $uri, $i));
+  }
+
+  /**
+   * Get filename from URI path
+   *
+   * @param string URI
+   * @return mixed null if error, string otherwise
+   */
+  private function getFilenameFromUri($uri)
   {
     // Parse URL into components and get file/base name
     $uriComponents = parse_url($uri);
+    $filename = basename($uriComponents['path']);
 
-    // Initialize web browser
-    $timeout = sfConfig::get("app_download_timeout");
-    $browser = new sfWebBrowser(array(), null, array('Timeout' => $timeout));
-    $browser->get($uri);
-
-    if ($browser->get($uri)->responseIsError() || 1 > strlen(($filename = basename($uriComponents['path']))))
+    if (1 > strlen($filename))
     {
-      return false;
+      throw new sfException(sprintf('Couldn\'t parse filename from uri %s', $uri));
     }
 
-    return array($filename, $browser->getResponseText());
+    return $filename;
   }
 
   /**
    * Populate a digital object from a resource pointed to by a URI
    * This is for, eg. importing encoded digital objects from XML
    *
-   * @param string  $uri  URI pointing to the resource
-   * @return boolean  success or failure
+   * @param string $uri remote digital object URI
+   * @param array $options Optional arguments
+   *
+   * @return QubitDigitalObject this object
    */
   public function importFromURI($uri, $options = array())
   {
-    list($filename, $contents) = $this->downloadExternalObject($uri);
-    if (false === $this->localPath = Qubit::saveTemporaryFile($filename, $contents))
-    {
-      throw new sfException('Encountered error fetching external resource.');
-    }
+    $filename = $this->getFilenameFromUri($uri);
 
-    $asset = new QubitAsset($uri, $contents);
-    $this->assets[] = $asset;
-
-    // Set digital object as external URI
+    // Set general properties that don't require downloading the asset
     $this->usageId = QubitTerm::EXTERNAL_URI_ID;
-
     $this->name = $filename;
     $this->path = $uri;
+    $this->setMimeAndMediaType();
+
+    // If not creating derivatives right now, don't download the resource
+    if (!$this->createDerivatives)
+    {
+      return $self;
+    }
+
+    // Download the remote resource bitstream
+    $contents = $this->downloadExternalObject($uri, $options);
+
+    // Save downloaded bitstream to a temp file
+    if (false === $this->localPath = Qubit::saveTemporaryFile($filename, $contents))
+    {
+      throw new sfException(sprintf('Error writing downloaded file to "%s".', $this->localPath));
+    }
+
+    // Attach downloaded file to digital object
+    $asset = new QubitAsset($this->localPath);
+    $this->assets[] = $asset;
+
+    // Set properties derived from file contents
     $this->checksum = $asset->getChecksum();
     $this->checksumType = $asset->getChecksumAlgorithm();
     $this->byteSize = strlen($contents);
-    $this->setMimeAndMediaType();
+
+    return $self;
   }
 
   /**
@@ -2250,7 +2316,8 @@ class QubitDigitalObject extends BaseDigitalObject
   {
     if (null === $this->localPath && QubitTerm::EXTERNAL_URI_ID == $this->usageId)
     {
-      list($filename, $contents) = $this->downloadExternalObject($this->path);
+      $filename = $this->getFilenameFromUri($this->path);
+      $contents = $this->downloadExternalObject($this->path);
       $this->localPath = Qubit::saveTemporaryFile($filename, $contents);
     }
 
@@ -2928,7 +2995,9 @@ class QubitDigitalObject extends BaseDigitalObject
       // Create new temporary copy from external resources if the old copy is missing
       if (!isset($path) || !file_exists($path))
       {
-        list($filename, $contents) = $this->downloadExternalObject($this->getPath());
+        $filename = $this->getFilenameFromUri($this->getPath());
+        $contents = $this->downloadExternalObject($this->getPath());
+
         if (false === $path = Qubit::saveTemporaryFile($filename, $contents))
         {
           return;

lib/task/import/csvImportTask.class.php
@@ -77,8 +77,33 @@ EOF;
         null,
         sfCommandOption::PARAMETER_NONE,
         "Attempt to update if description has already been imported."
+      ),
+      new sfCommandOption(
+        'skip-derivatives',
+        null,
+        sfCommandOption::PARAMETER_NONE,
+        "Skip creation of digital object derivatives."
       )
     ));
+  }
+
+  /**
+   * Echo and log a message
+   *
+   * @see sfTask::log()
+   * @see sfLoggger::log()
+   *
+   * @param string $message N.B. sfTask::log() accepts an array or string, but
+   *                        sfLogger::log() expects a string only
+   * @param string $priority See sfLogger for priority levels
+   */
+  public function log($message, $priority = sfLogger::INFO)
+  {
+    // Echo message
+    parent::log($message);
+
+    // Log message
+    sfContext::getInstance()->getLogger()->log($message, $priority);
   }
 
   /**
@@ -417,11 +442,11 @@ EOF;
           // Delete any digital objects that exist for this information object
           $criteria = new Criteria;
           $criteria->add(QubitDigitalObject::INFORMATION_OBJECT_ID, $self->object->id);
-          $results = QubitDigitalObject::get($criteria);
+          $results = QubitDigitalObject::getOne($criteria);
 
           if ($results !== null)
           {
-            $results[0]->delete();
+            $results->delete();
           }
         }
       },
@@ -956,31 +981,54 @@ EOF;
         // if both a URI and path are provided, the former is preferred.
         if ($uri = $self->rowStatusVars['digitalObjectURI'])
         {
+          $do = new QubitDigitalObject;
+          $do->informationObject = $self->object;
+
+          if ($self->status['options']['skip-derivatives'])
+          {
+            // Don't download remote resource or create derivatives
+            $do->createDerivatives = false;
+          }
+          else
+          {
+            // Try downloading external object up to three times (2 retries)
+            $options = array('downloadRetries' => 2);
+          }
+
+          // Catch digital object import errors to avoid killing whole import
           try
           {
-            if (!downloadExternalURIWithRetries($uri, $self->object))
-            {
-              $this->log("Failed to download $uri after multiple tries. Continuing task...");
-            }
+            $do->importFromURI($uri, $options);
+            $do->save($conn);
           }
           catch (Exception $e)
           {
-            print "\nFailed to download '$uri': ". $e->getMessage() ."\n";
+            // Log error
+            $this->log($e->getMessage(), sfLogger::ERR);
           }
         }
         else if ($path = $self->rowStatusVars['digitalObjectPath'])
         {
-          if (false === $content = file_get_contents($path))
+          $do = new QubitDigitalObject;
+          $do->usageId = QubitTerm::MASTER_ID;
+          $do->informationObject = $self->object;
+
+          // Don't create derivatives (reference, thumb)
+          if ($self->status['options']['skip-derivatives'])
           {
-            $this->log("Unable to read file: ".$path);
+            $do->createDerivatives = false;
           }
-          else
+
+          $do->assets[] = new QubitAsset($path);
+
+          try
           {
-            $do = new QubitDigitalObject;
-            $do->assets[] = new QubitAsset($path, $content);
-            $do->usageId = QubitTerm::MASTER_ID;
-            $do->informationObject = $self->object;
             $do->save($conn);
+          }
+          catch (Exception $e)
+          {
+            // Log error
+            $this->log($e->getMessage(), sfLogger::ERR);
           }
         }
       }
@@ -1091,42 +1139,4 @@ function refreshTaxonomyTerms($taxonomyId)
   $result = QubitFlatfileImport::loadTermsFromTaxonomies(array($taxonomyId => 'terms'));
 
   return $result['terms'];
-}
-
-/**
- * Downloads digital objects from a URI. Will retry a few times if we get any timeouts.
- *
- * @param string uri  The path to the external digital object, e.g.: https://www.example.com/hi.jpg
- * @param QubitInformationObject infoObj  The information object to associate this digital object with.
- *
- * @return bool  True if the digital object downloaded / saved, false if not.
- */
-function downloadExternalURIWithRetries($uri, $infoObj)
-{
-  $MAX_RETRIES = 3;
-
-  for ($i = 0; $i < $MAX_RETRIES; $i++)
-  {
-    try
-    {
-      $do = new QubitDigitalObject;
-
-      $do->importFromURI($uri);
-      $do->informationObject = $infoObj;
-      $do->save();
-    }
-    catch (Exception $e)
-    {
-      if ($e->getCode() === CURLE_OPERATION_TIMEDOUT)
-      {
-        continue;
-      }
-
-      throw $e;
-    }
-
-    break;
-  }
-
-  return $i < $MAX_RETRIES;
 }

commit 93f1797b7b900332fabec46dc83dd15d69d0e337

Fix digital object upload, refs #9748
Fix regression introduced in commit 03c17eaf4546457e6ac00b2f4a0b517bbf96a9fb

apps/qubit/modules/informationobject/actions/addDigitalObjectAction.class.php
@@ -122,7 +122,6 @@ class InformationObjectAddDigitalObjectAction extends sfAction
       try
       {
         $digitalObject->importFromURI($this->form->getValue('url'));
-        $this->resource->digitalObjects[] = $digitalObject;
       }
       catch (sfException $e)
       {
@@ -131,5 +130,6 @@ class InformationObjectAddDigitalObjectAction extends sfAction
       }
     }
 
+    $this->resource->digitalObjects[] = $digitalObject;
   }
 }