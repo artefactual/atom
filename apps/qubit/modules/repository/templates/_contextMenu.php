71ef03a342cff6d485d38fce9ebcf6e362b758bb
Mike Gale <mikeg@artefactual.com>
2016-05-06 16:10:23 -0700

    Hide upload stats when editing descs, refs #9275
    Hide repository upload statistics in context menu when we're not actually
    on a repository page. These stats were previously appearing when editing
    archival descriptions, weren't useful, and led to timeout issues when there
    were huge datasets.
    
    bob

--- apps/qubit/modules/repository/templates/_contextMenu.php
+++ apps/qubit/modules/repository/templates/_contextMenu.php
@@ -1,9 +1,9 @@
 <?php echo get_component('repository', 'logo') ?>
 
-<?php if (QubitAcl::check($resource, 'update')): ?>
-  <?php include_component('repository', 'uploadLimit', array('resource' => $resource)) ?>
-<?php endif; ?>
-
 <?php if ($resource->getRawValue() instanceof QubitRepository): ?>
+  <?php if (QubitAcl::check($resource, 'update')): ?>
+    <?php include_component('repository', 'uploadLimit', array('resource' => $resource)) ?>
+  <?php endif; ?>
+
   <?php include_component('repository', 'holdings', array('resource' => $resource)) ?>
 <?php endif; ?>
