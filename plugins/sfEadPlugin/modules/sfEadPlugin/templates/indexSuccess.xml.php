cfc5050478bc01e8e0717d2d836d013ecf5255a3
Mike Gale <mikeg@artefactual.com>
2016-05-06 15:52:30 -0700

    Tidy XML exported for EAC, refs #9606 note-4
    I re-arranged the EAC export structure a bit so it matches the EAD plugin's style and we can tidy the XML.
    I also refactored the code a bit to use ob_get_clean for brevity.

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccess.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccess.xml.php
@@ -1,10 +1,6 @@
 <?php
   ob_start();
-
   include('indexSuccessHeader.xml.php');
   include('indexSuccessBody.xml.php');
-
-  $result = ob_get_contents();
-  ob_end_clean();
-  echo Qubit::tidyXml($result);
+  echo Qubit::tidyXml(ob_get_clean());
 ?>
