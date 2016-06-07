16dd76dff74cb4bb1d59bdddfef4a1dbe9813671
David Lung <e1026620@student.tuwien.ac.at>
2016-06-07 13:46:42 +0200

    added 'return to list'-buttons to user/group index pages

--- plugins/qbAclPlugin/modules/aclGroup/templates/_showActions.php
+++ plugins/qbAclPlugin/modules/aclGroup/templates/_showActions.php
@@ -15,6 +15,10 @@
         <?php if (QubitAcl::check($group, 'create')): ?>
           <li><?php echo link_to (__('Add new'), array('module' => 'aclGroup', 'action' => 'add'), array('class' => 'c-btn')) ?></li>
         <?php endif; ?>
+        
+        <?php if (QubitAcl::check($group, 'list')): ?>
+          <li><?php echo link_to (__('Return to group list'), array('module' => 'aclGroup', 'action' => 'list'), array('class' => 'c-btn')) ?></li>
+        <?php endif; ?>
 
     </ul>
 
