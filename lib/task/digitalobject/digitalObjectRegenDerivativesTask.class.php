4e17b63fcbfd177deb519a1b29417a99c4857257
Mike Cantelon <mcantelon@gmail.com>
2016-06-30 15:16:32 -0700

    Fix regenerate derivatives by type. refs #9964
    When regenerating digital object derivatives, using the CLI task, and
    specifying type all derivatives would be deleted. Fixed.

--- lib/task/digitalobject/digitalObjectRegenDerivativesTask.class.php
+++ lib/task/digitalobject/digitalObjectRegenDerivativesTask.class.php
@@ -174,9 +174,25 @@ EOF;
 
   public static function regenerateDerivatives(&$digitalObject, $type = null)
   {
+    // Determine usage ID for type
+    switch($type)
+    {
+      case "reference":
+        $usageId = QubitTerm::REFERENCE_ID;
+        break;
+
+      case "thumbnail":
+        $usageId = QubitTerm::THUMBNAIL_ID;
+        break;
+
+      default:
+        $usageId = QubitTerm::MASTER_ID;
+    }
+
     // Delete existing derivatives
     $criteria = new Criteria;
     $criteria->add(QubitDigitalObject::PARENT_ID, $digitalObject->id);
+    $criteria->add(QubitDigitalObject::USAGE_ID, $usageId);
 
     foreach(QubitDigitalObject::get($criteria) as $derivative)
     {
@@ -190,20 +206,6 @@ EOF;
       {
         $property->delete();
       }
-    }
-
-    switch($type)
-    {
-      case "reference":
-        $usageId = QubitTerm::REFERENCE_ID;
-        break;
-
-      case "thumbnail":
-        $usageId = QubitTerm::THUMBNAIL_ID;
-        break;
-
-      default:
-        $usageId = QubitTerm::MASTER_ID;
     }
 
     $digitalObject->createRepresentations($usageId, $conn);
