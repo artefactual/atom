eb308de5aa4d9ab8b57a92db49161e29d1a3b921
Mike Cantelon <mcantelon@gmail.com>
2016-09-13 22:28:04 -0700

    Added new alert types, refs #10321
    Added ability to create "info" and "success" Bootstrap alerts. Replaced
    CSS ID for each notice type with an "app-alert" class (and tweaks JavaScript
    to use it).

--- apps/qubit/templates/_alerts.php
+++ apps/qubit/templates/_alerts.php
@@ -1,13 +1,8 @@
-<?php if ($sf_user->hasFlash('notice')): ?>
-  <div id="notice-alerts" class="alert">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <?php echo $sf_user->getFlash('notice', ESC_RAW) ?>
-  </div>
-<?php endif; ?>
-
-<?php if ($sf_user->hasFlash('error')): ?>
-  <div id="error-alerts" class="alert alert-error">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <?php echo $sf_user->getFlash('error', ESC_RAW) ?>
-  </div>
-<?php endif; ?>
+<?php foreach(array('notice', 'info', 'error', 'success') as $alertType): ?>
+  <?php if ($sf_user->hasFlash($alertType)): ?>
+    <div class="alert app-alert alert-<?php echo $alertType ?>">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <?php echo $sf_user->getFlash($alertType, ESC_RAW) ?>
+    </div>
+  <?php endif; ?>
+<?php endforeach; ?>
