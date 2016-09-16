8f5cd04f9b2c8d9259024bf0b07b4ac4adbb9b77
Mike Cantelon <mcantelon@gmail.com>
2016-09-15 17:26:24 -0700

    Update repo map Gmaps API key use, refs #10330
    Changed display of the repository's Google Map to use the API key stored as a
    setting rather than a config variable. Removed the unused config variable.

--- plugins/sfIsdiahPlugin/modules/sfIsdiahPlugin/templates/indexSuccess.php
+++ plugins/sfIsdiahPlugin/modules/sfIsdiahPlugin/templates/indexSuccess.php
@@ -73,8 +73,8 @@
 
 <?php end_slot() ?>
 
-<?php if (isset($latitude) && isset($longitude) && null !== $key = sfConfig::get('app_google_maps_api_key')): ?>
-  <div id="front-map" class="simple-map" data-key="<?php echo $key ?>" data-latitude="<?php echo $latitude ?>" data-longitude="<?php echo $longitude ?>"></div>
+<?php if (isset($latitude) && isset($longitude) && $mapApiKey = sfConfig::get('app_google_maps_api_key')): ?>
+  <div id="front-map" class="simple-map" data-key="<?php echo $mapApiKey ?>" data-latitude="<?php echo $latitude ?>" data-longitude="<?php echo $longitude ?>"></div>
 <?php endif; ?>
 
 <section id="identifyArea">
