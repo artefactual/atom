2c0e74930aebb780be872ef43b67c33803c5d241
José Raddaoui Marín <raddaouimarin@gmail.com>
2016-10-12 17:54:05 +0200

    Header menu fixes in AC theme, refs #10418
    Use language menu toggle setting and add clipboard menu

--- plugins/arArchivesCanadaPlugin/templates/_header.php
+++ plugins/arArchivesCanadaPlugin/templates/_header.php
@@ -7,7 +7,10 @@
     <nav>
       <?php echo get_component('menu', 'userMenu') ?>
       <?php echo get_component('menu', 'quickLinksMenu') ?>
-      <?php echo get_component('menu', 'changeLanguageMenu') ?>
+      <?php if (sfConfig::get('app_toggleLanguageMenu')): ?>
+        <?php echo get_component('menu', 'changeLanguageMenu') ?>
+      <?php endif; ?>
+      <?php echo get_component('menu', 'clipboardMenu') ?>
       <?php echo get_component('menu', 'mainMenu', array('sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID())) ?>
     </nav>
   </div>
