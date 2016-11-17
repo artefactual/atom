01ce0d22ef4ab73a42974678d8e97196d73660d3
Mike Gale <mikeg@artefactual.com>
2016-11-17 15:48:42 -0800

    Add box label HTML async report

--- lib/job/arGenerateCsvReportJob.class.php
+++ lib/job/arGenerateCsvReportJob.class.php
@@ -27,7 +27,8 @@ class arGenerateCsvReportJob extends arBaseJob
   /**
    * @see arBaseJob::$requiredParameters
    */
-  protected $extraRequiredParameters = array('objectId', 'reportType', 'reportTypeLabel', 'reportFormat');
+  protected
+    $extraRequiredParameters = array('objectId', 'reportType', 'reportTypeLabel', 'reportFormat');
 
   private
     $resource = null;
@@ -38,12 +39,13 @@ class arGenerateCsvReportJob extends arBaseJob
     boxLabelTemplatePath = 'apps/qubit/modules/informationobject/templates/boxLabelSuccess.php',
     reportsDir = 'downloads/reports';
 
-  private $templatePaths = array(
-    'itemList' => self::itemOrFileTemplatePath,
-    'fileList' => self::itemOrFileTemplatePath,
-    'storageLocations' => self::storageLocationsTemplatePath,
-    'boxLabel' => self::boxLabelTemplatePath,
-  );
+  private
+    $templatePaths = array(
+      'itemList' => self::itemOrFileTemplatePath,
+      'fileList' => self::itemOrFileTemplatePath,
+      'storageLocations' => self::storageLocationsTemplatePath,
+      'boxLabel' => self::boxLabelTemplatePath,
+    );
 
   public function runJob($parameters)
   {
@@ -92,14 +94,15 @@ class arGenerateCsvReportJob extends arBaseJob
         break;
 
       case 'boxLabel':
+        $results = $this->getBoxLabelResults();
+
         if ('csv' === $this->params['reportFormat'])
         {
-          $ret = $this->writeBoxLabelCsv();
+          $ret = $this->writeBoxLabelCsv($results);
         }
         else
         {
-          //$ret = $this->writeHtml($results);
-          throw new sfException('box label html report not implemeneted');
+          $ret = $this->writeHtml($results);
         }
 
         break;
@@ -205,18 +208,14 @@ class arGenerateCsvReportJob extends arBaseJob
     return $results;
   }
 
-  private function writeBoxLabelCsv()
+  private function getBoxLabelResults()
   {
-    if (null === $fh = fopen($this->filename, 'w'))
-    {
-      throw new sfException('Unable to open file '.$this->filename.' - please check permissions.');
-    }
-
-    fputcsv($fh, array('referenceCode', 'physicalObjectName', 'title', 'creationDates'));
+    $results = array();
 
     foreach ($this->resource->descendants->andSelf()->orderBy('rgt') as $informationObject)
     {
-      // Creation dates
+      $creationDates = array();
+
       foreach ($informationObject->getDates(array('type_id' => QubitTerm::CREATION_ID)) as $item)
       {
         $creationDates[] = $item->getDate(array('cultureFallback' => true));
@@ -225,9 +224,36 @@ class arGenerateCsvReportJob extends arBaseJob
       // Write reference code, container name, title, creation dates
       foreach ($informationObject->getPhysicalObjects() as $item)
       {
-        fputcsv($fh, array($informationObject->referenceCode, $item->__toString(), $informationObject->__toString(),
-                implode($creationDates, '|')));
+        $results[] = array(
+          'referenceCode' => $informationObject->referenceCode,
+          'physicalObjectName' => $item->__toString(),
+          'title' => $informationObject->__toString(),
+          'creationDates' => implode($creationDates, '|'),
+        );
       }
+    }
+
+    return $results;
+  }
+
+  private function writeBoxLabelCsv($results)
+  {
+    if (!count($results))
+    {
+      $this->info($this->i18n->__('No results found for box label report.'));
+      return true;
+    }
+
+    if (null === $fh = fopen($this->filename, 'w'))
+    {
+      throw new sfException('Unable to open file '.$this->filename.' - please check permissions.');
+    }
+
+    fputcsv($fh, array_keys($results[0]));
+
+    foreach ($results as $item)
+    {
+      fputcsv($fh, $item);
     }
 
     fclose($fh);
@@ -330,14 +356,6 @@ class arGenerateCsvReportJob extends arBaseJob
       $includeThumbnails = $this->params['includeThumbnails'];
       $sortBy = $this->params['sortBy'];
       $reportTypeLabel = $this->params['reportTypeLabel'];
-    }
-    else if ('storageLocations' === $this->params['reportType'])
-    {
-      $physicalObjects = $results;
-    }
-    else if ('boxLabel' === $this->params['reportType'])
-    {
-      throw new  sfException('box label html not implemeneted');
     }
 
     ob_start();
