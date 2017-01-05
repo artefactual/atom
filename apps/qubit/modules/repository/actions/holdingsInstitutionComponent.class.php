70be8540d78b84be62844bdfca35e52dbaf8b72d
Steve Breker <sbreker@artefactual.com>
2017-01-05 22:29:57 +0000

    Institutional Block components, Refs# 10733
    Institutional Scoping enhancements.
    
    Commit includes:
    
    - new browse menu component which provides the ability to configure custom
    menu items separately from the standard browse menu. Also provides different
    text labelling than the standard browse component.
    
    - new holdings component which uses the new browse institution component.
    This component is the 'Institutional Block' and is present when institutional
    scoping is turned on, and the user is currently scoped to an institution (not
    browsing globally). Provides search of information objects within the scoped
    repository.
    
    - custom css to support the browse institution component.

---
+++ apps/qubit/modules/repository/actions/holdingsInstitutionComponent.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class RepositoryHoldingsInstitutionComponent extends sfComponent
+{
+  public function execute($request)
+  {
+    $page = 1;
+    $limit = sfConfig::get('app_hits_per_page', 10);
+    $resultSet = RepositoryHoldingsAction::getHoldings($this->resource->id, $page, $limit);
+
+    $this->pager = new QubitSearchPager($resultSet);
+    $this->pager->setPage($page);
+    $this->pager->setMaxPerPage($limit);
+    $this->pager->init();
+  }
+}
