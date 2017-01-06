44baf9480d5c9eb549e630e53602c62b1074f749
Steve Breker <sbreker@artefactual.com>
2017-01-06 17:33:04 +0000

    Inst scoping stylesheet component, Refs #10733
    This commit includes two new stylesheet components. Both components will
    trigger the background theme for the repository they are part of when the
    Institutional Scoping feature is turned on and the user is scoped to a repo.
    
    The application of the Repository theme to these pages is intended to give
    visual cues to the user that they are currently scoped to an institution.
    
    These two components differ in their logic to detect when the repository
    page style should be applied. Repositories always have the stylesheet applied;
    the info obj module should only have it applied if the Institutional Scoping
    feature is turned on, and the Achival Description plugin modules have
    different display logic for the stylesheet depending on whether the Inst
    Scoping feature is turned on or off.
    
    - The information object module component will trigger the theme when the
    institutional scoping setting is turned on and the search-realm is set.
    This is new behaviour for the information object module and is strictly
    dependent on the Institutional scoping feature being turned on.
    
    - The sfIsadPlugin component will first look at the institutional scoping
    setting and examine the search-realm for a repo. If institutional scoping
    setting is off, it will fall back to the original behaviour for the Archival
    Descr plugin modules: try to determine the repo from the sf_route and display
    the repo stylesheet if found. This component replaces the use of the repository
    stylesheet component in the Archival Description plugin modules and
    incorporates new logic for the inst scoping feature.

---
+++ plugins/sfIsadPlugin/modules/sfIsadPlugin/actions/stylesheetComponent.class.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class sfIsadPluginStylesheetComponent extends sfComponent
+{
+  public function execute($request)
+  {
+    // If institutional scoping is on, repo comes from the search-realm
+    if (sfConfig::get('app_enable_institutional_scoping') && sfContext::getInstance()->user->hasAttribute('search-realm') )
+    {
+      $repository = QubitRepository::getById(sfContext::getInstance()->user->getAttribute('search-realm'));
+    }
+    // If the feature is off, fall back to the repository component rules.
+    else if (!sfConfig::get('app_enable_institutional_scoping'))
+    {
+      if (!isset($request->getAttribute('sf_route')->resource))
+      {
+        return sfView::NONE;
+      }
+
+      $resource = $request->getAttribute('sf_route')->resource;
+
+      switch (true)
+      {
+        case $resource instanceof QubitInformationObject:
+          $repository = $resource->getRepository(array('inherit' => true));
+
+          break;
+
+        case $resource instanceof QubitRepository:
+          $repository = $resource;
+
+          break;
+
+        default:
+          return sfView::NONE;
+      }
+    }
+    else
+    {
+      return sfView::NONE;
+    }
+
+    if (null === $repository || null === $repository->backgroundColor)
+    {
+      return sfView::NONE;
+    }
+
+    // Get value
+    $this->backgroundColor = $repository->backgroundColor->__toString();
+
+    if (0 == strlen($this->backgroundColor))
+    {
+      return sfView::NONE;
+    }
+  }
+}
