679cd770cd2c1fb6a5dabedbeb3a57b9b726acc3
Mike Cantelon <mcantelon@gmail.com>
2017-02-22 23:08:41 +0000

    Added option to display available document types.

--- lib/task/search/arPopulateTask.class.php
+++ lib/task/search/arPopulateTask.class.php
@@ -30,7 +30,8 @@ class arSearchPopulateTask extends sfBaseTask
     $this->addOptions(array(
       new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', 'qubit'),
       new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'cli'),
-      new sfCommandOption('exclude-types', null, sfCommandOption::PARAMETER_OPTIONAL, 'Exclude document type(s) (command-separated) from indexing')));
+      new sfCommandOption('exclude-types', null, sfCommandOption::PARAMETER_OPTIONAL, 'Exclude document type(s) (command-separated) from indexing'),
+      new sfCommandOption('show-types', null, sfCommandOption::PARAMETER_NONE, 'Show available document type(s), that can be excluded, before indexing')));
 
     $this->namespace = 'search';
     $this->name = 'populate';
@@ -43,6 +44,8 @@ in the current project. It may take quite a while to run.
 To exclude a document type, use the --exclude-types option. For example:
 
   php symfony search:populate --exclude-types="term,actor"
+
+To see a list of available document types that can be excluded use the --show-types option.
 EOF;
   }
 
@@ -50,10 +53,24 @@ EOF;
   {
     sfContext::createInstance($this->configuration);
 
+    // If show-types flag set, show types available to index
+    if (!empty($options['show-types']))
+    {
+      $this->log(sprintf('Available document types that can be excluded: %s', implode(', ', $this->availableDocumentTypes())));
+      $this->ask('Press the Enter key to continue indexing or CTRL-C to abort...');
+    }
+
     new sfDatabaseManager($this->configuration);
 
     $excludeTypes = (!empty($options['exclude-types'])) ? explode(',', strtolower($options['exclude-types'])) : null;
 
     QubitSearch::getInstance()->populate($excludeTypes);
+  }
+
+  protected function availableDocumentTypes()
+  {
+    $types = array_keys(QubitSearch::getInstance()->loadMappings()->asArray());
+    sort($types);
+    return $types;
   }
 }
