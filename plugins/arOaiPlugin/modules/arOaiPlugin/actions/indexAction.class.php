1eb5246915b01c95fdcf064af558b44b3b695d08
Mike Cantelon <mcantelon@gmail.com>
2017-02-25 06:58:57 +0000

    Whitespace fix.

--- plugins/arOaiPlugin/modules/arOaiPlugin/actions/indexAction.class.php
+++ plugins/arOaiPlugin/modules/arOaiPlugin/actions/indexAction.class.php
@@ -27,14 +27,14 @@
 class arOaiPluginIndexAction extends sfAction
 {
   public $oaiErrorArr = array(
-    'badArgument'=>'The request includes illegal arguments, is missing required arguments, includes a repeated argument, or values for arguments have an illegal syntax.',
-    'badResumptionToken'=>'The value of the resumptionToken argument is invalid or expired.',
-    'badVerb'=>'Value of the verb argument is not a legal OAI-PMH verb, the verb argument is missing, or the verb argument is repeated.',
-    'cannotDisseminateFormat'=>'The metadata format identified by the value given for the metadataPrefix argument is not supported by the item or by the repository.',
-    'idDoesNotExist'=>'The value of the identifier argument is unknown or illegal in this repository.',
-    'noRecordsMatch'=>'The combination of the values of the from, until, set and metadataPrefix arguments results in an empty list.',
-    'noMetadataFormats'=>'There are no metadata formats available for the specified item.',
-    'noSetHierarchy'=>'The repository does not support sets.'
+    'badArgument' => 'The request includes illegal arguments, is missing required arguments, includes a repeated argument, or values for arguments have an illegal syntax.',
+    'badResumptionToken' => 'The value of the resumptionToken argument is invalid or expired.',
+    'badVerb' => 'Value of the verb argument is not a legal OAI-PMH verb, the verb argument is missing, or the verb argument is repeated.',
+    'cannotDisseminateFormat' => 'The metadata format identified by the value given for the metadataPrefix argument is not supported by the item or by the repository.',
+    'idDoesNotExist' => 'The value of the identifier argument is unknown or illegal in this repository.',
+    'noRecordsMatch' => 'The combination of the values of the from, until, set and metadataPrefix arguments results in an empty list.',
+    'noMetadataFormats' => 'There are no metadata formats available for the specified item.',
+    'noSetHierarchy' => 'The repository does not support sets.'
   );
 
   public $oaiVerbArr = array('Identify', 'ListMetadataFormats', 'ListSets', 'ListRecords', 'ListIdentifiers', 'GetRecord');
