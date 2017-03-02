33e0100034d77c8c7eac2e0b73793566ace3d981
Mike Gale <mikeg@artefactual.com>
2017-03-02 14:40:07 -0800

    Restore clear clipboard btn for anonymous users
    An isAuthenticated check was preventing non-authenticated users from seeing the
    clear clipboard button in the actions toolbar. This rectifies that.

--- apps/qubit/modules/user/templates/clipboardSuccess.php
+++ apps/qubit/modules/user/templates/clipboardSuccess.php
@@ -45,8 +45,8 @@
 
   <section class="actions">
     <ul>
+      <li><?php echo link_to(__('Clear %1 clipboard', array('%1' => lcfirst($uiLabels[$type]))), array('module' => 'user', 'action' => 'clipboardClear', 'type' => $entityType), array('class' => 'c-btn c-btn-delete')) ?></li>
       <?php if (isset($pager) && $pager->hasResults() && $sf_user->isAuthenticated()): ?>
-        <li><?php echo link_to (__('Clear %1 clipboard', array('%1' => lcfirst($uiLabels[$type]))), array('module' => 'user', 'action' => 'clipboardClear', 'type' => $entityType), array('class' => 'c-btn c-btn-delete')) ?></li>
         <li><?php echo link_to(__('Export'), array('module' => 'object', 'action' => 'export', 'objectType' => $type), array('class' => 'c-btn')) ?></li>
       <?php endif; ?>
     </ul>
