cfdb31327915e896c9721d72d015d7a1c33cd518
Anthony Sheehy <35136121+AnthonyCSheehy@users.noreply.github.com>
2018-01-18 10:52:21 +0000

    Update adAction.class.php
    Class name camelcase-ified...

--- apps/qubit/modules/settings/actions/adAction.class.php
+++ apps/qubit/modules/settings/actions/adAction.class.php
@@ -17,7 +17,7 @@
  * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
  */
 
-class SettingsadAction extends DefaultEditAction
+class SettingsADAction extends DefaultEditAction
 {
   // Arrays not allowed in class constants
   public static
