b0ea2eb3e7fd1be8dc8604666a36f473ab8e0b88
Mike Gale <mikeg@artefactual.com>
2018-02-05 16:38:49 -0800

    Clean up name access points section for templates
    No long display non-subject name access points under the name access points
    section of the archival description pages.

--- apps/qubit/modules/informationobject/templates/_nameAccessPoints.php
+++ apps/qubit/modules/informationobject/templates/_nameAccessPoints.php
@@ -10,13 +10,16 @@
 
   <div>
     <ul>
-      <?php $actorsShown = array(); ?>
-      <?php foreach ($resource->getActorEvents() as $item): ?>
-        <?php if (isset($item->actor) && !isset($actorsShown[$item->actor->id])): ?>
-          <li><?php echo link_to(render_title($item->actor), array($item->actor)) ?> <span class="note2">(<?php echo $item->type->getRole() ?>)</span></li>
-          <?php $actorsShown[$item->actor->id] = true; ?>
-        <?php endif; ?>
-      <?php endforeach; ?>
+      <?php if (isset($sidebar)): ?>
+        <?php $actorsShown = array(); ?>
+        <?php foreach ($resource->getActorEvents() as $item): ?>
+          <?php if (isset($item->actor) && !isset($actorsShown[$item->actor->id])): ?>
+            <li><?php echo link_to(render_title($item->actor), array($item->actor)) ?> <span class="note2">(<?php echo $item->type->getRole() ?>)</span></li>
+            <?php $actorsShown[$item->actor->id] = true; ?>
+          <?php endif; ?>
+        <?php endforeach; ?>
+      <?php endif; ?>
+
       <?php foreach ($resource->relationsRelatedBysubjectId as $item): ?>
         <?php if (isset($item->type) && QubitTerm::NAME_ACCESS_POINT_ID == $item->type->id): ?>
           <li><?php echo link_to(render_title($item->object), array($item->object, 'module' => 'actor')) ?><span class="note2"> <?php if (!isset($mods)): ?>(<?php echo __('Subject') ?>)<?php endif; ?></span></li>
