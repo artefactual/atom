2fa527c18c4a2da8146e822312c64ac8795c599d
Mike Cantelon <mcantelon@gmail.com>
2018-02-18 09:47:52 +0000

    Change actor search operator, refs #11943
    Change the default search operator, for actor search, from OR to AND.

--- apps/qubit/modules/actor/actions/browseAction.class.php
+++ apps/qubit/modules/actor/actions/browseAction.class.php
@@ -110,7 +110,7 @@ class ActorBrowseAction extends DefaultBrowseAction
     else
     {
       $queryText = new \Elastica\Query\QueryString(arElasticSearchPluginUtil::escapeTerm($request->subquery));
-      $queryText->setDefaultOperator('OR');
+      $queryText->setDefaultOperator('AND');
       arElasticSearchPluginUtil::setFields($queryText, 'actor');
 
       $this->search->queryBool->addMust($queryText);
