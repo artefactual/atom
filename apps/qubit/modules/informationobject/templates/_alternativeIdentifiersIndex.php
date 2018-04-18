278f800d3c065149f1af9dbdf92dcf7819747fc3
Mike Cantelon <mcantelon@gmail.com>
2018-04-18 17:59:29 +0000

    work

--- apps/qubit/modules/informationobject/templates/_alternativeIdentifiersIndex.php
+++ apps/qubit/modules/informationobject/templates/_alternativeIdentifiersIndex.php
@@ -4,7 +4,7 @@
 
   <div>
     <?php foreach ($resource->getProperties(null, 'alternativeIdentifiers') as $item): ?>
-      <?php echo render_show(render_value($item->name), render_value($item->value)) ?>
+      <?php echo render_show(render_value($item->name), render_value($item->getValue(array('cultureFallback' => true)))) ?>
     <?php endforeach; ?>
   </div>
 
