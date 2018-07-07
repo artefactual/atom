787376c02c17184e58538e7d44b4d4f2f80c8736
Mike Cantelon <mcantelon@gmail.com>
2018-07-07 03:16:38 +0000

    Add tooltip for calculate dates link, refs #12082
    Added a TITLE attribute to the calculate dates link to explain the calculate
    dates functionality.

--- apps/qubit/modules/informationobject/templates/_calculateDatesLink.php
+++ apps/qubit/modules/informationobject/templates/_calculateDatesLink.php
@@ -2,7 +2,7 @@
   <li class="separator"><h4><?php echo __('Tasks') ?></h4></li>
 
   <li>
-    <a href="<?php echo url_for(array($resource, 'module' => 'informationobject', 'action' => 'calculateDates')) ?>">
+    <a href="<?php echo url_for(array($resource, 'module' => 'informationobject', 'action' => 'calculateDates')) ?>" title="<?php echo __("Click 'Calculate dates' to recalculate the start and end dates of a parent-level description. A job runs in the background, accounting for the earliest and most recent dates across all the child descriptions. The results display in the Start and End fields of the edit page.") ?>">
       <i class="fa fa-calendar"></i>
       <?php echo __('Calculate dates') ?>
     </a>
