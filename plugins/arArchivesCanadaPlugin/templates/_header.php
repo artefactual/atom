026f4442afe66172b009ae15b939f477b4074040
José Raddaoui Marín <raddaouimarin@gmail.com>
2018-08-30 14:52:24 +0200

    Fix arArchivesCanadaPlugin header, refs #12403
    Add missing GPDR banner and site URL check to theme plugin header.

--- plugins/arArchivesCanadaPlugin/templates/_header.php
+++ plugins/arArchivesCanadaPlugin/templates/_header.php
@@ -2,6 +2,14 @@
 
 <?php echo get_component('default', 'updateCheck') ?>
 
+<?php echo get_component('default', 'privacyMessage') ?>
+
+<?php if ($sf_user->isAdministrator() && (string)QubitSetting::getByName('siteBaseUrl') === ''): ?>
+  <div class="site-warning">
+    <?php echo link_to(__('Please configure your site base URL'), 'settings/siteInformation', array('rel' => 'home', 'title' => __('Home'))) ?>
+  </div>
+<?php endif; ?>
+
 <?php if ($sf_user->isAuthenticated()): ?>
   <div id="top-bar">
     <nav>
