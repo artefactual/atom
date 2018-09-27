1718ca39c47e9e680588116cb1907af9c5c05f07
David Juhasz <djjuhasz@gmail.com>
2018-09-27 23:53:18 +0000

    Fix date duplication with old headers, refs #12467
    * Fix strict type comparison bug
    * Simplify checks for matching event on update

--- lib/task/import/csvImportBaseTask.class.php
+++ lib/task/import/csvImportBaseTask.class.php
@@ -223,8 +223,10 @@ abstract class csvImportBaseTask extends arBaseTask
     {
       if (!isset($eventData['type']))
       {
-        // Creation is the default event type
-        $eventTypeId = QubitTerm::CREATION_ID;
+        // Creation is the default event type. Cast variable as string to avoid
+        // a type mismatch when testing if it's a duplicate event in 
+        // QubitFlatfileImport::hasDuplicateEvent()
+        $eventTypeId = (string) QubitTerm::CREATION_ID;
       }
       else
       {
@@ -235,53 +237,53 @@ abstract class csvImportBaseTask extends arBaseTask
         unset($eventData['type']);
       }
 
+      // If in update mode, check if the import event data matches an existing 
+      // event
+      if ($import->matchAndUpdate && 
+        null !== $event = self::matchExistingEvent($import->object->id, $eventTypeId, $eventData['actorName']))
+      {
+        $eventData['eventId'] = $event->id;
+      }
+
       // Add row culture to fetch place term in event creation/update
       $eventData['culture'] = $import->columnValue('culture');
 
-      // If in update mode and CSV event data references an existing actor,
-      // see if the event seems like it should be updated rather than created.
-      if ($import->matchAndUpdate && !empty($eventData['actorName'])
-          && null != $actor = QubitActor::getByAuthorizedFormOfName($eventData['actorName']))
-      {
-        // Check events to determine whether event should be updated rather than created
-        $criteria = new Criteria;
-        $criteria->add(QubitEvent::TYPE_ID, $eventTypeId);
-        $criteria->add(QubitEvent::OBJECT_ID, $import->object->id);
-        $criteria->add(QubitEvent::ACTOR_ID, $actor->id);
-
-        foreach(QubitEvent::get($criteria) as $event)
-        {
-          // Work out whether all CSV date fields are empty
-          $dateFieldsEmpty = empty($eventData['date']) && empty($eventData['startDate']) && empty($eventData['endDate']);
-
-          // Work out whether CSV date fields are different than what's currently stored
-          $datesDifferent = $event->date != $eventData['date'] || $event->startDate != $eventData['startDate']
-             || $event->endDate != $eventData['endDate'];
-
-          // Update is actor bio populated or if CSV fields empty or different from stored
-          if (!empty($eventData['actorHistory']) || $dateFieldsEmpty || $datesDifferent)
-          { 
-            $eventData['eventId'] = $event->id;
-            break;
-          }
-        }
-      }
-      else if ($import->matchAndUpdate && empty($eventData['actorName']))
-      {
-        // Check events to determine whether event should be updated rather than created
-        $criteria = new Criteria;
-        $criteria->add(QubitEvent::TYPE_ID, $eventTypeId);
-        $criteria->add(QubitEvent::OBJECT_ID, $import->object->id);
-        $criteria->add(QubitEvent::ACTOR_ID, null, Criteria::ISNULL);
-
-        if ((null !== $event = QubitEvent::getOne($criteria))
-           && ($event->startDate != $eventData['startDate'] || $event->endDate != $eventData['endDate']))
-        {
-          $eventData['eventId'] = $event->id;
-        }
-      }
-
       $import->createOrUpdateEvent($eventTypeId, $eventData);
+    }
+  }
+
+  static function matchExistingEvent($objectId, $typeId, $actorName)
+  {
+    // Check for a matching event to update
+    $criteria = new Criteria;
+    $criteria->add(QubitEvent::TYPE_ID, $typeId);
+    $criteria->add(QubitEvent::OBJECT_ID, $objectId);
+    
+    // Search for a related event linked to the provided actor name
+    if (!isset($actorName))
+    {
+      // If no actor name is provided, check for a related event with no actor
+      $criteria->add(QubitEvent::ACTOR_ID, null, Criteria::ISNULL);
+    }
+    else
+    {
+      if (null !== $actor = QubitActor::getByAuthorizedFormOfName($actorName))
+      {
+        // If we found a matching actor, then check for an event related to
+        // that actor
+        $criteria->add(QubitEvent::ACTOR_ID, $actor->id);
+      }
+      else
+      {
+        // The provided actor name doesn't exist, so create a new event
+        return;
+      }
+    }
+
+    // Return the matching event, if one is found
+    if (null !== $event = QubitEvent::getOne($criteria))
+    {
+      return $event;
     }
   }
 
