d30f07fd6e1449174048bdf057c9f8efa0ba9bfe
Matt Innes <matthew.f.innes@gmail.com>
2021-01-17 00:54:47 -0500

    Add responsive progress bar for audio
    - Add responsive sizing to fix behaviour where audio progress bar extends beyond the right edge, particularly on small screens.

--- apps/qubit/modules/digitalobject/templates/_showAudio.php
+++ apps/qubit/modules/digitalobject/templates/_showAudio.php
@@ -3,7 +3,7 @@
 <?php if (QubitTerm::REFERENCE_ID == $usageType): ?>
 
   <?php if ($showFlashPlayer): ?>
-    <audio class="mediaelement-player" src="<?php echo public_path($representation->getFullPath()) ?>"></audio>
+    <audio style="max-width:100%;" class="mediaelement-player" src="<?php echo public_path($representation->getFullPath()) ?>"></audio>
   <?php else: ?>
     <div style="text-align: center">
       <?php echo image_tag($representation->getFullPath(), array('style' => 'border: #999 1px solid', 'alt' => '')) ?>
