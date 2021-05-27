2c8f75102432ab0ca0e2ae006c8feb90791aa89a
Matt Innes <matthew.f.innes@gmail.com>
2021-05-27 13:44:38 -0400

    Add title attributes to view options, refs #13510

--- apps/qubit/modules/default/templates/_viewPicker.php
+++ apps/qubit/modules/default/templates/_viewPicker.php
@@ -3,9 +3,9 @@
 <div class="btn-group">
   <?php echo link_to(' ', ['module' => $module, 'action' => 'browse', 'view' => $cardView] +
                       $sf_data->getRaw('sf_request')->getParameterHolder()->getAll(),
-                      ['class' => 'btn fa fa-th-large '.($view === $cardView ? 'active' : '')]); ?>
+                      ['class' => 'btn fa fa-th-large '.($view === $cardView ? 'active' : ''), 'title' => __('Card view')]); ?>
 
   <?php echo link_to(' ', ['module' => $module, 'action' => 'browse', 'view' => $tableView] +
                       $sf_data->getRaw('sf_request')->getParameterHolder()->getAll(),
-                      ['class' => 'btn fa fa-list '.($view === $tableView ? 'active' : '')]); ?>
+                      ['class' => 'btn fa fa-list '.($view === $tableView ? 'active' : ''), 'title' => __('Table view')]); ?>
 </div>
