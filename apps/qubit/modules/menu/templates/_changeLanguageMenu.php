0e595c27e5688aee84bd8b5891085431157987f2
Matt Innes <matthew.f.innes@gmail.com>
2021-06-03 22:20:54 -0400

    Initial letters to uc in lang menu, refs #13515
    Initial letters of language names in the language menu are now
    uppercase to allow for consistency across i18n cultures.

--- apps/qubit/modules/menu/templates/_changeLanguageMenu.php
+++ apps/qubit/modules/menu/templates/_changeLanguageMenu.php
@@ -16,7 +16,7 @@
       <ul>
         <?php foreach ($langCodes as $value) { ?>
           <li<?php if ($sf_user->getCulture() == $value) { ?> class="active"<?php } ?>>
-            <?php echo link_to(format_language($value, $value), ['sf_culture' => $value] + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()); ?>
+            <?php echo link_to(ucfirst(format_language($value, $value)), ['sf_culture' => $value] + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()); ?>
           </li>
         <?php } ?>
       </ul>
