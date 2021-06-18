1b7de0c521514741c9f4bdda98685612378c2b29
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-18 13:51:37 +0200

    Start arDominionB5Plugin
    - Add Bootstrap 5 and jQuery 3.6 to NPM dependencies.
    - Initialize theme plugin with:
      - Basic configuration.
      - Layout templates using and adapted to Bootstrap 5, and without other
        components (JS and CSS from view. yml files).
      - Layout partials (start and end) to avoid duplication.
      - WIP image for themes form.

---
+++ plugins/arDominionB5Plugin/templates/_layout_start.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="<?php echo $sf_user->getCulture(); ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction; ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include_title(); ?>
+    <?php echo get_component('default', 'tagManager', ['code' => 'script']); ?>
+    <link rel="shortcut icon" href="<?php echo public_path('favicon.ico'); ?>"/>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <!-- TODO: Move jQuery after footer when all JS is also there -->
+    <script src="/node_modules/jquery/dist/jquery.min.js"></script>
+  </head>
+  <body class="<?php echo $sf_context->getModuleName(); ?> <?php echo $sf_context->getActionName(); ?>">
+    <?php echo get_component('default', 'tagManager', ['code' => 'noscript']); ?>
+    <?php echo get_partial('header'); ?>
+    <?php include_slot('pre'); ?>
