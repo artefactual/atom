892d07041f83eb4c587d24da855c9c7986b30c88
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-18 13:51:37 +0200

    Start arDominionB5Plugin
    - Add Bootstrap 5 and jQuery 3.6 to NPM dependencies.
    - Initialize theme plugin with:
      - Basic configuration.
      - Layout templates using and adapted to Bootstrap 5, and without other
        components (JS and CSS from view. yml files).
      - Layout partials (start and end) to avoid duplication.
      - WIP image for themes form.

---
+++ plugins/arDominionB5Plugin/templates/layout_1col.php
@@ -0,0 +1,15 @@
+<?php echo get_partial('layout_start'); ?>
+
+<div id="wrapper" class="container-xl" role="main">
+  <?php echo get_partial('alerts'); ?>
+  <?php include_slot('title'); ?>
+  <?php include_slot('before-content'); ?>
+  <?php if (!include_slot('content')) { ?>
+    <div id="content">
+      <?php echo $sf_content; ?>
+    </div>
+  <?php } ?>
+  <?php include_slot('after-content'); ?>
+</div>
+
+<?php echo get_partial('layout_end'); ?>
