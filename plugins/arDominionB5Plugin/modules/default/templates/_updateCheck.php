caebd0e06680a6772f650d01632e630da7a766f4
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-20 21:22:35 +0200

    Add update check component to arDominionB5Plugin
    - Duplication required in action to avoid JS always.
    - Template updated to use the B5 classes and clean-up.
    - Add temporary CSS to style it like the main theme.

---
+++ plugins/arDominionB5Plugin/modules/default/templates/_updateCheck.php
@@ -0,0 +1,9 @@
+<div id="update-check" class="text-center p-1">
+  <?php echo __(
+      'A %1%release %2% upgrade is available.',
+      [
+          '%1%' => '<a href="https://www.accesstomemory.org/download/" target="_blank">',
+          '%2%' => $lastVersion.'</a>',
+      ]
+  ); ?>
+</div>
