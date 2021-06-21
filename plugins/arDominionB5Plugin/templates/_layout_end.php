f0ab1dbf1d58c8a98aaec83a926d25d75a0c36d1
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-21 07:31:47 +0200

    Add privacy message to arDominionB5Plugin
    - Use new Bootstrap 5 alert.
    - Use default close button for now.
    - Simplify JS and move it after footer.
    - Move focus to navbar brand after alert close.
    - Remove alert margin and border radius.

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -1,5 +1,6 @@
     <?php include_slot('post'); ?>
     <?php echo get_partial('footer'); ?>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/plugins/arDominionB5Plugin/js/privacyMessage.js"></script>
   </body>
 </html>
