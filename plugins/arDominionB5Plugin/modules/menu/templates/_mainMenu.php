371e5318a6847add6609a03d58d5520a051abea1
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-21 16:40:45 +0200

    Continue arDominionB5Plugin header
    - Add collapsable section and toggle button.
    - Tweak responsive with flex and spacing classes.
    - Add menus and fake search box.
    - Avoid QubitMenu::displayHierarchyAsList and add a new function to
      QubitMenu to check user access outside displayHierarchyAsList.
    - Increase defaul min-width in header drop-downs.
    - Clipboard menu still incomplete (needs JS part).

---
+++ plugins/arDominionB5Plugin/modules/menu/templates/_mainMenu.php
@@ -0,0 +1,28 @@
+<?php foreach ([$addMenu, $manageMenu, $importMenu, $adminMenu] as $menu) { ?>
+  <?php if ($menu && ('add' == $menu->getName() || 'manage' == $menu->getName()) || $sf_user->isAdministrator()) { ?>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="<?php echo $menu->getName(); ?>-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="<?php echo $menu->getName(); ?>-menu">
+        <li>
+          <h5 class="dropdown-item-text">
+            <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
+          </h5>
+        </li>
+        <li><hr class="dropdown-divider"></li>
+        <?php foreach ($menu->getChildren() as $child) { ?>
+          <?php if ($child->checkUserAccess()) { ?>
+            <li>
+              <?php echo link_to(
+                  $child->getLabel(['cultureFallback' => true]),
+                  $child->getPath(['getUrl' => true, 'resolveAlias' => true]),
+                  ['class' => 'dropdown-item']
+              ); ?>
+            </li>
+          <?php } ?>
+        <?php } ?>
+      </ul>
+    </li>
+  <?php } ?>
+<?php } ?>
