056c040d1db8ac82fcc25c439c11623635e775c2
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-21 17:04:19 +0200

    Add missing layout and margin to containers

---
+++ plugins/arDominionB5Plugin/templates/layout.php
@@ -0,0 +1,8 @@
+<?php echo get_partial('layout_start'); ?>
+
+<div id="wrapper" class="container-xl my-3" role="main">
+  <?php echo get_partial('alerts'); ?>
+  <?php echo $sf_content; ?>
+</div>
+
+<?php echo get_partial('layout_end'); ?>
