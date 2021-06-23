1727766dcf6f32902b06ebc645d3fb443c5a3bcb
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-23 14:51:35 +0200

    Improve header layout and responsiveness

--- plugins/arDominionB5Plugin/modules/menu/templates/_browseMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_browseMenu.php
@@ -1,14 +1,13 @@
-<div class="dropdown me-2 mb-2 mb-lg-0">
+<div class="dropdown me-2 mb-2 mb-lg-0 mt-2">
   <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="browse-menu" data-bs-toggle="dropdown" aria-expanded="false">
     <?php echo $browseMenu->getLabel(['cultureFallback' => true]); ?>
   </button>
-  <ul class="dropdown-menu" aria-labelledby="browse-menu">
+  <ul class="dropdown-menu mt-2" aria-labelledby="browse-menu">
     <li>
-      <h5 class="dropdown-item-text">
+      <h6 class="dropdown-header">
         <?php echo $browseMenu->getLabel(['cultureFallback' => true]); ?>
-      </h5>
+      </h6>
     </li>
-    <li><hr class="dropdown-divider"></li>
     <?php foreach ($browseMenu->getChildren() as $child) { ?>
       <?php if ($child->checkUserAccess()) { ?>
         <li>
