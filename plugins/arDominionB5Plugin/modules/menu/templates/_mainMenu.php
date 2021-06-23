62a989ef7319bf8b7e4b6c2800dff09626a7a862
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-23 14:51:35 +0200

    Improve header layout and responsiveness

--- plugins/arDominionB5Plugin/modules/menu/templates/_mainMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_mainMenu.php
@@ -1,17 +1,17 @@
 <?php foreach ([$addMenu, $manageMenu, $importMenu, $adminMenu] as $menu) { ?>
   <?php if ($menu && ('add' == $menu->getName() || 'manage' == $menu->getName()) || $sf_user->isAdministrator()) { ?>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="<?php echo $menu->getName(); ?>-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <li class="nav-item dropdown d-flex flex-column">
+      <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="<?php echo $menu->getName(); ?>-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
         <i class="fas fa-2x fa-<?php echo $icons[$menu->getName()]; ?>" aria-hidden="true"></i>
-        <span class="sr-only"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
+        <span class="d-lg-none ms-2" aria-hidden="true"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
+        <span class="visually-hidden"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
       </a>
       <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="<?php echo $menu->getName(); ?>-menu">
         <li>
-          <h5 class="dropdown-item-text">
+          <h6 class="dropdown-header">
             <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
-          </h5>
+          </h6>
         </li>
-        <li><hr class="dropdown-divider"></li>
         <?php foreach ($menu->getChildren() as $child) { ?>
           <?php if ($child->checkUserAccess()) { ?>
             <li>
--- plugins/arDominionB5Plugin/modules/menu/templates/_userMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_userMenu.php
@@ -1,15 +1,14 @@
 <?php if ($showLogin) { ?>
-  <div class="dropdown my-2 my-lg-0">
-    <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="user-menu" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+  <div class="dropdown my-2 me-1">
+    <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="user-menu" data-bs-toggle="dropdown" aria-expanded="false">
       <?php echo $menuLabels['login']; ?>
     </button>
-    <div class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="user-menu">
-      <li>
-        <h5 class="dropdown-item-text">
+    <div class="dropdown-menu dropdown-menu-lg-end mt-2" aria-labelledby="user-menu">
+      <div>
+        <h6 class="dropdown-header">
           <?php echo __('Have an account?'); ?>
-        </h5>
-      </li>
-      <li><hr class="dropdown-divider"></li>
+        </h6>
+      </div>
       <?php echo $form->renderFormTag(url_for(['module' => 'user', 'action' => 'login']), ['class' => 'mx-3 my-2']); ?>
         <?php echo $form->renderHiddenFields(); ?>
         <?php echo $form->email->renderRow(['class' => 'form-control form-control-sm mb-3']); ?>
@@ -21,18 +20,17 @@
     </div>
   </div>
 <?php } elseif ($sf_user->isAuthenticated()) { ?>
-  <div class="dropdown my-2 my-lg-0">
-    <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="user-menu" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+  <div class="dropdown my-2 me-1">
+    <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="user-menu" data-bs-toggle="dropdown" aria-expanded="false">
       <?php echo $sf_user->user->username; ?>
     </button>
-    <ul class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="user-menu">
+    <ul class="dropdown-menu dropdown-menu-lg-end mt-2" aria-labelledby="user-menu">
       <li>
-        <h5 class="dropdown-item-text">
+        <h6 class="dropdown-header">
           <?php echo image_tag($gravatar, ['alt' => '']); ?>&nbsp;
           <?php echo __('Hi, %1%', ['%1%' => $sf_user->user->username]); ?>
-        </h5>
+        </h6>
       </li>
-      <li><hr class="dropdown-divider"></li>
       <li><?php echo link_to($menuLabels['myProfile'], [$sf_user->user, 'module' => 'user'], ['class' => 'dropdown-item']); ?></li>
       <li><?php echo link_to($menuLabels['logout'], ['module' => 'user', 'action' => 'logout'], ['class' => 'dropdown-item']); ?></li>
     </ul>
