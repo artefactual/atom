8c97ffd5b47593b63ac5a065d8571f9a5c2ee40c
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-23 14:51:35 +0200

    Improve header layout and responsiveness

--- plugins/arDominionB5Plugin/templates/_header.php
+++ plugins/arDominionB5Plugin/templates/_header.php
@@ -16,28 +16,28 @@
           <?php echo image_tag('/plugins/arDominionB5Plugin/images/logo', ['alt' => __('AtoM logo'), 'class' => 'd-inline-block me-2']); ?>
         <?php } ?>
         <?php if (sfConfig::get('app_toggleTitle')) { ?>
-          <span class="align-middle"><?php echo esc_specialchars(sfConfig::get('app_siteTitle')); ?></span>
+          <span class="align-middle text-wrap"><?php echo esc_specialchars(sfConfig::get('app_siteTitle')); ?></span>
         <?php } ?>
       </a>
     <?php } ?>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="<?php echo __('Toggle navigation'); ?>">
+    <button class="navbar-toggler my-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false" aria-label="<?php echo __('Toggle navigation'); ?>">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse flex-wrap justify-content-end" id="navbar-content">
-      <div class="d-flex flex-wrap flex-lg-nowrap me-auto mt-2 mt-lg-0">
+      <div class="d-flex flex-wrap flex-lg-nowrap me-auto mb-lg-1">
         <?php echo get_component('menu', 'browseMenu', ['sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID()]); ?>
-        <form class="d-flex mb-2 mb-lg-0">
+        <form class="d-flex mb-2 mb-lg-0 mt-2">
           <div class="input-group flex-nowrap">
             <input class="form-control form-control-sm" type="search" placeholder="<?php echo __('Search'); ?>" aria-label="<?php echo __('Search'); ?>">
             <button class="btn btn-sm btn-secondary" type="submit">
               <i class="fas fa-search" aria-hidden="true"></i>
-              <span class="sr-only"><?php echo __('Search'); ?></span>
+              <span class="visually-hidden"><?php echo __('Search'); ?></span>
             </button>
           </div>
         </form>
       </div>
       <div class="d-flex flex-nowrap flex-column flex-lg-row align-items-strech align-items-lg-center">
-        <ul class="navbar-nav ms-lg-2">
+        <ul class="navbar-nav mx-lg-2">
           <?php echo get_component('menu', 'mainMenu', ['sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID()]); ?>
           <?php echo get_component('menu', 'clipboardMenu'); ?>
           <?php if (sfConfig::get('app_toggleLanguageMenu')) { ?>
