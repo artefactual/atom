5f8eb4b507f5268ab49f26d401aa7258a2bf8a1b
Steve Breker <sbreker@artefactual.com>
2021-06-23 19:11:40 +0000

    Make Uppy work in the B5 theme
    - Removed reference to javascript 'Qubit' data structure used to pass
    data from the template to multiFileUpload.js.
    - Add references to vendor/uppy minified js and CSS.
    
    Going forward we would want to install Uppy Core, Dashboard and
    XHRUpload via NPM but this will require aidding a .js bundler.

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -1,6 +1,8 @@
     <?php include_slot('post'); ?>
     <?php echo get_partial('footer'); ?>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/vendor/uppy/uppy-bundle.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/privacyMessage.js"></script>
+    <script src="/plugins/arDominionB5Plugin/js/multiFileUpload.js"></script>
   </body>
 </html>
