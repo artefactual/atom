11702c0f00092c0da6799a66482c35bd1488c7cc
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-24 13:50:57 +0200

    Start using Sass
    - Add `sass` to development dependencies.
    - Update NPM build scripts and add one to watch changes.
    - Update build themes CSS task to compile Sass themes.
    - Add arDominionB5Plugin/build folder to .gitignore.
    - Update temporary CSS to use Sass and B5 variables.
    - Use HTML classes to style where possible.
    - Change layout to include only the new built CSS.

--- plugins/arDominionB5Plugin/modules/default/templates/_updateCheck.php
+++ plugins/arDominionB5Plugin/modules/default/templates/_updateCheck.php
@@ -1,8 +1,8 @@
-<div class="site-warning text-center p-1">
+<div class="bg-primary text-center p-1">
   <?php echo __(
       'A %1%release %2% upgrade is available.',
       [
-          '%1%' => '<a href="https://www.accesstomemory.org/download/" target="_blank">',
+          '%1%' => '<a href="https://www.accesstomemory.org/download/" target="_blank" class="text-white">',
           '%2%' => $lastVersion.'</a>',
       ]
   ); ?>
