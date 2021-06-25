647e4e1e6e0a86e9e10414abf48459cf2f8e09f4
Steve Breker <sbreker@artefactual.com>
2021-06-25 22:51:00 +0000

    Make MediaElement work on B5 theme
    - Added MediaElement to package.json to install via npm.
    - Removed refs to MediaElement js and css from the show* actions.
    - Added class 'mejs__player' to the show* templates on <video> and
    <audio> elements.
    - Load options using the <video> and <audio> element attribute
    'data-mejsoptions'.
    - Added reference to MediaElement minified js and css to B5 plugin start
    and end templates.

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -1,6 +1,7 @@
     <?php include_slot('post'); ?>
     <?php echo get_partial('footer'); ?>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/node_modules/mediaelement/build/mediaelement-and-player.min.js"></script>
     <script src="/vendor/uppy/uppy-bundle.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/privacyMessage.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/multiFileUpload.js"></script>
