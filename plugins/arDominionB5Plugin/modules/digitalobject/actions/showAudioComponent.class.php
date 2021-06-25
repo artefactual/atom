26c890c1885579fb3d7e0201e59292aad3bcab60
Steve Breker <sbreker@artefactual.com>
2021-06-25 22:51:00 +0000

    Make MediaElement work on B5 theme
    - Added MediaElement to package.json to install via npm.
    - Removed refs to MediaElement js and css from the show* actions.
    - Added class 'mejs__player' to the show* templates on <video> and
    <audio> elements.
    - Load options using the <video> and <audio> element attribute
    'data-mejsoptions'.
    - Added reference to MediaElement minified js and css to B5 plugin start
    and end templates.

---
+++ plugins/arDominionB5Plugin/modules/digitalobject/actions/showAudioComponent.class.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Digital Object audio display component.
+ *
+ * @author     Jesús García Crespo <correo@sevein.com>
+ */
+class DigitalObjectShowAudioComponent extends sfComponent
+{
+    /**
+     * Show a representation of a digital object image.
+     *
+     * @param sfWebRequest $request
+     */
+    public function execute($request)
+    {
+        // Get representation by usage type
+        $this->representation = $this->resource->getRepresentationByUsage($this->usageType);
+
+        // If we can't find a representation for this object, try their parent
+        if (!$this->representation && ($parent = $this->resource->parent)) {
+            $this->representation = $parent->getRepresentationByUsage($this->usageType);
+        }
+
+        // Set up display of video in mediaelement
+        if ($this->representation) {
+            $this->showMediaPlayer = true;
+        } else {
+            $this->showMediaPlayer = false;
+
+            $this->representation = QubitDigitalObject::getGenericRepresentation($this->resource->mimeType, $this->usageType);
+        }
+    }
+}
