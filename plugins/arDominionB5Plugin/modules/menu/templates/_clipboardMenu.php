d30df1d70e357aabc39f74da58fab0ecc1eb81a6
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-26 22:25:50 +0200

    Add tooltips to navigation icons
    And use defaults in SCSS vars and arrow functions in JS.

--- plugins/arDominionB5Plugin/modules/menu/templates/_clipboardMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_clipboardMenu.php
@@ -1,13 +1,13 @@
 <li class="nav-item dropdown d-flex flex-column">
-  <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="<?php echo $menu->getName(); ?>-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <i class="fas fa-2x fa-paperclip" aria-hidden="true"></i>
+  <a class="nav-link dropdown-toggle d-flex align-items-center p-0" href="#" id="<?php echo $menu->getName(); ?>-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <i class="fas fa-2x fa-paperclip px-0 px-lg-2 py-2" aria-hidden="true" data-tooltip="<?php echo $menu->getLabel(['cultureFallback' => true]); ?>"></i>
     <span class="d-lg-none ms-2" aria-hidden="true"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
     <span class="visually-hidden"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
   </a>
   <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="<?php echo $menu->getName(); ?>-menu">
     <li>
       <h6 class="dropdown-header">
-        <?php echo __('Clipboard'); ?>
+        <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
       </h6>
     </li>
     <?php foreach ($menu->getChildren() as $child) { ?>
