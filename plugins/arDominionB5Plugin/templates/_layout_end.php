b666071ff02b5aae16a3d45abdefb77f0b734609
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-06-27 22:43:17 +0200

    Add search box autocomplete dropdown
    - Add `bootstrap-autocomplete` dependency.
    - Manage JS with a mix of bootstrap-autocomplete and B5 dropdown.
    - Format results as dropdown links.
    - Add entity headers with Font Awesome icons and increase font-size.
    - Tweak nav bar layout.

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -1,10 +1,12 @@
     <?php include_slot('post'); ?>
     <?php echo get_partial('footer'); ?>
     <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/node_modules/bootstrap-autocomplete/dist/latest/bootstrap-autocomplete.min.js"></script>
     <script src="/node_modules/mediaelement/build/mediaelement-and-player.min.js"></script>
     <script src="/vendor/uppy/uppy-bundle.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/privacyMessage.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/multiFileUpload.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/tooltip.js"></script>
+    <script src="/plugins/arDominionB5Plugin/js/searchBox.js"></script>
   </body>
 </html>
