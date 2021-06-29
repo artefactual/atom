478e38399bd39870cab78af8db7ee4d656c289ab
Matt Innes <matthew.f.innes@gmail.com>
2021-06-28 23:05:20 -0400

    Remove text anchors from image cards, refs #13535
    - Unneccessary anchor being flagged as accessibility issue
    
    Squashed commit of the following:
    
    commit 81443898122066641d516db72bc4b975764d79f5
    Author: Matt Innes <matthew.f.innes@gmail.com>
    Date:   Mon Jun 28 22:57:28 2021 -0400
    
        Restore org conditional for edge cases
    
    commit 8c3c8020a60381597c6a3a05df3da0fd04f18f80
    Author: Matt Innes <matthew.f.innes@gmail.com>
    Date:   Thu May 27 14:02:00 2021 -0400
    
        Fix indentation
    
    commit 416ba2c21d2284e7335c719114fe497bd7a35f77
    Author: Matt Innes <matthew.f.innes@gmail.com>
    Date:   Thu May 27 13:48:34 2021 -0400
    
        Whitespace cleanup
    
    commit a6d87e403642c1320069f7b7edb090d042431038
    Author: Matt Innes <matthew.f.innes@gmail.com>
    Date:   Thu May 27 13:25:06 2021 -0400
    
        Relegate only-text link to only-text bricks
    
        - Some additional logic cleanup

--- apps/qubit/modules/informationobject/templates/_cardViewResults.php
+++ apps/qubit/modules/informationobject/templates/_cardViewResults.php
@@ -7,12 +7,9 @@
     <?php $title = get_search_i18n($doc, 'title', ['allowEmpty' => false, 'culture' => $selectedCulture]); ?>
 
     <?php if (!empty($doc['hasDigitalObject'])) { ?>
-      <div class="brick">
-    <?php } else { ?>
-      <div class="brick brick-only-text">
-    <?php } ?>
 
-      <a href="<?php echo url_for(['module' => 'informationobject', 'slug' => $doc['slug']]); ?>">
+      <div class="brick">
+
         <?php if (
             isset($doc['digitalObject'])
             && !empty($doc['digitalObject']['thumbnailPath'])
@@ -28,18 +25,22 @@
           <?php echo link_to(image_tag(QubitDigitalObject::getGenericIconPathByMediaTypeId($doc['digitalObject']['mediaTypeId']),
             ['alt' => isset($doc['digitalObject']['digitalObjectAltText']) ? $doc['digitalObject']['digitalObjectAltText'] : truncate_text(strip_markdown($title), 100)]),
             ['module' => 'informationobject', 'slug' => $doc['slug']]); ?>
-
-        <?php } else { ?>
-
-          <h5><?php echo render_title($title); ?></h5>
-
         <?php } ?>
-      </a>
 
-      <div class="bottom">
-        <?php echo get_component('clipboard', 'button', ['slug' => $doc['slug'], 'wide' => false, 'repositoryOrDigitalObjBrowse' => true, 'type' => 'informationObject']); ?><?php echo render_title($title); ?>
+    <?php } else { ?>
+
+      <div class="brick brick-only-text">
+
+        <a href="<?php echo url_for(['module' => 'informationobject', 'slug' => $doc['slug']]); ?>">
+          <h5><?php echo render_title($title); ?></h5>
+        </a>
+    <?php } ?>
+
+        <div class="bottom">
+          <?php echo get_component('clipboard', 'button', ['slug' => $doc['slug'], 'wide' => false, 'repositoryOrDigitalObjBrowse' => true, 'type' => 'informationObject']); ?><?php echo render_title($title); ?>
+        </div>
+
       </div>
-    </div>
   <?php } ?>
 
 </section>
