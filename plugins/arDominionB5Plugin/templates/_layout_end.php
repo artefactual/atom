cc3f112ca8556f0b2483660fdf955774acf7743d
Steve Breker <sbreker@artefactual.com>
2021-06-30 00:42:52 +0000

    Replace Imageflow with Accessible-Slick
    Using two Accessible-Slick sliders:
    - nav slider has large images and is used to select image
    - clicking nav slider image will display Description view page.
    - second 'for' slider displays digital object title.
    - instructionsText is translatable.
    - prev next buttons translatable
    - Use alt text on carousel images; fall back to title when empty
    - Added ability to collapse the carousel section. Open by default.

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -4,10 +4,12 @@
     <script src="/node_modules/bootstrap-autocomplete/dist/latest/bootstrap-autocomplete.min.js"></script>
     <script src="/node_modules/mediaelement/build/mediaelement-and-player.min.js"></script>
     <script src="/vendor/uppy/uppy-bundle.js"></script>
+    <script src="/node_modules/@accessible360/accessible-slick/slick/slick.min.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/privacyMessage.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/multiFileUpload.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/tooltip.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/searchBox.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/navbarWrap.js"></script>
+    <script src="/plugins/arDominionB5Plugin/js/imageflow.js"></script>
   </body>
 </html>
