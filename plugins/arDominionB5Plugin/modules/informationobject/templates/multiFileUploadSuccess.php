43e64c679a1f24b9c3fb43f38ef516d3f100e1b9
Steve Breker <sbreker@artefactual.com>
2021-07-13 20:00:52 +0000

    Uppy multifile upload title update issue
    A bug was introduced when porting this script to B5 whereby image title
    updates were not being saved.
    
    - Relocated data elements to a div that includes all the nodes that
    multiFileUpload.js needs access to. This was causing issues selecting
    the data attributes and the form buttons etc.
    - Changed the selected class name to kebab case as this was preventing
    correct selection.
    - update the Uppy data elements to use 'kebab-case'.

--- plugins/arDominionB5Plugin/modules/informationobject/templates/multiFileUploadSuccess.php
+++ plugins/arDominionB5Plugin/modules/informationobject/templates/multiFileUploadSuccess.php
@@ -33,78 +33,77 @@
     </div>
 
   <?php } else { ?>
+    <div class="multifileupload-form"
+      data-multifileupload-max-file-size="<?php echo $maxFileSize; ?>"
+      data-multifileupload-max-post-size="<?php echo $maxPostSize; ?>"
+      data-multifileupload-upload-response-path="<?php echo $uploadResponsePath; ?>"
+      data-multifileupload-slug="<?php echo $resource->slug; ?>"
+      data-multifileupload-thumb-width = 150;
+      data-multifileupload-i18n-max-file-size-message="<?php echo __('Maximum file size: '); ?>"
+      data-multifileupload-i18n-max-post-size-message="<?php echo __('Maximum total upload size: '); ?>"
+      data-multifileupload-i18n-max-size-note="<?php echo __('%{maxFileSizeMessage}; %{maxPostSizeMessage}'); ?>"
+      data-multifileupload-i18n-retry="<?php echo __('Retry'); ?>"
+      data-multifileupload-i18n-info-object-title="<?php echo __('Title'); ?>"
+      data-multifileupload-i18n-save="<?php echo __('Save'); ?>"
+      data-multifileupload-i18n-add-more-files="<?php echo __('Add more files'); ?>"
+      data-multifileupload-i18n-add-more="<?php echo __('Add more'); ?>"
+      data-multifileupload-i18n-adding-more-files="<?php echo __('Adding more files'); ?>"
+      data-multifileupload-i18n-some-files-failed-error="<?php echo __('Some files failed to upload. Press the \\\'Import\\\' button to continue importing anyways, or press \\\'Retry\\\' to re-attempt upload.'); ?>"
+      data-multifileupload-i18n-retry-success="<?php echo __('Files successfully uploaded! Press the \\\'Import\\\' button to complete importing these files.'); ?>"
+      data-multifileupload-i18n-file-selected="<?php echo __('%{smart_count} file selected'); ?>"
+      data-multifileupload-i18n-files-selected="<?php echo __('%{smart_count} files selected'); ?>"
+      data-multifileupload-i18n-uploading="<?php echo __('Uploading'); ?>"
+      data-multifileupload-i18n-complete="<?php echo __('Complete'); ?>"
+      data-multifileupload-i18n-upload-failed="<?php echo __('Upload failed'); ?>"
+      data-multifileupload-i18n-remove-file="<?php echo __('Remove file'); ?>"
+      data-multifileupload-i18n-drop-file="<?php echo __('Drop files here, paste or %{browse}'); ?>"
+      data-multifileupload-i18n-file-uploaded-of-total="<?php echo __('%{complete} of %{smart_count} file uploaded'); ?>"
+      data-multifileupload-i18n-files-uploaded-of-total="<?php echo __('%{complete} of %{smart_count} files uploaded'); ?>"
+      data-multifileupload-i18n-data-uploaded-of-total="<?php echo __('%{complete} of %{total}'); ?>"
+      data-multifileupload-i18n-time-left="<?php echo __('%{time} left'); ?>"
+      data-multifileupload-i18n-cancel="<?php echo __('Cancel'); ?>"
+      data-multifileupload-i18n-edit="<?php echo __('Edit'); ?>"
+      data-multifileupload-i18n-back="<?php echo __('Back'); ?>"
+      data-multifileupload-i18n-editing="<?php echo __('Editing %{file}'); ?>"
+      data-multifileupload-i18n-uploading-file="<?php echo __('Uploading %{smart_count} file'); ?>"
+      data-multifileupload-i18n-uploading-files="<?php echo __('Uploading %{smart_count} files'); ?>"
+      data-multifileupload-i18n-importing="<?php echo __('Importing digital objects - please wait...'); ?>"
+      data-multifileupload-i18n-failed-to-upload="<?php echo __('Failed to upload %{file}'); ?>"
+      data-multifileupload-i18n-size-error="<?php echo __('Skipping file %{fileName} because file size %{fileSize} is larger than file size limit of %{maxSize} MB'); ?>"
+      data-multifileupload-i18n-no-files-error="<?php echo __('Please add a file to begin uploading.'); ?>"
+      data-multifileupload-i18n-no-successful-files-error="<?php echo __('Files not uploaded successfully. Please retry.'); ?>"
+      data-multifileupload-i18n-post-size-error="<?php echo __('Upload limit of %{maxPostSize} MB reached. Unable to add additional files.'); ?>"
+      data-multifileupload-i18n-alert-close="<?php echo __('Close'); ?>">
 
-    <?php echo $form->renderGlobalErrors(); ?>
+      <?php echo $form->renderGlobalErrors(); ?>
 
-    <?php echo $form->renderFormTag(url_for([$resource, 'module' => 'informationobject', 'action' => 'multiFileUpload']), ['id' => 'multiFileUploadForm', 'style' => 'inline']); ?>
+      <?php echo $form->renderFormTag(url_for([$resource, 'module' => 'informationobject', 'action' => 'multiFileUpload']), ['id' => 'multiFileUploadForm', 'style' => 'inline']); ?>
 
-      <?php echo $form->renderHiddenFields(); ?>
+        <?php echo $form->renderHiddenFields(); ?>
 
-      <div class="accordion">
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="upload-heading">
-            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#upload-collapse" aria-expanded="true" aria-controls="upload-collapse">
-              <?php echo __('Import multiple digital objects'); ?>
-            </button>
-          </h2>
-          <div id="upload-collapse" class="accordion-collapse collapse show" aria-labelledby="upload-heading">
-            <div class="accordion-body">
-              <div class="alert alert-info" role="alert">
-                <p><?php echo __('Add your digital objects by dragging and dropping local files into the pane below, or by clicking the browse link to open your local file explorer.'); ?></p>
-                <p><?php echo __('The Title and Level of description values entered on this page will be applied to each child description created for the associated digital objects - \'%dd%\' represents an incrementing 2-value number, so by default descriptions created via this uploader will be named image 01, image 02, etc.'); ?></p>
-                <p><?php echo __('You will also be able to review and individually modify each description title on the next page after clicking "Upload."'); ?></p>
-              </div>
+        <div class="accordion">
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="upload-heading">
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#upload-collapse" aria-expanded="true" aria-controls="upload-collapse">
+                <?php echo __('Import multiple digital objects'); ?>
+              </button>
+            </h2>
+            <div id="upload-collapse" class="accordion-collapse collapse show" aria-labelledby="upload-heading">
+              <div class="accordion-body">
+                <div class="alert alert-info" role="alert">
+                  <p><?php echo __('Add your digital objects by dragging and dropping local files into the pane below, or by clicking the browse link to open your local file explorer.'); ?></p>
+                  <p><?php echo __('The Title and Level of description values entered on this page will be applied to each child description created for the associated digital objects - \'%dd%\' represents an incrementing 2-value number, so by default descriptions created via this uploader will be named image 01, image 02, etc.'); ?></p>
+                  <p><?php echo __('You will also be able to review and individually modify each description title on the next page after clicking "Upload."'); ?></p>
+                </div>
 
-              <?php echo $form->title
-                  ->help(__('The "<strong>%dd%</strong>" placeholder will be replaced with a incremental number (e.g. \'image <strong>01</strong>\', \'image <strong>02</strong>\')'))
-                  ->label(__('Title'))
-                  ->renderRow(); ?>
+                <?php echo $form->title
+                    ->help(__('The "<strong>%dd%</strong>" placeholder will be replaced with a incremental number (e.g. \'image <strong>01</strong>\', \'image <strong>02</strong>\')'))
+                    ->label(__('Title'))
+                    ->renderRow(); ?>
 
-              <?php echo $form->levelOfDescription
-                  ->label(__('Level of description'))
-                  ->renderRow(); ?>
-
-              <div class="multiFileUpload"
-                data-multiFileUpload-maxFileSize="<?php echo $maxFileSize; ?>"
-                data-multiFileUpload-maxPostSize="<?php echo $maxPostSize; ?>"
-                data-multiFileUpload-uploadResponsePath="<?php echo $uploadResponsePath; ?>"
-                data-multiFileUpload-slug="<?php echo $resource->slug; ?>"
-                data-multiFileUpload-thumbWidth = 150;
-                data-multiFileUpload-i18nMaxFileSizeMessage="<?php echo __('Maximum file size: '); ?>"
-                data-multiFileUpload-i18nMaxPostSizeMessage="<?php echo __('Maximum total upload size: '); ?>"
-                data-multiFileUpload-i18nMaxSizeNote="<?php echo __('%{maxFileSizeMessage}; %{maxPostSizeMessage}'); ?>"
-                data-multiFileUpload-i18nRetry="<?php echo __('Retry'); ?>"
-                data-multiFileUpload-i18nInfoObjectTitle="<?php echo __('Title'); ?>"
-                data-multiFileUpload-i18nSave="<?php echo __('Save'); ?>"
-                data-multiFileUpload-i18nAddMoreFiles="<?php echo __('Add more files'); ?>"
-                data-multiFileUpload-i18nAddMore="<?php echo __('Add more'); ?>"
-                data-multiFileUpload-i18nAddingMoreFiles="<?php echo __('Adding more files'); ?>"
-                data-multiFileUpload-i18nSomeFilesFailedError="<?php echo __('Some files failed to upload. Press the \\\'Import\\\' button to continue importing anyways, or press \\\'Retry\\\' to re-attempt upload.'); ?>"
-                data-multiFileUpload-i18nRetrySuccess="<?php echo __('Files successfully uploaded! Press the \\\'Import\\\' button to complete importing these files.'); ?>"
-                data-multiFileUpload-i18nFileSelected="<?php echo __('%{smart_count} file selected'); ?>"
-                data-multiFileUpload-i18nFilesSelected="<?php echo __('%{smart_count} files selected'); ?>"
-                data-multiFileUpload-i18nUploading="<?php echo __('Uploading'); ?>"
-                data-multiFileUpload-i18nComplete="<?php echo __('Complete'); ?>"
-                data-multiFileUpload-i18nUploadFailed="<?php echo __('Upload failed'); ?>"
-                data-multiFileUpload-i18nRemoveFile="<?php echo __('Remove file'); ?>"
-                data-multiFileUpload-i18nDropFile="<?php echo __('Drop files here, paste or %{browse}'); ?>"
-                data-multiFileUpload-i18nFileUploadedOfTotal="<?php echo __('%{complete} of %{smart_count} file uploaded'); ?>"
-                data-multiFileUpload-i18nFilesUploadedOfTotal="<?php echo __('%{complete} of %{smart_count} files uploaded'); ?>"
-                data-multiFileUpload-i18nDataUploadedOfTotal="<?php echo __('%{complete} of %{total}'); ?>"
-                data-multiFileUpload-i18nTimeLeft="<?php echo __('%{time} left'); ?>"
-                data-multiFileUpload-i18nCancel="<?php echo __('Cancel'); ?>"
-                data-multiFileUpload-i18nEdit="<?php echo __('Edit'); ?>"
-                data-multiFileUpload-i18nBack="<?php echo __('Back'); ?>"
-                data-multiFileUpload-i18nEditing="<?php echo __('Editing %{file}'); ?>"
-                data-multiFileUpload-i18nUploadingFile="<?php echo __('Uploading %{smart_count} file'); ?>"
-                data-multiFileUpload-i18nUploadingFiles="<?php echo __('Uploading %{smart_count} files'); ?>"
-                data-multiFileUpload-i18nImporting="<?php echo __('Importing digital objects - please wait...'); ?>"
-                data-multiFileUpload-i18nFailedToUpload="<?php echo __('Failed to upload %{file}'); ?>"
-                data-multiFileUpload-i18nSizeError="<?php echo __('Skipping file %{fileName} because file size %{fileSize} is larger than file size limit of %{maxSize} MB'); ?>"
-                data-multiFileUpload-i18nNoFilesError="<?php echo __('Please add a file to begin uploading.'); ?>"
-                data-multiFileUpload-i18nNoSuccessfulFilesError="<?php echo __('Files not uploaded successfully. Please retry.'); ?>"
-                data-multiFileUpload-i18nPostSizeError="<?php echo __('Upload limit of %{maxPostSize} MB reached. Unable to add additional files.'); ?>"
-                data-multiFileUpload-i18n-alert-close="<?php echo __('Close'); ?>">
+                <?php echo $form->levelOfDescription
+                    ->label(__('Level of description'))
+                    ->renderRow(); ?>
 
                 <h3><?php echo __('Digital objects'); ?></h3>
 
@@ -119,15 +118,14 @@
             </div>
           </div>
         </div>
-      </div>
 
-      <ul class="actions nav gap-2">
-        <li><?php echo link_to(__('Cancel'), [$resource, 'module' => 'informationobject'], ['class' => 'btn atom-btn-outline-light', 'role' => 'button']); ?></li>
-        <li><input class="btn atom-btn-outline-success" type="submit" value="<?php echo __('Upload'); ?>"></li>
-      </ul>
+        <ul class="actions nav gap-2">
+          <li><?php echo link_to(__('Cancel'), [$resource, 'module' => 'informationobject'], ['class' => 'btn atom-btn-outline-light', 'role' => 'button']); ?></li>
+          <li><input class="btn atom-btn-outline-success" type="submit" value="<?php echo __('Upload'); ?>"></li>
+        </ul>
 
-    </form>
-
+      </form>
+    </div>
   <?php } ?>
 
 <?php end_slot(); ?>
