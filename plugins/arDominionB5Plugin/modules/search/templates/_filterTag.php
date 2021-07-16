d3ce8a361f821e534f74cecbdcf6d04ab8824b9e
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-16 07:38:22 +0200

    Add filter tags
    - Add a new button style for content sections (white and gray).
    - Add filter tags section layout and use new button style.

---
+++ plugins/arDominionB5Plugin/modules/search/templates/_filterTag.php
@@ -0,0 +1,10 @@
+<a 
+  href="<?php echo url_for(
+      ['module' => $module, 'action' => $action]
+      + $sf_data->getRaw('getParams')
+  ); ?>"
+  class="btn btn-sm atom-btn-white"
+  aria-label="<?php echo __('Remove filter'); ?>">
+  <?php echo $label ?: render_title($object); ?>
+  <i aria-hidden="true" class="fas fa-times ms-2"></i>
+</a>
