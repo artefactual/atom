415faffa7f32b9f16c4893d49cecc316812d95c9
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-16 10:07:50 +0200

    Add facets header
    Combine facets header and collapsible button and show it in
    all cases for now.

---
+++ plugins/arDominionB5Plugin/modules/term/templates/_sidebar.php
@@ -0,0 +1,64 @@
+<div class="sidebar-lowering-sort">
+
+  <?php if (!$showTreeview) { ?>
+    <?php echo get_component('term', 'treeView', ['browser' => false]); ?>
+  <?php } else { ?>
+
+    <?php echo get_component('term', 'treeView', ['browser' => false, 'tabs' => true, 'pager' => $listPager]); ?>
+
+    <h2 class="d-grid">
+      <button class="btn btn-lg atom-btn-white text-wrap mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-aggregations" aria-expanded="true" aria-controls="collapse-aggregations">
+        <?php echo sfConfig::get('app_ui_label_facetstitle'); ?>
+      </button>
+    </h2>
+
+    <div class="collapse show" id="collapse-aggregations">
+
+      <?php echo get_partial('search/aggregation', [
+          'id' => '#facet-languages',
+          'label' => __('Language'),
+          'name' => 'languages',
+          'aggs' => $aggs,
+          'filters' => $search->filters, ]); ?>
+
+      <?php if (QubitTaxonomy::PLACE_ID != $resource->taxonomyId) { ?>
+        <?php echo get_partial('search/aggregation', [
+            'id' => '#facet-places',
+            'label' => sfConfig::get('app_ui_label_place'),
+            'name' => 'places',
+            'aggs' => $aggs,
+            'filters' => $search->filters, ]); ?>
+        <?php } ?>
+
+      <?php if (QubitTaxonomy::SUBJECT_ID != $resource->taxonomyId) { ?>
+        <?php echo get_partial('search/aggregation', [
+            'id' => '#facet-subjects',
+            'label' => sfConfig::get('app_ui_label_subject'),
+            'name' => 'subjects',
+            'aggs' => $aggs,
+            'filters' => $search->filters, ]); ?>
+      <?php } ?>
+
+      <?php if (QubitTaxonomy::GENRE_ID != $resource->taxonomyId) { ?>
+        <?php echo get_partial('search/aggregation', [
+            'id' => '#facet-genres',
+            'label' => sfConfig::get('app_ui_label_genre'),
+            'name' => 'genres',
+            'aggs' => $aggs,
+            'filters' => $search->filters, ]); ?>
+      <?php } ?>
+
+      <?php if (QubitTaxonomy::ACTOR_OCCUPATION_ID != $resource->taxonomyId) { ?>
+        <?php echo get_partial('search/aggregation', [
+            'id' => '#facet-occupations',
+            'label' => __('Occupation'),
+            'name' => 'occupations',
+            'aggs' => $aggs,
+            'filters' => $search->filters, ]); ?>
+      <?php } ?>
+
+    </div>
+
+  <?php } ?>
+
+</div>
