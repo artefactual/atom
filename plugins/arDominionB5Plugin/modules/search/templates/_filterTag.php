cac348efb336b3a89cac2d4c16237157e6fcce7d
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-17 12:51:23 +0200

    Improve filter tags accessibility

--- plugins/arDominionB5Plugin/modules/search/templates/_filterTag.php
+++ plugins/arDominionB5Plugin/modules/search/templates/_filterTag.php
@@ -3,8 +3,8 @@
       ['module' => $module, 'action' => $action]
       + $sf_data->getRaw('getParams')
   ); ?>"
-  class="btn btn-sm atom-btn-white"
-  aria-label="<?php echo __('Remove filter'); ?>">
+  class="btn btn-sm atom-btn-white">
+  <span class="visually-hidden"><?php echo __('Remove filter:'); ?></span>
   <?php echo $label ?: render_title($object); ?>
   <i aria-hidden="true" class="fas fa-times ms-2 text-primary"></i>
 </a>
