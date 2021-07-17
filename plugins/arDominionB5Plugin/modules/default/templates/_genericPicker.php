f1100ac2c4bf8c04789ce70a6efcd8f4930192ea
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-17 11:39:50 +0200

    Update header option buttons and pickers
    - Use white button with orange accent for print and similar buttons.
    - Use white button and B5 dropdown for sort options.
    - Update view picker and include text inside buttons.
    - Make imageflow and translation links buttons smaller for consistency.
    - Add orange accent to filter tags close icon.
    - Fix buttons styling.

---
+++ plugins/arDominionB5Plugin/modules/default/templates/_genericPicker.php
@@ -0,0 +1,31 @@
+<?php
+
+$options = $sf_data->getRaw('options');
+$param = $sf_data->getRaw('param');
+if (isset($sf_request->{$param}, $options[$sf_request->{$param}])) {
+    $active = $sf_request->{$param};
+} else {
+    $active = array_key_first($options);
+}
+
+?>
+
+<div class="dropdown d-inline-block">
+  <button class="btn btn-sm atom-btn-white dropdown-toggle" type="button" id="<?php echo $param; ?>-button" data-bs-toggle="dropdown" aria-expanded="false">
+    <?php echo $sf_data->getRaw('label'); ?>
+  </button>
+  <ul class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="<?php echo $param; ?>-button">
+    <?php foreach ($options as $key => $value) { ?>
+      <li>
+        <a
+          href="<?php echo url_for(
+              ['module' => $sf_request->module, 'action' => $sf_request->action, $param => $key]
+              + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()
+          ); ?>"
+          class="dropdown-item<?php echo $active == $key ? ' active' : ''; ?>">
+          <?php echo $value; ?>
+        </a>
+      </li>
+    <?php } ?>
+  </ul>
+</div>
