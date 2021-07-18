c215e21d3759c3af0374ef0274cb49d123041280
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-18 19:40:36 +0200

    Collapse aggregations on small screens
    Render aggregations collapsed by default and use JS to detect the
    current Bootstrap breakpoint and expand them on medium and over.

--- plugins/arDominionB5Plugin/js/aggregations.js
+++ plugins/arDominionB5Plugin/js/aggregations.js
@@ -3,13 +3,24 @@
   'use strict';
 
   $(() => {
-    // Open the first three aggregations
-    $('.aggregation .collapse').each((index, item) => {
-      var collapse = bootstrap.Collapse.getOrCreateInstance(
-        item, {toggle: false}
-      );
-      if (index < 3) collapse.show();
-    });
+    var $aggs = $('#collapse-aggregations');
+    if ($aggs.length > 0) {
+      // Use a div with Bootstrap classes to check the current
+      // breakpoint based on its visibility, expand the main
+      // aggregations section on medium screens and open the
+      // first three aggregations.
+      var $div = $('<div class="d-none d-md-block">');
+      if ($div.appendTo($('body')).is(':visible')) {
+        bootstrap.Collapse.getOrCreateInstance($aggs);
+        $('.aggregation .collapse').each((index, item) => {
+          var agg = bootstrap.Collapse.getOrCreateInstance(
+            item, {toggle: false}
+          );
+          if (index < 3) agg.show();
+        });
+      }
+      $div.remove();
+    }
   });
   
 })(jQuery);
--- plugins/arDominionB5Plugin/modules/actor/templates/browseSuccess.php
+++ plugins/arDominionB5Plugin/modules/actor/templates/browseSuccess.php
@@ -27,12 +27,12 @@
   <?php slot('sidebar'); ?>
 
     <h2 class="d-grid">
-      <button class="btn btn-lg atom-btn-white text-wrap mb-3" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-aggregations" aria-expanded="true" aria-controls="collapse-aggregations">
+      <button class="btn btn-lg atom-btn-white collapsed text-wrap" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-aggregations" aria-expanded="false" aria-controls="collapse-aggregations">
         <?php echo sfConfig::get('app_ui_label_facetstitle'); ?>
       </button>
     </h2>
 
-    <div class="collapse show" id="collapse-aggregations">
+    <div class="collapse" id="collapse-aggregations">
 
       <?php echo get_partial('search/aggregation', [
           'id' => '#facet-languages',
