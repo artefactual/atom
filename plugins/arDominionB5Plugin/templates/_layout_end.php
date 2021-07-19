e0dff5b91e0c93faf8ff5bb75f05063ae1c2d1e5
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-19 22:04:08 +0200

    Add search result expander
    - Add and use `jquery-expander` dependency.
    - Create new JS for expander sections, using translations.
    - Hide content until the expander is applied.

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -4,6 +4,7 @@
     <script src="/node_modules/bootstrap-autocomplete/dist/latest/bootstrap-autocomplete.min.js"></script>
     <script src="/node_modules/mediaelement/build/mediaelement-and-player.min.js"></script>
     <script src="/node_modules/@accessible360/accessible-slick/slick/slick.min.js"></script>
+    <script src="/node_modules/jquery-expander/jquery.expander.min.js"></script>
     <script src="/vendor/uppy/uppy-bundle.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/privacyMessage.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/multiFileUpload.js"></script>
@@ -12,6 +13,7 @@
     <script src="/plugins/arDominionB5Plugin/js/navbarWrap.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/imageflow.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/aggregations.js"></script>
+    <script src="/plugins/arDominionB5Plugin/js/expander.js"></script>
     <script src="/js/editingHistory.js"></script>
   </body>
 </html>
