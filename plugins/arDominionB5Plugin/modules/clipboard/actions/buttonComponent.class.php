2fba686b986398f83749771eb487244f7898453a
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-19 16:08:35 +0200

    Style search results and clipboard button

---
+++ plugins/arDominionB5Plugin/modules/clipboard/actions/buttonComponent.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class ClipboardButtonComponent extends sfComponent
+{
+    public function execute($request)
+    {
+        // Not needed in Bootstrap 5 themes
+    }
+}
