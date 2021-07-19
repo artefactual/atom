2527a7531eb3ea11edf62b2fbce3a2cb03653064
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-19 18:08:35 +0200

    Allow content to wrap in multiple lines
    - Sort dropdowns.
    - View options buttons text.
    - Aggregation items, even breaking words (but not counts).

--- plugins/arDominionB5Plugin/modules/default/templates/_viewPicker.php
+++ plugins/arDominionB5Plugin/modules/default/templates/_viewPicker.php
@@ -1,6 +1,6 @@
 <div class="btn-group btn-group-sm" role="group" aria-label="<?php echo __('View options'); ?>">
   <a
-    class="btn atom-btn-white<?php echo $view === $cardView ? ' active' : ''; ?>"
+    class="btn atom-btn-white text-wrap<?php echo $view === $cardView ? ' active' : ''; ?>"
     <?php echo $view === $cardView ? 'aria-current="page"' : ''; ?>
     href="<?php echo url_for(
         ['module' => $module, 'action' => 'browse', 'view' => $cardView]
@@ -10,7 +10,7 @@
     <?php echo __('Card view'); ?>
   </a>
   <a
-    class="btn atom-btn-white<?php echo $view === $tableView ? ' active' : ''; ?>"
+    class="btn atom-btn-white text-wrap<?php echo $view === $tableView ? ' active' : ''; ?>"
     <?php echo $view === $tableView ? 'aria-current="page"' : ''; ?>
     href="<?php echo url_for(
         ['module' => $module, 'action' => 'browse', 'view' => $tableView]
