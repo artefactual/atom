828e9951ba7e63138af8173b69cb697ade30976f
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-20 14:15:37 +0200

    Update clipboard
    - Tweak JS to find and use the proper elements.
    - Remove Drupal behaviour usage (duplicating code ATM).
    - Normalize button toggle (check tooltip need based on data attribute).
    - Use Bootstrap 5 tooltips.
    - Fix alerts and target container.
    - Modify count badge to add screen reader text and a B5 badge.
    - Restore button wide short text
    - Restore wide parameter to avoid changing the partial calls.
    - Style added button text.

--- plugins/arDominionB5Plugin/modules/clipboard/templates/_button.php
+++ plugins/arDominionB5Plugin/modules/clipboard/templates/_button.php
@@ -2,13 +2,11 @@
   class="btn atom-btn-white ms-auto clipboard"
   data-clipboard-slug="<?php echo $slug; ?>"
   data-clipboard-type="<?php echo $type; ?>"
-  <?php if (!$showText) { ?>
-    data-tooltip="<?php echo __('Add to clipboard'); ?>"
-  <?php } ?>
-  data-clipboard-add="<?php echo __('Add to clipboard'); ?>"
-  data-clipboard-remove="<?php echo __('Remove from clipboard'); ?>">
+  <?php echo $wide ? '' : 'data-tooltip="true"'; ?>
+  data-title="<?php echo $wide ? __('Add') : __('Add to clipboard'); ?>"
+  data-alt-title="<?php echo $wide ? __('Remove') : __('Remove from clipboard'); ?>">
   <i class="fas fa-lg fa-paperclip" aria-hidden="true"></i>
-  <span class="<?php echo !$showText ? 'visually-hidden' : 'ms-1'; ?>">
-    <?php echo __('Add to clipboard'); ?>
+  <span class="<?php echo $wide ? 'ms-2' : 'visually-hidden'; ?>">
+    <?php echo $wide ? __('Add') : __('Add to clipboard'); ?>
   </span>
 </button>
