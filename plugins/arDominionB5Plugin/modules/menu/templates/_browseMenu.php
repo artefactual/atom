e2f21776d0becc541de702eccc793958545ecdc1
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-20 14:03:57 +0200

    Restore menu ids

--- plugins/arDominionB5Plugin/modules/menu/templates/_browseMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_browseMenu.php
@@ -10,7 +10,7 @@
     </li>
     <?php foreach ($browseMenu->getChildren() as $child) { ?>
       <?php if ($child->checkUserAccess()) { ?>
-        <li>
+        <li <?php echo isset($child->name) ? 'id="node_'.$child->name.'"' : ''; ?>>
           <?php echo link_to(
               $child->getLabel(['cultureFallback' => true]),
               $child->getPath(['getUrl' => true, 'resolveAlias' => true]),
