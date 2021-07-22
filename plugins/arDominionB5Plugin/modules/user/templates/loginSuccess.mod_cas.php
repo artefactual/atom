f668d7c994a374249ad95dc8e3a884597f82b355
Jesús García Crespo <jesus@sevein.com>
2021-07-22 13:31:38 +0200

    Add CAS-specific templates

---
+++ plugins/arDominionB5Plugin/modules/user/templates/loginSuccess.mod_cas.php
@@ -0,0 +1,36 @@
+<?php decorate_with('layout_1col'); ?>
+<?php use_helper('Javascript'); ?>
+
+<?php slot('content'); ?>
+
+  <div class="row">
+
+    <div class="offset4 span4">
+
+      <div id="content">
+
+        <?php if ('user' != $sf_request->module || 'login' != $sf_request->action) { ?>
+          <h1><?php echo __('Please log in to access that page'); ?></h1>
+        <?php } else { ?>
+          <h1><?php echo __('Log in'); ?></h1>
+        <?php } ?>
+
+        <?php if ($form->hasErrors()) { ?>
+          <?php echo $form->renderGlobalErrors(); ?>
+        <?php } ?>
+
+        <?php echo $form->renderFormTag(url_for(['module' => 'user', 'action' => 'login'])); ?>
+
+          <?php echo $form->renderHiddenFields(); ?>
+
+          <button type="submit" class="btn atom-btn-secondary"><?php echo __('Log in'); ?></button>
+
+        </form>
+
+      </div>
+
+    </div>
+
+  </div>
+
+<?php end_slot(); ?>
