d4e1a832cfd6b4eca164de14c751826f7ca4fef4
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-23 17:24:39 +0200

    Include Qubit and Drupal JS global variables
    Maintain Qubit and Drupal global variables for legacy reasons.
    Used to share functions and prototypes, and to be able to trigger
    JS functions over dinamically loaded content. Move the expander
    to a Drupal behavior and attach behaviors after clipboard load.

---
+++ plugins/arDominionB5Plugin/js/qubit.js
@@ -0,0 +1,15 @@
+// Maintain Qubit and Drupal global variables
+// for legacy reasons. Used to share functions
+// and prototypes, and to be able to trigger
+// JS functions over dinamically loaded content.
+var Qubit = {};
+var Drupal = {behaviors: {}};
+
+Drupal.attachBehaviors = context => {
+  context = context || document;
+  // Can't use arrow function here in order
+  // to access `this` and its methods.
+  $.each(Drupal.behaviors, function() {
+    this.attach(context);
+  });
+};
