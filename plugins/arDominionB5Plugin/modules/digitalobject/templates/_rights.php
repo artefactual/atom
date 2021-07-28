72ebc624dd241602f4c0f1535b52c5f70febc188
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-07-28 11:40:22 -0600

    Style content container of ISAD index page
    * adds a `content` slot to the ISAD index template and styles all its
      partials and components to use BS5 classes and a 3-9 column layout
    * modifies the existing `render_show` helper to render fields and
      values using BS5 classes and introduces a new `isSubField` option to
      render fields inside of fields using `flex` containers
    * attaches the `expander.js` behaviour to top level field values

---
+++ plugins/arDominionB5Plugin/modules/digitalobject/templates/_rights.php
@@ -0,0 +1,43 @@
+<section>
+
+  <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationObject'), render_b5_section_label(__('%1% (%2%) rights area', ['%1%' => sfConfig::get('app_ui_label_digitalobject'), '%2%' => $resource->usage])), [$resource, 'module' => 'digitalobject', 'action' => 'edit'], ['title' => __('Edit %1%', ['%1%' => mb_strtolower(sfConfig::get('app_ui_label_digitalobject'))])]); ?>
+
+  <?php foreach ($resource->getRights() as $item) { ?>
+
+    <?php echo get_partial('right/right', ['resource' => $item->object, 'object' => $item]); ?>
+
+  <?php } ?>
+
+</section>
+
+<section>
+
+  <?php if ($child = $resource->getChildByUsageId(QubitTerm::REFERENCE_ID)) { ?>
+
+    <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationObject'), render_b5_section_label(__('%1% (%2%) rights area', ['%1%' => sfConfig::get('app_ui_label_digitalobject'), '%2%' => $child->usage])), [$resource, 'module' => 'digitalobject', 'action' => 'edit'], ['title' => __('Edit %1%', ['%1%' => mb_strtolower(sfConfig::get('app_ui_label_digitalobject'))])]); ?>
+
+    <?php foreach ($child->getRights() as $item) { ?>
+
+      <?php echo get_partial('right/right', ['resource' => $item->object, 'object' => $resource]); ?>
+
+    <?php } ?>
+
+  <?php } ?>
+
+</section>
+
+<section>
+
+  <?php if ($child = $resource->getChildByUsageId(QubitTerm::THUMBNAIL_ID)) { ?>
+
+    <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationObject'), render_b5_section_label(__('%1% (%2%) rights area', ['%1%' => sfConfig::get('app_ui_label_digitalobject'), '%2%' => $child->usage])), [$resource, 'module' => 'digitalobject', 'action' => 'edit'], ['title' => __('Edit %1%', ['%1%' => mb_strtolower(sfConfig::get('app_ui_label_digitalobject'))])]); ?>
+
+    <?php foreach ($child->getRights() as $item) { ?>
+
+      <?php echo get_partial('right/right', ['resource' => $item->object, 'object' => $resource]); ?>
+
+    <?php } ?>
+
+  <?php } ?>
+
+</section>
