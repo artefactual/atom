fc46b7594b5428474d70986787e0c843e8d82986
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-28 07:46:54 +0200

    Update user permissions edit pages dialog/tables
    - Use static backdrop vertically centered B5 modal (previously dialog) .
    - Create _aclModal.php to render the modal and the empty permissions
      table hidden by default. Make it work with all entity types and multiple
      modals.
    - Update ACL dialog Javascript to make it work with all user/group
      permission dialogs. Change how the table is obtained and updated to
      allow a better separation of concerns. Fix highlight and avoid addition of
      existing tables.
    - Reuse permissions table from all entity types, make it responsive and
      style its content (radio buttons/headers).
    - Make "Add permission by ..." a proper button.
    - Remove inconsistent multi-line heading from repository page.
    - Reduce bold and bolder font weights.

--- plugins/arDominionB5Plugin/modules/user/templates/editInformationObjectAclSuccess.php
+++ plugins/arDominionB5Plugin/modules/user/templates/editInformationObjectAclSuccess.php
@@ -1,5 +1,17 @@
 <h1><?php echo __('Edit %1% permissions of %2%', ['%1%' => lcfirst(sfConfig::get('app_ui_label_informationobject')), '%2%' => render_title($resource)]); ?></h1>
 
+<?php echo get_partial('aclGroup/aclModal', [
+    'entityType' => 'informationobject',
+    'label' => sfConfig::get('app_ui_label_informationobject'),
+    'basicActions' => $basicActions,
+]); ?>
+
+<?php echo get_partial('aclGroup/aclModal', [
+    'entityType' => 'repository',
+    'label' => sfConfig::get('app_ui_label_repository'),
+    'basicActions' => $basicActions,
+]); ?>
+
 <?php echo $form->renderGlobalErrors(); ?>
 
 <?php echo $form->renderFormTag(url_for([$resource, 'module' => 'user', 'action' => 'editInformationObjectAcl']), ['id' => 'editForm']); ?>
@@ -15,9 +27,11 @@
       </h2>
       <div id="all-collapse" class="accordion-collapse collapse show" aria-labelledby="all-heading">
         <div class="accordion-body">
-          <div class="form-item">
-            <?php echo get_component('aclGroup', 'aclTable', ['object' => QubitInformationObject::getRoot(), 'permissions' => $root, 'actions' => $basicActions]); ?>
-          </div>
+          <?php echo get_component('aclGroup', 'aclTable', [
+              'object' => QubitInformationObject::getRoot(),
+              'permissions' => $root,
+              'actions' => $basicActions,
+          ]); ?>
         </div>
       </div>
     </div>
@@ -31,24 +45,26 @@
         <div class="accordion-body">
           <?php if (0 < count($informationObjects)) { ?>
             <?php foreach ($informationObjects as $informationObjectId => $permissions) { ?>
-              <div class="form-item">
-                <?php echo get_component('aclGroup', 'aclTable', ['object' => QubitInformationObject::getById($informationObjectId), 'permissions' => $permissions, 'actions' => $basicActions]); ?>
-              </div>
+              <?php echo get_component('aclGroup', 'aclTable', [
+                  'object' => QubitInformationObject::getById($informationObjectId),
+                  'permissions' => $permissions,
+                  'actions' => $basicActions,
+              ]); ?>
             <?php } ?>
           <?php } ?>
 
-          <!-- Add info object div - cut by aclDialog.js
-          <div class="form-item" id="addInformationObject">
-            <label for="addInformationObject"><?php echo __('%1% name', ['%1%' => sfConfig::get('app_ui_label_informationobject')]); ?></label>
-            <select name="addInformationObject" id="addInformationObject" class="form-autocomplete"></select>
-            <input class="list" type="hidden" value="<?php echo url_for(['module' => 'informationobject', 'action' => 'autocomplete']); ?>"/>
-          </div>
-          -->
-
-          <div class="form-item">
-            <label for="addInformationObjectLink"><?php echo __('Add permissions by %1%', ['%1%' => lcfirst(sfConfig::get('app_ui_label_informationobject'))]); ?></label>
-            <a id="addInformationObjectLink" href="#"><?php echo __('Add %1%', ['%1%' => lcfirst(sfConfig::get('app_ui_label_informationobject'))]); ?></a>
-          </div>
+          <button
+            class="btn atom-btn-white text-wrap"
+            type="button"
+            id="acl-add-informationobject"
+            data-bs-toggle="modal"
+            data-bs-target="#acl-modal-container-informationobject">
+            <i class="fas fa-plus me-1" aria-hidden="true"></i>
+            <?php echo __(
+                'Add permissions by %1%',
+                ['%1%' => lcfirst(sfConfig::get('app_ui_label_informationobject'))]
+            ); ?>
+          </button>
         </div>
       </div>
     </div>
@@ -62,24 +78,26 @@
         <div class="accordion-body">
           <?php if (0 < count($repositories)) { ?>
             <?php foreach ($repositories as $repository => $permissions) { ?>
-              <div class="form-item">
-                <?php echo get_component('aclGroup', 'aclTable', ['object' => QubitRepository::getBySlug($repository), 'permissions' => $permissions, 'actions' => $basicActions]); ?>
-              </div>
+              <?php echo get_component('aclGroup', 'aclTable', [
+                  'object' => QubitRepository::getBySlug($repository),
+                  'permissions' => $permissions,
+                  'actions' => $basicActions,
+              ]); ?>
             <?php } ?>
           <?php } ?>
 
-          <!-- Add repository div - cut by aclDialog.js
-          <div class="form-item" id="addRepository">
-            <label for="addRepository"><?php echo __('%1% name', ['%1%' => sfConfig::get('app_ui_label_repository')]); ?></label>
-            <select name="addRepository" id="addRepository" class="form-autocomplete"></select>
-            <input class="list" type="hidden" value="<?php echo url_for(['module' => 'repository', 'action' => 'autocomplete']); ?>"/>
-          </div>
-          -->
-
-          <div class="form-item">
-            <label for="addRepositoryLink"><?php echo __('Add permissions by %1%', ['%1%' => lcfirst(sfConfig::get('app_ui_label_repository'))]); ?></label>
-            <a id="addRepositoryLink" href="#"><?php echo __('Add %1%', ['%1%' => lcfirst(sfConfig::get('app_ui_label_repository'))]); ?></a>
-          </div>
+          <button
+            class="btn atom-btn-white text-wrap"
+            type="button"
+            id="acl-add-repository"
+            data-bs-toggle="modal"
+            data-bs-target="#acl-modal-container-repository">
+            <i class="fas fa-plus me-1" aria-hidden="true"></i>
+            <?php echo __(
+                'Add permissions by %1%',
+                ['%1%' => lcfirst(sfConfig::get('app_ui_label_repository'))]
+            ); ?>
+          </button>
         </div>
       </div>
     </div>
