e9c37985c5ccd602b51a9ca3e140efddfaccf962
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-07-28 11:40:22 -0600

    Style content container of ISAD index page
    * adds a `content` slot to the ISAD index template and styles all its
      partials and components to use BS5 classes and a 3-9 column layout
    * modifies the existing `render_show` helper to render fields and
      values using BS5 classes and introduces a new `isSubField` option to
      render fields inside of fields using `flex` containers
    * attaches the `expander.js` behaviour to top level field values

---
+++ plugins/arDominionB5Plugin/modules/informationobject/templates/_findingAidLink.php
@@ -0,0 +1,6 @@
+<div class="field <?php echo render_b5_show_field_css_classes(); ?>">
+  <?php echo render_b5_show_label($label); ?>
+  <div class="findingAidLink <?php echo render_b5_show_value_css_classes(); ?>">
+    <a href="<?php echo public_path($path); ?>" target="_blank"><?php echo $filename; ?></a>
+  </div>
+</div>
