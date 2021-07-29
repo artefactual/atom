b7378a84387750a00f3ba2d1bca590457e309208
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-07-29 16:04:27 -0600

    Style errors alert

--- plugins/arDominionB5Plugin/modules/sfIsdiahPlugin/templates/indexSuccess.php
+++ plugins/arDominionB5Plugin/modules/sfIsdiahPlugin/templates/indexSuccess.php
@@ -10,8 +10,8 @@
   <h1><?php echo render_title($resource); ?></h1>
 
   <?php if (isset($errorSchema)) { ?>
-    <div class="messages error">
-      <ul>
+    <div class="messages error alert alert-danger" role="alert">
+      <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
         <?php foreach ($errorSchema as $error) { ?>
           <?php $error = sfOutputEscaper::unescape($error); ?>
           <li><?php echo $error->getMessage(); ?></li>
