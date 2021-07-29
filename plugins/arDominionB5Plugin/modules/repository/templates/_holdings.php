732ca3ad4649e7804ffa4d02a7e596dc111a0dce
Jesús García Crespo <jesus@sevein.com>
2021-07-29 20:54:06 +0200

    Add sidebar paged lists

---
+++ plugins/arDominionB5Plugin/modules/repository/templates/_holdings.php
@@ -0,0 +1,9 @@
+<form class="sidebar-search" role="search" aria-label="<?php echo sfConfig::get('app_ui_label_holdings'); ?>" action="<?php echo url_for(['module' => 'informationobject', 'action' => 'browse']); ?>">
+  <input type="hidden" name="repos" value="<?php echo $resource->id; ?>">
+  <div class="input-prepend input-append">
+    <input type="text" name="query" aria-label="<?php echo __('Search %1%', ['%1%' => sfConfig::get('app_ui_label_holdings')]); ?>" placeholder="<?php echo __('Search %1%', ['%1%' => strtolower(sfConfig::get('app_ui_label_holdings'))]); ?>">
+    <button class="btn" type="submit" aria-label=<?php echo __('Search'); ?>>
+      <i aria-hidden="true" class="fas fa-search"></i>
+    </button>
+  </div>
+</form>
