7564239d2692d4c0249920d7f056e9f072cb797f
Jesús García Crespo <jesus@sevein.com>
2021-07-29 20:54:06 +0200

    Add sidebar paged lists

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -25,6 +25,7 @@
     <script src="/plugins/arDominionB5Plugin/js/collapse.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/masonry.js"></script>
     <script src="/plugins/arDominionB5Plugin/js/aclModal.js"></script>
+    <script src="/plugins/arDominionB5Plugin/js/sidebarPaginatedList.js"></script>
     <script src="/plugins/sfTranslatePlugin/js/l10n_client.js"></script>
     <script src="/js/blank.js"></script>
     <script src="/js/editingHistory.js"></script>
