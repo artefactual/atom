f236319349bedac2f0dcd08127f7d0edc9c0e736
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-07-29 20:11:18 +0200

    Style nav pills with white buttons

--- plugins/arDominionB5Plugin/modules/aclGroup/templates/_tabs.php
+++ plugins/arDominionB5Plugin/modules/aclGroup/templates/_tabs.php
@@ -1,7 +1,7 @@
 <nav>
-  <ul class="nav nav-pills mb-2">
+  <ul class="nav nav-pills mb-2 d-flex gap-2">
   <?php foreach ($groupsMenu->getChildren() as $child) { ?>
-      <?php $options = ['class' => 'nav-link']; ?>
+      <?php $options = ['class' => 'btn atom-btn-white active-orange text-wrap']; ?>
       <?php if (
           str_replace('%currentId%', $sf_request->id, $child->path)
           == $sf_context->getRouting()->getCurrentInternalUri()
