2f304dfd3eaa188089db0a9bbb0ac38995a965de
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-08-02 09:56:35 -0600

    Style content container of accession index page

--- plugins/arDominionB5Plugin/modules/accession/templates/indexSuccess.php
+++ plugins/arDominionB5Plugin/modules/accession/templates/indexSuccess.php
@@ -18,10 +18,11 @@
 <?php slot('before-content'); ?>
 
   <?php if (isset($errorSchema)) { ?>
-    <div class="messages error">
-      <ul>
+    <div class="messages error alert alert-danger" role="alert">
+      <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
         <?php foreach ($errorSchema as $error) { ?>
-          <li><?php echo $error; ?></li>
+          <?php $error = sfOutputEscaper::unescape($error); ?>
+          <li><?php echo $error->getMessage(); ?></li>
         <?php } ?>
       </ul>
     </div>
@@ -31,175 +32,164 @@
 
 <?php end_slot(); ?>
 
-<div class="section" id="basicInfo">
+<?php slot('content'); ?>
 
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Basic info').'</h2>', [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'basic-collapse', 'title' => __('Edit basic info')]); ?>
+  <div id="content" class="p-0">
 
-  <?php echo render_show(__('Accession number'), $resource->identifier); ?>
+    <div class="section border-bottom" id="basicInfo">
 
-  <?php echo get_partial('accession/alternativeIdentifiersIndex', ['resource' => $resource]); ?>
+      <?php echo link_to_if(QubitAcl::check($resource, 'update'), render_b5_section_label(__('Basic info')), [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'basic-collapse', 'title' => __('Edit basic info'), 'class' => 'text-primary']); ?>
 
-  <?php echo render_show(__('Acquisition date'), render_value(Qubit::renderDate($resource->date))); ?>
+      <?php echo render_show(__('Accession number'), $resource->identifier); ?>
 
-  <?php echo render_show(__('Immediate source of acquisition'), render_value($resource->getSourceOfAcquisition(['cultureFallback' => true]))); ?>
+      <?php echo get_partial('accession/alternativeIdentifiersIndex', ['resource' => $resource]); ?>
 
-  <?php echo render_show(__('Location information'), render_value($resource->getLocationInformation(['cultureFallback' => true]))); ?>
+      <?php echo render_show(__('Acquisition date'), render_value_inline(Qubit::renderDate($resource->date))); ?>
 
-</div> <!-- /.section#basicInfo -->
+      <?php echo render_show(__('Immediate source of acquisition'), render_value($resource->getSourceOfAcquisition(['cultureFallback' => true]))); ?>
 
-<div class="section" id="donorArea">
+      <?php echo render_show(__('Location information'), render_value($resource->getLocationInformation(['cultureFallback' => true]))); ?>
 
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Donor/Transferring body area').'</h2>', [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'donor-collapse', 'title' => __('Edit donor/transferring body area')]); ?>
+    </div> <!-- /.section#basicInfo -->
 
-  <?php foreach (QubitRelation::getRelationsBySubjectId($resource->id, ['typeId' => QubitTerm::DONOR_ID]) as $item) { ?>
+    <div class="section border-bottom" id="donorArea">
 
-    <?php echo render_show(__('Related donor'), link_to(esc_specialchars(render_title($item->object)), [$item->object, 'module' => 'donor'])); ?>
+      <?php echo link_to_if(QubitAcl::check($resource, 'update'), render_b5_section_label(__('Donor/Transferring body area')), [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'donor-collapse', 'title' => __('Edit donor/transferring body area'), 'class' => 'text-primary']); ?>
 
-    <?php foreach ($item->object->contactInformations as $contactItem) { ?>
-      <?php echo get_partial('contactinformation/contactInformation', ['contactInformation' => $contactItem]); ?>
-    <?php } ?>
+      <?php foreach (QubitRelation::getRelationsBySubjectId($resource->id, ['typeId' => QubitTerm::DONOR_ID]) as $item) { ?>
 
-  <?php } ?>
+        <?php echo render_show(__('Related donor'), link_to(esc_specialchars(render_title($item->object)), [$item->object, 'module' => 'donor'])); ?>
 
-</div> <!-- /.section#donorArea -->
-
-<div class="section" id="administrativeArea">
-
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('Administrative area').'</h2>', [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'admin-collapse', 'title' => __('Edit administrative area')]); ?>
-
-  <?php echo render_show(__('Acquisition type'), render_value($resource->acquisitionType)); ?>
-
-  <?php echo render_show(__('Resource type'), render_value($resource->resourceType)); ?>
-
-  <?php echo render_show(__('Title'), render_value($resource->getTitle(['cultureFallback' => true]))); ?>
-
-  <div class="field">
-    <h3><?php echo __('Creators'); ?></h3>
-   <div>
-     <ul>
-       <?php $actorsShown = []; ?>
-       <?php foreach (QubitRelation::getRelationsByObjectId($resource->id, ['typeId' => QubitTerm::CREATION_ID]) as $item) { ?>
-         <?php if (!isset($actorsShown[$item->subject->id])) { ?>
-           <li><?php echo link_to(render_title($item->subject), [$item->subject, 'module' => 'actor']); ?></li>
-         <?php } ?>
-         <?php $actorsShown[$item->subject->id] = true; ?>
-       <?php } ?>
-     </ul>
-   </div>
-  </div>
-
-  <div class="field">
-    <h3><?php echo __('Date(s)'); ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->getDates() as $item) { ?>
-          <li>
-            <?php echo render_value_inline(Qubit::renderDateStartEnd($item->getDate(['cultureFallback' => true]), $item->startDate, $item->endDate)); ?> (<?php echo $item->getType(['cultureFallback' => true]); ?>)
-          </li>
+        <?php foreach ($item->object->contactInformations as $contactItem) { ?>
+          <?php echo get_partial('contactinformation/contactInformation', ['contactInformation' => $contactItem]); ?>
         <?php } ?>
-      </ul>
-    </div>
-  </div>
 
-  <div class="field">
-    <h3><?php echo __('Event(s)'); ?></h3>
-    <div>
-      <ul>
-        <?php foreach ($resource->accessionEvents as $event) { ?>
-          <li>
-            <?php echo $event->getDate(); ?> (<?php echo $event->type->getName(['cultureFallback' => true]); ?>): <?php echo $event->getAgent(['cultureFallback' => true]); ?>
-            <?php $note = $event->getNote(); ?>
-            <?php if (null !== $note && !empty($noteText = $note->getContent(['cultureFallback' => true]))) { ?>
-              <p><?php echo $noteText; ?></p>
+      <?php } ?>
+
+    </div> <!-- /.section#donorArea -->
+
+    <div class="section border-bottom" id="administrativeArea">
+
+      <?php echo link_to_if(QubitAcl::check($resource, 'update'), render_b5_section_label(__('Administrative area')), [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'admin-collapse', 'title' => __('Edit administrative area'), 'class' => 'text-primary']); ?>
+
+      <?php echo render_show(__('Acquisition type'), render_value_inline($resource->acquisitionType)); ?>
+
+      <?php echo render_show(__('Resource type'), render_value_inline($resource->resourceType)); ?>
+
+      <?php echo render_show(__('Title'), render_value_inline($resource->getTitle(['cultureFallback' => true]))); ?>
+
+      <?php
+          $actorsShown = [];
+          $creators = [];
+          foreach (QubitRelation::getRelationsByObjectId($resource->id, ['typeId' => QubitTerm::CREATION_ID]) as $item) {
+              if (!isset($actorsShown[$item->subject->id])) {
+                  $creators[] = link_to(render_title($item->subject), [$item->subject, 'module' => 'actor']);
+              }
+              $actorsShown[$item->subject->id] = true;
+          }
+          echo render_show(__('Creators'), $creators);
+      ?>
+
+      <div class="field <?php echo render_b5_show_field_css_classes(); ?>">
+        <?php echo render_b5_show_label(__('Date(s)')); ?>
+        <div class="<?php echo render_b5_show_value_css_classes(); ?>">
+          <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
+            <?php foreach ($resource->getDates() as $item) { ?>
+              <li>
+                <?php echo render_value_inline(Qubit::renderDateStartEnd($item->getDate(['cultureFallback' => true]), $item->startDate, $item->endDate)); ?> (<?php echo $item->getType(['cultureFallback' => true]); ?>)
+              </li>
             <?php } ?>
-          </li>
-        <?php } ?>
-      </ul>
-    </div>
-  </div>
-
-  <?php echo render_show(__('Archival/Custodial history'), render_value($resource->getArchivalHistory(['cultureFallback' => true]))); ?>
-
-  <?php echo render_show(__('Scope and content'), render_value($resource->getScopeAndContent(['cultureFallback' => true]))); ?>
-
-  <?php echo render_show(__('Appraisal, destruction and scheduling'), render_value($resource->getAppraisal(['cultureFallback' => true]))); ?>
-
-  <?php echo render_show(__('Physical condition'), render_value($resource->getPhysicalCharacteristics(['cultureFallback' => true]))); ?>
-
-  <?php echo render_show(__('Received extent units'), render_value($resource->getReceivedExtentUnits(['cultureFallback' => true]))); ?>
-
-  <?php echo render_show(__('Processing status'), render_value($resource->processingStatus)); ?>
-
-  <?php echo render_show(__('Processing priority'), render_value($resource->processingPriority)); ?>
-
-  <?php echo render_show(__('Processing notes'), render_value($resource->getProcessingNotes(['cultureFallback' => true]))); ?>
-
-  <div class="field">
-    <h3><?php echo __('Accruals'); ?></h3>
-    <div>
-      <ul>
-        <?php foreach (QubitRelation::getRelationsByObjectId($resource->id, ['typeId' => QubitTerm::ACCRUAL_ID]) as $item) { ?>
-          <li><?php echo link_to(render_title($item->subject), [$item->subject, 'module' => 'accession']); ?></li>
-        <?php } ?>
-      </ul>
-    </div>
-  </div>
-
-  <div class="field">
-    <h3><?php echo __('Accrual to'); ?></h3>
-    <div>
-      <ul>
-        <?php foreach (QubitRelation::getRelationsBySubjectId($resource->id, ['typeId' => QubitTerm::ACCRUAL_ID]) as $item) { ?>
-          <li><?php echo link_to(render_title($item->object), [$item->object, 'module' => 'accession']); ?></li>
-          <?php $accrued = true; ?>
-        <?php } ?>
-      </ul>
-    </div>
-  </div>
-
-</div> <!-- /.section#administrativeArea -->
-
-<div class="section" id="rightsArea">
-
-  <h2><?php echo __('Rights area'); ?></h2>
-
-  <?php echo get_component('right', 'relatedRights', ['resource' => $resource]); ?>
-
-</div> <!-- /.section#rightsArea -->
-
-<div class="section" id="informationObjectArea">
-
-  <?php echo link_to_if(QubitAcl::check($resource, 'update'), '<h2>'.__('%1% area', ['%1%' => sfConfig::get('app_ui_label_informationobject')]).'</h2>', [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'io-collapse', 'title' => __('Edit %1% area', ['%1%' => sfConfig::get('app_ui_label_informationobject')])]); ?>
-
-  <?php foreach (QubitRelation::getRelationsByObjectId($resource->id, ['typeId' => QubitTerm::ACCESSION_ID]) as $item) { ?>
-
-    <div class="field">
-      <h3><?php echo sfConfig::get('app_ui_label_informationobject'); ?></h3>
-      <div>
-        <?php echo link_to(esc_specialchars(render_title($item->subject)), [$item->subject, 'module' => 'informationobject']); ?>
+          </ul>
+        </div>
       </div>
-    </div>
 
-  <?php } ?>
-
-</div> <!-- /.section#deaccessionArea -->
-
-<div class="section" id="deaccessionArea">
-
-  <h2><?php echo __('Deaccession area'); ?></h2>
-
-  <?php foreach ($resource->deaccessions as $item) { ?>
-
-    <div class="field">
-      <h3><?php echo __('Deaccession'); ?></h3>
-      <div>
-        <?php echo link_to(render_title($item, false), [$item, 'module' => 'deaccession']); ?>
+      <div class="field <?php echo render_b5_show_field_css_classes(); ?>">
+        <?php echo render_b5_show_label(__('Event(s)')); ?>
+        <div class="<?php echo render_b5_show_value_css_classes(); ?>">
+          <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
+            <?php foreach ($resource->accessionEvents as $event) { ?>
+              <li>
+                <?php echo $event->getDate(); ?> (<?php echo $event->type->getName(['cultureFallback' => true]); ?>): <?php echo $event->getAgent(['cultureFallback' => true]); ?>
+                <?php $note = $event->getNote(); ?>
+                <?php if (null !== $note && !empty($noteText = $note->getContent(['cultureFallback' => true]))) { ?>
+                  <p><?php echo $noteText; ?></p>
+                <?php } ?>
+              </li>
+            <?php } ?>
+          </ul>
+        </div>
       </div>
-    </div>
 
-  <?php } ?>
+      <?php echo render_show(__('Archival/Custodial history'), render_value($resource->getArchivalHistory(['cultureFallback' => true]))); ?>
 
-</div> <!-- /.section#deaccessionArea -->
+      <?php echo render_show(__('Scope and content'), render_value($resource->getScopeAndContent(['cultureFallback' => true]))); ?>
+
+      <?php echo render_show(__('Appraisal, destruction and scheduling'), render_value($resource->getAppraisal(['cultureFallback' => true]))); ?>
+
+      <?php echo render_show(__('Physical condition'), render_value($resource->getPhysicalCharacteristics(['cultureFallback' => true]))); ?>
+
+      <?php echo render_show(__('Received extent units'), render_value($resource->getReceivedExtentUnits(['cultureFallback' => true]))); ?>
+
+      <?php echo render_show(__('Processing status'), render_value_inline($resource->processingStatus)); ?>
+
+      <?php echo render_show(__('Processing priority'), render_value_inline($resource->processingPriority)); ?>
+
+      <?php echo render_show(__('Processing notes'), render_value($resource->getProcessingNotes(['cultureFallback' => true]))); ?>
+
+      <?php
+          $accruals = [];
+          foreach (QubitRelation::getRelationsByObjectId($resource->id, ['typeId' => QubitTerm::ACCRUAL_ID]) as $item) {
+              $accruals[] = link_to(render_title($item->subject), [$item->subject, 'module' => 'accession']);
+          }
+          echo render_show(__('Accruals'), $accruals);
+      ?>
+
+      <?php
+          $accrualsTo = [];
+          foreach (QubitRelation::getRelationsBySubjectId($resource->id, ['typeId' => QubitTerm::ACCRUAL_ID]) as $item) {
+              $accrualsTo[] = link_to(render_title($item->object), [$item->object, 'module' => 'accession']);
+              $accrued = true;
+          }
+          echo render_show(__('Accrual to'), $accrualsTo);
+      ?>
+
+    </div> <!-- /.section#administrativeArea -->
+
+    <div class="section border-bottom" id="rightsArea">
+
+      <?php echo render_b5_section_label(__('Rights area')); ?>
+
+      <?php echo get_component('right', 'relatedRights', ['resource' => $resource]); ?>
+
+    </div> <!-- /.section#rightsArea -->
+
+    <div class="section border-bottom" id="informationObjectArea">
+
+      <?php echo link_to_if(QubitAcl::check($resource, 'update'), render_b5_section_label(__('%1% area', ['%1%' => sfConfig::get('app_ui_label_informationobject')])), [$resource, 'module' => 'accession', 'action' => 'edit'], ['anchor' => 'io-collapse', 'title' => __('Edit %1% area', ['%1%' => sfConfig::get('app_ui_label_informationobject')]), 'class' => 'text-primary']); ?>
+
+      <?php foreach (QubitRelation::getRelationsByObjectId($resource->id, ['typeId' => QubitTerm::ACCESSION_ID]) as $item) { ?>
+
+        <?php echo render_show(sfConfig::get('app_ui_label_informationobject'), link_to(esc_specialchars(render_title($item->subject)), [$item->subject, 'module' => 'informationobject'])); ?>
+
+      <?php } ?>
+
+    </div> <!-- /.section#deaccessionArea -->
+
+    <div class="section border-bottom" id="deaccessionArea">
+
+      <?php echo render_b5_section_label(__('Deaccession area')); ?>
+
+      <?php foreach ($resource->deaccessions as $item) { ?>
+
+        <?php echo render_show(__('Deaccession'), link_to(render_title($item, false), [$item, 'module' => 'deaccession'])); ?>
+
+      <?php } ?>
+
+    </div> <!-- /.section#deaccessionArea -->
+
+  </div>
+
+<?php end_slot(); ?>
 
 <?php slot('after-content'); ?>
   <ul class="actions nav gap-2">
