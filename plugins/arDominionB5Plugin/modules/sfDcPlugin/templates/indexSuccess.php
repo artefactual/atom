7c938f757491111db66d15339d18fa9ac8d75f8e
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-08-02 16:37:54 -0600

    Style content of DC index page

--- plugins/arDominionB5Plugin/modules/sfDcPlugin/templates/indexSuccess.php
+++ plugins/arDominionB5Plugin/modules/sfDcPlugin/templates/indexSuccess.php
@@ -9,8 +9,8 @@
   <?php echo get_component('informationobject', 'descriptionHeader', ['resource' => $resource, 'title' => (string) $dc, 'hideLevelOfDescription' => true]); ?>
 
   <?php if (isset($errorSchema)) { ?>
-    <div class="messages error">
-      <ul>
+    <div class="messages error alert alert-danger" role="alert">
+      <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
         <?php foreach ($errorSchema as $error) { ?>
           <?php $error = sfOutputEscaper::unescape($error); ?>
           <li><?php echo $error->getMessage(); ?></li>
@@ -53,20 +53,20 @@
   <?php echo get_component('digitalobject', 'show', ['link' => $digitalObjectLink, 'resource' => $resource->digitalObjectsRelatedByobjectId[0], 'usageType' => QubitTerm::REFERENCE_ID]); ?>
 <?php } ?>
 
-<section id="elementsArea">
+<section id="elementsArea" class="border-bottom">
 
-  <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationObject'), '<h2>'.__('Elements area').'</h2>', [$resource, 'module' => 'informationobject', 'action' => 'edit'], ['anchor' => 'elements-collapse', 'title' => __('Edit elements area')]); ?>
+  <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationObject'), render_b5_section_label(__('Elements area')), [$resource, 'module' => 'informationobject', 'action' => 'edit'], ['anchor' => 'elements-collapse', 'title' => __('Edit elements area'), 'class' => 'text-primary']); ?>
 
   <?php echo render_show(__('Identifier'), $resource->identifier); ?>
 
-  <?php echo render_show(__('Title'), render_value($resource->getTitle(['cultureFallback' => true]))); ?>
+  <?php echo render_show(__('Title'), render_value_inline($resource->getTitle(['cultureFallback' => true]))); ?>
 
   <?php $actorsShown = []; ?>
   <?php foreach ($resource->getCreators() as $item) { ?>
     <?php if (!isset($actorsShown[$item->id])) { ?>
-      <div class="field">
-        <h3><?php echo __('Creator'); ?></h3>
-        <div>
+      <div class="field <?php echo render_b5_show_field_css_classes(); ?>">
+        <?php echo render_b5_show_label(__('Creator')); ?>
+        <div class="<?php echo render_b5_show_value_css_classes(); ?>">
           <?php echo link_to(render_title($item), [$item, 'module' => 'actor']); ?><?php if (0 < strlen($value = $item->getDatesOfExistence(['cultureFallback' => true]))) { ?> <span class="note2">(<?php echo $value; ?>)</span><?php } ?>
         </div>
       </div>
@@ -75,18 +75,18 @@
   <?php } ?>
 
   <?php foreach ($resource->getPublishers() as $item) { ?>
-    <div class="field">
-      <h3><?php echo __('Publisher'); ?></h3>
-      <div>
+    <div class="field <?php echo render_b5_show_field_css_classes(); ?>">
+      <?php echo render_b5_show_label(__('Publisher')); ?>
+      <div class="<?php echo render_b5_show_value_css_classes(); ?>">
         <?php echo link_to(render_title($item), [$item, 'module' => 'actor']); ?><?php if ($value = $item->getDatesOfExistence(['cultureFallback' => true])) { ?> <span class="note2">(<?php echo $value; ?>)</span><?php } ?>
       </div>
     </div>
   <?php } ?>
 
   <?php foreach ($resource->getContributors() as $item) { ?>
-    <div class="field">
-      <h3><?php echo __('Contributor'); ?></h3>
-      <div>
+    <div class="field <?php echo render_b5_show_field_css_classes(); ?>">
+      <?php echo render_b5_show_label(__('Contributor')); ?>
+      <div class="<?php echo render_b5_show_value_css_classes(); ?>">
         <?php echo link_to(render_title($item), [$item, 'module' => 'actor']); ?><?php if ($value = $item->getDatesOfExistence(['cultureFallback' => true])) { ?> <span class="note2">(<?php echo $value; ?>)</span><?php } ?>
       </div>
     </div>
@@ -94,15 +94,17 @@
 
   <?php echo get_partial('informationobject/dates', ['resource' => $resource]); ?>
 
-  <?php foreach ($resource->getSubjectAccessPoints() as $item) { ?>
-    <?php echo render_show(__('Subject'), link_to(render_title($item->term), [$item->term, 'module' => 'term'])); ?>
-  <?php } ?>
+  <?php
+      $subjects = [];
+      foreach ($resource->getSubjectAccessPoints() as $item) {
+          $subjects[] = link_to(render_title($item->term), [$item->term, 'module' => 'term']);
+      }
+      echo render_show(__('Subjects'), $subjects);
+  ?>
 
   <?php echo render_show(__('Description'), render_value($resource->getScopeAndContent(['cultureFallback' => true]))); ?>
 
-  <?php foreach ($dc->type as $item) { ?>
-    <?php echo render_show(__('Type'), render_value($item)); ?>
-  <?php } ?>
+  <?php echo render_show(__('Types'), $dc->type); ?>
 
   <?php foreach ($dc->format as $item) { ?>
     <?php echo render_show(__('Format'), render_value($item)); ?>
@@ -110,15 +112,23 @@
 
   <?php echo render_show(__('Source'), render_value($resource->getLocationOfOriginals(['cultureFallback' => true]))); ?>
 
-  <?php foreach ($resource->language as $code) { ?>
-    <?php echo render_show(__('Language'), format_language($code)); ?>
-  <?php } ?>
+  <?php
+      $languages = [];
+      foreach ($resource->language as $code) {
+          $languages[] = format_language($code);
+      }
+      echo render_show(__('Languages'), $languages);
+  ?>
 
   <?php echo render_show_repository(__('Relation (isLocatedAt)'), $resource); ?>
 
-  <?php foreach ($dc->coverage as $item) { ?>
-    <?php echo render_show(__('Coverage (spatial)'), link_to(render_title($item), [$item, 'module' => 'term'])); ?>
-  <?php } ?>
+  <?php
+      $coverage = [];
+      foreach ($dc->coverage as $item) {
+          $coverage[] = link_to(render_title($item), [$item, 'module' => 'term']);
+      }
+      echo render_show(__('Coverage (spatial)'), $coverage);
+  ?>
 
   <?php echo render_show(__('Rights'), render_value($resource->getAccessConditions(['cultureFallback' => true]))); ?>
 
@@ -126,10 +136,10 @@
 
 <?php if ($sf_user->isAuthenticated()) { ?>
 
-  <section id="rightsArea">
+  <section id="rightsArea" class="border-bottom">
 
     <?php if (QubitAcl::check($resource, 'update')) { ?>
-      <h2><?php echo __('Rights area'); ?> </h2>
+      <?php echo render_b5_section_label(__('Rights area')); ?>
     <?php } ?>
 
     <?php echo get_component('right', 'relatedRights', ['resource' => $resource]); ?>
@@ -146,9 +156,9 @@
 
 <?php } ?>
 
-<section id="accessionArea">
+<section id="accessionArea" class="border-bottom">
 
-  <h2><?php echo __('Accession area'); ?></h2>
+  <?php echo render_b5_section_label(__('Accession area')); ?>
 
   <?php echo get_component('informationobject', 'accessions', ['resource' => $resource]); ?>
 
