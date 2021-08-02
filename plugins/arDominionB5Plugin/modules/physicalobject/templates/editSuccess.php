fd9402d02f3e1b9ce0b7cc24d84deb59d544428d
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-08-02 15:34:17 -0600

    Style content of physical object index page

--- plugins/arDominionB5Plugin/modules/physicalobject/templates/editSuccess.php
+++ plugins/arDominionB5Plugin/modules/physicalobject/templates/editSuccess.php
@@ -12,9 +12,9 @@
   <?php echo $form->renderGlobalErrors(); ?>
 
   <?php if (isset($sf_request->getAttribute('sf_route')->resource)) { ?>
-    <?php echo $form->renderFormTag(url_for([$resource, 'module' => 'physicalobject', 'action' => 'edit'])); ?>
+    <?php echo $form->renderFormTag(url_for([$resource, 'module' => 'physicalobject', 'action' => 'edit']), ['id' => 'editForm']); ?>
   <?php } else { ?>
-    <?php echo $form->renderFormTag(url_for(['module' => 'physicalobject', 'action' => 'add'])); ?>
+    <?php echo $form->renderFormTag(url_for(['module' => 'physicalobject', 'action' => 'add']), ['id' => 'editForm']); ?>
   <?php } ?>
 
     <?php echo $form->renderHiddenFields(); ?>
@@ -22,11 +22,11 @@
     <div class="accordion">
       <div class="accordion-item">
         <h2 class="accordion-header" id="edit-heading">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#edit-collapse" aria-expanded="true" aria-controls="edit-collapse">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#edit-collapse" aria-expanded="false" aria-controls="edit-collapse">
             <?php echo __('Edit %1%', ['%1%' => sfConfig::get('app_ui_label_physicalobject')]); ?>
           </button>
         </h2>
-        <div id="edit-collapse" class="accordion-collapse collapse show" aria-labelledby="edit-heading">
+        <div id="edit-collapse" class="accordion-collapse collapse" aria-labelledby="edit-heading">
           <div class="accordion-body">
             <?php echo render_field($form->name, $resource); ?>
             <?php echo render_field($form->location, $resource); ?>
