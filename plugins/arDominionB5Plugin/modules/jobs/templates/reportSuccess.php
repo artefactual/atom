df8153a0a4e5c49bb4637dd5c75afeeee410e376
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-08-03 12:50:15 -0600

    Style content of job index page

--- plugins/arDominionB5Plugin/modules/jobs/templates/reportSuccess.php
+++ plugins/arDominionB5Plugin/modules/jobs/templates/reportSuccess.php
@@ -4,76 +4,56 @@
   <h1><?php echo __('Job report'); ?></h1>
 <?php end_slot(); ?>
 
-<section id="report-overview-area">
-  <h2><?php echo __('Overview'); ?></h2>
+<section id="report-overview-area" class="border-bottom">
+  <?php echo render_b5_section_label(__('Overview')); ?>
 
-  <div class="job-report-field">
-    <div><?php echo __('Name'); ?></div>
-    <div><?php echo render_value($job->name); ?></div>
-  </div>
+  <?php echo render_show(__('Name'), render_value_inline($job->name)); ?>
 
-  <div class="job-report-field">
-    <div><?php echo __('Id'); ?></div>
-    <div><?php echo render_value($job->id); ?></div>
-  </div>
+  <?php echo render_show(__('Id'), render_value_inline($job->id)); ?>
 
-  <div class="job-report-field">
-    <div><?php echo __('Creation date'); ?></div>
-    <div><?php echo render_value($job->getCreationDateString()); ?></div>
-  </div>
+  <?php echo render_show(__('Creation date'), render_value_inline($job->getCreationDateString())); ?>
 
-  <div class="job-report-field">
-    <div><?php echo __('Completion date'); ?></div>
-    <div><?php echo render_value($job->getCompletionDateString()); ?></div>
-  </div>
+  <?php echo render_show(__('Completion date'), render_value_inline($job->getCompletionDateString())); ?>
 
-  <div class="job-report-field">
-    <div><?php echo __('Status'); ?></div>
-    <div>
-      <?php echo render_value($job->getStatusString()); ?>
-      <?php if (QubitTerm::JOB_STATUS_COMPLETED_ID == $job->statusId) { ?>
-        <i class="fa fa-check-square" id="job-check-color"></i>
-      <?php } elseif (QubitTerm::JOB_STATUS_ERROR_ID == $job->statusId) { ?>
-        <i class="fa fa-exclamation-triangle" id="job-warning-color"></i>
-      <?php } elseif (QubitTerm::JOB_STATUS_IN_PROGRESS_ID == $job->statusId) { ?>
-        <i class="fa fa-cogs" id="job-cogs-color"></i>
-      <?php } ?>
-    </div>
-  </div>
+  <?php
+      $icon = '';
+      if (QubitTerm::JOB_STATUS_COMPLETED_ID == $job->statusId) {
+          $icon = '<i aria-hidden="true" class="fa fa-check-square ms-2 text-success"></i>';
+      } elseif (QubitTerm::JOB_STATUS_ERROR_ID == $job->statusId) {
+          $icon = '<i aria-hidden="true" class="fa fa-exclamation-triangle ms-2 text-danger"></i>';
+      } elseif (QubitTerm::JOB_STATUS_IN_PROGRESS_ID == $job->statusId) {
+          $icon = '<i aria-hidden="true" class="fa fa-cogs ms-2 text-secondary"></i>';
+      }
+      $status = render_value_inline($job->getStatusString()).$icon;
+      echo render_show(__('Status'), $status);
+  ?>
 
-  <div class="job-report-field">
-    <div><?php echo __('Creator'); ?></div>
-    <div><?php echo render_value(QubitJob::getUserString($job)); ?></div>
-  </div>
+  <?php echo render_show(__('Creator'), render_value_inline(QubitJob::getUserString($job))); ?>
 
   <?php if ($job->getObjectModule() && $job->getObjectSlug()) { ?>
-    <div class="job-report-field">
-      <div><?php echo __('Associated record'); ?></div>
-      <div><?php echo link_to(__('Link'), ['module' => $job->getObjectModule(), 'slug' => $job->getObjectSlug()]); ?></div>
-    </div>
+    <?php echo render_show(__('Associated record'), link_to(__('Link'), ['module' => $job->getObjectModule(), 'slug' => $job->getObjectSlug()])); ?>
   <?php } ?>
 
   <?php if ($job->downloadPath) { ?>
-    <div class="job-report-field">
-      <div><?php echo __('Download path'); ?></div>
-      <div><?php echo link_to(__('Link'), public_path($job->downloadPath)); ?></div>
-    </div>
+    <?php echo render_show(__('Download path'), link_to(__('Link'), public_path($job->downloadPath))); ?>
   <?php } ?>
 </section>
 
-<section id="log-area">
+<section id="log-area" class="border-bottom">
   <?php if (!empty($errorOutput)) { ?>
-    <h2><?php echo __('Error(s)'); ?></h2>
-    <div>
-      <pre id="job-log-error-output"><?php echo render_value($errorOutput); ?></pre>
+    <div class="border-bottom">
+      <?php echo render_b5_section_label(__('Error(s)')); ?>
+      <div class="alert alert-danger m-2" role="alert">
+        <pre id="job-log-error-output"><?php echo render_value_inline($errorOutput); ?></pre>
+      </div>
     </div>
   <?php } ?>
 
-  <h2><?php echo __('Log'); ?></h2>
-  <div>
+  <?php echo render_b5_section_label(__('Log')); ?>
+  <div class="bg-secondary text-white rounded p-3 m-2">
     <?php $output = trim($job->output); ?>
     <?php if (0 < strlen($output)) { ?>
-      <pre id="job-log-output"><?php echo $output; ?></pre>
+      <pre id="job-log-output" class="mb-0"><?php echo $output; ?></pre>
     <?php } else { ?>
       <p id="job-log-output-empty"><?php echo __('Empty'); ?></p>
     <?php } ?>
