4367083e72c38e5db9c10b82e692919892069cec
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-08-05 12:44:59 +0200

    Tweak carousel paddings/margins

--- plugins/arDominionB5Plugin/modules/digitalobject/templates/_imageflow.php
+++ plugins/arDominionB5Plugin/modules/digitalobject/templates/_imageflow.php
@@ -15,7 +15,7 @@
     </h2>
     <div id="collapse-carousel" class="accordion-collapse collapse show" aria-labelledby="heading-carousel">
       <div class="accordion-body bg-secondary px-5 pt-4 pb-3">
-        <div id="atom-slider-images">
+        <div id="atom-slider-images" class="mb-0">
           <?php foreach ($thumbnails as $item) { ?>
             <a title="<?php echo $item->parent->object; ?>" href="<?php echo url_for([$item->parent->object, 'module' => 'informationobject']); ?>">
               <?php echo image_tag($item->getFullPath(), ['class' => 'img-thumbnail mx-2', 'longdesc' => url_for([$item->parent->object, 'module' => 'informationobject']), 'alt' => strip_markdown($item->getDigitalObjectAltText() ?: $item->parent->object)]); ?>
@@ -25,14 +25,14 @@
 
         <div id="atom-slider-title">
           <?php foreach ($thumbnails as $item) { ?>
-            <a href="<?php echo url_for([$item->parent->object, 'module' => 'informationobject']); ?>" class="text-white text-center mt-1">
+            <a href="<?php echo url_for([$item->parent->object, 'module' => 'informationobject']); ?>" class="text-white text-center mt-2 mb-1">
               <?php echo strip_markdown($item->parent->object); ?>
             </a>
           <?php } ?>
         </div>
 
         <?php if (isset($limit) && $limit < $total) { ?>
-          <div class="text-white text-center mt-1">
+          <div class="text-white text-center mt-2 mb-1">
             <?php echo __('Results %1% to %2% of %3%', ['%1%' => 1, '%2%' => $limit, '%3%' => $total]); ?>
             <a class='btn atom-btn-outline-light btn-sm ms-2' href="<?php echo url_for([
                 'module' => 'informationobject',
