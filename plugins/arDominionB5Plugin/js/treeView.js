0b9c81d81a309b2654637ff06dd9b10aacfae3ac
Jesús García Crespo <jesus@sevein.com>
2021-08-06 23:14:18 +0200

    Use Node.contains() to simplify code

--- plugins/arDominionB5Plugin/js/treeView.js
+++ plugins/arDominionB5Plugin/js/treeView.js
@@ -24,39 +24,6 @@
     event.stopPropagation();
   }
 
-  function indexOf(value, array) {
-    var i = 0,
-      l = array.length,
-      v;
-
-    if (typeof value === "undefined") {
-      return -1;
-    }
-
-    if (value.constructor === String) {
-      for (; i < l; i = i + 1) {
-        if (value.localeCompare(array[i]) === 0) {
-          return i;
-        }
-      }
-    } else {
-      for (; i < l; i = i + 1) {
-        v = array[i];
-        if (v.constructor === String) {
-          if (v.localeCompare(value) === 0) {
-            return i;
-          }
-        } else {
-          if (v === value) {
-            return i;
-          }
-        }
-      }
-    }
-
-    return -1;
-  }
-
   class Treeview {
     constructor($element) {
       this.$element = $element;
@@ -305,7 +272,7 @@
       }
     }
     scroll(e) {
-      if (indexOf(e.target, this.$element.get()) >= 0) {
+      if (e.target.contains(this.$element.get(0))) {
         this.notify(e);
       }
     }
