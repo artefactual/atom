c1fe46217ebced566d296890445fd6f5856623c9
Mike Cantelon <mcantelon@gmail.com>
2021-08-08 14:04:26 -0700

    [C] UNOG Preservica integration plugin

---
+++ plugins/arUnogPreservicaPlugin/modules/arUnogPreservicaPlugin/actions/digitalobjectsCreateAction.class.php
@@ -0,0 +1,147 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class arUnogPreservicaPluginDigitalObjectsCreateAction extends QubitApiAction
+{
+    protected function post($request, $payload)
+    {
+        $slug = $payload->slug;
+        $uuid = $payload->uuid;
+        $scopeId = $payload->scopeId;
+
+        // Fail if either a slug or UUID hasn't been provided
+        if (empty($slug) || empty($uuid)) {
+            $message = $this->context->i18n->__('Both slug and UUID must be specified.');
+
+            throw new QubitApiBadRequestException($message);
+        }
+
+        // Attempt to load information object
+        if (empty($io = QubitInformationObject::getBySlug($slug)) || 'QubitInformationObject' != get_class($io)) {
+            $message = $this->context->i18n->__('Information object not found.');
+
+            throw new QubitApiBadRequestException($message);
+        }
+
+        // Fail if a digital object already exists for the information object
+        if (!empty($io->getDigitalObject())) {
+            $message = $this->context->i18n->__('Information object already has a digital object.');
+
+            throw new QubitApiBadRequestException($message);
+        }
+
+        // Get Preservica digital object details
+        $username = sfConfig::get('app_preservica_username');
+        $password = sfConfig::get('app_preservica_password');
+        $host = sfConfig::get('app_preservica_host');
+
+        $client = new arUnogPreservicaPluginRestClient($host, $username, $password);
+
+        // Fail if credentials are invalid
+        if (empty($client->token)) {
+            $message = $this->context->i18n->__('Invalid token/credentials.');
+
+            throw new QubitApiUnknownException($message);
+        }
+
+        // Fail if no object with that UUID exists
+        if (empty($objectData = $client->getObjectDetails($uuid))) {
+            $message = $this->context->i18n->__('Error fetching object data from Preservica.');
+
+            throw new QubitApiUnknownException($message);
+        }
+
+        $cmisId = $objectData->value->id;
+        $filename = $client->getObjectDetailsPropertyByName($objectData, 'cmis:contentStreamFileName');
+
+        // Download Preservica digital object's thumbnail to a temp file
+        $thumbTempFilePath = $client->downloadThumbnailToTempDir($cmisId, $filename);
+
+        // Create digital object with thumbnail as representation derivative
+        $do = new QubitDigitalObject();
+        $do->objectId = $io->id;
+        $do->importFromFile($thumbTempFilePath);
+        $do->save();
+
+        // Remove thumbnail temp dir and file
+        unlink($thumbTempFilePath);
+        rmdir(dirname($thumbTempFilePath));
+
+        // Store transcript, if one exists, as digital object property
+        if (!empty($fullText = $client->getFullText($uuid))) {
+            $this->storeTranscript($fullText, $do->id);
+        }
+
+        // Add property containing Preservica UUID
+        $this->addAlternativeIdentifier($do->id, arUnogPreservicaPluginConfiguration::PRESERVICA_UUID_PROPERTY_NAME, $uuid);
+
+        // Optionally add property containing Scope ID
+        if (!empty($scopeId)) {
+            $this->addAlternativeIdentifier($io->id, 'Scope ID', $scopeId);
+        }
+
+        $this->response->setStatusCode(201);
+
+        // Update information object's Elasticsearch document
+        QubitSearch::getInstance()->update($io, ['updateDescendants' => true]);
+
+        return ['id' => (int) $do->id, 'slug' => $do->slug];
+    }
+
+    private function addAlternativeIdentifier($objectId, $label, $id)
+    {
+        $property = new QubitProperty();
+        $property->objectId = $objectId;
+        $property->name = $label;
+        $property->scope = 'alternativeIdentifiers';
+
+        $property->setValue($id, ['sourceCulture' => true]);
+        $property->save();
+    }
+
+    private function storeTranscript($text, $doId)
+    {
+        // NOTE:
+        //
+        // This logic is replicated from the QubitDigitalObject model.
+        // If we add this functionality to upstream AtoM we should create
+        // a static method in the QubitDigitalObject model and use that.
+
+        // Truncate PDF text to <64KB to fit in `property.value` column
+        $text = mb_strcut($text, 0, 65535);
+
+        // Update or create 'transcript' property
+        $criteria = new Criteria();
+        $criteria->add(QubitProperty::OBJECT_ID, $doId);
+
+        $criteria->add(QubitProperty::NAME, 'transcript');
+
+        if (null === $property = QubitProperty::getOne($criteria)) {
+            $property = new QubitProperty();
+            $property->objectId = $doId;
+            $property->name = 'transcript';
+            $property->scope = 'Text extracted from source PDF file\'s text layer using Preservica API';
+        }
+
+        $property->value = $text;
+        $property->indexOnSave = false;
+
+        $property->save($connection);
+    }
+}
---
+++ plugins/arUnogPreservicaPlugin/modules/digitalobject/templates/_metadata.php
@@ -0,0 +1,254 @@
+<?php use_helper('Date'); ?>
+
+<section>
+
+  <?php if ($relatedToIo) { ?>
+    <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'informationobject'), '<h2>'.__('%1% metadata', ['%1%' => sfConfig::get('app_ui_label_digitalobject')]).'</h2>', [$resource, 'module' => 'digitalobject', 'action' => 'edit'], ['title' => __('Edit %1%', ['%1%' => mb_strtolower(sfConfig::get('app_ui_label_digitalobject'))])]); ?>
+  <?php } elseif ($relatedToActor) { ?>
+    <?php echo link_to_if(SecurityPrivileges::editCredentials($sf_user, 'actor'), '<h2>'.__('%1% metadata', ['%1%' => sfConfig::get('app_ui_label_digitalobject')]).'</h2>', [$resource, 'module' => 'digitalobject', 'action' => 'edit'], ['title' => __('Edit %1%', ['%1%' => mb_strtolower(sfConfig::get('app_ui_label_digitalobject'))])]); ?>
+  <?php } ?>
+
+  <?php if ($showOriginalFileMetadata || $showPreservationCopyMetadata) { ?>
+
+    <fieldset class="collapsible digital-object-metadata single">
+      <legend><?php echo __('Preservation Copies'); ?></legend>
+
+      <?php if ($showOriginalFileMetadata) { ?>
+
+        <div class="digital-object-metadata-header">
+          <h3><?php echo __('Original file'); ?> <i class="fa fa-archive<?php if (!$canAccessOriginalFile) { ?> inactive<?php } ?>" aria-hidden="true"></i></h3>
+        </div>
+
+        <div class="digital-object-metadata-body">
+          <?php if ($showOriginalFileName) { ?>
+            <?php echo render_show(__('Filename'), render_value($resource->object->originalFileName), ['fieldLabel' => 'originalFileName']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFormatName) { ?>
+            <?php echo render_show(__('Format name'), render_value($resource->object->formatName), ['fieldLabel' => 'originalFileFormatName']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFormatVersion) { ?>
+            <?php echo render_show(__('Format version'), render_value($resource->object->formatVersion), ['fieldLabel' => 'originalFileFormatVersion']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFormatRegistryKey) { ?>
+            <?php echo render_show(__('Format registry key'), render_value($resource->object->formatRegistryKey), ['fieldLabel' => 'originalFileFormatRegistryKey']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFormatRegistryName) { ?>
+            <?php echo render_show(__('Format registry name'), render_value($resource->object->formatRegistryName), ['fieldLabel' => 'originalFileFormatRegistryName']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFileSize) { ?>
+            <?php echo render_show(__('Filesize'), hr_filesize(intval((string) $resource->object->originalFileSize)), ['fieldLabel' => 'originalFileSize']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFileIngestedAt) { ?>
+            <?php echo render_show(__('Ingested'), format_date($originalFileIngestedAt, 'f'), ['fieldLabel' => 'originalFileIngestedAt']); ?>
+          <?php } ?>
+
+          <?php if ($showOriginalFilePermissions) { ?>
+            <?php echo render_show(__('Permissions'), render_value($accessStatement), ['fieldLabel' => 'originalFilePermissions']); ?>
+          <?php } ?>
+
+          <?php if ($sf_user->isAuthenticated() && $relatedToIo) { ?>
+            <?php if ($storageServicePluginEnabled) { ?>
+              <?php include_partial(
+                'arStorageService/aipDownload', ['resource' => $resource]
+              ); ?>
+            <?php } else { ?>
+              <?php echo render_show(
+                __('File UUID'),
+                render_value($resource->object->objectUUID),
+                ['fieldLabel' => 'objectUUID']
+              ); ?>
+              <?php echo render_show(
+                __('AIP UUID'),
+                render_value($resource->object->aipUUID),
+                ['fieldLabel' => 'aipUUID']
+              ); ?>
+            <?php } ?>
+          <?php } ?>
+
+        </div>
+
+      <?php } ?>
+
+      <?php if ($showPreservationCopyMetadata) { ?>
+
+        <div class="digital-object-metadata-header">
+          <h3><?php echo __('Preservation copy'); ?> <i class="fa fa-archive<?php if (!$canAccessPreservationCopy) { ?> inactive<?php } ?>" aria-hidden="true"></i></h3>
+        </div>
+
+        <div class="digital-object-metadata-body">
+          <?php if ($showPreservationCopyFileName) { ?>
+            <?php echo render_show(__('Filename'), render_value($resource->object->preservationCopyFileName), ['fieldLabel' => 'preservationCopyFileName']); ?>
+          <?php } ?>
+
+          <?php if ($showPreservationCopyFileSize) { ?>
+            <?php echo render_show(__('Filesize'), hr_filesize(intval((string) $resource->object->preservationCopyFileSize)), ['fieldLabel' => 'preservationCopyFileSize']); ?>
+          <?php } ?>
+
+          <?php if ($showPreservationCopyNormalizedAt) { ?>
+            <?php echo render_show(__('Normalized'), format_date($preservationCopyNormalizedAt, 'f'), ['fieldLabel' => 'preservactionCopyNormalizedAt']); ?>
+          <?php } ?>
+
+          <?php if ($showPreservationCopyPermissions) { ?>
+            <?php echo render_show(__('Permissions'), render_value($accessStatement), ['fieldLabel' => 'preservationCopyPermissions']); ?>
+          <?php } ?>
+
+        </div>
+
+      <?php } ?>
+
+    </fieldset>
+
+  <?php } ?>
+
+  <?php $preservicaUUID = $resource->getPropertyByName(arUnogPreservicaPluginConfiguration::PRESERVICA_UUID_PROPERTY_NAME)->__toString(); ?>
+
+  <?php if ($showMasterFileMetadata || $showReferenceCopyMetadata || $showThumbnailCopyMetadata || $preservicaUUID) { ?>
+
+    <fieldset class="collapsible digital-object-metadata single">
+      <legend><?php echo __('Access Copies'); ?></legend>
+
+      <?php if ($showMasterFileMetadata || $preservicaUUID) { ?>
+
+        <div class="digital-object-metadata-header">
+          <h3><?php echo __('Master file'); ?> <i class="fa fa-file<?php if (!$canAccessMasterFile) { ?> inactive<?php } ?>" aria-hidden="true"></i></h3>
+        </div>
+
+        <div class="digital-object-metadata-body">
+          <?php if ($showMasterFileGoogleMap) { ?>
+            <div id="front-map" class="simple-map" data-key="<?php echo $googleMapsApiKey; ?>" data-latitude="<?php echo $latitude; ?>" data-longitude="<?php echo $longitude; ?>"></div>
+          <?php } ?>
+
+          <?php if ($showMasterFileGeolocation) { ?>
+            <?php echo render_show(__('Latitude'), render_value($latitude), ['fieldLabel' => 'latitude']); ?>
+            <?php echo render_show(__('Longitude'), render_value($longitude), ['fieldLabel' => 'longitude']); ?>
+          <?php } ?>
+
+          <?php if ($showMasterFileURL) { ?>
+            <?php echo render_show(__('URL'), render_value($resource->path), ['fieldLabel' => 'url']); ?>
+          <?php } ?>
+
+          <?php if ($showMasterFileName) { ?>
+            <?php if ($canAccessMasterFile) { ?>
+              <?php if (empty($preservicaUUID)) { ?>
+                <?php echo render_show(__('Filename'), link_to(render_value_inline($resource->name), $resource->object->getDigitalObjectUrl(), ['target' => '_blank']), ['fieldLabel' => 'filename']); ?>
+              <?php } else { ?>
+                <?php echo render_show(__('Filename'), link_to(render_value_inline($resource->name), url_for([$resource->object, 'sf_route' => 'preservica_download_master']), ['target' => '_blank']), ['fieldLabel' => 'filename']); ?>
+              <?php } ?>
+            <?php } else { ?>
+              <?php echo render_show(__('Filename'), render_value($resource->name), ['fieldLabel' => 'filename']); ?>
+            <?php } ?>
+          <?php } ?>
+
+          <?php if ($showMasterFileMediaType) { ?>
+            <?php echo render_show(__('Media type'), render_value($resource->mediaType), ['fieldLabel' => 'mediaType']); ?>
+          <?php } ?>
+
+          <?php if ($showMasterFileMimeType) { ?>
+            <?php echo render_show(__('Mime-type'), render_value($resource->mimeType), ['fieldLabel' => 'mimeType']); ?>
+          <?php } ?>
+
+          <?php if ($showMasterFileSize) { ?>
+            <?php echo render_show(__('Filesize'), hr_filesize($resource->byteSize), ['fieldLabel' => 'filesize']); ?>
+          <?php } ?>
+
+          <?php if ($showMasterFileCreatedAt) { ?>
+            <?php echo render_show(__('Uploaded'), format_date($resource->createdAt, 'f'), ['fieldLabel' => 'uploaded']); ?>
+          <?php } ?>
+
+          <?php if ($showMasterFilePermissions) { ?>
+            <?php echo render_show(__('Permissions'), render_value($masterFileDenyReason), ['fieldLabel' => 'masterFilePermissions']); ?>
+          <?php } ?>
+
+        </div>
+
+      <?php } ?>
+
+      <?php if ($showReferenceCopyMetadata) { ?>
+
+        <div class="digital-object-metadata-header">
+          <h3><?php echo __('Reference copy'); ?> <i class="fa fa-file<?php if (!$canAccessReferenceCopy) { ?> inactive<?php } ?>" aria-hidden="true"></i></h3>
+        </div>
+
+        <div class="digital-object-metadata-body">
+          <?php if ($showReferenceCopyFileName) { ?>
+            <?php if ($canAccessReferenceCopy && $sf_user->isAuthenticated()) { ?>
+              <?php echo render_show(__('Filename'), link_to(render_value_inline($referenceCopy->name), $referenceCopy->getFullPath(), ['target' => '_blank']), ['fieldLabel' => 'referenceCopyFileName']); ?>
+            <?php } else { ?>
+              <?php echo render_show(__('Filename'), render_value($referenceCopy->name), ['fieldLabel' => 'referenceCopyFileName']); ?>
+            <?php } ?>
+          <?php } ?>
+
+          <?php if ($showReferenceCopyMediaType) { ?>
+            <?php echo render_show(__('Media type'), render_value($referenceCopy->mediaType), ['fieldLabel' => 'referenceCopyFileName']); ?>
+          <?php } ?>
+
+          <?php if ($showReferenceCopyMimeType) { ?>
+            <?php echo render_show(__('Mime-type'), render_value($referenceCopy->mimeType), ['fieldLabel' => 'referenceCopyMimeType']); ?>
+          <?php } ?>
+
+          <?php if ($showReferenceCopyFileSize) { ?>
+            <?php echo render_show(__('Filesize'), hr_filesize($referenceCopy->byteSize), ['fieldLabel' => 'referenceCopyFileSize']); ?>
+          <?php } ?>
+
+          <?php if ($showReferenceCopyCreatedAt) { ?>
+            <?php echo render_show(__('Uploaded'), format_date($referenceCopy->createdAt, 'f'), ['fieldLabel' => 'referenceCopyUploaded']); ?>
+          <?php } ?>
+
+          <?php if ($showReferenceCopyPermissions) { ?>
+            <?php echo render_show(__('Permissions'), render_value($referenceCopyDenyReason), ['fieldLabel' => 'referenceCopyPermissions']); ?>
+          <?php } ?>
+
+        </div>
+
+      <?php } ?>
+
+      <?php if ($showThumbnailCopyMetadata) { ?>
+
+        <div class="digital-object-metadata-header">
+          <h3><?php echo __('Thumbnail copy'); ?> <i class="fa fa-file<?php if (!$canAccessThumbnailCopy) { ?> inactive<?php } ?>" aria-hidden="true"></i></h3>
+        </div>
+
+        <div class="digital-object-metadata-body">
+          <?php if ($showThumbnailCopyFileName) { ?>
+            <?php if ($canAccessThumbnailCopy) { ?>
+              <?php echo render_show(__('Filename'), link_to(render_value_inline($thumbnailCopy->name), $thumbnailCopy->getFullPath(), ['target' => '_blank']), ['fieldLabel' => 'thumbnailCopyFileName']); ?>
+            <?php } else { ?>
+              <?php echo render_show(__('Filename'), render_value($thumbnailCopy->name), ['fieldLabel' => 'thumbnailCopyFileName']); ?>
+            <?php } ?>
+          <?php } ?>
+
+          <?php if ($showThumbnailCopyMediaType) { ?>
+            <?php echo render_show(__('Media type'), render_value($thumbnailCopy->mediaType), ['fieldLabel' => 'thumbnailCopyFileName']); ?>
+          <?php } ?>
+
+          <?php if ($showThumbnailCopyMimeType) { ?>
+            <?php echo render_show(__('Mime-type'), render_value($thumbnailCopy->mimeType), ['fieldLabel' => 'thumbnailCopyMimeType']); ?>
+          <?php } ?>
+
+          <?php if ($showThumbnailCopyFileSize) { ?>
+            <?php echo render_show(__('Filesize'), hr_filesize($thumbnailCopy->byteSize), ['fieldLabel' => 'thumbnailCopyFileSize']); ?>
+          <?php } ?>
+
+          <?php if ($showThumbnailCopyCreatedAt) { ?>
+            <?php echo render_show(__('Uploaded'), format_date($thumbnailCopy->createdAt, 'f'), ['fieldLabel' => 'thumbnailCopyUploaded']); ?>
+          <?php } ?>
+
+          <?php if (!empty($thumbnailCopyDenyReason)) { ?>
+            <?php echo render_show(__('Permissions'), render_value($thumbnailCopyDenyReason), ['fieldLabel' => 'thumbnailCopyPermissions']); ?>
+          <?php } ?>
+
+        </div>
+
+      <?php } ?>
+
+    </fieldset>
+
+  <?php } ?>
+
+</section>
