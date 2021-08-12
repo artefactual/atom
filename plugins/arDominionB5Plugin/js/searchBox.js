bf6bfaf02fac536059a6b0cb4adfce0a72735554
Jesús García Crespo <jesus@sevein.com>
2021-08-12 17:54:48 +0200

    Install Prettier

--- plugins/arDominionB5Plugin/js/searchBox.js
+++ plugins/arDominionB5Plugin/js/searchBox.js
@@ -1,10 +1,9 @@
-($ => {
-
-  'use strict';
+(($) => {
+  "use strict";
 
   $(() => {
-    var $input = $('#search-box-input');
-    var $results = $('#search-box-results');
+    var $input = $("#search-box-input");
+    var $results = $("#search-box-results");
     var dropdown = bootstrap.Dropdown.getOrCreateInstance($input);
 
     // Set up Bootstrap autocomplete:
@@ -16,19 +15,18 @@
     // - Avoid no results and results dropdown, and use custom
     //   search results dropdown body.
     $input.autoComplete({
-      bootstrapVersion: '4',
-      noResultsText: '',
+      bootstrapVersion: "4",
+      noResultsText: "",
       events: {
         search: (query, callback, $element) => {
-          var data = {'query': query};
+          var data = { query: query };
           var $repos = $('#search-box input[name="repos"]:checked');
           if ($repos.length && $repos.val()) {
             data.repos = $repos.val();
           }
-          $.ajax(
-            $element.data('url'),
-            {data: data}
-          ).done(res => callback(res));
+          $.ajax($element.data("url"), { data: data }).done((res) =>
+            callback(res)
+          );
         },
         searchPost: (response, $element) => {
           if (response.length && $element.val().length >= 3) {
@@ -36,29 +34,28 @@
             dropdown.show();
           } else {
             dropdown.hide();
-            $results.html('');
+            $results.html("");
           }
           return [];
-        }
-      }
+        },
+      },
     });
 
     // Hide dropdown when the input is lower than 3 chars.
     // Bootstrap autocomplete `typed` event is not triggered
     // on all changes to the input.
-    $input.on('input', event => {
+    $input.on("input", (event) => {
       if (event.target.value.length < 3) {
         dropdown.hide();
-        $results.html('');
+        $results.html("");
       }
     });
 
     // Prevent showing an empty dropdown
-    $input.on('show.bs.dropdown', event => {
+    $input.on("show.bs.dropdown", (event) => {
       if ($results.children().length == 0) {
         event.preventDefault();
       }
     });
   });
-
 })(jQuery);
