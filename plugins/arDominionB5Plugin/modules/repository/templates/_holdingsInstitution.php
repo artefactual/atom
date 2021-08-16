c552cbe91665d78149d5ff99b84db27b343b13ca
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-08-16 11:17:06 +0200

    Tweak sidebar margins/paddings

--- plugins/arDominionB5Plugin/modules/repository/templates/_holdingsInstitution.php
+++ plugins/arDominionB5Plugin/modules/repository/templates/_holdingsInstitution.php
@@ -1,12 +1,8 @@
-<section class="card mb-4">
-
-  <div class="p-4">
-    <?php include_component('repository', 'logo'); ?>
-  </div>
-
+<section class="card mb-3">
   <div class="card-body">
+    <?php include_component('repository', 'logo'); ?>
 
-    <form class="mb-4" role="search" aria-label="<?php echo sfConfig::get('app_ui_label_institutionSearchHoldings'); ?>" action="<?php echo url_for(['module' => 'informationobject', 'action' => 'browse']); ?>">
+    <form class="mb-3" role="search" aria-label="<?php echo sfConfig::get('app_ui_label_institutionSearchHoldings'); ?>" action="<?php echo url_for(['module' => 'informationobject', 'action' => 'browse']); ?>">
       <input type="hidden" name="repos" value="<?php echo $resource->id; ?>">
       <label for="institution-search-query" class="h5 mb-2 form-label"><?php echo sfConfig::get('app_ui_label_institutionSearchHoldings'); ?></label>
       <div class="input-group">
@@ -18,7 +14,5 @@
     </form>
 
     <?php echo get_component('menu', 'browseMenuInstitution', ['sf_cache_key' => 'dominion-b5'.$sf_user->getCulture().$sf_user->getUserID()]); ?>
-
   </div>
-
 </section>
