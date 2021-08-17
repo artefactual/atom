f15c32b5734fa993db67bde9281c9e9333399b37
Jesús García Crespo <jesus@sevein.com>
2021-08-17 08:06:08 +0200

    Update alerts layout

--- plugins/arDominionB5Plugin/modules/donor/templates/indexSuccess.php
+++ plugins/arDominionB5Plugin/modules/donor/templates/indexSuccess.php
@@ -10,7 +10,7 @@
 <?php slot('before-content'); ?>
 
   <?php if (isset($errorSchema)) { ?>
-    <div class="messages error alert alert-danger" role="alert">
+    <div class="alert alert-danger" role="alert">
       <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
         <?php foreach ($errorSchema as $error) { ?>
           <?php $error = sfOutputEscaper::unescape($error); ?>
