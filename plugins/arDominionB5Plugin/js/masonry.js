d8cbc6a1644140324bd37bf4e9446c30d9f4d3d8
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-08-26 15:54:32 -0600

    Fix JS errors when elements are missing

--- plugins/arDominionB5Plugin/js/masonry.js
+++ plugins/arDominionB5Plugin/js/masonry.js
@@ -2,6 +2,9 @@
   "use strict";
 
   const $container = $(".masonry");
+  if (!$container.length) {
+    return;
+  }
 
   // Rely on imagesloaded to init Masonry once all images have loaded.
   // DOMContentLoaded triggers too early, window.onload triggers too late.
