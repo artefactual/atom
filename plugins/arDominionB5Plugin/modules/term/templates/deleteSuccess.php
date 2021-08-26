2be53f60562d5ffe7cf4f1a3988b13582eb501a6
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-08-26 14:35:47 +0200

    Tweak content sections
    Remove them where a table is used as main content and tweak paddings.

--- plugins/arDominionB5Plugin/modules/term/templates/deleteSuccess.php
+++ plugins/arDominionB5Plugin/modules/term/templates/deleteSuccess.php
@@ -5,37 +5,34 @@
 <?php end_slot(); ?>
 
 <?php slot('content'); ?>
-
   <?php echo $form->renderGlobalErrors(); ?>
-
   <?php echo $form->renderFormTag(url_for([$resource, 'module' => 'term', 'action' => 'delete']), ['method' => 'delete']); ?>
-
     <?php echo $form->renderHiddenFields(); ?>
 
-    <div id="content">
+    <?php if (
+        0 < $resource->getRelatedObjectCount()
+        || 0 < count($resource->events)
+        || 0 < $count
+    ) { ?>
+      <div id="content" class="p-3 pb-0">
+        <?php if (0 < $resource->getRelatedObjectCount()) { ?>
+          <?php echo __('This term is used in %1% records. The term will be deleted from these records.', ['%1%' => $resource->getRelatedObjectCount()]); ?>
+          <div class="mb-3">
+            <?php echo __('The related object(s) will <strong>not</strong> be deleted'); ?>
+          </div>
+        <?php } ?>
 
-      <?php if (0 < $resource->getRelatedObjectCount()) { ?>
-        <h2><?php echo __('This term is used in %1% records. The term will be deleted from these records.', ['%1%' => $resource->getRelatedObjectCount()]); ?></h2>
-        <div class="text-section">
-          <?php echo __('The related object(s) will <strong>not</strong> be deleted'); ?>
-        </div>
-      <?php } ?>
-
-      <?php if (0 < count($resource->events)) { ?>
-        <h2><?php echo __('It\'s used in %1% events that will also be deleted', ['%1%' => count($resource->events)]); ?></h2>
-        <div class="delete-list">
+        <?php if (0 < count($resource->events)) { ?>
+          <?php echo __('It\'s used in %1% events that will also be deleted', ['%1%' => count($resource->events)]); ?>
           <ul>
             <?php foreach ($resource->events as $item) { ?>
               <li><?php echo render_value_inline(Qubit::renderDateStartEnd($item->getDate(['cultureFallback' => true]), $item->startDate, $item->endDate)); ?> (<?php echo render_title($resource); ?>) <?php echo link_to(render_title($item->object), [$item->object, 'module' => 'informationobject']); ?></li>
             <?php } ?>
           </ul>
-        </div>
-      <?php } ?>
+        <?php } ?>
 
-      <?php if (0 < $count) { ?>
-        <h2><?php echo __('It has %1% descendants that will also be deleted', ['%1%' => $count]); ?></h2>
-        <div class="delete-list">
-
+        <?php if (0 < $count) { ?>
+          <?php echo __('It has %1% descendants that will also be deleted', ['%1%' => $count]); ?>
           <ul>
             <?php foreach ($resource->descendants as $index => $item) { ?>
               <li><?php echo link_to(render_title($item), [$item, 'module' => 'term']); ?></li>
@@ -46,16 +43,13 @@
           </ul>
 
           <?php if ($previewIsLimited) { ?>
-            <hr />
-            <p>
+            <div class="alert alert-warning">
               <?php echo __('Only %1% terms were shown.', ['%1%' => $previewSize]); ?>
-            </p>
+            </div>
           <?php } ?>
-
-        </div>
-      <?php } ?>
-
-    </div>
+        <?php } ?>
+      </div>
+    <?php } ?>
 
     <ul class="actions nav gap-2">
       <li><?php echo link_to(__('Cancel'), [$resource, 'module' => 'term'], ['class' => 'btn atom-btn-outline-light', 'role' => 'button']); ?></li>
