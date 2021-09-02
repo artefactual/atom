5f4052a6e8fe3795915ed847e6fd47996f4e6dff
Jesús García Crespo <jesus@sevein.com>
2021-09-02 21:08:39 +0200

    Add new sections to settings/global

--- plugins/arDominionB5Plugin/modules/settings/templates/globalSuccess.php
+++ plugins/arDominionB5Plugin/modules/settings/templates/globalSuccess.php
@@ -22,52 +22,112 @@
 
     <div class="accordion">
       <div class="accordion-item">
-        <h2 class="accordion-header" id="global-heading">
-          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#global-collapse" aria-expanded="true" aria-controls="global-collapse">
-            <?php echo __('Global settings'); ?>
+        <h2 class="accordion-header" id="version-heading">
+          <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#version-collapse" aria-expanded="true" aria-controls="version-collapse">
+            <?php echo __('Version'); ?>
           </button>
         </h2>
-        <div id="global-collapse" class="accordion-collapse collapse show" aria-labelledby="global-heading">
+        <div id="version-collapse" class="accordion-collapse collapse show" aria-labelledby="version-heading">
           <div class="accordion-body">
             <?php echo render_field($globalForm->version); ?>
 
             <?php echo render_field($globalForm->check_for_updates); ?>
-
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="search-heading">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#search-collapse" aria-expanded="false" aria-controls="search-collapse">
+            <?php echo __('Search and browse'); ?>
+          </button>
+        </h2>
+        <div id="search-collapse" class="accordion-collapse collapse" aria-labelledby="search-heading">
+          <div class="accordion-body">
             <?php echo render_field($globalForm->hits_per_page); ?>
-
-            <?php echo render_field($globalForm->escape_queries); ?>
 
             <?php echo render_field($globalForm->sort_browser_user); ?>
 
             <?php echo render_field($globalForm->sort_browser_anonymous); ?>
 
-            <?php echo render_field($globalForm->default_repository_browse_view); ?>
-
             <?php echo render_field($globalForm->default_archival_description_browse_view); ?>
 
+            <?php echo render_field($globalForm->default_repository_browse_view); ?>
+
+            <?php echo render_field($globalForm->escape_queries); ?>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="presentation-heading">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#presentation-collapse" aria-expanded="false" aria-controls="presentation-collapse">
+            <?php echo __('Presentation'); ?>
+          </button>
+        </h2>
+        <div id="presentation-collapse" class="accordion-collapse collapse" aria-labelledby="presentation-heading">
+          <div class="accordion-body">
+            <?php echo render_field($globalForm->show_tooltips); ?>
+
+            <?php echo render_field($globalForm->draft_notification_enabled); ?>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="multirepo-heading">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#multirepo-collapse" aria-expanded="false" aria-controls="multirepo-collapse">
+            <?php echo __('Multi-repository'); ?>
+          </button>
+        </h2>
+        <div id="multirepo-collapse" class="accordion-collapse collapse" aria-labelledby="multirepo-heading">
+          <div class="accordion-body">
             <?php echo render_field($globalForm->multi_repository); ?>
 
-            <?php echo render_field($globalForm->audit_log_enabled); ?>
-
+            <?php echo render_field($globalForm->enable_institutional_scoping); ?>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="permalinks-heading">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#permalinks-collapse" aria-expanded="false" aria-controls="permalinks-collapse">
+            <?php echo __('Permalinks'); ?>
+          </button>
+        </h2>
+        <div id="permalinks-collapse" class="accordion-collapse collapse" aria-labelledby="permalinks-heading">
+          <div class="accordion-body">
             <?php echo render_field($globalForm->slug_basis_informationobject); ?>
 
             <?php echo render_field($globalForm->permissive_slug_creation); ?>
-
-            <?php echo render_field($globalForm->show_tooltips); ?>
-
-            <?php echo render_field($globalForm->defaultPubStatus); ?>
-
-            <?php echo render_field($globalForm->draft_notification_enabled); ?>
-
-            <?php echo render_field($globalForm->sword_deposit_dir); ?>
-
-            <?php echo render_field($globalForm->google_maps_api_key); ?>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="system-heading">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#system-collapse" aria-expanded="false" aria-controls="system-collapse">
+            <?php echo __('System'); ?>
+          </button>
+        </h2>
+        <div id="system-collapse" class="accordion-collapse collapse" aria-labelledby="system-heading">
+          <div class="accordion-body">
+            <?php echo render_field($globalForm->audit_log_enabled); ?>
 
             <?php echo render_field($globalForm->generate_reports_as_pub_user); ?>
 
-            <?php echo render_field($globalForm->enable_institutional_scoping); ?>
-
             <?php echo render_field($globalForm->cache_xml_on_save); ?>
+
+            <?php echo render_field($globalForm->defaultPubStatus); ?>
+          </div>
+        </div>
+      </div>
+      <div class="accordion-item">
+        <h2 class="accordion-header" id="integrations-heading">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#integrations-collapse" aria-expanded="false" aria-controls="integrations-collapse">
+            <?php echo __('Integrations'); ?>
+          </button>
+        </h2>
+        <div id="integrations-collapse" class="accordion-collapse collapse" aria-labelledby="integrations-heading">
+          <div class="accordion-body">
+            <?php echo render_field($globalForm->google_maps_api_key); ?>
+
+            <?php echo render_field($globalForm->sword_deposit_dir); ?>
           </div>
         </div>
       </div>
