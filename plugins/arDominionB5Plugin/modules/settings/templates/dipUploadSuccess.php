e933623b5fcecb7687c955068f9c6b85fe6d029a
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-09 19:59:13 +0200

    Tweak settings forms
    - Add global errors and hidden fields where missing.
    - Use form renderFormTag method.
    - Add OAI settings saved flash alert.
    - Consider CSRF field error as a form global error.
    - Style inputs in Storage Service settings.
    - Fix identifiers form field name.

--- plugins/arDominionB5Plugin/modules/settings/templates/dipUploadSuccess.php
+++ plugins/arDominionB5Plugin/modules/settings/templates/dipUploadSuccess.php
@@ -16,8 +16,8 @@
 
   <?php echo $form->renderGlobalErrors(); ?>
 
-  <form action="<?php echo url_for('settings/dipUpload'); ?>" method="post">
-
+  <?php echo $form->renderFormTag(url_for(['module' => 'settings', 'action' => 'dipUpload'])); ?>
+   
     <?php echo $form->renderHiddenFields(); ?>
 
     <div class="accordion">
