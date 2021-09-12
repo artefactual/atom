c250bc90b01b6f8827d3a81dcbc8871c1f96c520
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-12 14:48:29 +0200

    Tweak header icons

--- plugins/arDominionB5Plugin/modules/menu/templates/_changeLanguageMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_changeLanguageMenu.php
@@ -1,10 +1,10 @@
 <li class="nav-item dropdown d-flex flex-column">
   <a class="nav-link dropdown-toggle d-flex align-items-center p-0" href="#" id="language-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <i class="fas fa-2x fa-globe-europe px-0 px-lg-2 py-2" aria-hidden="true" data-tooltip="<?php echo __('Language'); ?>"></i>
-    <span class="d-lg-none ms-2" aria-hidden="true"><?php echo __('Language'); ?></span>
+    <i class="fas fa-2x fa-fw fa-globe-europe px-0 px-lg-2 py-2" aria-hidden="true" data-tooltip="<?php echo __('Language'); ?>"></i>
+    <span class="d-lg-none mx-1" aria-hidden="true"><?php echo __('Language'); ?></span>
     <span class="visually-hidden"><?php echo __('Language'); ?></span>  
   </a>
-  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="language-menu">
+  <ul class="dropdown-menu dropdown-menu-end mb-2" aria-labelledby="language-menu">
     <li>
       <h6 class="dropdown-header">
         <?php echo __('Language'); ?>
--- plugins/arDominionB5Plugin/modules/menu/templates/_clipboardMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_clipboardMenu.php
@@ -12,11 +12,11 @@
     data-export-alert-message="<?php echo __('The clipboard is empty for this entity type.'); ?>"
     data-export-check-url="<?php echo url_for(['module' => 'clipboard', 'action' => 'exportCheck']); ?>"
     data-delete-alert-message="<?php echo __('Note: clipboard items unclipped in this page will be removed from the clipboard when the page is refreshed. You can re-select them now, or reload the page to remove them completely. Using the sort or print preview buttons will also cause a page reload - so anything currently deselected will be lost!'); ?>">
-    <i class="fas fa-2x fa-paperclip px-0 px-lg-2 py-2" aria-hidden="true" data-tooltip="<?php echo $menu->getLabel(['cultureFallback' => true]); ?>"></i>
-    <span class="d-lg-none ms-2" aria-hidden="true"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
+    <i class="fas fa-2x fa-fw fa-paperclip px-0 px-lg-2 py-2" aria-hidden="true" data-tooltip="<?php echo $menu->getLabel(['cultureFallback' => true]); ?>"></i>
+    <span class="d-lg-none mx-1" aria-hidden="true"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
     <span class="visually-hidden"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
   </a>
-  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="<?php echo $menu->getName(); ?>-menu">
+  <ul class="dropdown-menu dropdown-menu-end mb-2" aria-labelledby="<?php echo $menu->getName(); ?>-menu">
     <li>
       <h6 class="dropdown-header">
         <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
