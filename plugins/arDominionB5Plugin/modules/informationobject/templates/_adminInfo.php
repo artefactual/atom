2aab9a281d458b489cb08bc37e93c8a02ca3b9e8
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-13 07:13:53 +0200

    Update IO edit form in all templates

--- plugins/arDominionB5Plugin/modules/informationobject/templates/_adminInfo.php
+++ plugins/arDominionB5Plugin/modules/informationobject/templates/_adminInfo.php
@@ -1,6 +1,12 @@
 <div class="accordion-item">
   <h2 class="accordion-header" id="admin-heading">
-    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#admin-collapse" aria-expanded="false" aria-controls="admin-collapse">
+    <button
+      class="accordion-button collapsed"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#admin-collapse"
+      aria-expanded="false"
+      aria-controls="admin-collapse">
       <?php echo __('Administration area'); ?>
     </button>
   </h2>
@@ -9,46 +15,59 @@
       <div class="row">
 
         <div class="col-md-6">
-
-          <div class="field">
-            <h3><?php echo __('Source language'); ?></h3>
-            <div>
+          <div class="mb-3">
+            <h3 class="fs-6 mb-2">
+              <?php echo __('Source language'); ?>
+            </h3>
+            <span class="text-muted">
               <?php if (isset($resource->sourceCulture)) { ?>
                 <?php if ($sf_user->getCulture() == $resource->sourceCulture) { ?>
                   <?php echo format_language($resource->sourceCulture); ?>
                 <?php } else { ?>
                   <div class="default-translation">
-                    <?php echo link_to(format_language($resource->sourceCulture), ['sf_culture' => $resource->sourceCulture] + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()); ?>
+                    <?php echo link_to(
+                        format_language($resource->sourceCulture),
+                        ['sf_culture' => $resource->sourceCulture]
+                            + $sf_data->getRaw('sf_request')->getParameterHolder()->getAll()
+                    ); ?>
                   </div>
                 <?php } ?>
               <?php } else { ?>
                 <?php echo format_language($sf_user->getCulture()); ?>
               <?php } ?>
-            </div>
+            </span>
           </div>
 
           <?php if (isset($resource->updatedAt)) { ?>
-            <div class="field">
-              <h3><?php echo __('Last updated'); ?></h3>
-              <div>
+            <div class="mb-3">
+              <h3 class="fs-6 mb-2">
+                <?php echo __('Last updated'); ?>
+              </h3>
+              <span class="text-muted">
                 <?php echo format_date($resource->updatedAt, 'f'); ?>
-              </div>
+              </span>
             </div>
           <?php } ?>
 
-          <div class="field">
-            <h3><?php echo __('Source name'); ?></h3>
-            <div>
-              <?php foreach ($resource->fetchAllKeymapEntries() as $keymap) { ?>
-                <p><?php echo $keymap->sourceName; ?></p>
-              <?php } ?>
+          <?php if (0 < count($keymapEntries = $resource->fetchAllKeymapEntries())) { ?>
+            <div class="mb-3">
+              <h3 class="fs-6 mb-2">
+                <?php echo __('Source name'); ?>
+              </h3>
+              <span class="text-muted">
+                <?php foreach ($keymapEntries as $keymap) { ?>
+                  <p><?php echo $keymap->sourceName; ?></p>
+                <?php } ?>
+              </span>
             </div>
-          </div>
+          <?php } ?>
         </div>
 
         <div class="col-md-6">
-          <?php echo $form->displayStandard->label(__('Display standard'))->renderRow(); ?>
-          <?php echo $form->displayStandardUpdateDescendants->label(__('Make this selection the new default for existing children'))->renderRow(); ?>
+          <?php echo render_field($form->displayStandard->label(__('Display standard'))); ?>
+          <?php echo render_field($form->displayStandardUpdateDescendants->label(__(
+              'Make this selection the new default for existing children'
+          ))); ?>
         </div>
 
       </div>
