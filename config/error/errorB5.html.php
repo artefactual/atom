08f45d00570af16d09d98624037a2711c1af4ec6
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-14 19:45:01 +0200

    Update error page

--- config/error/errorB5.html.php
+++ config/error/errorB5.html.php
@@ -8,8 +8,23 @@
   </head>
   <body class="admin error">
     <div id="wrapper" class="container text-center">
-      <div id="content" class="d-inline-block mt-5 p-5">
-        <h2>Oops! An Error Occurred</h2>
+      <div id="content" class="d-inline-block mt-5 p-5 text-start">
+        <?php $icons = [
+            'bug',
+            'dumpster-fire',
+            'meh-rolling-eyes',
+            'bomb',
+            'exclamation-circle',
+            'skull-crossbones',
+            'book-dead',
+            'heart-broken',
+            'dizzy',
+            'robot'
+        ]; ?>
+        <h2 class="mb-4">
+          <i class="fas fa-lg fa-<?php echo $icons[array_rand($icons)]; ?> me-2" aria-hidden="true"></i>
+          Oops! An Error Occurred
+        </h2>
 
         <p>
           Sorry, something went wrong.<br>
@@ -18,7 +33,7 @@
 
         <p class="mb-0">
           Try again a little later or ask in the
-          <a href="http://groups.google.ca/group/ica-atom-users">
+          <a href="https://groups.google.com/g/ica-atom-users">
             discussion group.
           </a><br>
           <a href="javascript:history.go(-1)">Back to previous page.</a>
