e61ff3156ef110888d4541c21bc20bcdf34bf3b0
Steve Breker <sbreker@artefactual.com>
2021-09-15 21:14:29 +0000

    Updated layout of rename template
    Updated the form layout so all fields are left justified and stacked.
    <hr /> tags used to provide visual separation of the three sections.

--- plugins/arDominionB5Plugin/modules/informationobject/templates/renameSuccess.php
+++ plugins/arDominionB5Plugin/modules/informationobject/templates/renameSuccess.php
@@ -32,62 +32,44 @@
             <p><?php echo __('Use this interface to update the description title, slug (permalink), and/or %1% filename.', ['%1%' => mb_strtolower(sfConfig::get('app_ui_label_digitalobject'))]); ?></p>
             <hr />
 
-            <div class="row mb-3">
-              <div class="col-md-3">
-                <div class="rename-form-field-toggle form-check">
-                  <input class="form-check-input" type="checkbox" id="rename_enable_title" checked>
-                  <label class="form-check-label" for="rename_enable_title">
-                    <?php echo __('Update title'); ?>
-                  </label>
-                </div>
-              </div>
-              <div class="col-md-8 offset-md-1">
-                <?php echo render_field($form->title
-                    ->label(__('Title'))
-                    ->help(__('Editing the description title will automatically update the slug field if the "Update slug" checkbox is selected - you can still edit it after.')), $resource); ?>
-              </div>
-              <p><?php echo __('Original title'); ?>: <em><?php echo $resource->title; ?></em></p>
+            <div class="rename-form-field-toggle form-check mb-4">
+              <input class="form-check-input" type="checkbox" id="rename_enable_title" checked>
+              <label class="form-check-label" for="rename_enable_title">
+                <?php echo __('Update title'); ?>
+              </label>
             </div>
+            <?php echo render_field($form->title
+                ->label(__('Title'))
+                ->help(__('Editing the description title will automatically update the slug field if the "Update slug" checkbox is selected - you can still edit it after.')), $resource); ?>
+            <p><?php echo __('Original title'); ?>: <em><?php echo $resource->title; ?></em></p>
             <hr />
 
             <div id="rename-slug-warning" class="alert alert-danger" role="alert" style="display: none;">
               <?php echo __('A slug based on this title already exists so a number has been added to pad the slug.'); ?>
             </div>
-            <div class="row mb-3">
-              <div class="col-md-3">
-                <div class="rename-form-field-toggle form-check">
-                  <input class="form-check-input" type="checkbox" id="rename_enable_slug" checked>
-                  <label class="form-check-label" for="rename_enable_slug">
-                    <?php echo __('Update slug'); ?>
-                  </label>
-                </div>
-              </div>
-              <div class="col-md-8 offset-md-1">
-                <?php echo render_field($form->slug
-                    ->label(__('Slug'))
-                    ->help(__('Do not use any special characters or spaces in the slug - only lower case alphanumeric characters (a-z, 0-9) and dashes (-) will be saved. Other characters will be stripped out or replaced. Editing the slug will not automatically update the other fields.')), $resource); ?>
-              </div>
-              <p><?php echo __('Original slug'); ?>: <em><?php echo $resource->slug; ?></em></p>
+            <div class="rename-form-field-toggle form-check mb-4">
+              <input class="form-check-input" type="checkbox" id="rename_enable_slug" checked>
+              <label class="form-check-label" for="rename_enable_slug">
+                <?php echo __('Update slug'); ?>
+              </label>
             </div>
+            <?php echo render_field($form->slug
+                ->label(__('Slug'))
+                ->help(__('Do not use any special characters or spaces in the slug - only lower case alphanumeric characters (a-z, 0-9) and dashes (-) will be saved. Other characters will be stripped out or replaced. Editing the slug will not automatically update the other fields.')), $resource); ?>
+            <p><?php echo __('Original slug'); ?>: <em><?php echo $resource->slug; ?></em></p>
 
             <?php if (count($resource->digitalObjectsRelatedByobjectId) > 0) { ?>
               <hr />
-              <div class="row mb-3">
-                <div class="col-md-3">
-                  <div class="rename-form-field-toggle form-check">
-                    <input class="form-check-input" type="checkbox" id="rename_enable_filename" checked>
-                    <label class="form-check-label" for="rename_enable_filename">
-                      <?php echo __('Update filename'); ?>
-                    </label>
-                  </div>
-                </div>
-                <div class="col-md-8 offset-md-1">
-                  <?php echo render_field($form->filename
-                      ->label(__('Filename'))
-                      ->help(__('Do not use any special characters or spaces in the filename - only lower case alphanumeric characters (a-z, 0-9) and dashes (-) will be saved. Other characters will be stripped out or replaced. Editing the filename will not automatically update the other fields.')), $resource); ?>
-                </div>
-                <p><?php echo __('Original filename'); ?>: <em><?php echo $resource->digitalObjectsRelatedByobjectId[0]->name; ?></em></p>
+              <div class="rename-form-field-toggle form-check mb-4">
+                <input class="form-check-input" type="checkbox" id="rename_enable_filename" checked>
+                <label class="form-check-label" for="rename_enable_filename">
+                  <?php echo __('Update filename'); ?>
+                </label>
               </div>
+              <?php echo render_field($form->filename
+                  ->label(__('Filename'))
+                  ->help(__('Do not use any special characters or spaces in the filename - only lower case alphanumeric characters (a-z, 0-9) and dashes (-) will be saved. Other characters will be stripped out or replaced. Editing the filename will not automatically update the other fields.')), $resource); ?>
+              <p><?php echo __('Original filename'); ?>: <em><?php echo $resource->digitalObjectsRelatedByobjectId[0]->name; ?></em></p>
             <?php } ?>
           </div>
         </div>
