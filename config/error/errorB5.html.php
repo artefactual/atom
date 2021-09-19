a5dfb49bdcdbe99e1d2e9f7eae3850b00c10057e
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-20 01:11:37 +0200

    Normalize error and permission pages

--- config/error/errorB5.html.php
+++ config/error/errorB5.html.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="/plugins/arDominionB5Plugin/build/css/min.css">
   </head>
   <body class="admin error">
-    <div id="wrapper" class="container text-center">
-      <div id="content" class="d-inline-block mt-5 p-5 text-start">
+    <div id="wrapper" class="container-xxl pt-3 text-center" role="main">
+      <div id="content" class="d-inline-block mt-5 text-start" role="alert">
         <?php $icons = [
             'bug',
             'dumpster-fire',
@@ -21,23 +21,25 @@
             'dizzy',
             'robot',
         ]; ?>
-        <h2 class="mb-4">
-          <i class="fas fa-lg fa-<?php echo $icons[array_rand($icons)]; ?> me-2" aria-hidden="true"></i>
+        <h1 class="h2 mb-0 p-3 border-bottom d-flex align-items-center">
+          <i class="fas fa-fw fa-lg fa-<?php echo $icons[array_rand($icons)]; ?> me-3" aria-hidden="true"></i>
           Oops! An Error Occurred
-        </h2>
+        </h1>
 
-        <p>
-          Sorry, something went wrong.<br>
-          The server returned a <code class="ms-1"><?php echo $code; ?> <?php echo $text; ?></code>.
-        </p>
+        <div class="p-3">
+          <p>
+            Sorry, something went wrong.<br>
+            The server returned a <code class="ms-1"><?php echo $code; ?> <?php echo $text; ?></code>.
+          </p>
 
-        <p class="mb-0">
-          Try again a little later or ask in the
-          <a href="https://groups.google.com/g/ica-atom-users">
-            discussion group.
-          </a><br>
-          <a href="javascript:history.go(-1)">Back to previous page.</a>
-        </p>
+          <p class="mb-0">
+            Try again a little later or ask in the
+            <a href="https://groups.google.com/g/ica-atom-users">
+              discussion group.
+            </a><br>
+            <a href="javascript:history.go(-1)">Back to previous page.</a>
+          </p>
+        </div>
       </div>
     </div>
   </body>
