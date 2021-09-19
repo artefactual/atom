f0b5b48ed4a2a3671a642bbba851de477f995a0b
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-20 01:11:37 +0200

    Normalize error and permission pages

--- plugins/arDominionB5Plugin/modules/admin/templates/error404Success.php
+++ plugins/arDominionB5Plugin/modules/admin/templates/error404Success.php
@@ -1,20 +1,22 @@
-<section class="w-50 mx-auto rounded border border-dark mt-3 border-1 bg-white" role="alert">
+<div class="text-center">
+  <div id="content" class="d-inline-block mt-5 text-start" role="alert">
+    <h1 class="h2 mb-0 p-3 border-bottom d-flex align-items-center">
+      <i class="fas fa-fw fa-lg fa-times me-3" aria-hidden="true"></i>
+      <?php echo __('Sorry, page not found'); ?>
+    </h1>
 
-  <h1 class="border-bottom px-3 py-2">
-    <i class="fa fa-times me-2 text-danger" aria-hidden="true"></i>
-    <?php echo __('Sorry, page not found'); ?>
-  </h1>
+    <div class="p-3">
+      <p>
+        <?php echo __('Did you type the URL correctly?'); ?><br>
+        <?php echo __('Did you follow a broken link?'); ?>
+      </p>
 
-  <p class="px-3">
-    <?php echo __('Did you type the URL correctly?'); ?><br />
-    <?php echo __('Did you follow a broken link?'); ?>
-  </p>
-
-  <div class="px-3">
-    <p>
-      <a href="javascript:history.go(-1)"><?php echo __('Back to previous page'); ?></a><br />
-      <?php echo link_to(__('Go to homepage'), '@homepage'); ?>
-    </p>
+      <p class="mb-0">
+        <a href="javascript:history.go(-1)">
+          <?php echo __('Back to previous page.'); ?>
+        </a><br>
+        <?php echo link_to(__('Go to homepage.'), '@homepage'); ?>
+      </p>
+    </div>
   </div>
-
-</section>
+</div>
