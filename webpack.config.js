91b15d93b356a508063c2e13c398c82cd1daeebb
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-20 12:06:06 +0200

    Move styles build to Webpack (WIP)
    - Add sass, css, style and resolve-url loaders, which allows to move all
      styles to the build process. With the current setup the styles are
      added to the JS bundle, which is loaded with `defer` at the moment
      causing a slow page load (development mode makes this worst).
    - Add build and watch scripts to package.json.
    - Add webpack.entry.js to load main JS and SCSS files.
    - Remove relative path from some SCSS imports (Webpack looks for
      them in the node_modules folder automatically).

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./plugins/arDominionB5Plugin/js/main.js",
+  entry: "./plugins/arDominionB5Plugin/webpack.entry.js",
   output: {
     path: __dirname + "/plugins/arDominionB5Plugin/build",
     filename: "js/bundle.[contenthash].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./plugins/arDominionB5Plugin/templates/_layout_start_webpack.php",
+      template:
+        "./plugins/arDominionB5Plugin/templates/_layout_start_webpack.php",
       filename: "../templates/_layout_start.php",
       publicPath: "/plugins/arDominionB5Plugin/build",
       inject: false,
-    })
-  ]
+    }),
+  ],
 };
