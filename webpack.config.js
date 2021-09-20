0cd041f0abf5f34194841dc6c1b52a3af54feedb
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-20 06:54:17 +0200

    Add basic bundle of custom JS
    - Install Webpack and Webpack CLI.
    - Add config to bundle all custom JS in development mode.
    - Update NPM on CI env.

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "development",
+  entry: "./plugins/arDominionB5Plugin/js/main.js",
+  output: {
+    path: __dirname + "/plugins/arDominionB5Plugin/build/js",
+    filename: "bundle.js",
+  },
+};
