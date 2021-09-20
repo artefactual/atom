fd7d428f15269c7dc9026020b35647db933a2080
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-20 08:17:50 +0200

    Use HtmlWebpackPlugin and hash bundle file
    - Add content hash to bundle file for caches.
    - Use HtmlWebpackPlugin to update the layout start template with the
      current bundle file on build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); 
+
 module.exports = {
   mode: "development",
   entry: "./plugins/arDominionB5Plugin/js/main.js",
   output: {
-    path: __dirname + "/plugins/arDominionB5Plugin/build/js",
-    filename: "bundle.js",
+    path: __dirname + "/plugins/arDominionB5Plugin/build",
+    filename: "js/bundle.[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./plugins/arDominionB5Plugin/templates/_layout_start_webpack.php",
+      filename: "../templates/_layout_start.php",
+      publicPath: "/plugins/arDominionB5Plugin/build",
+      inject: false,
+    })
+  ]
 };
