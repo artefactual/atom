83c431f2e9a426ec6f5988c3820cf82ff391a13b
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-22 09:40:50 +0200

    Add unavailableB5.php

---
+++ config/unavailableB5.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AtoM</title>
+    <link rel="stylesheet" href="/plugins/arDominionB5Plugin/build/css/min.css">
+  </head>
+  <body class="admin unavailable">
+    <div id="wrapper" class="container-xxl pt-3 text-center" role="main">
+      <div id="content" class="d-inline-block mt-5 text-start" role="alert">
+        <h1 class="h2 mb-0 p-3 border-bottom d-flex align-items-center">
+          <i class="fas fa-fw fa-lg fa-tools me-3" aria-hidden="true"></i>
+          Website Temporarily Unavailable
+        </h1>
+
+        <div class="p-3">
+          <p>
+            Please try again in a few seconds...
+          </p>
+
+          <p class="mb-0">
+            <a href="javascript:window.location.reload()">Try again: Reload Page.</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
