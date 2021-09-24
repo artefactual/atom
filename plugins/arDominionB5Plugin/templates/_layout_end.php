5cbd6a7cd3fa3ba67577cdac9e38699fca2d0536
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-24 15:57:25 +0200

    Include YUI vendor files in Webpack build

--- plugins/arDominionB5Plugin/templates/_layout_end.php
+++ plugins/arDominionB5Plugin/templates/_layout_end.php
@@ -1,8 +1,4 @@
     <?php include_slot('post'); ?>
     <?php echo get_partial('footer'); ?>
-    <script src="/vendor/yui/yahoo-dom-event/yahoo-dom-event.js"></script>
-    <script src="/vendor/yui/connection/connection-min.js"></script>
-    <script src="/vendor/yui/datasource/datasource-min.js"></script>
-    <script src="/vendor/yui/autocomplete/autocomplete-min.js"></script>
   </body>
 </html>
