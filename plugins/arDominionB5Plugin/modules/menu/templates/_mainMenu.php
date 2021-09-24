62ef95c006c03856ab71b75d4d2ed5babccf7411
José Raddaoui Marín <raddaouimarin@gmail.com>
2021-09-24 13:00:31 +0200

    Normalize tooltips
    - Use expected elements and attributes.
    - Use ES6 import and avoid jQuery in tooltip.js.

--- plugins/arDominionB5Plugin/modules/menu/templates/_mainMenu.php
+++ plugins/arDominionB5Plugin/modules/menu/templates/_mainMenu.php
@@ -1,10 +1,31 @@
 <?php foreach ([$addMenu, $manageMenu, $importMenu, $adminMenu] as $menu) { ?>
-  <?php if ($menu && ('add' == $menu->getName() || 'manage' == $menu->getName()) || $sf_user->isAdministrator()) { ?>
+  <?php if (
+      $menu && ('add' == $menu->getName()
+      || 'manage' == $menu->getName())
+      || $sf_user->isAdministrator()
+  ) { ?>
     <li class="nav-item dropdown d-flex flex-column">
-      <a class="nav-link dropdown-toggle d-flex align-items-center p-0" href="#" id="<?php echo $menu->getName(); ?>-menu" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        <i class="fas fa-2x fa-fw fa-<?php echo $icons[$menu->getName()]; ?> px-0 px-lg-2 py-2" aria-hidden="true" data-tooltip="<?php echo $menu->getLabel(['cultureFallback' => true]); ?>"></i>
-        <span class="d-lg-none mx-1" aria-hidden="true"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
-        <span class="visually-hidden"><?php echo $menu->getLabel(['cultureFallback' => true]); ?></span>
+      <a
+        class="nav-link dropdown-toggle d-flex align-items-center p-0"
+        href="#"
+        id="<?php echo $menu->getName(); ?>-menu"
+        role="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false">
+        <i
+          class="fas fa-2x fa-fw fa-<?php echo $icons[$menu->getName()]; ?> px-0 px-lg-2 py-2"
+          data-bs-toggle="tooltip"
+          data-bs-placement="bottom"
+          data-bs-custom-class="d-none d-lg-block"
+          title="<?php echo $menu->getLabel(['cultureFallback' => true]); ?>"
+          aria-hidden="true">
+        </i>
+        <span class="d-lg-none mx-1" aria-hidden="true">
+          <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
+        </span>
+        <span class="visually-hidden">
+          <?php echo $menu->getLabel(['cultureFallback' => true]); ?>
+        </span>
       </a>
       <ul class="dropdown-menu dropdown-menu-end mb-2" aria-labelledby="<?php echo $menu->getName(); ?>-menu">
         <li>
