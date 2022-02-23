0800206d49d831a8a014b1683ffffe6b79d1657a
José Raddaoui Marín <raddaouimarin@gmail.com>
2022-02-23 17:32:54 +0100

    Run prettier

--- plugins/arDominionB5Plugin/js/settingsFindingAid.js
+++ plugins/arDominionB5Plugin/js/settingsFindingAid.js
@@ -1,28 +1,33 @@
-jQuery(
-    function ($) {
-        let $faControls = $('#finding-aid-collapse > .accordion-body > div').slice(1, 4);
+(($) => {
+  "use strict";
 
-        function isEnabled() {
-            return $('#finding_aid_finding_aids_enabled_1').is(':checked');
-        }
+  $(() => {
+    let $faControls = $("#finding-aid-collapse > .accordion-body > div").slice(
+      1,
+      4
+    );
 
-        function initialize() {
-            if (!isEnabled()) {
-                $faControls.hide();
-            }
-        }
-
-        function toggle() {
-            if (isEnabled()) {
-                $faControls.show();
-            } else {
-                $faControls.hide();
-            }
-        }
-
-        // Main
-        initialize();
-        $('#finding_aid_finding_aids_enabled_0').on('change', toggle);
-        $('#finding_aid_finding_aids_enabled_1').on('change', toggle);
+    function isEnabled() {
+      return $("#finding_aid_finding_aids_enabled_1").is(":checked");
     }
-);
+
+    function initialize() {
+      if (!isEnabled()) {
+        $faControls.hide();
+      }
+    }
+
+    function toggle() {
+      if (isEnabled()) {
+        $faControls.show();
+      } else {
+        $faControls.hide();
+      }
+    }
+
+    // Main
+    initialize();
+    $("#finding_aid_finding_aids_enabled_0").on("change", toggle);
+    $("#finding_aid_finding_aids_enabled_1").on("change", toggle);
+  });
+})(jQuery);
