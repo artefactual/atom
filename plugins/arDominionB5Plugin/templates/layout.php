c6e6625c2bbca48bbfa1871603544bd7299b87c6
José Raddaoui Marín <raddaouimarin@gmail.com>
2022-08-11 10:21:04 +0200

    Add skip to main content link, refs #13530
    First element inside the body and only visible on focus (first tab).

--- plugins/arDominionB5Plugin/templates/layout.php
+++ plugins/arDominionB5Plugin/templates/layout.php
@@ -1,8 +1,10 @@
 <?php echo get_partial('layout_start'); ?>
 
-<div id="wrapper" class="container-xxl pt-3 flex-grow-1" role="main">
+<div id="wrapper" class="container-xxl pt-3 flex-grow-1">
   <?php echo get_partial('alerts'); ?>
-  <?php echo $sf_content; ?>
+  <div id="main-column" role="main">
+    <?php echo $sf_content; ?>
+  </div>
 </div>
 
 <?php echo get_partial('layout_end'); ?>
