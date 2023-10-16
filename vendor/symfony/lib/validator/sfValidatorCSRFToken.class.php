873a1c3b0395f02ebdb4a60be618adaea836ca1f
mlnkng <melaniekung@live.com>
2023-10-16 08:00:45 -0700

    Provide informative csrf attack message. (#1678)

--- vendor/symfony/lib/validator/sfValidatorCSRFToken.class.php
+++ vendor/symfony/lib/validator/sfValidatorCSRFToken.class.php
@@ -27,7 +27,7 @@ class sfValidatorCSRFToken extends sfValidatorBase
 
     $this->setOption('required', true);
 
-    $this->addMessage('csrf_attack', 'CSRF attack detected.');
+    $this->addMessage('csrf_attack', 'CSRF Token Expired. Please refresh the page and log in again.');
   }
 
   /**
