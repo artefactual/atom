23be9459c6204ce0388377ae34db433d2191cfe7
mlnkng <melaniekung@live.com>
2024-06-10 09:59:20 -0700

    Update BoolQuery and NestedQuery.

--- plugins/arSolrPlugin/lib/query/arSolrNestedQuery.class.php
+++ plugins/arSolrPlugin/lib/query/arSolrNestedQuery.class.php
@@ -19,5 +19,35 @@
 
 class arSolrNestedQuery extends arSolrAbstractQuery
 {
-  // TODO
+  /**
+   * sort.
+   *
+   * @var string
+   */
+  protected $sort = '';
+
+  /**
+   * sort.
+   *
+   * @var string
+   */
+  protected $filter = '';
+
+  /**
+   * Set sort field and direction (asc/desc).
+   *
+   * @param $field field to sort by
+   * @param $direction direction to sort by, must be 'asc' or 'desc'
+   */
+  public function setSort($field, $direction = 'desc') {
+    $this->sort = [
+        "sort" => ($field . " " . $direction)
+    ];
+  }
+
+  public function addFilter($field, $query) {
+    $this->filter = [
+        "filter" => ['"'.$field.':'.$query.'"']
+    ];
+  }
 }
