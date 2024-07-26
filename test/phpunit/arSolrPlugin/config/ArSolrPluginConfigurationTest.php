dd7a91fe39c6a374f8bd9474764b94ba4ae4f3af
Steve Breker <sbreker@artefactual.com>
2024-07-25 17:45:25 -0700

    Add unit test file for arSolrPluginConfiguration

---
+++ test/phpunit/arSolrPlugin/config/ArSolrPluginConfigurationTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once 'plugins/arSolrPlugin/config/arSolrPluginConfiguration.class.php';
+
+/**
+ * @internal
+ *
+ * @covers \arSolrPluginConfiguration
+ * @covers \BaseSetting
+ * @covers \BaseSettingI18n
+ * @covers \QubitApcUniversalClassLoader
+ * @covers \QubitQuery
+ */
+class ArSolrPluginConfigurationTest extends TestCase
+{
+    protected $pluginConfiguration;
+
+    public function solrPluginConfigurationProvider(): array
+    {
+        $this->sfProjectConfigurationObj = new sfProjectConfiguration();
+        $this->qubitConfigrationObj = new qubitConfiguration('test', false);
+
+        return [
+            'Supply sfProjectConfiguration only' => [$this->sfProjectConfigurationObj, null, null],
+            'Supply qubitConfiguration only' => [$this->qubitConfigrationObj, null, null],
+            'Supply sfProjectConfiguration with params' => [$this->sfProjectConfigurationObj, 'plugins/arSolrPlugin', 'arSolrPlugin'],
+            'Supply qubitConfiguration with params' => [$this->qubitConfigrationObj, 'plugins/arSolrPlugin', 'arSolrPlugin'],
+        ];
+    }
+
+    /**
+     * @dataProvider solrPluginConfigurationProvider
+     *
+     * @param mixed $obj
+     * @param mixed $rootDir
+     * @param mixed $name
+     */
+    public function testInitializeWithProjectConfiguration($obj, $rootDir, $name)
+    {
+        $this->pluginConfiguration = new arSolrPluginConfiguration($obj, $rootDir, $name);
+        $this->pluginConfiguration->initialize();
+
+        $this->assertTrue($this->pluginConfiguration instanceof arSolrPluginConfiguration, 'Assert plugin object is arSolrPluginConfiguration');
+
+        $this->assertTrue(is_array(arSolrPluginConfiguration::$config), 'Assert plugin config result is array');
+    }
+}
