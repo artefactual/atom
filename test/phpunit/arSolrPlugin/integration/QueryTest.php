8fcc321d60bb255b880a3faae5f963133c1ab001
Steve Breker <sbreker@artefactual.com>
2024-08-15 09:43:46 -0700

    Add Solr integration test capability

---
+++ test/phpunit/arSolrPlugin/integration/QueryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ * @group solr_integration
+ *
+ * @covers \arSolrExistsQuery
+ */
+class QueryTest extends TestCase
+{
+    public function setUp(): void {}
+
+    public function testQuery() {
+        $this->assertTrue(true, "true is not true!");
+    }
+}
