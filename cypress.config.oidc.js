86b97288859d4ea847a2c14bce930934921c8e66
Steve Breker <sbreker@artefactual.com>
2024-10-23 15:30:58 -0700

    Set Cypress cfg chromeWebSecurity to false

--- cypress.config.oidc.js
+++ cypress.config.oidc.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
     retries: {'runMode': 2},
     baseUrl: 'http://localhost:63001',
     specPattern: 'cypress/e2e/oidc/**/*.cy.js',
-    supportFile: 'cypress/support/e2e.js'
+    supportFile: 'cypress/support/e2e.js',
+    chromeWebSecurity: false,
   },
 })
