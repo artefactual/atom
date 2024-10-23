cf104979df4ffb28fdc9cabee049dbaa1fe4f600
Steve Breker <sbreker@artefactual.com>
2024-10-23 15:45:16 -0700

    Update test
    - remove check for url contents
    - try email field

--- cypress/e2e/oidc/login.cy.js
+++ cypress/e2e/oidc/login.cy.js
@@ -3,9 +3,9 @@ describe('SSO Login with Keycloak', () => {
     cy.visit('/')
     cy.contains('Log in').should('be.visible').click()
     cy.contains('Log in with SSO').should('be.visible').click()
-    cy.url({ timeout: 1000 }).should('include', 'oidc/login');
-    cy.url({ timeout: 1000 }).should('include', 'realms/demo/protocol/openid-connect/auth')
-    // cy.get('#username').type(Cypress.env('adminEmail'))
+    //cy.url({ timeout: 1000 }).should('include', 'oidc/login');
+    //cy.url({ timeout: 1000 }).should('include', 'realms/demo/protocol/openid-connect/auth')
+    cy.get('#username').type(Cypress.env('adminEmail'))
     // cy.get('#password').type(Cypress.env('adminPassword'))
 
     // cy.get('#kc-login').click();
