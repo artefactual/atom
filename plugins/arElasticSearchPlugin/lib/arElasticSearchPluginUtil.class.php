0259ed052eb7a2a4401363aae70ce0739daddf85
Anvit Srivastav <asrivastav@artefactual.com>
2024-11-14 11:19:34 -0800

    Update mapping condition for _all fields
    Update condition in arElasticSearchPluginUtil to only get string
    fields included in _all if copy_to is explicitly set to _all in the
    mappping.

--- plugins/arElasticSearchPlugin/lib/arElasticSearchPluginUtil.class.php
+++ plugins/arElasticSearchPlugin/lib/arElasticSearchPluginUtil.class.php
@@ -482,8 +482,8 @@ class arElasticSearchPluginUtil
                 // Get string fields included in _all
                 elseif (
                     (
-                        !isset($propertyProperties['copy_to'])
-                        || ('_all' == $propertyProperties['copy_to'])
+                        isset($propertyProperties['copy_to'])
+                        && ('_all' == $propertyProperties['copy_to'])
                     ) && (
                         isset($propertyProperties['type'])
                         && 'text' == $propertyProperties['type']
