23e9bebc2e6a135cf6081536dab7c1b322548eb8
Matt Innes <matthew.f.innes@gmail.com>
2024-12-02 19:33:27 -0800

    Expand related materials titles
    Adds level of description, refcode, and draft status to
    related materials titles.

--- plugins/arDominionB5Plugin/modules/informationobject/templates/_relatedMaterialDescriptions.php
+++ plugins/arDominionB5Plugin/modules/informationobject/templates/_relatedMaterialDescriptions.php
@@ -10,15 +10,48 @@
     <ul class="<?php echo render_b5_show_list_css_classes(); ?>">
       <?php foreach ($resource->relationsRelatedBysubjectId as $item) { ?>
         <?php if ($sf_user->isAuthenticated() || QubitTerm::PUBLICATION_STATUS_PUBLISHED_ID == $item->object->getPublicationStatus()->statusId) { ?>
+          <?php $itemTitle = $item->object->__toString(); ?>
+
+          <?php if (isset($item->object->levelOfDescription) || isset($item->object->identifier)) { ?>
+            <?php $itemTitle = "- {$itemTitle}"; ?>
+              <?php if (isset($item->object->identifier)) { ?>
+                <?php $itemTitle = "{$item->object->referenceCode} {$itemTitle}"; ?>
+              <?php } ?>
+              <?php if (isset($item->object->levelOfDescription)) { ?>
+                <?php $itemTitle = "{$item->object->levelOfDescription} {$itemTitle}"; ?>
+              <?php } ?>
+          <?php } ?>
+
+          <?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $item->object->getPublicationStatus()->statusId) { ?>
+            <?php $itemTitle .= " ({$item->object->getPublicationStatus()})"; ?>
+          <?php } ?>
+
           <?php if (isset($item->type) && QubitTerm::RELATED_MATERIAL_DESCRIPTIONS_ID == $item->type->id) { ?>
-            <li><?php echo link_to(render_title($item->object), [$item->object, 'module' => 'informationobject']); ?></li>
+            <li><?php echo link_to($itemTitle, [$item->object, 'module' => 'informationobject']); ?></li>
           <?php } ?>
         <?php } ?>
       <?php } ?>
+
       <?php foreach ($resource->relationsRelatedByobjectId as $item) { ?>
         <?php if ($sf_user->isAuthenticated() || QubitTerm::PUBLICATION_STATUS_PUBLISHED_ID == $item->subject->getPublicationStatus()->statusId) { ?>
+          <?php $itemTitle = $item->subject->__toString(); ?>
+
+          <?php if (isset($item->subject->levelOfDescription) || isset($item->subject->identifier)) { ?>
+            <?php $itemTitle = "- {$itemTitle}"; ?>
+              <?php if (isset($item->subject->identifier)) { ?>
+                <?php $itemTitle = "{$item->subject->referenceCode} {$itemTitle}"; ?>
+              <?php } ?>
+              <?php if (isset($item->subject->levelOfDescription)) { ?>
+                <?php $itemTitle = "{$item->subject->levelOfDescription} {$itemTitle}"; ?>
+              <?php } ?>
+          <?php } ?>
+
+          <?php if (QubitTerm::PUBLICATION_STATUS_DRAFT_ID == $item->subject->getPublicationStatus()->statusId) { ?>
+            <?php $itemTitle .= " ({$item->subject->getPublicationStatus()})"; ?>
+          <?php } ?>
+
           <?php if (isset($item->type) && QubitTerm::RELATED_MATERIAL_DESCRIPTIONS_ID == $item->type->id) { ?>
-            <li><?php echo link_to(render_title($item->subject), [$item->subject, 'module' => 'informationobject']); ?></li>
+            <li><?php echo link_to($itemTitle, [$item->subject, 'module' => 'informationobject']); ?></li>
           <?php } ?>
         <?php } ?>
       <?php } ?>
