e69c55c93829c920f027323611994070200af075
melaniekung <mkung@artefactual.com>
2024-12-16 13:26:10 -0800

    Revert changes to project config
    - Revert changes to project config pointing to invalid phing path

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -56,9 +56,6 @@ class ProjectConfiguration extends sfProjectConfiguration
 
         $this->enablePlugins($plugins);
 
-        sfConfig::set('sf_phing_path', sfConfig::get('sf_root_dir').DIRECTORY_SEPARATOR.'vendor/phing/phing');
-        sfConfig::set('sf_propel_path', sfConfig::get('sf_root_dir').DIRECTORY_SEPARATOR.'symfony/lib/plugins/sfPropelPlugin/lib/vendor/propel/');
-
         $this->dispatcher->connect(
             'debug.web.load_panels',
             ['arWebDebugPanel', 'listenToLoadDebugWebPanelEvent']
