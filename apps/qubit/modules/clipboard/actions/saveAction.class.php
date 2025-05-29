86308742bbeda79093fc81b6b83b3b7937b36600
Matt Innes <matthew.f.innes@gmail.com>
2025-05-28 18:50:51 -0700

    Simplify grammatical number text

--- apps/qubit/modules/clipboard/actions/saveAction.class.php
+++ apps/qubit/modules/clipboard/actions/saveAction.class.php
@@ -49,20 +49,9 @@ class ClipboardSaveAction extends sfAction
 
             $loadUrl = $this->context->routing->generate(null, ['module' => 'clipboard', 'action' => 'load']);
 
-            if (1 == $itemsCount) {
-                $itemsUnit = $this->context->i18n->__('item');
-            } else {
-                $itemsUnit = $this->context->i18n->__('items');
-            }
-
             $retention = sfconfig::get('app_clipboard_save_max_age');
-            if (1 == $retention) {
-                $retentionUnit = $this->context->i18n->__('day');
-            } else {
-                $retentionUnit = $this->context->i18n->__('days');
-            }
 
-            $message = $this->context->i18n->__('Clipboard saved with %1% %2%. Clipboard ID is <b>%3%</b>. Please write this number down. When you want to reload this clipboard in the future, open the Clipboard menu, select <a class="alert-link" href="%4%">Load clipboard</a>, and enter this number in the Clipboard ID field. Clipboards are eligible for deletion after <b>%5% %6%</b>.', ['%1%' => $itemsCount, '%2%' => $itemsUnit, '%3%' => $password, '%4%' => $loadUrl, '%5%' => $retention, '%6%' => $retentionUnit]);
+            $message = $this->context->i18n->__('Clipboard saved with %1% item(s). Clipboard ID is <b>%2%</b>. Please write this number down. When you want to reload this clipboard in the future, open the Clipboard menu, select <a class="alert-link" href="%3%">Load clipboard</a>, and enter this number in the Clipboard ID field. Clipboards are eligible for deletion after <b>%4% day(s)</b>.', ['%1%' => $itemsCount, '%2%' => $password, '%3%' => $loadUrl, '%4%' => $retention]);
             $this->response->setStatusCode(200);
             $responseData = ['success' => $message];
         }
