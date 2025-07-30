d87e126bfd8cf1e1252bd08c02ded9af3ac9c761
Johan Pieterse <pieterse.johan3@gmail.com>
2025-07-30 13:59:44 +0200

    Add files via upload
    Shopping cart

---
+++ apps/qubit/modules/cart/actions/removeCartAction.class.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * This file is part of Qubit Toolkit.
+ *
+ * Qubit Toolkit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Qubit Toolkit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qubit Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Physical Object edit component.
+ *
+ * @package    qubit
+ * @subpackage Cart
+ * @author     Johan Pieterse <johan.pieterse@sita.co.za>
+ * @version    SVN: $Id
+ */
+class cartRemoveCartAction extends DefaultEditAction {
+
+	protected function earlyExecute() {
+		//$this->form->getValidatorSchema()->setOption('allow_extra_fields', true);
+		$this->resource = $this->getRoute()->resource;
+		
+		// Check that this isn't the root
+		if (!isset($this->resource->parent)) {
+			$this->forward404();
+		}
+
+	}
+	
+	public function execute($request) {
+		 $params = $this->request->id;
+		 
+		$this->resource = $this->getRoute()->resource;
+				
+        $sql = 'DELETE FROM cart WHERE id = "' . $params . '";';
+//echo $sql."<br>";
+        QubitPdo::prepareAndExecute($sql);
+
+		$this->redirect(array($this->resource, 'module' => 'cart', 'action' => 'browse'));
+	}
+}
---
+++ apps/qubit/modules/cart/templates/browseSuccess.php
@@ -0,0 +1,113 @@
+<?php use_helper('Date') ?>
+
+<h1><?php echo __('Browse Cart Items %1%', array('%1%' => sfConfig::get('app_ui_label_cart'))) ?></h1>
+
+<table class="table table-bordered sticky-enabled"> 
+  <thead>
+	<style>
+		blink {
+			color: #2d38be;
+			font-size: 15px;
+			font-weight: bold;
+		}
+	</style>
+    <tr>
+      <th>
+        <?php echo __('Archival Description') ?>
+      </th>
+	  <th>
+        <?php echo __('Level of Description') ?>
+      </th>
+	  <th>
+        <?php echo __('Remove item') ?>
+      </th>
+    </tr>
+  </thead><tbody>
+  	<?php echo $form->renderGlobalErrors() ?>
+	<?php echo $form->renderFormTag(url_for(array($resource, 'module' => 'cart', 'action' => 'browse'))) ?>
+	<?php echo $form->renderHiddenFields() ?>
+	<?php if ($pager->getNbResults() == 0) { ?>
+			<tr>
+				<td colspan=3>
+					<blink><b><?php echo "Cart is empty" ?></b></blink>
+				</td>
+			</tr>
+			
+		<?php }	?>
+    <?php foreach ($pager->getResults() as $item) { 	?>
+	  <tr class="<?php echo 0 == @++$row % 2 ? 'even' : 'odd' ?>">
+		<td>
+			<?php $informationObjectsCart = QubitInformationObject::getById($item->archivalDescriptionId); ?>
+			<?php if (isset($informationObjectsCart->identifier)) { ?> <?php echo link_to($informationObjectsCart, array($informationObjectsCart, 'module' => 'informationobject')) ?> <?php } ?>
+		</td>
+		<td>
+			
+			<?php echo $informationObjectsCart->levelOfDescription ?>
+		</td>
+		<td>
+			<?php echo link_to("Remove" , array($this->resource, 'module' => 'cart', 'action' => 'removeCart', 'id' => $item->id)) ?>
+		</td>
+	  </tr>
+    <?php } ?>
+  </tbody>
+</table>
+<?php echo get_partial('default/pager', array('pager' => $pager)) ?>
+   <section id="content">
+		<fieldset class="collapsible">
+		<fieldset class="collapsible">
+		<legend><?php echo __('Request To Publish area') ?></legend>
+			<tr>
+				<td colspan=3>
+				</td>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_name->label(__('Name'))->renderRow(); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_surname->label(__('Surname'))->renderRow(); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_phone->label(__('Phone Number'))->renderRow(); ?>
+				</td> 
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_email->label(__('e-Mail Address'))->renderRow(); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_institution->label(__('Institution'))->renderRow(); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_planned_use->label(__('Planned use'))->renderRow(); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_need_image_by->label(__('Need image/s by'))->renderRow(); ?>
+				</td>
+			</tr>
+			<tr>
+				<td colspan=3>
+					<?php echo $form->rtp_motivation->label(__('Motivation'))->renderRow(); ?>
+				</td>
+			</tr>
+		</fieldset> 
+    </section>
+	<section class="actions">
+		<table width="100%" cellspacing=0 border="0" cellpadding="0" align="left" summary="">
+		  <ul class="clearfix links">
+			<li><?php echo link_to(__('Back to List'), array('module' => 'requesttopublish', 'action' => 'browse'), array('title' => __('Back to list'), 'class' => 'c-btn')) ?></li>
+			<?php if ($resource->unique_identifier != $this->context->user->getAttribute('user_id') && $pager->getNbResults() > 0) { ?>
+					<li><input class="c-btn c-btn-submit" type="submit" id="requesttopublishcart"  value="<?php echo __('Submit') ?>"/></li>
+			<?php } ?>
+		  </ul>
+		</table>
+	</section>
+
