de88ee58ec512c70387f94150c97c4994f522194
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-08-05 11:33:10 -0500

    Run prettier

--- plugins/arDominionB5Plugin/js/clipboard.js
+++ plugins/arDominionB5Plugin/js/clipboard.js
@@ -11,7 +11,8 @@ import Tooltip from "bootstrap/js/dist/tooltip";
 
       this.storage = localStorage;
       this.types = ["informationObject", "actor", "repository", "accession"];
-      this.showAccessions = this.$element.attr("data-show-accessions") == "1" ? true : false;
+      this.showAccessions =
+        this.$element.attr("data-show-accessions") == "1" ? true : false;
       this.initialItems = {
         informationObject: [],
         actor: [],
@@ -463,11 +464,11 @@ import Tooltip from "bootstrap/js/dist/tooltip";
       if (!$buttonSpan.length && totalCount > 0) {
         this.$element.append(
           '<span class="clipboard-count position-absolute top-0 start-0' +
-          ' badge rounded-pill bg-primary">' +
-          totalCount +
-          '<span class="visually-hidden">' +
-          this.$element.data("total-count-label") +
-          "</span></span>"
+            ' badge rounded-pill bg-primary">' +
+            totalCount +
+            '<span class="visually-hidden">' +
+            this.$element.data("total-count-label") +
+            "</span></span>"
         );
       } else if (totalCount > 0) {
         $buttonSpan.text(totalCount);
@@ -488,7 +489,9 @@ import Tooltip from "bootstrap/js/dist/tooltip";
 
       var accessionLabel = "";
       if (this.showAccessions) {
-        accessionLabel = this.$menuHeaderCount.attr("data-accession-object-label");
+        accessionLabel = this.$menuHeaderCount.attr(
+          "data-accession-object-label"
+        );
       }
 
       if (isRTL) {
@@ -534,8 +537,8 @@ import Tooltip from "bootstrap/js/dist/tooltip";
 
       var $alert = $(
         '<div class="alert ' +
-        type +
-        ' alert-dismissible fade show" role="alert">'
+          type +
+          ' alert-dismissible fade show" role="alert">'
       ).append(message);
       var closeButton =
         '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="' +
