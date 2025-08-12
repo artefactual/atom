00c00dae2e402c687700d3489361e96d8aafca00
melaniekung <mkung@artefactual.com>
2025-08-12 12:28:31 -0700

    Simplify inventory terms query

--- apps/qubit/modules/informationobject/actions/inventoryAction.class.php
+++ apps/qubit/modules/informationobject/actions/inventoryAction.class.php
@@ -115,8 +115,7 @@ class InformationObjectInventoryAction extends DefaultBrowseAction
         $q1 = new \Elastica\Query\Term();
         $q1->setTerm('ancestors', $resource->id);
         $queryBool->addMust($q1);
-        $q2 = new \Elastica\Query\Terms('levelOfDescriptionId');
-        $q2->setTerms(self::getLevels());
+        $q2 = new \Elastica\Query\Terms('levelOfDescriptionId', self::getLevels());
         $queryBool->addMust($q2);
 
         $i18n = sprintf('i18n.%s.', sfContext::getInstance()->getUser()->getCulture());
