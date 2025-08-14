31c8672d8cfd7206a371c4daca407d6ccdbbb692
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-08-14 14:36:26 -0500

    Add missing second arg (do not use "and" logic)

--- plugins/arDominionB5Plugin/modules/accession/templates/browseSuccess.php
+++ plugins/arDominionB5Plugin/modules/accession/templates/browseSuccess.php
@@ -19,7 +19,7 @@
 <?php end_slot(); ?>
 
 <?php slot('content'); ?>
-    <?php $canExportAccessions = $sf_user->hasCredential(['editor', 'administrator']); ?>
+    <?php $canExportAccessions = $sf_user->hasCredential(['editor', 'administrator'], false); ?>
 
     <div class="d-flex flex-wrap gap-2 mb-3">
       <?php if ($sf_user->isAuthenticated() && $canExportAccessions && !isset($sf_request->subquery)) { ?>
