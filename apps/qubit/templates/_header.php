6bcdb2c0dfc3167708acbd9199dccbd1171ba586
melaniekung <mkung@artefactual.com>
2025-09-03 11:13:57 -0700

    Move BS5 templates to root dir.

--- apps/qubit/templates/_header.php
+++ apps/qubit/templates/_header.php
@@ -1,63 +1,78 @@
-<?php echo get_component('default', 'bs2DeprecationMessage'); ?>
-
-<?php echo get_component('default', 'updateCheck'); ?>
+<div class="visually-hidden-focusable p-3 border-bottom">
+  <a class="btn btn-sm btn-secondary" href="#main-column">
+    <?php echo __('Skip to main content'); ?>
+  </a>
+</div>
 
 <?php echo get_component('default', 'privacyMessage'); ?>
 
+<?php echo get_component('default', 'updateCheck'); ?>
+
 <?php if ($sf_user->isAdministrator() && '' === (string) QubitSetting::getByName('siteBaseUrl')) { ?>
-  <div class="site-warning">
-    <?php echo link_to(__('Please configure your site base URL'), 'settings/siteInformation', ['rel' => 'home', 'title' => __('Home')]); ?>
+  <div class="alert alert-warning rounded-0 text-center mb-0" role="alert">
+    <?php echo link_to(__('Please configure your site base URL'), 'settings/siteInformation', ['class' => 'alert-link']); ?>
   </div>
 <?php } ?>
 
-<header id="top-bar">
-
-  <?php if (sfConfig::get('app_toggleLogo')) { ?>
-    <?php echo link_to(image_tag('logo', ['alt' => 'AtoM']), '@homepage', ['id' => 'logo', 'rel' => 'home']); ?>
-  <?php } ?>
-
-  <?php if (sfConfig::get('app_toggleTitle')) { ?>
-    <h1 id="site-name">
-      <?php echo link_to('<span>'.esc_specialchars(sfConfig::get('app_siteTitle')).'</span>', '@homepage', ['rel' => 'home', 'title' => __('Home')]); ?>
-    </h1>
-  <?php } ?>
-
-  <div id="search-bar">
-
-    <?php echo get_component('menu', 'browseMenu', ['sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID()]); ?>
-
-    <?php echo get_component('search', 'box'); ?>
-
-  </div>
-
-  <nav>
-
-    <?php echo get_component('menu', 'userMenu'); ?>
-
-    <?php echo get_component('menu', 'quickLinksMenu'); ?>
-
-    <?php if (sfConfig::get('app_toggleLanguageMenu')) { ?>
-      <?php echo get_component('menu', 'changeLanguageMenu'); ?>
+<?php $bgColor = sfConfig::get('app_header_background_colour'); ?>
+<?php if (!empty($bgColor)) { ?>
+  <style <?php echo __(sfConfig::get('csp_nonce')); ?>>
+    #top-bar {
+      background-color: <?php echo $bgColor; ?> !important;
+    }
+  </style>
+<?php } ?>
+<header id="top-bar" class="navbar navbar-expand-lg navbar-dark" role="navigation" aria-label="<?php echo __('Main navigation'); ?>">
+  <div class="container-fluid">
+    <?php if (sfConfig::get('app_toggleLogo') || sfConfig::get('app_toggleTitle')) { ?>
+      <a class="navbar-brand d-flex flex-wrap flex-lg-nowrap align-items-center py-0 me-0" href="<?php echo url_for('@homepage'); ?>" title="<?php echo __('Home'); ?>" rel="home">
+        <?php if (file_exists(sfConfig::get('sf_web_dir').'/uploads/logo.png')) { ?>
+          <?php $logoLoc = '../../uploads'.DIRECTORY_SEPARATOR.'logo.png'; ?>
+        <?php } else { ?>
+          <?php $logoLoc = '/plugins/arDominionB5Plugin/images/logo.png'; ?>
+        <?php } ?>
+        <?php if (sfConfig::get('app_toggleLogo')) { ?>
+          <?php echo image_tag($logoLoc, ['alt' => __('AtoM logo'), 'class' => 'd-inline-block my-2 me-3', 'height' => '35']); ?>
+        <?php } ?>
+        <?php if (sfConfig::get('app_toggleTitle') && !empty(sfConfig::get('app_siteTitle'))) { ?>
+          <span class="text-wrap my-1 me-3"><?php echo esc_specialchars(sfConfig::get('app_siteTitle')); ?></span>
+        <?php } ?>
+      </a>
     <?php } ?>
-
-    <?php echo get_component('menu', 'clipboardMenu'); ?>
-
-    <?php echo get_component('menu', 'mainMenu', ['sf_cache_key' => $sf_user->getCulture().$sf_user->getUserID()]); ?>
-
-  </nav>
-
-  <?php echo get_component_slot('header'); ?>
-
+    <button class="navbar-toggler atom-btn-secondary my-2 me-1 px-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-content" aria-controls="navbar-content" aria-expanded="false">
+      <i 
+        class="fas fa-2x fa-fw fa-bars" 
+        data-bs-toggle="tooltip"
+        data-bs-placement="bottom"
+        title="<?php echo __('Toggle navigation'); ?>"
+        aria-hidden="true">
+      </i>
+      <span class="visually-hidden"><?php echo __('Toggle navigation'); ?></span>
+    </button>
+    <div class="collapse navbar-collapse flex-wrap justify-content-end me-1" id="navbar-content">
+      <div class="d-flex flex-wrap flex-lg-nowrap flex-grow-1">
+        <?php echo get_component('menu', 'browseMenu', ['sf_cache_key' => 'dominion-b5'.$sf_user->getCulture().$sf_user->getUserID()]); ?>
+        <?php echo get_component('search', 'box'); ?>
+      </div>
+      <div class="d-flex flex-nowrap flex-column flex-lg-row align-items-strech align-items-lg-center">
+        <ul class="navbar-nav mx-lg-2">
+          <?php echo get_component('menu', 'mainMenu', ['sf_cache_key' => 'dominion-b5'.$sf_user->getCulture().$sf_user->getUserID()]); ?>
+          <?php echo get_component('menu', 'clipboardMenu'); ?>
+          <?php if (sfConfig::get('app_toggleLanguageMenu')) { ?>
+            <?php echo get_component('menu', 'changeLanguageMenu'); ?>
+          <?php } ?>
+          <?php echo get_component('menu', 'quickLinksMenu'); ?>
+        </ul>
+        <?php echo get_component('menu', 'userMenu'); ?>
+      </div>
+    </div>
+  </div>
 </header>
 
-<?php if (sfConfig::get('app_toggleDescription')) { ?>
-  <div id="site-slogan">
-    <div class="container">
-      <div class="row">
-        <div class="span12">
-          <span><?php echo esc_specialchars(sfConfig::get('app_siteDescription')); ?></span>
-        </div>
-      </div>
+<?php if (sfConfig::get('app_toggleDescription') && !empty(sfConfig::get('app_siteDescription'))) { ?>
+  <div class="bg-secondary text-white">
+    <div class="container-xl py-1">
+      <?php echo esc_specialchars(sfConfig::get('app_siteDescription')); ?>
     </div>
   </div>
 <?php } ?>
--- apps/qubit/templates/layout.php
+++ apps/qubit/templates/layout.php
@@ -1,36 +1,10 @@
-<!DOCTYPE html>
-<html lang="<?php echo $sf_user->getCulture(); ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction; ?>">
-  <head>
-    <?php echo get_partial('default/googleAnalytics'); ?>
-    <?php echo get_component('default', 'tagManager', ['code' => 'script']); ?>
-    <?php include_http_metas(); ?>
-    <?php include_metas(); ?>
-    <?php include_title(); ?>
-    <link rel="shortcut icon" href="<?php echo public_path('favicon.ico'); ?>"/>
-    <?php include_stylesheets(); ?>
-    <?php include_component_slot('css'); ?>
-    <?php if ($sf_context->getConfiguration()->isDebug()) { ?>
-      <script type="text/javascript" charset="utf-8">
-        less = { env: 'development', optimize: 0, relativeUrls: true };
-      </script>
-    <?php } ?>
-    <?php include_javascripts(); ?>
-  </head>
-  <body class="yui-skin-sam <?php echo $sf_context->getModuleName(); ?> <?php echo $sf_context->getActionName(); ?>">
+<?php echo get_partial('layout_start'); ?>
 
-    <?php echo get_component('default', 'tagManager', ['code' => 'noscript']); ?>
+<div id="wrapper" class="container-xxl pt-3 flex-grow-1">
+  <?php echo get_partial('alerts'); ?>
+  <div id="main-column" role="main">
+    <?php echo $sf_content; ?>
+  </div>
+</div>
 
-    <?php echo get_partial('header'); ?>
-
-    <div id="wrapper" class="container" role="main">
-
-      <?php echo get_partial('alerts'); ?>
-
-      <?php echo $sf_content; ?>
-
-    </div>
-
-    <?php echo get_partial('footer'); ?>
-
-  </body>
-</html>
+<?php echo get_partial('layout_end'); ?>
