1b8ccd11c5c7bf5507f239a193836f504bbb77d7
giridhar-se <giridharreddykatasani545@gmail.com>
2025-11-04 09:29:33 -0600

    Frix: lint issues of Php-cs-fixer

--- plugins/arElasticSearchPlugin/lib/arElasticSearchPluginQuery.class.php
+++ plugins/arElasticSearchPlugin/lib/arElasticSearchPluginQuery.class.php
@@ -41,9 +41,8 @@ class arElasticSearchPluginQuery
         // but older Elastic clients do not include this method.
         // Older Elastica doesn’t have that method, so we check first
         // and fall back to setting the track_total_hits param manually.
-
         if (method_exists($this->query, 'setTrackTotalHits')) {
-            $this->query->setTrackTotalHits(true);     
+            $this->query->setTrackTotalHits(true);
         } else {
             $this->query->setParam('track_total_hits', true);
         }
