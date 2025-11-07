855f2f38c22195bd24a85272c39a8ae154eb1841
Daniel Lovegrove <d.lovegrove11@gmail.com>
2025-11-07 14:53:38 -0600

    Move print preview CSS to plugin

--- plugins/arDominionB5Plugin/templates/_layout_start_webpack.php
+++ plugins/arDominionB5Plugin/templates/_layout_start_webpack.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-<html lang="<?php echo $sf_user->getCulture(); ?>" dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction; ?>">
+<html lang="<?php echo $sf_user->getCulture(); ?>"
+      dir="<?php echo sfCultureInfo::getInstance($sf_user->getCulture())->direction; ?>"
+      media="<?php echo isset($_GET['media']) ? htmlspecialchars($_GET['media'], ENT_QUOTES, 'UTF-8') : 'screen'; ?>">
   <head>
     <?php echo get_partial('default/googleAnalytics'); ?>
     <meta charset="utf-8">
@@ -13,7 +15,6 @@
     <?php } ?>
     <link rel="shortcut icon" href="<?php echo $faviconLoc; ?>">
     <%= htmlWebpackPlugin.tags.headTags %>
-    <?php include_stylesheets(); ?>
     <?php echo get_component_slot('css'); ?>
   </head>
   <body class="d-flex flex-column min-vh-100 <?php echo $sf_context->getModuleName(); ?> <?php echo $sf_context->getActionName(); ?><?php echo sfConfig::get('app_show_tooltips') ? ' show-edit-tooltips' : ''; ?>">
