56a50c36678030aa13115e7ce98f7b432b350345
Ash Singhal <ash.singhal@umanitoba.ca>
2025-11-19 13:22:17 -0600

    Add external js for handling back navigation

---
+++ js/historyNavigation.js
@@ -0,0 +1,26 @@
+(($) => {
+  "use strict";
+
+  $(() => {
+    historyNavigation();
+  });
+
+  function historyNavigation() {
+    document.querySelectorAll('.js-back-link').forEach(function (el) {
+      el.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        if (window.history.length > 1) {
+          window.history.back();
+        } else {
+          const fallbackUrl = el.getAttribute('data-fallback-url');
+          if (fallbackUrl) {
+            window.location.href = fallbackUrl;
+            return;
+          }
+        }
+      });
+    });
+  }
+
+})(jQuery);
