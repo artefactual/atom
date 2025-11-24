5f7db106db0b7f8c0bded2fa4ee43a58479003f7
Ash Singhal <ash.singhal@umanitoba.ca>
2025-11-24 13:48:30 -0600

    Fix prettier issues in historyNavigation.js

--- js/historyNavigation.js
+++ js/historyNavigation.js
@@ -7,13 +7,13 @@
 
   function historyNavigation() {
     document.querySelectorAll('[data-action="back"]').forEach(function (el) {
-      el.addEventListener('click', function (e) {
+      el.addEventListener("click", function (e) {
         e.preventDefault();
 
         if (window.history.length > 1) {
           window.history.back();
         } else {
-          const fallbackUrl = el.getAttribute('data-fallback-url');
+          const fallbackUrl = el.getAttribute("data-fallback-url");
           if (fallbackUrl) {
             window.location.href = fallbackUrl;
             return;
@@ -22,5 +22,4 @@
       });
     });
   }
-
 })(jQuery);
