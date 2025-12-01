7d0edfe231c2f2da2886a211baaa0640f3973498
Ash Singhal <ash.singhal@umanitoba.ca>
2025-12-01 12:04:56 -0600

    Revert "Exit the function early by using return statement"
    This reverts commit f027fc365cd4d1e107e275c16b0907f895d9c406.

--- js/historyNavigation.js
+++ js/historyNavigation.js
@@ -8,12 +8,12 @@
 
         if (window.history.length > 1) {
           window.history.back();
-          return;
-        }
-
-        const fallbackUrl = el.getAttribute("data-fallback-url");
-        if (fallbackUrl) {
-          window.location.href = fallbackUrl;
+        } else {
+          const fallbackUrl = el.getAttribute("data-fallback-url");
+          if (fallbackUrl) {
+            window.location.href = fallbackUrl;
+            return;
+          }
         }
       });
     });
