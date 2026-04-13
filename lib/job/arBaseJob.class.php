b206086d430a485c79c282aca33dcd43446cbc6e
mel <mkung@artefactual.com>
2026-04-13 11:35:36 -0700

    Avoid logging full file paths for imports

--- lib/job/arBaseJob.class.php
+++ lib/job/arBaseJob.class.php
@@ -102,7 +102,13 @@ class arBaseJob extends Net_Gearman_Job_Common
                 $this->deleteOldUnauthenticatedJobs();
             }
 
-            $this->logger->info($this->i18n->__('Parameters: '.json_encode($parameters)));
+            $parametersToLog = $parameters;
+
+            // Avoid logging file tmp_name for security reasons
+            if (isset($parametersToLog['file']) && is_array($parametersToLog['file'])) {
+                unset($parametersToLog['file']['tmp_name']);
+            }
+            $this->logger->info($this->i18n->__('Parameters: '.json_encode($parametersToLog)));
 
             $this->runJob($parameters);
 
