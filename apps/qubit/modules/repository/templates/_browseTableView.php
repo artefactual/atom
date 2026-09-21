b3b16247dde1087282aa1793ed8f580f07998572
SHRISHTY <92874887+ShrishtyJainTech@users.noreply.github.com>
2026-09-21 15:15:29 -0700

    Remove bullet list from table view (#2371)

--- apps/qubit/modules/repository/templates/_browseTableView.php
+++ apps/qubit/modules/repository/templates/_browseTableView.php
@@ -72,7 +72,7 @@
         <td>
           <?php if (isset($doc['thematicAreas'])) { ?>
             <?php foreach ($doc['thematicAreas'] as $areaTerm) { ?>
-              <li><?php echo render_value_inline(QubitTerm::getById($areaTerm)); ?></li>
+              <p><?php echo render_value_inline(QubitTerm::getById($areaTerm)); ?></p>
             <?php } ?>
           <?php } ?>
         </td>
