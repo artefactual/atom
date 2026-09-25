22cc7d391404e9c6992de38666d200d3bb5a4fe9
melaniekung. <71947221+melaniekung@users.noreply.github.com>
2026-09-25 16:39:18 -0700

    Add Image carousel user interface label. (#2337)

---
+++ lib/task/migrate/migrations/arMigration0199.class.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Access to Memory (AtoM) software.
+ *
+ * Access to Memory (AtoM) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Access to Memory (AtoM) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Access to Memory (AtoM).  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Add new UI labels for Image carousel.
+ *
+ * @package    AccesstoMemory
+ * @subpackage migration
+ */
+class arMigration0199
+{
+    public const VERSION = 199;
+    public const MIN_MILESTONE = 2;
+
+    /**
+     * Upgrade.
+     *
+     * @param mixed $configuration
+     *
+     * @return bool True if the upgrade succeeded, False otherwise
+     */
+    public function up($configuration)
+    {
+        $setting = new QubitSetting();
+        $setting->name = 'carousel';
+        $setting->scope = 'ui_label';
+        $setting->editable = 1;
+        $setting->deleteable = 0;
+        $setting->source_culture = 'en';
+        $setting->setValue('Image Carousel', ['culture' => 'en']);
+        $setting->save();
+
+        return true;
+    }
+}
